a6ec465120815a0928ebb10b79a748e4727a78b5
talintj <talintj@a00a5322-12db-0310-a70b-8735589c885e>
2001-05-22 19:19:19 +0000

    A few simple subroutines for doing unit tests.
    git-svn-id: http://develop.twiki.org/svn/trunk@323 a00a5322-12db-0310-a70b-8735589c885e

---
+++ tools/test/TestUtil.pm
@@ -0,0 +1,95 @@
+package TestUtil;
+
+use strict;
+
+use Carp;
+
+use vars qw(
+   $routine $doTrace $totalCount $okayCount $okay
+);
+
+
+sub trace
+{
+   my( $message ) = @_;
+
+   if( $doTrace ) {
+      print "--- Trace: $message\n";
+   }
+}
+
+sub check
+{
+   my( $condition, $message, $extra ) = @_;
+   
+   if( $extra ) {
+      $message .= "; $extra";
+   }
+
+   if( ! $condition ) {
+      $okay = 0;
+      carp "*** $message";
+   } else {
+      trace( "--- Trace: $message" );
+   }
+}
+
+sub checkNot
+{
+   my ($condition, $message ) = @_;
+
+   if( $condition ) {
+      $okay = 0;
+      carp "*** $condition, $message";
+   } else {
+      trace("--- Trace: $message");
+   }
+}
+
+sub check_match
+{
+   my( $text, $pattern, $extra ) = @_;
+
+   check($text =~ /$pattern/, "Failed $text match $pattern", $extra );
+}
+
+sub check_equal
+{
+   my( $result, $expected, $extra ) = @_;
+
+   check( $result eq $expected, "\"$result\" should be \"$expected\"", $extra );
+}
+
+sub enter
+{
+   my( $theroutine ) = @_;
+   
+   $routine = $theroutine;
+   
+   $totalCount++;
+   $okay = 1;
+
+   trace( "Entering $routine" );
+} 
+
+sub leave
+{
+   if( $okay ) {
+      $okayCount++;
+   } else {
+      print "=== $routine failed";
+   }
+   trace( "Leaving $routine" );
+}   
+
+sub testSummary
+{
+   my $failedCount = $totalCount - $okayCount;
+   my $summary = "Total tests  = $totalCount\n" .
+                 "Okay  tests  = $okayCount\n"  .
+                 "Failed tests = $failedCount\n";
+   return $summary;
+}
+
+1;
+
