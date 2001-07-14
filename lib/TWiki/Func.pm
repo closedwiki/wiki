f52ede22064be0df46ce7cb2e11d1c3ef74b1b43
peterthoeny <peterthoeny@a00a5322-12db-0310-a70b-8735589c885e>
2001-07-14 10:53:42 +0000

    Codev.OfficialCoreSubsForPlugins
    git-svn-id: http://develop.twiki.org/svn/trunk@468 a00a5322-12db-0310-a70b-8735589c885e

---
+++ lib/TWiki/Func.pm
@@ -0,0 +1,250 @@
+#
+# TWiki WikiClone (see wiki.pm for $wikiversion and other info)
+#
+# Copyright (C) 2000 Peter Thoeny, Peter@Thoeny.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
+#
+# Notes:
+# - Latest version at http://twiki.org/
+# - Installation instructions in $dataDir/TWiki/TWikiDocumentation.txt
+# - Customize variables in TWiki.cfg when installing TWiki.
+# - Upgrading TWiki is easy as long as you use Plugins.
+# - Check web server error logs for errors, i.e. % tail /var/log/httpd/error_log
+#
+# This is the module with official funcions. Plugins should
+# ONLY use functions published in this module. If you use
+# other functions you might impose a security hole and you
+# will likely need to change your plugin when you upgrade
+# TWiki.
+
+package TWiki::Func;
+
+use strict;
+
+# =========================
+# get a preferences value
+# =========================
+sub getPreferencesValue
+{
+#   my( $theKey, $theWeb ) = @_;
+    # $theKey is "MYPLUGIN_COLOR" to get the "Set COLOR" setting in "MyPlugin" topic
+    # $theWeb is optional (does not apply to settings of plugin topics)
+    return &TWiki::Prefs::getPreferencesValue( @_ );
+}
+
+# =========================
+# get a preferences flag value
+# =========================
+sub getPreferencesFlag
+{
+#   my( $theKey ) = @_;
+    # $theKey is "MYPLUGIN_SHOWHELP" to get the "Set SHOWHELP" setting in "MyPlugin" topic
+    return &TWiki::Prefs::getPreferencesFlag( @_ );
+}
+
+# =========================
+# extract the value from a name="value" attribute pair
+# =========================
+sub extractNameValuePair
+{
+#   my( $theAttr, $theName ) = @_;
+    # extract attributes from variables
+    # i.e. %TEST{"nameless" name1="val1" name2="val2"}%
+    # - first extract text between {...} to get: "nameless" name1="val1" name2="val2"
+    # - then call this on the text:
+    #     my $noname = &TWiki::Func::extractNameValuePair( $text );
+    #     my $name1  = &TWiki::Func::extractNameValuePair( $text, "name1" );
+    #     my $name2  = &TWiki::Func::extractNameValuePair( $text, "name2" );
+
+    return &TWiki::extractNameValuePair( @_ );
+}
+
+# =========================
+# log Warning that may require admin intervention to data/warning.txt
+# =========================
+sub writeWarning
+{
+#   my( $theText ) = @_;
+    return &TWiki::writeWarning( @_ );
+}
+
+# =========================
+# log debug message to data/debug.txt
+# =========================
+sub writeDebug
+{
+#   my( $theText ) = @_;
+    return &TWiki::writeDebug( @_ );
+}
+
+# =========================
+# get data directory (topic file root)
+# =========================
+sub getDataDir
+{
+    return &TWiki::getDataDir();
+}
+
+# =========================
+# get pub directory (file attachment root)
+# =========================
+sub getPubDir
+{
+    return &TWiki::getPubDir();
+}
+
+# =========================
+# get pub URL path
+# =========================
+sub getPubUrlPath
+{
+    return &TWiki::getPubUrlPath();
+}
+
+# =========================
+# compose fully qualified view URL
+# =========================
+sub getViewUrl
+{
+#   my( $theWeb, $theTopic ) = @_;
+    return &TWiki::getViewUrl( @_ );
+}
+
+# =========================
+# compose fully qualified "oops" dialog URL
+# =========================
+sub getOopsUrl
+{
+#   my( $theWeb, $theTopic, $theTemplate, @theParams ) = @_;
+    # up to 4 parameters in @theParams
+    return &TWiki::getOopsUrl( @_ );
+}
+
+# =========================
+# expand all common %VARIABLES%
+# =========================
+sub expandCommonVariables
+{
+#   my( $theText, $theTopic, $theWeb ) = @_;
+    return &TWiki::handleCommonTags( @_ );
+}
+
+# =========================
+# render text in Wiki syntax
+# =========================
+sub renderText
+{
+#   my( $theText, $theWeb ) = @_;
+    return &TWiki::getRenderedVersion( @_ );
+}
+
+# =========================
+# get list of all public webs
+# =========================
+sub getPublicWebList
+{
+    return &TWiki::getPublicWebList();
+}
+
+# =========================
+# get list of all topics in a web
+# =========================
+sub getTopicList
+{
+#   my( $theWeb ) = @_;
+    return &TWiki::Store::getTopicNames ( @_ );
+}
+
+# =========================
+# test if web exists
+# =========================
+sub webExists
+{
+#   my( $theWeb ) = @_;
+    return &TWiki::Store::webExists( @_ );
+}
+
+# =========================
+# test if topic exists
+# =========================
+sub topicExists
+{
+#   my( $theTopic ) = @_;
+    return &TWiki::Store::topicExists( @_ );
+}
+
+# =========================
+# read a topic "as is" (with embedded meta data)
+# =========================
+sub readTopic
+{
+#   my( $theWebName, $theTopic ) = @_;
+    return &TWiki::Store::readWebTopic( @_ );
+}
+
+# =========================
+# read a template file
+# =========================
+sub readTemplate
+{
+#   my( $theName, $theSkin ) = @_;
+    return &TWiki::Store::readTemplate( @_ );
+}
+
+# =========================
+# read text file, low level
+# =========================
+sub readFile
+{
+#   my( $theFileName ) = @_;
+    return &TWiki::Store::readFile( @_ );
+}
+
+# =========================
+# save text file, low level
+# =========================
+sub saveFile
+{
+#   my( $theFileName, $theText ) = @_;
+    return &TWiki::Store::saveFile( @_ );
+}
+
+# =========================
+# Write HTML header
+# =========================
+sub writeHeader
+{
+#   my( $theQuery ) = @_;
+    return &TWiki::writeHeader( @_ );
+}
+
+# =========================
+# get CGI query object
+# =========================
+sub getCgiQuery
+{
+    return &TWiki::getCgiQuery();
+}
+
+# =========================
+# redirect to URL
+# =========================
+sub redirectCgiQuery
+{
+#   my( $theQuery, $theUrl ) = @_;
+    return &TWiki::redirect( @_ );
+}
+
+1;
+
+# EOF
