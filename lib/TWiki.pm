211c199dbf1cf0797b53861e560e257aa7b93d00
talintj <talintj@a00a5322-12db-0310-a70b-8735589c885e>
2001-08-14 16:40:08 +0000

    UseForm only special if DEFFORM present
    git-svn-id: http://develop.twiki.org/svn/trunk@599 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki.pm
+++ lib/TWiki.pm
@@ -1480,7 +1480,7 @@ sub renderFormData
     my @fields = $meta->find( "FIELD" );
     foreach my $field ( @fields ) {
         my $title = $field->{"title"};
-        next if( $title eq "UseForm" );
+        next if( $title eq "UseForm" && TWiki::Form::useFormSpecial() );
         my $value = $field->{"value"};
         $metaText .= "<tr><th bgcolor=\"#99CCCC\" align=\"right\"> $title:</th><td align=\"left\"> $value </td></tr>\n";
     }
