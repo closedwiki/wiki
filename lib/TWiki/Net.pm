74a0b16c84430243854804272ea50c262ba000db
peterthoeny <peterthoeny@a00a5322-12db-0310-a70b-8735589c885e>
2002-11-10 09:12:26 +0000

    Fixed Codev.NetCantGetURLFromVhosts
    git-svn-id: http://develop.twiki.org/svn/trunk@987 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Net.pm
+++ lib/TWiki/Net.pm
@@ -52,7 +52,8 @@ sub getUrl
     }
     my $base64;
     my $result = '';
-    my $req = "GET http://$theHost$theUrl HTTP/1.1\r\n";
+    $theUrl = "/" unless( $theUrl );
+    my $req = "GET $theUrl HTTP/1.1\r\n";
 
     # RNF 22 Jan 2002 Support for vhosts and user authentication.
     $req .= "Host: $theHost\r\n";
