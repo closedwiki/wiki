beaf9a0c97aaf2a42e5f5ca0dbb9f8c79838ecb9
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2002-12-31 12:37:44 +0000

    Initial revision of JavaPaste.
    JavaPaste is a drag and drop java applet that, when a set of files is dragged onto it, calls upload.pl.
    It also uploads files such as pictures when these are pasted into the Applet.
    
    Work originally authored by Catherine Macleod.
    Drag and drop functionality and bug fixes coded by Main.RichardEllery
    Initial idea and lets-get-it-out-the-door motivation by Main.MartinCleaver,
    
    This code is still alpha quality. Caveat Emptor.
    
    
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@4691 a00a5322-12db-0310-a70b-8735589c885e

---
+++ JavaPasteAddOn/com/mrjc/twiki/addons/ClipboardHelper.java
@@ -0,0 +1,193 @@
+package com.mrjc.twiki.addons;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.Reader;
+//import java.util.Hashtable;
+
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+
+
+/**
+ *  Helper class for clipboard functions. 
+ *
+ *@author	Catherine Macleod 
+ */
+public class ClipboardHelper
+{
+	
+	Toolkit toolkit;
+	
+	public static void main(String args[])
+	{
+		ClipboardHelper app = new ClipboardHelper();
+	}
+	
+	/**
+	 * Returns object representing data on clipboard
+	 * 
+	 * @author	Catherine Macleod
+	 * @return	  
+	 */
+	public Object getClipboardContents()
+	{
+		// Use the Toolkit object to obtain access to the system clipboard
+		Clipboard clip=Toolkit.getDefaultToolkit().getSystemClipboard();
+		// Get the clipboard contents
+		return getData(clip.getContents(ClipboardHelper.this));
+		
+	}
+		
+	public Object getData(Transferable contents) {	
+		
+		Object data = null;
+		
+		if (contents!=null)
+		{
+			 // Get the data flavors associated with the clipboard contents
+			 DataFlavor flavors[]=contents.getTransferDataFlavors();
+			 DataFlavor flavor = null;
+			 File resultsFile;
+			 for(int i=0;i<flavors.length;++i)
+			 {
+			 	System.out.println("In loop " + i);
+				if (flavors[i].getRepresentationClass() != null) 
+				{
+					try
+					{
+						System.out.println(flavors[i]);
+						System.out.println("in loop:2");
+						data = contents.getTransferData(flavors[i]);
+						System.out.println("in loop:3");
+						break;	
+					}
+					catch (UnsupportedFlavorException ufe)
+					{
+						System.out.println("ClipboardHelper.getClipboardContentsDataType UnsupportedException: "+ufe);
+					}
+					catch (IOException ioe)
+					{
+						System.out.println("ClipboardHelper.getClipboardContentsDataType IOException: "+ioe);
+					}
+					
+				}
+
+			 }
+		}
+		return data;
+	}
+	
+		
+		
+	/**
+	 * Writes contents of the clipboard to a file
+	 * 
+	 * @author	Catherine Macleod
+	 * @param	o object containing data from clipboard 
+	 * @return	file containing clipboard contents  
+	 */
+	public File saveClipboardContentsToFile(Object o)
+	{
+		File file = null;
+		String dataType = o.getClass().getName();
+		try
+		{
+			if (o instanceof Image)
+			{
+			  file = new File(System.getProperty("user.dir")+System.getProperty("file.separator")+"myUpload.jpg");
+			  FileOutputStream out = new FileOutputStream(file);
+			  JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+			  JPEGEncodeParam param = encoder.getDefaultJPEGEncodeParam((BufferedImage)o);
+			  param.setQuality(1.0f, false);  // max quality
+			  encoder.setJPEGEncodeParam(param);
+			  encoder.encode((BufferedImage)o);
+			  out.flush();
+			  out.close();
+			}
+			else if (o instanceof String)
+			{
+				file = new File(System.getProperty("user.dir")+System.getProperty("file.separator")+"myUpload.txt");
+				FileWriter out = new FileWriter(file);
+				out.write((String)o);
+				out.flush();
+				out.close();
+			}
+			else if (o instanceof Reader)
+			{
+				StringBuffer results = new StringBuffer();
+				BufferedReader input = new BufferedReader((InputStreamReader)o);
+				String str;
+				while (null != ((str = input.readLine())))
+					results.append(str);
+
+				input.close ();
+
+				file = new File(System.getProperty("user.dir")+System.getProperty("file.separator")+"myUpload.doc");
+				FileWriter out = new FileWriter(file);
+				out.write(results.toString());
+				out.flush();
+				out.close();
+			}
+		  
+		}
+		catch (IOException ioe)
+		{
+			System.out.println("ClipboardHelper.getClipboardContents IOException: "+ioe);
+		}
+		return file;
+	}
+	
+	/**
+	 * Reads contents of a file.
+	 * 
+	 * @author	Catherine Macleod
+	 * @param	file file from which content is to be read 
+	 * @return	file contents  
+	 */
+	public static String readFileContents(File file)
+	{
+		byte[] buff = new byte[1024];
+		StringBuffer str = new StringBuffer();
+		try
+		{
+			FileInputStream fis = new FileInputStream(file);
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			int len;
+			while (bis.available() != 0)
+			{
+				len = bis.read(buff);
+				str.append(new String(buff));
+			}
+		
+			fis.close();
+		}
+		catch (FileNotFoundException fnfe)
+		{
+			fnfe.printStackTrace();
+		}
+		catch (IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+		return (str.toString()).trim();
+	}
+	
+		
+
+}
+
