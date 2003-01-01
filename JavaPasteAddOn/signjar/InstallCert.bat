d34e5ea42775428b0bd07f045141b970794d1e5d
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2003-01-01 20:52:12 +0000

    Updated Sign scripts Fixed bug which caused [B class to be retrieved from Data flavours instead of String
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@4696 a00a5322-12db-0310-a70b-8735589c885e

--- JavaPasteAddOn/signjar/InstallCert.bat
+++ JavaPasteAddOn/signjar/InstallCert.bat
@@ -4,5 +4,8 @@ copy %SIGNEDJAR% %LOCALUSERPROFILEDIR%
 copy %CERTIFICATEFILE% %LOCALUSERPROFILEDIR%
 
 cd %LOCALUSERPROFILEDIR%
+keytool -delete -alias user -keystore %KEYSTORE% -storepass %STOREPASS%
+pause
+
 keytool -import -alias user -file %CERTIFICATEFILE% -keystore %KEYSTORE% -storepass %STOREPASS%
 pause
