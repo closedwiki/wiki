dcf23a6a4d674ff7b57701a37da708c7a533205c
svenud <svenud@a00a5322-12db-0310-a70b-8735589c885e>
2003-01-18 01:07:24 +0000

    22Sept2001 initial commit
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@4744 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/UserCookiePlugin/lib/TWiki/Plugins/UserCookiePlugin.pm
@@ -0,0 +1,178 @@
+#
+# TWiki WikiClone ($wikiversion has version info)
+#
+# Copyright (C) 2000-2001 Andrea Sterbini, a.sterbini@flashnet.it
+# Copyright (C) 2001 Peter Thoeny, Peter@Thoeny.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
+#
+# =========================
+#
+# This is an empty TWiki plugin. Use it as a template
+# for your own plugins; see TWiki.TWikiPlugins for details.
+#
+# Each plugin is a package that contains the subs:
+#
+#   initPlugin           ( $topic, $web, $user, $installWeb )
+#   commonTagsHandler    ( $text, $topic, $web )
+#   startRenderingHandler( $text, $web )
+#   outsidePREHandler    ( $text )
+#   insidePREHandler     ( $text )
+#   endRenderingHandler  ( $text )
+#
+# initPlugin is required, all other are optional. 
+# For increased performance, all handlers except initPlugin are
+# disabled. To enable a handler remove the leading DISABLE_ from
+# the function name.
+# 
+# NOTE: To interact with TWiki use the official TWiki functions
+# in the &TWiki::Func module. Do not reference any functions or
+# variables elsewhere in TWiki!!
+
+
+# =========================
+package TWiki::Plugins::UserCookiePlugin; 	# change the package name!!!
+
+# =========================
+use vars qw(
+        $web $topic $user $installWeb $VERSION $debug
+        $exampleCfgVar
+    );
+
+$VERSION = '1.000';
+
+# =========================
+sub initPlugin
+{
+    ( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $TWiki::Plugins::VERSION < 1 ) {
+        &TWiki::Func::writeWarning( "Version mismatch between EmptyPlugin and Plugins.pm" );
+        return 0;
+    }
+
+    # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
+    $exampleCfgVar = &TWiki::Prefs::getPreferencesValue( "EMPTYPLUGIN_EXAMPLE" ) || "default";
+
+    # Get plugin debug flag
+    $debug = &TWiki::Func::getPreferencesFlag( "EMPTYPLUGIN_DEBUG" );
+
+    # Plugin correctly initialized
+    &TWiki::Func::writeDebug( "- TWiki::Plugins::EmptyPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
+    return 1;
+}
+
+# =========================
+sub DISABLE_commonTagsHandler
+{
+### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    &TWiki::Func::writeDebug( "- EmptyPlugin::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+
+    # This is the place to define customized tags and variables
+    # Called by sub handleCommonTags, after %INCLUDE:"..."%
+
+    # do custom extension rule, like for example:
+    # $_[0] =~ s/%XYZ%/&handleXyz()/geo;
+    # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/geo;
+}
+
+# =========================
+sub DISABLE_startRenderingHandler
+{
+### my ( $text, $web ) = @_;   # do not uncomment, use $_[0], $_[1] instead
+
+    &TWiki::Func::writeDebug( "- EmptyPlugin::startRenderingHandler( $_[1].$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion just before the line loop
+
+    # do custom extension rule, like for example:
+    # $_[0] =~ s/old/new/go;
+}
+
+# =========================
+sub DISABLE_outsidePREHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
+
+#   &TWiki::Func::writeDebug( "- EmptyPlugin::outsidePREHandler( $web.$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion, in loop outside of <PRE> tag.
+    # This is the place to define customized rendering rules.
+    # Note: This is an expensive function to comment out.
+    # Consider startRenderingHandler instead
+}
+
+# =========================
+sub DISABLE_insidePREHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
+
+#   &TWiki::Func::writeDebug( "- EmptyPlugin::insidePREHandler( $web.$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion, in loop inside of <PRE> tag.
+    # This is the place to define customized rendering rules.
+    # Note: This is an expensive function to comment out.
+    # Consider startRenderingHandler instead
+}
+
+# =========================
+sub DISABLE_endRenderingHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
+
+    &TWiki::Func::writeDebug( "- EmptyPlugin::endRenderingHandler( $web.$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion just after the line loop
+
+}
+
+# =========================
+sub initializeUserHandler
+{
+#    &TWiki::Func::writeDebug( "- initializeUserHandler( $web.$topic )" );
+    my (@rawCookies) = split (/; /,$ENV{'HTTP_COOKIE'});
+#    my (@rawCookies) = split (/; / , $_[0]);
+    my (%cookies);
+    my ($key, $val);
+    
+    my ($cookieName) = &TWiki::Func::getWikiToolName();
+    $cookieName = $cookieName."UserName";
+    
+    foreach (@rawCookies)
+    {
+#      &TWiki::Func::writeDebug( "cookie ($_)");
+      ($key, $val) = split(/=/,$_);
+      if ( ($key =~ /\Q$cookieName\E/i) && (length($val) > 0))
+      {
+         return $val;
+      }
+    }
+    return "guest";
+}
+# =========================
+
+sub registrationHandler
+{
+    my ($date) = "09/12/2011 00:00:00 GMT";
+ 
+    my ($domain) = &TWiki::Func::getUrlHost();
+    $domain =~ s/http:\/\/(.*)/$1/i;     # this may need to cater for https:// etc...
+
+    my ($cookieName) = &TWiki::Func::getWikiToolName();
+    $cookieName = $cookieName."UserName";
+
+    print "Set-Cookie: $cookieName=$_[1]; expires=$date; path=/; domain=$domain;\n";
+}
+
+1;
