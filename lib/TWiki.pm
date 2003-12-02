1a441f876dbb6750a94fcc6d66be330a4bec1cc4
peterthoeny <peterthoeny@a00a5322-12db-0310-a70b-8735589c885e>
2003-12-02 06:46:52 +0000

    Added nntp to protocols, see Codev.RenderIrcAsURL
    git-svn-id: http://develop.twiki.org/svn/trunk@1236 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki.pm
+++ lib/TWiki.pm
@@ -117,7 +117,7 @@ use vars qw(
 
 # ===========================
 # TWiki version:
-$wikiversion      = "30 Nov 2003";
+$wikiversion      = "01 Dec 2003";
 
 # ===========================
 # Key Global variables, required for writeDebug
@@ -180,7 +180,7 @@ $cgiQuery = 0;
 $noAutoLink = 0;
 $viewScript = "view";
 
-$linkProtocolPattern = "(file|ftp|gopher|http|https|irc|news|telnet)";
+$linkProtocolPattern = "(file|ftp|gopher|http|https|irc|news|nntp|telnet)";
 
 # Header patterns based on '+++'. The '###' are reserved for numbered headers
 $headerPatternDa = '^---+(\++|\#+)\s*(.+)\s*$';       # '---++ Header', '---## Header'
