135f5072240f0e54a4abf6a16ec4f0eccb4479ae
crawfordcurrie <crawfordcurrie@a00a5322-12db-0310-a70b-8735589c885e>
2004-03-12 17:22:56 +0000

    Changed to use viewauth. Moved templates into user topics.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@4902 a00a5322-12db-0310-a70b-8735589c885e

--- CommentPlugin/lib/TWiki/Plugins/CommentPlugin.pm
+++ CommentPlugin/lib/TWiki/Plugins/CommentPlugin.pm
@@ -15,7 +15,7 @@ $VERSION = '3.000';
 
 sub initPlugin {
   ( $topic, $web, $user, $installWeb ) = @_;
-  
+
   if( $TWiki::Plugins::VERSION < 1 ) {
     TWiki::Func::writeWarning( "Version mismatch between CommentPlugin and Plugins.pm $TWiki::Plugins::VERSION" );
     return 0;
@@ -28,20 +28,25 @@ sub initPlugin {
 
 sub commonTagsHandler {
   ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-  
-  TWiki::Func::writeDebug( "- CommentPlugin::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
-    
-  CommentPlugin::Comment::prompt( @_ );
-}
 
-sub beforeSaveHandler {
-  TWiki::Func::writeDebug( "- CommentPlugin::beforeSaveHandler( $_[2].$_[1] )" ) if $debug;
+  TWiki::Func::writeDebug( "- CommentPlugin::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+
   my $query = TWiki::Func::getCgiQuery();
-  if ( $query ) {
-    CommentPlugin::Comment::save( $query, @_ );
+
+  my $type = $query->param( 'comment_type' );
+  my $index = $query->param( 'comment_index' );
+  my $anchor = $query->param( 'comment_anchor' );
+  my $location = $query->param( 'comment_location' );
+
+  # the presence of these three urlparams indicates this is a comment save
+  # from a viewauth invocation.
+  if ( defined( $type ) && $type ne "" &&
+       ( defined( $index ) || defined( $anchor ) ||
+	 defined( $location ))) {
+    CommentPlugin::Comment::save( $_[2], $_[1], $type, $index, $anchor, $location );
   } else {
-    TWiki::Func::writeWarning("Comment plugin was unable to getCgiQuery");
-  };
+    CommentPlugin::Comment::prompt( @_ );
+  }
 }
 
 1;
