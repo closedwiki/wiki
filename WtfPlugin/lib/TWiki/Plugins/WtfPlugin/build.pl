953a1b58ab7301d673a1188c6f779f4fe5d02b3e
crawfordcurrie <crawfordcurrie@a00a5322-12db-0310-a70b-8735589c885e>
2004-06-20 08:31:05 +0000

    Initial version
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@4999 a00a5322-12db-0310-a70b-8735589c885e

---
+++ WtfPlugin/lib/TWiki/Plugins/WtfPlugin/build.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+#
+# Build file for the Wtf plugin
+#
+# Requires the environment variable TWIKI_SHARED to be
+# set to point at the shared code repository
+# Usage: ./build.pl [-n] [-v] [target]
+# where [target] is the optional build target (build, test,
+# install, release, uninstall), test is the default.`
+# Two command-line options are supported:
+# -n Don't actually do anything, just print commands
+# -v Be verbose
+#
+# Read the comments at the top of lib/TWiki/Plugins/Build.pm for
+# details of how the build process works, and what files you
+# have to provide and where.
+#
+# Standard preamble
+BEGIN {
+  use File::Spec;
+  my $cwd = `dirname $0`; chop($cwd);
+  my $basedir = File::Spec->rel2abs("../../../..", $cwd);
+  die "TWIKI_SHARED not set" unless ($ENV{TWIKI_SHARED});
+  unshift @INC, "$ENV{TWIKI_SHARED}/lib";
+  unshift @INC, $basedir;
+  unshift @INC, $cwd;
+}
+use TWiki::Plugins::Build;
+
+# Declare our build package
+{ package WtfPluginBuild;
+
+  @WtfPluginBuild::ISA = ( "TWiki::Plugins::Build" );
+
+  sub new {
+    my $class = shift;
+    return bless( $class->SUPER::new( "WtfPlugin" ), $class );
+  }
+}
+
+# Create the build object
+$build = new WtfPluginBuild();
+
+# Build the target on the command line, or the default target
+$build->build($build->{target});
+
