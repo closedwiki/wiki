f8da6a00c2ff5d7b389396ee4e5cc80d1fb92e5a
sdowideit <sdowideit@a00a5322-12db-0310-a70b-8735589c885e>
2004-07-21 02:14:22 +0000

    Codev/RenameTopicDoesMainMainUser - still prepends Main to WikiWords, but not blindly - uses TWiki::Store::normalizeWebTopicName which i recon should be moved into TWiki.pm
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@1590 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki.pm
+++ lib/TWiki.pm
@@ -1207,19 +1207,20 @@ sub userToWikiName
 
     $loginUser =~ s/$securityFilter//go;
     my $wUser = $userToWikiList{ $loginUser };
-    if ( $wUser ) {
-       if( $dontAddWeb ) {
-          return $wUser;
-       }
-       return "$mainWebname.$wUser";
+    if ( ! $wUser ) { #if we didn't find a user, just use the input param (NoPasswdUser, or de-registered)
+		$wUser = $loginUser;
+	}
+
+    if( ! $dontAddWeb ) {
+		my $web = "$mainWebname";
+		( $web, $wUser ) = TWiki::Store::normalizeWebTopicName( $web, $wUser );
+
+		if ( isWikiName($wUser) ) {
+            $wUser = "$web.$wUser";
+		}
     }
-    if ( $wikiToUserList{"$loginUser"} ) {
-       if( $dontAddWeb ) {
-          return $loginUser;
-       }
-       return "$mainWebname.$loginUser";
-    }
-    return $loginUser;
+   
+   return $wUser;
 }
 
 =pod
