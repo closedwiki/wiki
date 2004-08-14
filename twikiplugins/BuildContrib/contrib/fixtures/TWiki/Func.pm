a147b89d8816bfcea3aee1aa04941c174befb8c1
crawfordcurrie <crawfordcurrie@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-14 12:44:11 +0000

    Rationalising
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5037 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/BuildContrib/contrib/fixtures/TWiki/Func.pm
@@ -0,0 +1,350 @@
+=begin text
+
+---+ Package TWiki::Func - test fixture
+A test fixture module that provides an ultra-thin implementation of the
+functions of the TWiki::Func module that are required by plugins and add-ons.
+
+Only the methods encountered in testing to date are implemented.
+
+For full details, read the code.
+
+=cut
+
+package TWiki::Func;
+
+use BaseFixture;
+use strict;
+
+#$TWiki::Plugins::VERSION = 1; #Beijing
+$TWiki::Plugins::VERSION = 1.020; #Cairo
+
+
+sub getDataDir {
+  return "$BaseFixture::testDir/data";
+}
+
+sub getSkin {
+  return $BaseFixture::skin;
+}
+
+sub _handleTmplP {
+    my( $theVar ) = @_;
+
+    my $val = "";
+    if( ( %TWiki::Store::templateVars ) &&
+		( exists $TWiki::Store::templateVars{ $theVar } ) ) {
+        $val = $TWiki::Store::templateVars{ $theVar };
+        $val =~ s/%TMPL\:P{[\s\"]*(.*?)[\"\s]*}%/&_handleTmplP($1)/geo;
+    }
+    return $val;
+}
+
+sub redirectCgiQuery {
+  my ($query, $url ) = @_;
+  $BaseFixture::redirected = $url;
+}
+
+sub getCgiQuery {
+  return $BaseFixture::query;
+}
+
+sub  getWikiName {
+  return "TestRunner";
+}
+
+sub  getWikiUserName {
+  return getMainWebname().".TestRunner";
+}
+
+sub userToWikiName {
+  my $n = shift;
+  return "TestRunner" if ($n && $n =~ /testrunner$/);
+  return "TWikiGuest";
+}
+
+sub  wikiToUserName {
+  my $n = shift;
+
+  return "testrunner" if $n =~ /TestRunner$/;
+  return undef;
+}
+
+sub  formatGmTime {
+    my( $theTime, $theFormat ) = @_;
+
+    return gmtime( $theTime );
+}
+
+sub  getUrlHost {
+  return "host";
+}
+
+sub  getViewUrl {
+  my ($w,$t) = @_;
+  return getScriptUrl($w, $t, "view");
+}
+
+sub  getTwikiWebname {
+  return "TWiki";
+}
+
+sub  getScriptUrlPath {
+  return "$BaseFixture::pwd/../../../../../bin";
+}
+
+sub  getScriptUrl {
+  my ($w,$t,$s) = @_;
+  return "http://twiki/$s.cgi/$w/$t";
+}
+
+sub  getPubDir {
+  return "$BaseFixture::pwd/../../../../../pub";
+}
+
+sub getMainWebname {
+  return "Main";
+}
+
+sub  getTopicList {
+  my $web = shift;
+
+  opendir(DH, getDataDir()."/$web");
+  my @list = grep /^.*\.txt$/, readdir(DH);
+  closedir(DH);
+  foreach my $f (@list) {
+    $f =~ s/\.txt$//o;
+  }
+  return @list;
+}
+
+sub  webExists {
+  my $web = shift;
+  return ( -d getDataDir() . "/$web" );
+}
+
+sub topicExists {
+  my ( $web, $topic ) = @_;
+  return 0 unless webExists($web);
+  return -f getDataDir() . "/$web/$topic.txt";
+}
+
+sub checkTopicEditLock {
+  my ( $web, $topic ) = @_;
+  my $lockf = getDataDir()."/$web/$topic.lock";
+  if ( -f $lockf ) {
+    my $data = BaseFixture::readFile($lockf);
+    $data =~ m/^(.*)\n(.*)$/s;
+    return ("OOPSLOCKED", $1, $2);
+  }
+  return (undef, undef, 0);
+}
+
+sub setTopicEditLock {
+  my( $web, $topic, $lock ) = @_;
+  die unless ($web);
+  die unless ($topic);
+  if ($lock) {
+    BaseFixture::lockTopic($web, $topic, time());
+  } else {
+    BaseFixture::unlockTopic($web,$topic);
+  }
+}
+
+sub checkAccessPermission {
+  my ($type, $wikiName, $text, $topic, $web) = @_;
+
+  die unless ($type =~ /^(view|change|create)$/i);
+  die unless ($wikiName);
+  die unless ($web);
+  return 1;
+}
+
+sub readTopicText {
+  my ( $web,$topic,$rev ) = @_;
+  if (defined($rev) && $rev ne "") {
+	my $cmd = "/usr/bin/co -q -p -r1.$rev ".getDataDir() . "/$web/$topic.txt";
+	return `$cmd`;
+  }
+  return BaseFixture::readFile( getDataDir() . "/$web/$topic.txt" );
+}
+
+sub readTopic {
+  my ($web, $topic) = @_;
+
+  my $text = readTopicText($web, $topic);
+  $text =~ s/^%META.*?$//g;
+  return (undef, $text);
+}
+
+sub readTemplate {
+  my ($template,$skin) = @_;
+  $template .= ".$skin" if ($skin);
+  my $text = BaseFixture::readFile("$BaseFixture::pwd/../../../../../templates/$template.tmpl" );
+
+  my $result = "";
+  my $key  = "";
+  my $val  = "";
+  my $delim = "";
+  foreach( split( /(%TMPL\:)/, $text ) ) {
+    if( /^(%TMPL\:)$/ ) {
+      $delim = $1;
+    } elsif( ( /^DEF{[\s\"]*(.*?)[\"\s]*}%[\n\r]*(.*)/s ) && ( $1 ) ) {
+      # handle %TMPL:DEF{"key"}%
+      if( $key ) {
+	$TWiki::Store::templateVars{ $key } = $val;
+      }
+      $key = $1;
+      $val = $2 || "";
+    } elsif( /^END%[\n\r]*(.*)/s ) {
+      $TWiki::Store::templateVars{ $key } = $val;
+      $key = "";
+      $val = "";
+      $result .= $1 || "";
+    } elsif( $key ) {
+      $val    .= "$delim$_";
+    } else {
+      $result .= "$delim$_";
+    }
+  }
+
+  # handle %TMPL:P{"..."}% recursively
+  $result =~ s/%TMPL\:P{[\s\"]*(.*?)[\"\s]*}%/&handleTmplP($1)/geo;
+  $result =~ s|^(( {3})+)|"\t" x (length($1)/3)|geom;  # leading spaces to tabs
+  return $result;
+}
+
+sub  renderText {
+  my ($text) = @_;
+  return $text." RENDERED";
+}
+
+sub  expandCommonVariables {
+  my ($text) = @_;
+  my $wpd = getPubDir();
+  my $wpu = "file:$wpd";
+  $text =~ s/%ATTACHURL%/$wpu/eog;
+  $text =~ s/%ATTACHURLPATH%/$wpd/eog;
+  $text =~ s/%TMPL:P{\"?(.*?)\"?}%/&_handleTmplP($1)/geo;
+  $text =~ s/%([A-Za-z0-9_]+)%/&_evbl($1)/ego;
+  if ($BaseFixture::query) {
+    $text =~ s/%URLPARAM{(.*?)}%/&_eurlp($1)/ego;
+  }
+  return $text;
+}
+
+sub _eurlp {
+  my $ats = shift;
+  my $vbl = extractNameValuePair($ats);
+  my $repl = extractNameValuePair($ats, "newline");
+  if ( $BaseFixture::query->param($vbl)) {
+    my $val = $BaseFixture::query->param($vbl);
+    $val =~ s/\n/$repl/go if ($repl);
+    return $val;
+  }
+  return "%URLPARAM{\"$vbl\"}%";
+}
+
+sub _evbl {
+  my $vbl = shift;
+
+  my $var = getPreferencesValue($vbl);
+  if ($var) {
+    return $var
+  } else {
+    return "%".$vbl."%";
+  }
+}
+
+sub  getPreferencesValue {
+  my $what = shift;
+
+  return $BaseFixture::prefs{$what};
+}
+
+sub  getPreferencesFlag {
+  my $what = shift;
+
+  return $BaseFixture::prefs{$what};
+}
+
+sub writeDebug {
+  my ($text) = @_;
+  push(@BaseFixture::debug, $text);
+}
+
+sub writeWarning {
+  my ($text) = @_;
+  push(@BaseFixture::warning, $text);
+}
+
+sub extractNameValuePair
+{
+    my( $str, $name ) = @_;
+    my $TranslationToken= "\0";
+
+    my $value = "";
+    return $value unless( $str );
+    $str =~ s/\\\"/\\$TranslationToken/g;  # escape \"
+
+    if( $name ) {
+        # format is: %VAR{ ... name = "value" }%
+        if( $str =~ /(^|[^\S])$name\s*=\s*\"([^\"]*)\"/ ) {
+            $value = $2 if defined $2;  # distinguish between "" and "0"
+        }
+
+    } else {
+        # test if format: { "value" ... }
+        if( $str =~ /(^|\=\s*\"[^\"]*\")\s*\"(.*?)\"\s*(\w+\s*=\s*\"|$)/ ) {
+            # is: %VAR{ "value" }%
+            # or: %VAR{ "value" param="etc" ... }%
+            # or: %VAR{ ... = "..." "value" ... }%
+            # Note: "value" may contain embedded double quotes
+            $value = $2 if defined $2;  # distinguish between "" and "0";
+
+        } elsif( ( $str =~ /^\s*\w+\s*=\s*\"([^\"]*)/ ) && ( $1 ) ) {
+            # is: %VAR{ name = "value" }%
+            # do nothing, is not a standalone var
+
+        } else {
+            # format is: %VAR{ value }%
+            $value = $str;
+        }
+    }
+    $value =~ s/\\$TranslationToken/\"/go;  # resolve \"
+    return $value;
+}
+
+sub saveTopicText {
+  my ( $web, $topic, $text, $ignorePermissions, $dontNotify ) = @_;
+
+  die unless ($web);
+  die unless ($topic);
+  die unless ($text);
+
+  BaseFixture::writeTopic($web, $topic, $text);
+}
+
+sub formatTime {
+  my ($t,$f,$s) = @_;
+  my $st = "TIME";
+  $st = "$st$f" if ($f);
+  $st = "$st$s" if ($s);
+  return $st;
+}
+
+sub getRegularExpression {
+  my $name = shift;
+
+  if ($name eq "webNameRegex") {
+	return qr/[A-Z]+[A-Za-z0-9]*/;
+  } elsif ($name eq "wikiWordRegex") {
+	return qr/[A-Z]+[a-z]+[A-Z]\w*/;
+  } elsif ($name eq "anchorRegex") {
+	return qr/\#[A-Za-z0-9_]+/;
+  } else {
+	die "$name is not usable";
+  }
+}
+
+
+1;
---
+++ twikiplugins/BuildContrib/lib/TWiki/Contrib/Build.pm
@@ -0,0 +1,696 @@
+#
+# Copyright (C) 2004 C-Dot Consultants - All rights reserved
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
+#
+package TWiki::Contrib::Build;
+
+=begin text
+
+---+ Package TWiki::Contrib::Build
+Base class of build objects for TWiki packages. Creates a build environment
+that addresses most of the common requirements for building plugins and
+contrib modules.
+
+Works by defining targets as functions that can then be subclassed and added
+to. Just about as simple a build system as you could get.
+
+The list of files to be installed is determined from the MANIFEST.
+Only these files will get into the release zip.
+
+Requires the environment variable TWIKI_LIBS (a colon-separated path
+list) to be set to point at any required dependencies.
+
+The following help information is cursory; for full details, look at an example or read the code.
+
+---++ Targets
+The following targets will always exist:
+   1 build - check that everything is perl
+   1 test - run unit tests
+   1 install - install on local installation defined by $TWIKI_HOME
+   1 uninstall - uninstall from local installation defined by $TWIKI_HOME
+   1 pod - build POD documentation
+   1 release - build, pod and package a release zip
+   1 upload - build, pod, package and upload to twiki.org
+Note: if you override any of these targets it is generally wise to call the SUPER version of the target!
+---++ Standard directory structure
+The standard module directory structure mirrors the TWiki installation directory structure, so each file in the development directory structure is in the place it will be in in the actual installation. From the root, these are the key files:
+   * MANIFEST - required - list of files and descriptions to include in release zip
+      * each file is given by the full path to the file relative to the build TWiki installation directory. Wildcards may NOT be used.
+   * DEPENDENCIES - optional list of dependencies on other modules and descriptions
+      * Dependencies should be expressed as "name,version,type,description" where name is the name of the module, version is the version constraint (e.g. ">1.5"), type is its type (CPAN, perl, C etc) and description is a short description of the module and where to get it.
+      *  The special type "contrib" is used to refer to another plugin or code library. In this case "name" must be the fully-qualified package name, and the description should explain how to get the package.
+   * lib/ - as you expect to see in installation. Should also contain other Perl modules.
+      * lib/TWiki/
+         * lib/TWiki/Plugins/ - this is where your <plugin name>.pm file goes for plugins
+            * lib/TWiki/Plugins/<plugin name>/ - directory containing sub-modules used by your plugin, and your build script.
+         * lib/TWiki/Contrib/ - this is where your <contrib name>.pm file goes for contribs
+            * lib/TWiki/Contrib/<contrib name>/ - directory containing sub-modules used by your contrib, and your build script.
+   * data/ - as you expect to see in the installation
+   * pub/ - as you expect to see in the installation. You must list required directories, even if they are initially empty.
+   * templates/ - as you expect to see in installation
+      * templates/<skin name>/ - this is where templates for your skin go
+---++ Token expansion
+The build supports limited token expansion in =.txt= files. It expands the following tokens by default when the release target is built.
+| =%$<nop>MANIFEST%= | Expands to a TWiki table of MANIFEST contents |
+| =%$<nop>DEPENDENCIES%= | Expands to a comma-separated list of dependencies |
+| =%$<nop>DATE%= | Expands to today's date |
+| =%$<nop>VERSION%= | Expands to the VERSION number set in the plugin/contrib main .pm topic |
+| =%$<nop>POD%= | Expands POD text in all =.pm= files in the MANIFEST. Pod is generated for each module in the order of the MANIFEST. |
+---++ Methods
+
+=cut
+
+use strict;
+use File::Copy;
+use File::Spec;
+use Pod::Text;
+use POSIX;
+use diagnostics;
+use vars qw( $VERSION $basedir $shared $twiki_home $buildpldir $libpath $testrunner);
+
+$VERSION = 1.00;
+
+BEGIN {
+  use File::Spec;
+  $buildpldir = `dirname $0`; chop($buildpldir);
+  $buildpldir = File::Spec->rel2abs($buildpldir);
+  print "Building in $buildpldir\n";
+  $basedir = $buildpldir;
+  # Find the lib root
+  unless ($basedir =~ s/^(.*)[\\\/](lib[\\\/]TWiki[\\\/].*)$/$1/) {
+    die "Couldn't find lib/TWiki in $basedir";
+  }
+  $libpath = $2;
+  $libpath =~ s/[\\\/][^\\\/]*$//;
+
+  print "Basedir is $basedir\nComponent dir is $libpath\n";
+  $twiki_home = $ENV{"TWIKI_HOME"};
+  if ($ENV{TWIKI_LIBS}) {
+    foreach my $pc (split(/:/,$ENV{TWIKI_LIBS})) {
+      unless(grep(/$pc/,@INC)) {
+        unshift(@INC, $pc);
+      }
+    }
+  }
+  unless(grep(/$basedir\/lib/,@INC)) {
+    unshift(@INC, "$basedir/lib");
+  }
+  print "Using path ".join(":",@INC)."\n";
+
+  # find testrunner
+  foreach my $d ( @INC ) {
+    my $dir = `dirname $d`;
+    chop($dir);
+    $dir .= "/contrib";
+    if (-f "$dir/TestRunner.pl") {
+      $testrunner = "-I $dir/fixtures $dir/TestRunner.pl";
+      last;
+    }
+  }
+}
+
+=begin text
+
+---+++ new($project)
+| $project | Name of plugin, addon, contrib or skin |
+| $rootModule | Optional, if defined gives the name of the root .pm module that carries the VERSION and dependencies. Defaults to $project |
+Construct a new build object. Define the basic directory
+paths to places in the build/release. Read the manifest topic
+and build file and dependency lists. Parse command line to get
+target and options.
+
+=cut
+sub new {
+  my ( $class, $project, $rootModule ) = @_;
+  my $this = {};
+  
+  $this->{project} = $project;
+  $this->{target} = "test";
+  
+  my $n = 0;
+  my $done = 0;
+  while ($n <= $#ARGV) {
+	if ($ARGV[$n] =~ /^-/o) {
+	  $this->{$ARGV[$n]} = 1;
+	} else {
+	  $this->{target} = $ARGV[$n];
+	}
+	$n++;
+  }
+
+  chdir($basedir);
+  $basedir = `pwd`;
+  chop($basedir);
+  $this->{basedir} = $basedir;
+
+  # The following paths are all relative to the root of the twiki
+  # installation
+
+  # where the sub-modules live
+  $this->{libdir} = $libpath;
+
+  # the .pm module
+  if ($rootModule) {
+    $this->{pm} = "$libpath/$rootModule.pm";
+  } else {
+    $this->{pm} = "$libpath/$project.pm";
+  }
+
+  my $stubpath = $this->{pm};
+  $stubpath =~ s/.*[\\\/](TWiki[\\\/].*)\.pm/$1/;
+  $stubpath =~ s/[\\\/]/::/g;
+  $this->{STUB} = $stubpath;
+
+  # where data files live
+  $this->{data_twiki} = "data/TWiki";
+
+  # the root of the name of data files
+  $this->{data_twiki_module} = $this->{data_twiki}."/".$this->{project};
+
+  # read the manifest
+  my $manifest = "$basedir/MANIFEST";
+  open(PF, "<$manifest") ||
+	die "$manifest missing";
+  my $line;
+  while ($line = <PF>) {
+	if ( $line =~ /^(\S+)(\s+(\S.*))?\s*$/o ) {
+	  push(@{$this->{files}},
+		   { name => $1, description => ($3 || "") });
+	}
+  }
+  close(PF);
+  my $mantable = "";
+  foreach my $file (@{$this->{files}}) {
+	$mantable .= "\t| ==" . $file->{name} . "== | " .
+	  $file->{description} . " |\n";
+  }
+  $this->{MANIFEST} = $mantable;
+
+  my $deps = "$basedir/DEPENDENCIES";
+  if (-f $deps) {
+	open(PF, "<$deps") ||
+	  die "$deps open failed";
+	while ($line = <PF>) {
+	  if ($line =~ m/^(\w+)\s+(\w*)\s*(.*)$/o) {
+		push(@{$this->{dependencies}},
+			 { name=>$1, type=>$2, version=>"1.000", description=>$3 });
+	  } elsif ($line =~ m/^([^,]+),([^,]+),\s*(\w*)\s*,\s*(.+)$/o) {
+		push(@{$this->{dependencies}},
+			 { name=>$1, version=>$2, type=>$3, description=>$4 });
+	  }
+	}
+  } else {
+    print STDERR "WARNING: no $deps; dependencies will only be extracted from code\n";
+  }
+  close(PF);
+
+  my $version = "Unknown";
+
+  if (open(PF,"<$basedir/$this->{pm}")) {
+	my $text = "";
+	while ($line = <PF>) {
+	  $line =~ s/\s+//g;
+	  $text .= $line;
+	}
+    if ($text =~ /^\s*\$VERSION\s*=\s*(.*)\s*;+\s*$/m) {
+      $version = $1;
+    }
+	if ($text =~ /\@dependencies=\((.*?)\)/o ) {
+	  $text = $1;
+	  while ($text =~ s/package=>'(.*?)',constraint=>'(.*?)'//) {
+		my ($name,$ver,$found) = ($1,$2,0);
+		if ($name ne "TWiki::(Plugins|Contrib)") {
+		  foreach my $dep (@{$this->{dependencies}}) {
+			if ($dep->{name} eq $name) {
+			  $dep->{version} = $ver;
+			  $found = 1;
+			  last;
+			}
+		  }
+		  unless ($found) {
+			push(@{$this->{dependencies}},
+				 { name=>$name, version=>$ver, type=>'contrib', description=>$name });
+		  }
+		}
+	  }
+	}
+  } else {
+    print STDERR "WARNING: $this->{pm} not found; cannot extract VERSION or in-code dependencies\n";
+  }
+  close(PF);
+
+  my $deptable = "";
+  my $a = " align=\"left\"";
+  foreach my $dep (@{$this->{dependencies}}) {
+	my $v = $dep->{version};
+	$v =~ s/&/&amp;/go;
+	$v =~ s/>/&gt;/go;
+	$v =~ s/</&lt;/go;
+	$deptable .= "<tr><td$a>" .
+	  $dep->{name} . "</td><td$a>" .
+		$v . "</td><td$a>" .
+		  $dep->{description} . "</td></tr>";
+  }
+  $this->{DEPENDENCIES} = "None";
+  if ($deptable ne "") {
+	$this->{DEPENDENCIES} = "<table border=1><tr><th$a>Name</th><th$a>Version</th><th$a>Description</th></tr>$deptable</table>";
+  }
+
+  $this->{VERSION} = $version;
+
+  $this->{DATE} = POSIX::strftime("%T %d %B %Y", localtime);
+
+  return bless( $this, $class );
+}
+
+=begin text
+
+---+++ cd($dir)
+  Change to the given directory
+
+=cut
+sub cd {
+  my ($this, $file) = @_;
+  
+  if ($this->{-v} || $this->{-n}) {
+	print "cd $file\n";
+  }
+  if (!$this->{-n}) {
+	chdir($file) || die "Failed to cd to $file";
+  }
+}
+
+sub rm {
+  my ($this, $file) = @_;
+  
+  if ($this->{-v} || $this->{-n}) {
+	print "rm $file\n";
+  }
+  unless ($this->{-n}) {
+	unlink($file) || warn "Warning: Failed to delete $file";
+  }
+}
+
+=begin text
+
+---+++ makepath($to)
+Make a directory and all directories leading to it.
+
+=cut
+sub makepath {
+  my ($this, $to) = @_;
+
+  chop($to) if ($to =~ /\n$/o);
+
+  return if (-d $to || $this->{made_dir}->{$to});
+  $this->{made_dir}->{$to} = 1;
+
+  if (! -e $to) {
+	$this->makepath(`dirname $to`);
+	if ($this->{-v} || $this->{-n}) {
+	  print "mkdir $to\n";
+	}
+	unless ($this->{-n}) {
+	  mkdir($to) || warn "Warning: Failed to make $to: $!";
+	}
+  } else {
+	warn "Warning: $to exists and is not a directory; cannot create a dir over it";
+  }
+}
+
+=begin text
+
+---+++ cp($from, $to)
+Copy a single file from - to. Will automatically make intervening
+directories in the target. Also works for target directories.
+
+=cut
+sub cp {
+  my ($this, $from, $to) = @_;
+
+  die "Source file $from does not exist " unless ( $this->{-n} || -e $from);
+
+  $this->makepath(`dirname $to`);
+
+  if ($this->{-v} || $this->{-n}) {
+	print "cp $from $to\n";
+  }
+  unless ($this->{-n}) {
+	if ( -d $from ) {
+	  mkdir($to) || warn "Warning: Failed to make $to: $!";;
+	} else {
+	  File::Copy::copy($from, $to) ||
+		warn "Warning: Failed to copy $from to $to: $!";
+	}
+  }
+}
+
+=begin text
+
+---+++ prot($perms, $file)
+Set permissions on a file. Permissions should be expressed using POSIX
+chmod notation.
+
+=cut
+sub prot {
+  my ($this, $perms, $file) = @_;
+  
+  $this->sys_action("chmod $perms $file");
+}
+
+=begin text
+
+---+++ sys_action($cmd)
+Perform a "system" command.
+
+=cut
+sub sys_action {
+  my ($this, $cmd) = @_;
+  
+  if ($this->{-v} || $this->{-n}) {
+	print "$cmd\n";
+  }
+  unless ($this->{-n}) {
+	system($cmd);
+	die "Failed to $cmd\n" if ($?);
+  }
+}
+
+=begin text
+
+---+++ target_build
+Basic build target. By default does nothing, but subclasses may want to
+extend on that.
+
+=cut
+sub target_build {
+  my $this = shift;
+  # does nothing
+}
+
+=begin text
+
+---+++ target_test
+Basic Test::Unit test target, runs <project>Suite.
+
+=cut
+
+sub target_test {
+  my $this = shift;
+  $this->build("build");
+  my $testdir = "$basedir/$this->{libdir}/$this->{project}/test";
+  my $testsuite = $this->{project}."Suite";
+  if (!-f "$testdir/$testsuite.pm") {
+    print STDERR "WARNING: COULD NOT FIND ANY TESTS FOR '$this->{project}' IN $testdir/$testsuite.pm\n";
+    return;
+  }
+  unless($testrunner) {
+    print STDERR "WARNING: CANNOT RUN TESTS; ../contrib/TestRunner.pl not found in path\n";
+    return;
+  }
+
+  $this->cd($testdir);
+  my $inc = join(" -I", @INC);
+  $this->sys_action("perl -w -I$inc $testrunner $testsuite");
+}
+
+=begin text
+
+---+++ filter
+Expands tokens in a documentation topic.Four tokens are supported:
+   * %$MANIFEST% - TWiki table of files in MANIFEST
+   * %$DEPENDENCIES% - list of dependencies from DEPENDENCIES
+   * %$VERSION% version from $VERSION in main .pm
+   * %$DATE% - local date
+   * %$POD% - expands to the POD documentation for the package, excluding test modules.
+
+=cut
+
+sub filter {
+  my ($this, $from, $to) = @_;
+
+  return unless (-f $from);
+
+  open(IF, "<$from") || die "No source topic $from for filter";
+  unless ($this->{-n}) {
+	open(OF, ">$to") || die "No dest topic $to for filter";
+  }
+  my $line;	
+  while ($line = <IF>) {
+	$line =~ s/%\$(\w+)%/&_expand($this,$1)/geo;
+	print OF $line unless ($this->{-n});
+  }
+  close(IF);
+  print OF "<!-- Do _not_ attempt to edit this topic; it is auto-generated. Please add comments/questions/remarks to the Dev topic instead. -->\n";
+  close(OF) unless ($this->{-n});
+}
+
+sub _expand {
+  my ($this, $tok) = @_;
+  if (!$this->{$tok} && $tok eq "POD") {
+	$this->build("pod");
+  }
+  if (defined($this->{$tok})) {
+	if ($this->{-v} || $this->{-n}) {
+	  print "expand %\$$tok% to ".$this->{$tok}."\n";
+	}
+	return $this->{$tok};
+  } else {
+	return "%\$".$tok."%";
+  }
+}
+
+=begin text
+
+---+++ target_release
+Release target, builds release zip by creating a full release directory
+structure in /tmp and then zipping it in one go. Only files explicitly listed
+in the MANIFEST are released. Automatically runs =filter= on all =.txt= files
+in the MANIFEST.
+
+=cut
+sub target_release {
+  my $this = shift;
+  my $project = $this->{project};
+
+  $this->build("tests_zip");
+
+  my $tmpdir = "/tmp/$$";
+  $this->makepath($tmpdir);
+
+  $this->copy_fileset($this->{files}, $basedir, $tmpdir);
+  foreach my $file (@{$this->{files}}) {
+	if ($file->{name} =~ /\.txt$/) {
+	  my $txt = $file->{name};
+print "Filtering $txt\n";
+	  $this->filter("$basedir/$txt", "$tmpdir/$txt");
+	}
+  }
+  $this->cp("$tmpdir/".$this->{data_twiki_module}.".txt",
+			"$basedir/$project.txt");
+  $this->cd($tmpdir);
+  $this->sys_action("zip -r $project.zip *");
+  $this->sys_action("mv $tmpdir/$project.zip $basedir/$project.zip");
+  print "Release ZIP is $basedir/$project.zip\n";
+  print "Release TOPIC is $basedir/$project.txt\n";
+  $this->sys_action("rm -rf $tmpdir");
+}
+
+=begin text
+
+---+++ copy_fileset
+Copy all files in a file set from on directory root to another.
+
+=cut
+sub copy_fileset {
+  my ($this, $set, $from, $to) = @_;
+
+  my $uncopied = scalar(@$set);
+  print "Copying $uncopied files to $to\n";
+  foreach my $file (@$set) {
+	my $name = $file->{name};
+	if (! -e "$from/$name") {
+	  die "$from/$name does not exist - cannot copy\n";
+	}
+	$this->cp("$from/$name", "$to/$name");
+	#$this->prot("a+rx,u+w", "$to/$name");
+	$uncopied--;
+  }
+  die "Files left uncopied" if ($uncopied);
+}
+
+=begin text
+
+---+++ target_install
+Install target, installs to local twiki pointed at by TWIKI_HOME.
+
+=cut
+sub target_install {
+  my $this = shift;
+  $this->build("build");
+
+  my $twiki = $ENV{TWIKI_HOME};
+  die "TWIKI_HOME not set" unless $twiki;
+  $this->copy_fileset($this->{files}, $basedir, $twiki);
+}
+
+=begin text
+
+---+++ target_uninstall
+Uninstall target, uninstall from local twiki pointed at by TWIKI_HOME.
+
+=cut
+sub target_uninstall {
+  my $this = shift;
+  my $twiki = $ENV{TWIKI_HOME};
+  die "TWIKI_HOME not set" unless $twiki;
+  foreach my $file (@{$this->{files}}) {
+	$this->rm("$twiki/".$file->{name});
+  }
+}
+
+=begin text
+
+---+++ target_test_zip
+Make the tests zip file for inclusion in the release package.
+
+=cut
+sub target_tests_zip {
+  my $this = shift;
+
+  $this->cd("$basedir/".$this->{libdir});
+  $this->rm("test.zip");
+  if (-d 'test') {
+	$this->sys_action("zip -r test.zip test -x '*~' -x '*/CVS*' -x '*/testdata*'");
+  }
+}
+
+=begin text
+
+---+++ target_upload
+Upload to twiki.org. Prompts for username and password. Uploads the zip and
+the text topic to the appropriate places. Creates the topic on twiki.org if
+necessary. Requires curl.
+
+=cut
+
+sub target_upload {
+  my $this = shift;
+  $this->build("release");
+
+  my $user;
+  my $pass;
+  do {
+	print "Username on TWiki.org: ";
+	$user = <STDIN>;
+  } while ( !$user || $user =~ /^\s*$/ );
+  chop($user);
+  do {
+	print "Password: ";
+	$pass = <STDIN>;
+  } while (!$pass || $pass =~ /^\s*$/);
+  chop($pass);
+  my $curl = "curl -s -S -u $user:$pass";
+  my $to = $this->{project};
+  # Get the old form data and attach it to the update
+  my $oldform = `$curl http://TWiki.org/cgi-bin/view/Plugins/$to`;
+  my $opts = "";
+  foreach my $line ( split(/\n/, $oldform)) {
+	if ( $line =~ m/(TopicClassification|CVSModificationPolicy|DeveloperVersionInCVS|InstalledOnTWikiOrg|DemoUrl).*?<\/td><td.*?>(.*)<\/td>/o ) {
+	  my $val = _unhtml($2);
+	  if ($val && $val ne "") {
+		$opts .= " -F $1=$val";
+	  }
+	} elsif ( $line =~ m/(TestedOnTWiki|TestedOnOS|ShouldRunOnOS).*?<\/td><td.*?>(.*?)<\/td>/o ) {
+	  my $func = $1;
+	  foreach my $plaf ( split( /,/, _unhtml($2))) {
+		if ($plaf ne "") {
+		  $opts .= " -F $func$plaf=Yes";
+		}
+	  }
+	}
+  }
+  print `$curl -F text=\\<$basedir/$to.txt $opts http://TWiki.org/cgi-bin/save/Plugins/$to`;
+  die "Update of topic failed: $?" if ( $?);
+  print `$curl -F filepath=\\\@$basedir/$to.zip -F filename=$to.zip http://TWiki.org/cgi-bin/upload/Plugins/$to`;
+  die "Update of zip failed: $?" if ( $?);
+}
+
+sub _unhtml {
+  my $html = shift;
+
+  $html =~ s/<[^<>]*>//og;
+  $html =~ s/&\w+;//go;
+  $html =~ s/\s//go;
+
+  return $html;
+}
+
+=begin text
+
+---+++ target_pod
+
+Build POD documentation. This target defines =%$POD%= - it
+does not generate any output files. The target will be invoked
+automatically if =%$POD%= is used in a .txt file. POD documentation
+is intended for use by developers only.
+
+POD test in =.pm= files should use TWiki syntax or HTML. Packages should be
+introduced with a level 0 header, and each method in the package by
+a second level header. Make sure you document any global variables used
+by the module.
+
+=cut
+
+sub target_pod {
+  my $this = shift;
+  my $tmpfile = "/tmp/buildpod";
+  $this->{POD} = "";
+
+  foreach my $file (@{$this->{files}}) {
+	my $pmfile = $file->{name};
+	if ($pmfile =~ /\.pm$/o) {
+	  $pmfile = "$basedir/$pmfile";
+	  my $parser = new Pod::Text(indent => 0);
+	  $parser->parse_from_file($pmfile, $tmpfile);
+	  open(TMP, $tmpfile);
+	  while (<TMP>) {
+		$this->{POD} .= $_;
+	  }
+	  close(TMP);
+	}
+  }
+  unlink($tmpfile);
+}
+
+=begin text
+
+---+++ build($target)
+Build the given target
+
+=cut
+no strict "refs";
+sub build {
+  my $this = shift;
+  my $target = shift;
+  print "Building $target\n";
+  eval "\$this->target_$target()";
+  if ($@) {
+	print "Failed to build $target: $@\n";
+	die $@;
+  }
+  print "Built $target\n";
+}
+use strict "refs";
+
+1;
