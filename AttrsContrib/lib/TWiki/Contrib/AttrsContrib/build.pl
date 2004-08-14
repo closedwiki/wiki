f46103f98c85057f8f68522d35c722b15a05e8f3
crawfordcurrie <crawfordcurrie@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-14 12:40:02 +0000

    tidy up and ensure it all builds
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5035 a00a5322-12db-0310-a70b-8735589c885e

--- AttrsContrib/lib/TWiki/Contrib/AttrsContrib/build.pl
+++ AttrsContrib/lib/TWiki/Contrib/AttrsContrib/build.pl
@@ -33,7 +33,7 @@ use TWiki::Contrib::Build;
 
   sub new {
     my $class = shift;
-    return bless( $class->SUPER::new( "AttrsContrib" ), $class );
+    return bless( $class->SUPER::new( "AttrsContrib", "Attrs" ), $class );
   }
 
   # Example: Override the build target
