d48d3ddaf17450c2fa6bd4441fc0fcb0be387cf6
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-18 16:29:36 +0000

    Made so that it can be installed across multiple installations at the same time. localInstallation.md5 is likely to be broken.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5059 a00a5322-12db-0310-a70b-8735589c885e

---
+++ TWikiReleaseTrackerPlugin/install_it.sh
@@ -0,0 +1,9 @@
+export TWIKI_SHARED=`pwd`
+cd lib/TWiki/Plugins/TWikiReleaseTrackerPlugin
+
+for i in athenstwiki.mrjc.com/twiki beijingtwiki.mrjc.com/twiki cairotwiki.mrjc.com/twiki
+do
+   export TWIKI_HOME=/home/mrjc/$i
+   perl build.pl install
+done
+
