481894dea736fc065d1fa176cd44f5f9a6117d16
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-18 16:29:36 +0000

    Made so that it can be installed across multiple installations at the same time. localInstallation.md5 is likely to be broken.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5059 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/TRTConfig.pm
+++ twikiplugins/TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/TRTConfig.pm
@@ -5,8 +5,12 @@ use strict;
 BEGIN {
     $Common::excludeFilePattern = 'DEADJOE|.svn|\~$|\,v|.changes|.mailnotify|.session';
     $Common::websToIndex = 'Main|TWiki|Know|Trash';
-    $Common::installationDir = ""; # Uncomment for distro; #NB. assumes below, e.g. twiki/bin/view, not bin/view
 #    $Common::installationDir = "/home/mrjc/beijingtwiki.mrjc.com/"; #NB. assumes below, e.g. twiki/bin/view, not bin/view
+
+    my $twiki_home = $ENV{TWIKI_HOME};
+    if ($twiki_home) {
+	$Common::installationDir = "$twiki_home"; # Uncomment for distro; #NB. assumes below, e.g. twiki/bin/view, not bin/view
+    }
     $Common::downloadDir = "/home/mrjc/twikiplugindev/stage/twikiplugins/shared/tools/download/";
 }
 
