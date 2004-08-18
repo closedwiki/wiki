2011e39b978c6f2db1716db953be0c0e406a9192
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-18 22:29:13 +0000

    added cleaver.org
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5063 a00a5322-12db-0310-a70b-8735589c885e

--- TWikiReleaseTrackerPlugin/install_it.sh
+++ TWikiReleaseTrackerPlugin/install_it.sh
@@ -1,7 +1,7 @@
 export TWIKI_SHARED=`pwd`
 cd lib/TWiki/Plugins/TWikiReleaseTrackerPlugin
 
-for i in athenstwiki.mrjc.com/twiki beijingtwiki.mrjc.com/twiki cairotwiki.mrjc.com/twiki
+for i in athenstwiki.mrjc.com/twiki beijingtwiki.mrjc.com/twiki cairotwiki.mrjc.com/twiki cleaver.org/twiki
 do
    export TWIKI_HOME=/home/mrjc/$i
    perl build.pl install
