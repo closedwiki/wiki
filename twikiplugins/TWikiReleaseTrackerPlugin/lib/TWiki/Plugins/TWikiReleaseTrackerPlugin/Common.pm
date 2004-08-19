44859ce8d2501b628fd4378c5374a6e2107a73f3
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-19 14:18:22 +0000

    Reorganised Indexing from build to IndexDistributions. Probably broken
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5072 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/Common.pm
+++ twikiplugins/TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/Common.pm
@@ -1,48 +1,52 @@
 #! /usr/bin/perl -w
 
 use strict;
+
 package Common;
 use TRTConfig;
 
 #FIXME: needs tidying.
 
 my $pub;
-if (defined (&TWiki::Func::getPubDir)) {
-    $pub = TWiki::Func::getPubDir();
-} else {
-    $pub = "../../../../pub/";
+if ( defined(&TWiki::Func::getPubDir) ) {
+	$pub = TWiki::Func::getPubDir();
+}
+else {
+	$pub = "../../../../pub/";
 }
 
-unless (defined $Common::md5IndexDir) {
-    setIndexTopic("TWiki.TWikiReleaseTrackerPlugin");
+unless ( defined $Common::md5IndexDir ) {
+	setIndexTopic("TWiki.TWikiReleaseTrackerPlugin");
 }
 
-unless (-d $Common::md5IndexDir) {
-    die "Couldn't locate directory $Common::md5IndexDir - $!";
+unless ( -d $Common::md5IndexDir ) {
+	die "Couldn't locate directory $Common::md5IndexDir - $!";
 }
 
 {
-    no warnings;
-    sub debug {
-	my ($ans) = @_; 
+	no warnings;
+
+	sub debug {
+		my ($ans) = @_;
+
 #    print STDERR $ans; # you really don't want to forget to recomment this in a live environment
-    }
-    
-    sub relativeFromPathname {
-	my ($filename, $setpath) = @_;
-	my $ans = $filename;
-	$ans =~ s!$setpath!!;
-	return $ans;
-    }
+	}
 
-    sub setIndexTopic {
-	my ($webTopic) = @_;
-	my ($web, $topic) = ( $webTopic =~ m/(.+)\.(.+)/);
-	$Common::md5IndexDir = "$pub/$web/$topic/";
-    }
+	sub relativeFromPathname {
+		my ( $filename, $setpath ) = @_;
+		my $ans = $filename;
+		$ans =~ s!$setpath!!;
+		return $ans;
+	}
 
-    sub getPubDir {
-	return $pub;
-    }
+	sub setIndexTopic {
+		my ($webTopic) = @_;
+		my ( $web, $topic ) = ( $webTopic =~ m/(.+)\.(.+)/ );
+		$Common::md5IndexDir = "$pub/$web/$topic/";
+	}
+
+	sub getPubDir {
+		return $pub;
+	}
 }
 1;
