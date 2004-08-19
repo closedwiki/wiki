0e6d8aec0d175629d3b1546343b8e180f220eedc
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-19 02:52:22 +0000

    reformat
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5069 a00a5322-12db-0310-a70b-8735589c885e

--- TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/build.pl
+++ TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/build.pl
@@ -17,12 +17,11 @@
 # have to provide and where.
 #
 # Standard preamble
-BEGIN 
-{ 
-    print $ENV{TWIKI_LIBS};
-    foreach my $pc (split(/:/, $ENV{TWIKI_LIBS})) { 
-	unshift @INC, $pc; 
-    } 
+BEGIN {
+	print $ENV{TWIKI_LIBS};
+	foreach my $pc ( split( /:/, $ENV{TWIKI_LIBS} ) ) {
+		unshift @INC, $pc;
+	}
 }
 use TWiki::Contrib::Build;
 
@@ -36,168 +35,159 @@ use Common;
 
 use Cwd;
 my $runDir = cwd();
+
 # Declare our build package
 package TWikiReleaseTrackerPluginBuild;
 
-  @TWikiReleaseTrackerPluginBuild::ISA = ( "TWiki::Contrib::Build" );
+@TWikiReleaseTrackerPluginBuild::ISA = ("TWiki::Contrib::Build");
 
-  sub new {
-    my $class = shift;
-    my $this = bless( $class->SUPER::new( "TWikiReleaseTrackerPlugin" ), $class );
-    return $this;
-  }
+sub new {
+	my $class = shift;
+	my $this = bless( $class->SUPER::new("TWikiReleaseTrackerPlugin"), $class );
+	return $this;
+}
 
-  # Example: Override the build target
-  sub target_build {
-    my $this = shift;
+# Example: Override the build target
+sub target_build {
+	my $this = shift;
 
-    $this->SUPER::target_build();
+	$this->SUPER::target_build();
 
-    # Do other build stuff here
-  }
- 
-  sub target_install {
-    my $this = shift;
-    $this->SUPER::target_build();
-    target_indexLocalInstallation();
+	# Do other build stuff here
+}
 
-  }
+sub target_install {
+	my $this = shift;
+	$this->SUPER::target_build();
+	target_indexLocalInstallation();
 
-  sub target_indexLocalInstallation {
-    ensureInstallationDir();
-    indexLocalInstallation();
-    chdir ($runDir) || die "Can't cd into $runDir - $!";
-    FileDigest::saveIndex($Common::md5IndexDir."/localInstallation.md5");
-#    print FileDigest::dataOutline();
-  }
+}
 
-  sub target_indexReleases {
-      die "feature broken";
-      indexReleases("(?!beta)");
-      chdir ($runDir) || die "Can't cd into $runDir - $!";
-      FileDigest::saveIndex($Common::md5IndexDir."/releases.md5");
-  }
+sub target_indexLocalInstallation {
+	ensureInstallationDir();
+	indexLocalInstallation();
+	chdir($runDir) || die "Can't cd into $runDir - $!";
+	my $saveFile = $Common::md5IndexDir . "/localInstallation.md5" ;
+	print "saving to $saveFile";
+	FileDigest::saveIndex($saveFile);
 
-  sub target_indexBetaReleases {
-      indexReleases("beta");
-      chdir ($runDir) || die "Can't cd into $runDir - $!";
-      FileDigest::saveIndex($Common::md5IndexDir."/betas.md5");
-  }
+	#    print FileDigest::dataOutline();
+}
 
-  sub target_test {
-      my $this = shift;
-      ensureInstallationDir();
-      
-      print "Building TRTTestSuite\n";
-      chdir ($runDir."/test") || die "$! - can't cd into test dir";
-      print `perl testPluginTestSuite.pl`;
-      1;
-  }
+sub target_indexReleases {
+	die "feature broken";
+	indexReleases("(?!beta)");
+	chdir($runDir) || die "Can't cd into $runDir - $!";
+	FileDigest::saveIndex( $Common::md5IndexDir . "/releases.md5" );
+}
 
-  sub target_release {
-      my $this = shift;
-      if ($Common::installationDir ne "") {
-	  die "YOU FORGOT TO REMOVE THE DEFAULT INSTALL DIR!\n";
-      }
-      $this->SUPER::target_release();      
-  }
+sub target_indexBetaReleases {
+	indexReleases("beta");
+	chdir($runDir) || die "Can't cd into $runDir - $!";
+	FileDigest::saveIndex( $Common::md5IndexDir . "/betas.md5" );
+}
+
+sub target_test {
+	my $this = shift;
+	ensureInstallationDir();
+
+	print "Building TRTTestSuite\n";
+	chdir( $runDir . "/test" ) || die "$! - can't cd into test dir";
+	print `perl testPluginTestSuite.pl`;
+	1;
+}
+
+sub target_release {
+	my $this = shift;
+	if ( $Common::installationDir ne "" ) {
+		die "YOU FORGOT TO REMOVE THE DEFAULT INSTALL DIR!\n";
+	}
+	$this->SUPER::target_release();
+}
 
 # Create the build object
 my $builder = new TWikiReleaseTrackerPluginBuild();
 
 # Build the target on the command line, or the default target
 
-    if (@ARGV) {
-	$builder->build($builder->{target}); #NB. Buildpm picks up from ARGV
-    } else {
+if (@ARGV) {
+	$builder->build( $builder->{target} );    #NB. Buildpm picks up from ARGV
+}
+else {
 	$builder->build("test");
 	$builder->build("build");
-    }
-
-
-
+}
 
 sub indexLocalInstallation {
-    FileDigest::emptyIndexes();
-      print "Indexing localInstallation\n"; 
-      IndexDistributions::indexDistribution("localInstallation",
-					    $Common::installationDir,
-					    $Common::excludeFilePattern);
+	FileDigest::emptyIndexes();
+	print "Indexing localInstallation\n";
+	IndexDistributions::indexDistribution( "localInstallation",
+		$Common::installationDir, $Common::excludeFilePattern );
 
 }
 
-
-# These routines depend on a modified version of Crawfords' SharedCode 
+# These routines depend on a modified version of Crawfords' SharedCode
 sub target_indexReleasesOld {
-    my ($filterIn) = @_;
+	my ($filterIn) = @_;
 
-    chdir ($runDir) || die "Can't cd into $runDir - $!";
-    FileDigest::emptyIndexes();
-    my $dir = $Common::downloadDir;
+	chdir($runDir) || die "Can't cd into $runDir - $!";
+	FileDigest::emptyIndexes();
+	my $dir = $Common::downloadDir;
 
-    my @releases = getDirsListed($Common::downloadDir);
+	my @releases = getDirsListed($Common::downloadDir);
 
-    foreach my $release (@releases) {
-	next unless ($release =~ m/^TWiki/);
-	next unless ($release =~ m/$filterIn/ );
-	print "Indexing $release\n"; 
-	IndexDistributions::indexDistribution($release,
-					$dir.$release,
-					$Common::excludeFilePattern,
-					"twiki");
-    }
+	foreach my $release (@releases) {
+		next unless ( $release =~ m/^TWiki/ );
+		next unless ( $release =~ m/$filterIn/ );
+		print "Indexing $release\n";
+		IndexDistributions::indexDistribution( $release, $dir . $release,
+			$Common::excludeFilePattern, "twiki" );
+	}
 
-
-  }
-
-sub target_indexPlugins
-{
-    FileDigest::emptyIndexes();
-      chdir ($runDir) || die "Can't cd into $runDir - $!";
-      my $dir = $Common::downloadDir;
-      
-      my @modules = getDirsListed($dir);
-      
-      foreach my $module (@modules) {
-	  next if ($module =~ m/^TWiki/);
-	  print "$module\n"; 
-	  IndexDistributions::indexDistribution($module,
-						$dir."/".$module,
-						$Common::excludeFilePattern,
-						"twiki");
-      }
-      FileDigest::saveIndex($Common::md5IndexDir."/plugins.md5");
 }
 
+sub target_indexPlugins {
+	FileDigest::emptyIndexes();
+	chdir($runDir) || die "Can't cd into $runDir - $!";
+	my $dir = $Common::downloadDir;
 
+	my @modules = getDirsListed($dir);
+
+	foreach my $module (@modules) {
+		next if ( $module =~ m/^TWiki/ );
+		print "$module\n";
+		IndexDistributions::indexDistribution( $module, $dir . "/" . $module,
+			$Common::excludeFilePattern, "twiki" );
+	}
+	FileDigest::saveIndex( $Common::md5IndexDir . "/plugins.md5" );
+}
 
 sub getDirsListed {
-    my ($dir) = @_;
+	my ($dir) = @_;
 
-    use DirHandle;
-    my $dh = DirHandle->new($dir) || die "$! - $dir";
-    return sort
-#	   grep { -d }
-           grep { !/\./ }
-           $dh -> read();
+	use DirHandle;
+	my $dh = DirHandle->new($dir) || die "$! - $dir";
+	return sort
+
+	  #	   grep { -d }
+	  grep { !/\./ } $dh->read();
 }
 
-
 sub installsOfMine {
-    IndexDistributions::indexDistribution("athens", 
-			$ENV{HOME}."/athenstwiki.mrjc.com/", 
-			$Common::excludeFilePattern);
-      IndexDistributions::indexDistribution("beijing", 
-			  $ENV{HOME}."/beijingtwiki.mrjc.com/", 
-			  $Common::excludeFilePattern);
-      IndexDistributions::indexDistribution("cairo", 
-			  $ENV{HOME}."/cairotwiki.mrjc.com/",
-			  $Common::excludeFilePattern);
+	IndexDistributions::indexDistribution( "athens",
+		$ENV{HOME} . "/athenstwiki.mrjc.com/",
+		$Common::excludeFilePattern );
+	IndexDistributions::indexDistribution( "beijing",
+		$ENV{HOME} . "/beijingtwiki.mrjc.com/",
+		$Common::excludeFilePattern );
+	IndexDistributions::indexDistribution( "cairo",
+		$ENV{HOME} . "/cairotwiki.mrjc.com/",
+		$Common::excludeFilePattern );
 }
 
 sub ensureInstallationDir {
-    use TRTConfig;
-    if ($Common::installationDir eq "") {
-	die "You must edit TRTConfig to tell it where you've installed TWiki";
-    }
+	use TRTConfig;
+	if ( $Common::installationDir eq "" ) {
+		die "You must edit TRTConfig to tell it where you've installed TWiki";
+	}
 }
