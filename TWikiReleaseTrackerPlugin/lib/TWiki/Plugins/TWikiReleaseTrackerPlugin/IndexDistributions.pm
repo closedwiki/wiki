ffd29bd97ee9c20c7c015d96125ccab67d4f5c59
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-19 21:23:32 +0000

    *** empty log message ***
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5078 a00a5322-12db-0310-a70b-8735589c885e

--- TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/IndexDistributions.pm
+++ TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/IndexDistributions.pm
@@ -9,65 +9,64 @@ use Common;
 package IndexDistributions;
 
 sub indexDistribution {
-    use Cwd;
-    my $runDir = cwd();
+ use Cwd;
+ my $runDir = cwd();
 
-	my ( $distribution, $distributionLocation, $excludeFilePattern,
-		$pathPrefix ) = @_;
-	use File::Find;
-	unless ( defined $pathPrefix ) { $pathPrefix = "" }
+ my ( $distribution, $distributionLocation, $excludeFilePattern, $pathPrefix ) =
+   @_;
+ use File::Find;
+ unless ( defined $pathPrefix ) { $pathPrefix = "" }
 
-	my $preprocessCallback = sub {
-		my @ans = grep { !/$excludeFilePattern/ } @_;
-		return @ans;
-	};
+ my $preprocessCallback = sub {
+  my @ans = grep { !/$excludeFilePattern/ } @_;
+  return @ans;
+ };
 
-	my $findCallback = sub {
-		my $pathname = $File::Find::name;    #  complete pathname to the file.
-		Common::debug "$pathname\n";
-		my $relativePath =
-		  Common::relativeFromPathname( $pathname, $distributionLocation );
-		return unless includeInResults($relativePath);
-		return unless -f $pathname;
-		return if -z $pathname;
-		Common::debug "$pathname\n";
-		indexFile( $distribution, $distributionLocation, $pathname, $pathPrefix,
-			$relativePath );
-	};
-	find(
-		{
-			wanted     => $findCallback,
-			preprocess => $preprocessCallback,
-			follow     => 0
-		},
-		$distributionLocation
-	);
-    chdir($runDir);
+ my $findCallback = sub {
+  my $pathname = $File::Find::name;    #  complete pathname to the file.
+  Common::debug "$pathname\n";
+  my $relativePath =
+    Common::relativeFromPathname( $pathname, $distributionLocation );
+  return unless includeInResults($relativePath);
+  return unless -f $pathname;
+  return if -z $pathname;
+  Common::debug "$pathname\n";
+  indexFile( $distribution, $distributionLocation, $pathname, $pathPrefix,
+   $relativePath );
+ };
+ find(
+  {
+   wanted     => $findCallback,
+   preprocess => $preprocessCallback,
+   follow     => 0
+  },
+  $distributionLocation
+ );
+ chdir($runDir);
 }
 
 sub indexFile {
-	my ( $distribution, $distributionLocation, $file, $pathPrefix,
-		$relativePath )
-	  = @_;
-	my $digest = digestForFile($file);
-	Common::debug $relativePath. " = " . $digest . "\n";
-	FileDigest::addOccurance( $distribution, $pathPrefix . $relativePath,
-		$digest );
+ my ( $distribution, $distributionLocation, $file, $pathPrefix, $relativePath )
+   = @_;
+ my $digest = digestForFile($file);
+ Common::debug $relativePath. " = " . $digest . "\n";
+ FileDigest::addOccurance( $distribution, $pathPrefix . $relativePath,
+  $digest );
 }
 
 sub digestForFile {
-	my ($file) = @_;
-	my $fh = new FileHandle $file, "r";
-	unless ( defined $fh ) {
-		return "$!";
-	}
-	unless ( -s $fh ) {
-		return "EMPTY";
-	}
-	use Digest::MD5;
-	my $ctx = Digest::MD5->new;
-	$ctx->addfile($fh);
-	return $ctx->hexdigest();
+ my ($file) = @_;
+ my $fh = new FileHandle $file, "r";
+ unless ( defined $fh ) {
+  return "$!";
+ }
+ unless ( -s $fh ) {
+  return "EMPTY";
+ }
+ use Digest::MD5;
+ my $ctx = Digest::MD5->new;
+ $ctx->addfile($fh);
+ return $ctx->hexdigest();
 }
 
 #---------------------------------------------------
@@ -76,130 +75,135 @@ sub digestForFile {
 my $runDir;
 
 sub includeInResults {
-	my ($relativePath) = @_;
+ my ($relativePath) = @_;
 
-	#CodeSmell: should be able to do this in preprocessCallback
-	if (   ( $relativePath =~ m!data/(.*)/! )
-		or ( $relativePath =~ m!pub/(.*)/! ) )
-	{
-		my $web = $1;
+ #CodeSmell: should be able to do this in preprocessCallback
+ if (( $relativePath =~ m!data/(.*)/! )
+  or ( $relativePath =~ m!pub/(.*)/! ) )
+ {
+  my $web = $1;
 
-		#	    print "Index web '$web'?" ;
-		if ( $web =~ m/$Common::websToIndex/ ) {
-			return 1;
-			#		print "yes\n";
-		}
-		else {
-			return 0;
-			#		print "no\n";
-		}
-	}
-	return 1;
+  #	    print "Index web '$web'?" ;
+  if ( $web =~ m/$Common::websToIndex/ ) {
+   return 1;
+
+   #		print "yes\n";
+  }
+  else {
+   return 0;
+
+   #		print "no\n";
+  }
+ }
+ return 1;
 }
 
 sub indexLocalInstallation {
-	ensureInstallationDir();
-	FileDigest::emptyIndexes();
-	print "Indexing localInstallation\n";
-	IndexDistributions::indexDistribution( "localInstallation",
-		$Common::installationDir, $Common::excludeFilePattern );
-	saveIndex("localInstallation.md5");
+ my $ans;
+ ensureInstallationDir();
+ FileDigest::emptyIndexes();
+ $ans .=  "Indexing localInstallation\n";
+ IndexDistributions::indexDistribution( "localInstallation",
+  $Common::installationDir, $Common::excludeFilePattern );
+ $ans .= saveIndex("localInstallation.md5");
 
-#	print FileDigest::dataOutline();
+ #	print FileDigest::dataOutline();
 }
 
 sub indexReleases {
-    my $inclusionTest = sub { 
-	my ($download) = @_;
-	return 0 unless ($download =~ m/TWiki[0-9].*/);
-	return 0 if ($download =~ m/beta/);
-	return 1;
-    };
+ my $inclusionTest = sub {
+  my ($download) = @_;
+  return 0 unless ( $download =~ m/TWiki[0-9].*/ );
+  return 0 if ( $download =~ m/beta/ );
+  return 1;
+ };
 
-    indexDistributions($inclusionTest, "releases.md5");
+ indexDistributions( $inclusionTest, "releases.md5" );
 }
 
 sub indexBetaReleases {
-    my $inclusionTest = sub { 
-	my ($download) = @_;
-	return 0 unless ($download =~ m/TWiki[0-9].*/);
-	return 1 if ($download =~ m/beta/);
-	return 0;
-    };
+ my $inclusionTest = sub {
+  my ($download) = @_;
+  return 0 unless ( $download =~ m/TWiki[0-9].*/ );
+  return 1 if ( $download =~ m/beta/ );
+  return 0;
+ };
 
-    indexDistributions($inclusionTest, "betas.md5" );
+ indexDistributions( $inclusionTest, "betas.md5" );
 }
 
 sub indexPlugins {
-    my $inclusionTest = sub {
-        my ($download) = @_;
-        return 1 unless ($download =~ m/TWiki.*/);
-        return 0 if ($download =~ m/TWiki[0-9]/);
-        return 1; # e.g. TWikiCacheAddOn
-    };
+ my $inclusionTest = sub {
+  my ($download) = @_;
+  return 1 unless ( $download =~ m/TWiki.*/ );
+  return 0 if ( $download =~ m/TWiki[0-9]/ );
+  return 1;    # e.g. TWikiCacheAddOn
+ };
 
-    indexDistributions($inclusionTest, "plugins.md5" );
+ indexDistributions( $inclusionTest, "plugins.md5" );
 }
 
 sub getDirsListed {
-	my ($dir) = @_;
+ my ($dir) = @_;
 
-	use DirHandle;
-	my $dh = DirHandle->new($dir) || die "$! - $dir";
-	return sort
+ use DirHandle;
+ my $dh = DirHandle->new($dir) || die "$! - $dir";
+ return sort
 
-	  #	   grep { -d }
-	  grep { !/\./ } $dh->read();
+   #	   grep { -d }
+   grep { !/\./ } $dh->read();
 }
 
 sub installsOfMine {
-	IndexDistributions::indexDistribution( "athens",
-		$ENV{HOME} . "/athenstwiki.mrjc.com/",
-		$Common::excludeFilePattern );
-	IndexDistributions::indexDistribution( "beijing",
-		$ENV{HOME} . "/beijingtwiki.mrjc.com/",
-		$Common::excludeFilePattern );
-	IndexDistributions::indexDistribution( "cairo",
-		$ENV{HOME} . "/cairotwiki.mrjc.com/",
-		$Common::excludeFilePattern );
+ IndexDistributions::indexDistribution( "athens",
+  $ENV{HOME} . "/athenstwiki.mrjc.com/",
+  $Common::excludeFilePattern );
+ IndexDistributions::indexDistribution( "beijing",
+  $ENV{HOME} . "/beijingtwiki.mrjc.com/",
+  $Common::excludeFilePattern );
+ IndexDistributions::indexDistribution( "cairo",
+  $ENV{HOME} . "/cairotwiki.mrjc.com/",
+  $Common::excludeFilePattern );
 }
 
 sub ensureInstallationDir {
-	use TRTConfig;
-	if ( $Common::installationDir eq "" ) {
-		die "You must edit TRTConfig to tell it where you've installed TWiki";
-	}
-	print "$Common::installationDir\n";
+ use TRTConfig;
+ if ( $Common::installationDir eq "" ) {
+  die "You must edit TRTConfig to tell it where you've installed TWiki";
+ }
+# print "$Common::installationDir\n";
 }
 
 sub setRunDir {
-	($runDir) = @_;
+ ($runDir) = @_;
 }
 
 sub indexDistributions {
-    # This depends on a modified version of Crawfords' SharedCode
-	my ($filterInSub, $indexName) = @_;
 
-	FileDigest::emptyIndexes();
-	my $dir = $Common::downloadDir;
+ # This depends on a modified version of Crawfords' SharedCode
+ my ( $filterInSub, $indexName ) = @_;
+ my $ans;
+ FileDigest::emptyIndexes();
+ my $dir = $Common::downloadDir;
 
-	my @downloads = getDirsListed($Common::downloadDir);
+ my @downloads = getDirsListed($Common::downloadDir);
 
-	foreach my $download (@downloads) {
-	    next unless &$filterInSub($download);
-	    print "Indexing $download\n";
-	    IndexDistributions::indexDistribution( $download, $dir . $download,
-			$Common::excludeFilePattern, "twiki" );
-	}
+ foreach my $download (@downloads) {
+  next unless &$filterInSub($download);
+  $ans .= "Indexing $download\n";
+  IndexDistributions::indexDistribution( $download, $dir . $download,
+   $Common::excludeFilePattern, "twiki" );
+ }
 
-	saveIndex($indexName);
+ $ans .= saveIndex($indexName);
 }
 
 sub saveIndex {
-    my ($indexName) = @_;
-    my $saveFile = $Common::md5IndexDir . $indexName ;
-    print "saving to ".File::Spec->rel2abs($saveFile)."\n";
+ my ($indexName) = @_;
+ my $saveFile = $Common::md5IndexDir . $indexName;
+ my $ans = "saving to " . File::Spec->rel2abs($saveFile) . "\n";
 
-    FileDigest::saveIndex($saveFile);
+ FileDigest::saveIndex($saveFile);
+ return $ans;
 }
 1;
