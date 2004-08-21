9e63d0652b31e0e359e3062c29df8d318608f165
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-21 00:25:02 +0000

    Working well. About to make it so it will show you a match in a distro other than the one you are matching against.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5083 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/test/testPluginCompiles.pl
+++ twikiplugins/TWikiReleaseTrackerPlugin/lib/TWiki/Plugins/TWikiReleaseTrackerPlugin/test/testPluginCompiles.pl
@@ -8,7 +8,7 @@ BEGIN {
     chdir "../../../" || die "You must be above the test dir and have a normal twiki hierarchy - $!";
 }
 
-use lib "/home/mrjc/latestbetatwiki.mrjc.com/twiki/lib"; #FIXME - use Crawfords' TWIKI_LIBS convention
+#use lib "/home/mrjc/latestbetatwiki.mrjc.com/twiki/lib"; #FIXME - use Crawfords' TWIKI_LIBS convention
 #use lib "../lib";
 
 use TWiki;
@@ -16,7 +16,7 @@ use TWiki::Plugins;
 use TWiki::Func;
 
 unless (eval "use TWiki::Plugins::TWikiReleaseTrackerPlugin") {
-    print "$@\n";
+    print $@;
 }
  
 
