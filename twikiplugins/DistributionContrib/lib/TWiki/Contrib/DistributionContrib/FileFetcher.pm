786448d365028deccf90065dcceeadb4f6d68a92
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-22 20:26:14 +0000

    updated for file pulling
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5105 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/DistributionContrib/lib/TWiki/Contrib/DistributionContrib/FileFetcher.pm
+++ twikiplugins/DistributionContrib/lib/TWiki/Contrib/DistributionContrib/FileFetcher.pm
@@ -2,13 +2,33 @@
 use strict;
 use diagnostics;
 
-package TWiki::Contrib::FileFetcher;
-use Config;
-use LWP;
+package TWiki::Contrib::DistributionContrib::FileFetcher;
+use TWiki::Contrib::DistributionContrib::Config;
+use LWP::Simple;
 
-sub getDistributionFile {
+=pod
+Fetches a copy of a given file and distribution. 
+Saves according to TWiki::Contrib::DistributionContrib::Config;
+
+
+In:
+| $file | name of filename, e.g. bin/view |
+| $distribution | name of distribution, e.g. TWiki20020201 |
+
+Out:
+| $location | where the local copy of the file has been placed |
+
+
+=cut
+
+sub fetchDistributionFile {
 	my ( $file, $distribution ) = @_;
 	$file =~ s!^twiki/!!;
-	my $file2 = $serverUrl=. "/" . $distribution . "/" . $file;
-	return $file2;
+	my $fileUrl = $serverUrl=. "/" . $distribution . "/" . $file;
+	$ans = $saveFilename;
+	unless (mirror($fileUrl, $saveFilename)) {
+		TWiki::Func::writeWarning("Couldn't get $distribution:$file\n");
+		$ans = "Couldn't download";
+	}
+	return $ans;
 }
