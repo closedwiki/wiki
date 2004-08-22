bb1381ba4b2ad5b5041bfef33c0dee9e13491766
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-22 20:26:14 +0000

    updated for file pulling
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5105 a00a5322-12db-0310-a70b-8735589c885e

--- DistributionContrib/lib/TWiki/Contrib/DistributionContrib/Config.pm
+++ DistributionContrib/lib/TWiki/Contrib/DistributionContrib/Config.pm
@@ -1,11 +1,12 @@
 #! perl -w
 use strict;
-package TWiki::Contrib::Config;
+package TWiki::Contrib::DistributionContrib::Config;
 
-use vars qw($serverUrl);
-
+use vars qw($serverUrl $saveTopic $saveFilename);
 
 my $serverUrl = "http://twikitreleasetracker.mrjc.com";
+my $saveTopic = "TWiki.DistributionContrib";
+my $saveFilename = "remoteFileSavedLocally";
 
 1;
 
