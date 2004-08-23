e7c89da9985d535a0aaeabe580861fd184ff84c7
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-23 23:12:42 +0000

    The script now also has the capability to download TWikiReleases and store them in the same download directory, this resulting download directory is used by the DistributionContrib so that adminstrators can pick up a single file of a past release. The downloading of releases is commented out by default. [MRJC]
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5119 a00a5322-12db-0310-a70b-8735589c885e

--- BuildContrib/contrib/conformance_analyser.pl
+++ BuildContrib/contrib/conformance_analyser.pl
@@ -1,10 +1,29 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+
+# Copyright (C) 2004 C-Dot Consultants - All rights reserved
+# Portions (C) 2004 Martin Cleaver
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
 
 # Script that downloads zips and CVS for each of the plugins, skins and
 # addons, and then analyses them for "badness" i.e. the amount by which
 # they violate the standards for working in a TWiki context. The downloads
 # are performed to two subdirectories created where the script is run,
 # repository and download.
+#
+# The script also has the capability to download TWikiReleases and store them
+# in the same download directory, this resulting download directory is used by the
+# DistributionContrib so that adminstrators can pick up a single file of a past release.
+# The downloading of releases is commented out by default. [MRJC]
 #
 # Usage: -nodownload will skip the update from the web and re-analyse
 # previously downloaded data. -debug will switch on a verbose debug trace.
@@ -16,6 +35,12 @@ use Time::ParseDate;
 # Constants for URL fragments
 my $TWikiOrg = "http://twiki.org";
 my $View = "/cgi-bin/view";
+
+my $twikiReleaseURL = "$TWikiOrg/release/";
+
+# update this with your credentials.
+my $wgetOptions = "--http-user TWikiGuest --http-passwd guest";
+
 
 # Other constants
 my $red = "#FF9999";
@@ -49,6 +74,18 @@ my $update = 1;
 my $interactivePick = 1;
 my $debug = 0;
 
+# Database of analysis results
+# Top level hash fields and semantics are:
+# howbad - {module|token}
+# cvs - {module}[{file,download,cvs}] - cvs mismatch
+# illmod - {module}{illegal token}{file} - illegal token context
+# illtok - {illegal token}{module} - illegal token context
+# funcsyms - {TWiki::Func symbol} - symbols in Func
+# suspect - {module}{file}{code fragment} - suspect code
+# handlers - {token}{module} handler defined by module
+    my %data;
+
+
 # Analyse options
 foreach my $parm (@ARGV) {
   if ($parm =~ m/^-nod/o) {
@@ -63,201 +100,252 @@ foreach my $parm (@ARGV) {
   }
 }
 
-# Download list of modules from TWiki.org
-my @modules;
-getPackageList("PluginPackage", \@modules);
-getPackageList("SkinPackage", \@modules);
-getPackageList("AddOnPackage", \@modules);
+#downloadReleases();
+analyseConformance();
 
-# Database of analysis results
-# Top level hash fields and semantics are:
-# howbad - {module|token}
-# cvs - {module}[{file,download,cvs}] - cvs mismatch
-# illmod - {module}{illegal token}{file} - illegal token context
-# illtok - {illegal token}{module} - illegal token context
-# funcsyms - {TWiki::Func symbol} - symbols in Func
-# suspect - {module}{file}{code fragment} - suspect code
-# handlers - {token}{module} handler defined by module
-my %data;
-
+sub analyseConformance() {
+    my @modules = downloadModuleList();
+    
 # Download list of functions in Func.pm
-my $text = getTopic("Codev", "FuncDotPm");
-foreach my $line (split(/\n/, $text)) {
-  if ($line =~ /^<h3><a .*?<a .*?">\s*(\w+)/) {
-    $data{funcsyms}{$1} = 1;
-  }
-}
+    my $text = getTopic("Codev", "FuncDotPm");
+    foreach my $line (split(/\n/, $text)) {
+	if ($line =~ /^<h3><a .*?<a .*?">\s*(\w+)/) {
+           $data{funcsyms}{$1} = 1;
+        }
+    }
 
 # repository contains CVS, download contains expanded zips
-mkdir("repository") unless (-d "repository");
-mkdir("download") unless (-d "download");
+    mkdir("repository") unless (-d "repository");
+    mkdir("download") unless (-d "download");
 
 # The workhouse - download and analyse
-my $module;
-foreach $module (sort @modules) {
-  print STDERR "--- Updating $module\n";
-
-  updateCVS($module) if ($update);
-
-  my $zip = "__NO ZIP__";
-  $zip = updateDownload($module);
-
-  print STDERR "--- Analysing $module\n";
-
-  compareCVS2Download($module, $zip, \%data);
-
-  analyseCode($module, \%data);
+    my $module;
+    foreach $module (sort @modules) {
+	print STDERR "--- Updating $module\n";
+	
+	updateCVS($module) if ($update);
+	
+	my $zip = "__NO ZIP__";
+	$zip = updateDownload($module);
+	
+	print STDERR "--- Analysing $module\n";
+	
+	compareCVS2Download($module, $zip, \%data);
+	
+	analyseCode($module, \%data);
+    }
+    generateReport(\%data, @modules);
 }
 
+
+sub downloadReleaseList {
+    unlink "index.html";
+    `wget $wgetOptions $twikiReleaseURL`;
+    my $text = readFile("index.html");
+    my @releasesWanted = ();
+    foreach my $line (split(/\n/,$text)) {
+	my $url;
+	my $release;
+	if ($line =~ m!^<li> <a href="([\w_\.]+)\">(.*?)<\/a>!){
+           ($url, $release) = ($1,$2);
+           if ($release !~ m!beta!) {
+              push @releasesWanted, $release;
+           } elsif ($release =~ m/TWiki200[2|3|4|5|6|7|8|9]/) { #CODE_SMELL lazy
+             push @releasesWanted, $release;
+           }
+        }
+    }
+    print "Releases wanted ".join(", ", @releasesWanted)."\n";
+    return @releasesWanted;
+}
+
+sub downloadReleases {
+    my @releaseZips = downloadReleaseList();
+
+    foreach my $releaseZip (@releaseZips) {
+	my $dir = dirForRelease($releaseZip);
+	mkdir "download/$dir";
+	`wget $wgetOptions -P release/$dir $twikiReleaseURL/$releaseZip`
+    }
+
+
+    foreach my $releaseZip (@releaseZips) {
+	my $dir = dirForRelease($releaseZip);
+	`cd download/$dir && unzip $releaseZip`;
+    }
+}
+
+sub dirForRelease {
+    my ($zip) = @_;
+    my $dir = $zip;
+    $dir =~ s/\.zip$//;
+    return $dir;
+}
+
+sub downloadModuleList {
+# Download list of modules from TWiki.org
+    my @modules;
+    getPackageList("PluginPackage", \@modules);
+    getPackageList("SkinPackage", \@modules);
+    getPackageList("AddOnPackage", \@modules);
+    return @modules;
+}
+    
+
+
+sub generateReport {
+    my ($dataRef, @modules) = @_;
+    %data = %{$dataRef};
 # print results
-print "---+ Report on the current status of packages in the Plugins web\n";
-print RED("This report was script-generated on ".`date`."<p>");
-print "The goal of the analysis is to determine conformance to standards.\n";
-
-my $cvsReport = "";
-foreach $module (sort keys %{$data{cvs}}) {
-  my @records = @{$data{cvs}{$module}};
-  my $lc = scalar(@records);
-  foreach my $record (@records) {
-    my $cvt = $record->{cvs} ? gmtime($record->{cvs}) : "Not found";
-    my $dlt = $record->{download} ? gmtime($record->{download}) : "Not found";
-    my $ctc = $green;
-    my $dtc = $green;
-    if ($record->{cvs} && $record->{download}) {
-      if ($record->{cvs} > $record->{download}) {
-	$ctc = $green;
-	$dtc = $red;
-      } else {
-	$ctc = $red;
-	$dtc = $green;
-      }
-    } elsif ($record->{cvs}) {
-      $ctc = $green;
-      $dtc = $red;
-    } elsif ($record->{download}) {
-      $ctc = $red;
-      $dtc = $green;
+    print "---+ Report on the current status of packages in the Plugins web\n";
+    print RED("This report was script-generated on ".`date`."<p>");
+    print "The goal of the analysis is to determine conformance to standards.\n";
+    
+    my $cvsReport = "";
+    foreach my $module (sort keys %{$data{cvs}}) {
+	my @records = @{$data{cvs}{$module}};
+	my $lc = scalar(@records);
+	foreach my $record (@records) {
+	    my $cvt = $record->{cvs} ? gmtime($record->{cvs}) : "Not found";
+	    my $dlt = $record->{download} ? gmtime($record->{download}) : "Not found";
+	    my $ctc = $green;
+	    my $dtc = $green;
+	    if ($record->{cvs} && $record->{download}) {
+		if ($record->{cvs} > $record->{download}) {
+		    $ctc = $green;
+		    $dtc = $red;
+		} else {
+		    $ctc = $red;
+		    $dtc = $green;
+		}
+	    } elsif ($record->{cvs}) {
+		$ctc = $green;
+		$dtc = $red;
+	    } elsif ($record->{download}) {
+		$ctc = $red;
+		$dtc = $green;
+	    }
+	    $cvsReport .= TR(TDS($lc, $module),
+			     TD("<nop>".$record->{file}),
+			     TD_SHADE($dtc,$dlt),
+			     TD_SHADE($ctc,$cvt));
+	    $lc = 0;
+	}
     }
-    $cvsReport .= TR(TDS($lc, $module),
-		     TD("<nop>".$record->{file}),
-		     TD_SHADE($dtc,$dlt),
-		     TD_SHADE($ctc,$cvt));
-    $lc = 0;
-  }
-}
-
-if ($cvsReport ne "") {
-  print "---++ Modules with questionable CVS status\n";
-  print TABLE(THR("Module","File","Zip Time","CVS Time"), $cvsReport);
-}
-
-my $funcUsageReport = "";
-foreach my $key (sort {$data{funcsyms}{$a} <=> $data{funcsyms}{$b}} keys %{$data{funcsyms}}) {
-  $funcUsageReport .= TR(TD($key),TD($data{funcsyms}{$key} - 1));
-}
-
-if ($funcUsageReport ne "") {
-  print "---++ Usage of functions in Func\n";
-  print TABLE(THR("Function","Calls"), $funcUsageReport);
-}
-
-my $handlerReport = "";
-foreach my $h (@handlers) {
-  if ($data{handlers}{$h}) {
-	$handlerReport .= TR(TD($h),TD(join(", ", @{$data{handlers}{$h}})));
-  }
-}
-
-if ($handlerReport ne "") {
-  print "---++ Handlers defined by modules\n";
-  print TABLE(THR("Handler","Modules"), $handlerReport);
-}
-
-my $illegalCallsReport = "";
-my @badtoks = keys %{$data{illtok}};
-@badtoks = sort {$data{howbad}{$b} <=> $data{howbad}{$a}} @badtoks;
-foreach my $token (@badtoks) {
-  my @badmods = sort keys %{$data{illtok}{$token}};
-  $illegalCallsReport .= TR(TD("<nop>$token"),TD($data{howbad}{$token}),
-		    TD(join(" ", @badmods)));
-}
-
-if ($illegalCallsReport ne "") {
-  print "---++ Calls to TWiki symbols not published through TWiki::Func\n";
-  print TABLE(THR("Symbol","Calls","Callers"), $illegalCallsReport);
-}
-
+    
+    if ($cvsReport ne "") {
+	print "---++ Modules with questionable CVS status\n";
+	print TABLE(THR("Module","File","Zip Time","CVS Time"), $cvsReport);
+    }
+    
+    my $funcUsageReport = "";
+    foreach my $key (sort {$data{funcsyms}{$a} <=> $data{funcsyms}{$b}} keys %{$data{funcsyms}}) {
+	$funcUsageReport .= TR(TD($key),TD($data{funcsyms}{$key} - 1));
+    }
+    
+    if ($funcUsageReport ne "") {
+	print "---++ Usage of functions in Func\n";
+	print TABLE(THR("Function","Calls"), $funcUsageReport);
+    }
+    
+    my $handlerReport = "";
+    foreach my $h (@handlers) {
+	if ($data{handlers}{$h}) {
+	    $handlerReport .= TR(TD($h),TD(join(", ", @{$data{handlers}{$h}})));
+	}
+    }
+    
+    if ($handlerReport ne "") {
+	print "---++ Handlers defined by modules\n";
+	print TABLE(THR("Handler","Modules"), $handlerReport);
+    }
+    
+    my $illegalCallsReport = "";
+    my @badtoks = keys %{$data{illtok}};
+    @badtoks = sort {$data{howbad}{$b} <=> $data{howbad}{$a}} @badtoks;
+    foreach my $token (@badtoks) {
+	my @badmods = sort keys %{$data{illtok}{$token}};
+	$illegalCallsReport .= TR(TD("<nop>$token"),TD($data{howbad}{$token}),
+				  TD(join(" ", @badmods)));
+    }
+    
+    if ($illegalCallsReport ne "") {
+	print "---++ Calls to TWiki symbols not published through TWiki::Func\n";
+	print TABLE(THR("Symbol","Calls","Callers"), $illegalCallsReport);
+    }
+    
 # Table of each module, each token it calls, and what files call them
-my $badModsReport = "";
-my @badmods = sort keys %{$data{illmod}};
-foreach $module (@badmods) {
-  my @badtoks = keys %{$data{illmod}{$module}};
-  @badtoks = sort {$data{howbad}{$b} <=> $data{howbad}{$a}} @badtoks;
-  my $tokc = scalar(@badtoks);
-  foreach my $token (@badtoks) {
-    my @files = sort keys %{$data{illmod}{$module}{$token}};
-    my $desc = "";
-    foreach my $file (@files) {
-      $desc .= "<nop>$file (".$data{illmod}{$module}{$token}{$file}.")<br>";
+    my $badModsReport = "";
+    my @badmods = sort keys %{$data{illmod}};
+    foreach my $module (@badmods) {
+	my @badtoks = keys %{$data{illmod}{$module}};
+	@badtoks = sort {$data{howbad}{$b} <=> $data{howbad}{$a}} @badtoks;
+	my $tokc = scalar(@badtoks);
+	foreach my $token (@badtoks) {
+	    my @files = sort keys %{$data{illmod}{$module}{$token}};
+	    my $desc = "";
+	    foreach my $file (@files) {
+		$desc .= "<nop>$file (".$data{illmod}{$module}{$token}{$file}.")<br>";
+	    }
+	    $badModsReport .= TR(TDS($tokc,$module), TD("<nop>$token"), TD($desc));
+	    $tokc = 0;
+	}
     }
-    $badModsReport .= TR(TDS($tokc,$module), TD("<nop>$token"), TD($desc));
-    $tokc = 0;
-  }
-}
-
-if ($badModsReport ne "" ) {
-  print "---++ Analysis of illegal calls made by modules\n";
-  print TABLE(THR("Module","Symbol","File (calls)"), $badModsReport);
-}
-
+    
+    if ($badModsReport ne "" ) {
+	print "---++ Analysis of illegal calls made by modules\n";
+	print TABLE(THR("Module","Symbol","File (calls)"), $badModsReport);
+    }
+    
 # Table of each module and each file with questionable code
-my $questionableCodeReport = "";
-foreach $module (sort keys %{$data{suspect}}) {
-  my $filc = scalar(keys %{$data{suspect}{$module}});
-  foreach my $file (keys %{$data{suspect}{$module}}) {
-    if (defined($data{suspect}{$module}{$file})) {
-      $questionableCodeReport .= TR(TDS($filc,$module),
-			 TD("$file"),
-			 TD("\n<pre>\n".
-			    $data{suspect}{$module}{$file}.
-			    "</pre>\n"));
-      $filc = 0;
+    my $questionableCodeReport = "";
+    foreach my $module (sort keys %{$data{suspect}}) {
+	my $filc = scalar(keys %{$data{suspect}{$module}});
+	foreach my $file (keys %{$data{suspect}{$module}}) {
+	    if (defined($data{suspect}{$module}{$file})) {
+		$questionableCodeReport .= TR(TDS($filc,$module),
+					      TD("$file"),
+					      TD("\n<pre>\n".
+						 $data{suspect}{$module}{$file}.
+						 "</pre>\n"));
+		$filc = 0;
+	    }
+	}
     }
-  }
-}
-
-if ($questionableCodeReport ne "" ) {
-  print "---++ Other questionable code in modules\n";
-  print "\nQuestionable code is code that appears to read or write topics ";
-  print "or webs directly.\n\n";
-  print TABLE(THR("Module","File","Code Fragment"), $questionableCodeReport);
-}
-
-my $conformanceReport = "";
-my @sm = sort {$data{howbad}{$a} <=> $data{howbad}{$b}} @modules;
-my $n = $data{howbad}{@sm[scalar(@sm)-1]};
-my $i = 0;
-foreach $module (@sm) {
-  my $howbad = $data{howbad}{$module} || 0;
-  $conformanceReport .= TR_SHADE($howbad,$n,TD($module),TD($howbad));
-}
-
-if ($conformanceReport ne "") {
-  print "---++ Estimated module conformance\n";
-  print "Conformance is degree to which module conforms with published ";
-  print "interfaces. Low number *good*, high number *bad*\n";
-  print TABLE(THR("Module","Conformance rating"), $conformanceReport);
-}
-
-my $directivesReport = "";
-foreach my $find (sort keys(%{$data{directives}})) {
-  $directivesReport .=
-    TR(TD($find),TD(join(", ",sort(keys %{$data{directives}{$find}}))));
-}
-
-if ($directivesReport ne "") {
-  print "---++ Directives apparently expanded by modules\n";
-  print TABLE(THR("Directive", "Module(s)"), $directivesReport);
+    
+    if ($questionableCodeReport ne "" ) {
+	print "---++ Other questionable code in modules\n";
+	print "\nQuestionable code is code that appears to read or write topics ";
+	print "or webs directly.\n\n";
+	print TABLE(THR("Module","File","Code Fragment"), $questionableCodeReport);
+    }
+    
+    my $conformanceReport = "";
+    my @sm = sort {$data{howbad}{$a} <=> $data{howbad}{$b}} @modules;
+    my $n = $data{howbad}{@sm[scalar(@sm)-1]};
+    my $i = 0;
+    foreach my $module (@sm) {
+	my $howbad = $data{howbad}{$module} || 0;
+	$conformanceReport .= TR_SHADE($howbad,$n,TD($module),TD($howbad));
+    }
+    
+    if ($conformanceReport ne "") {
+	print "---++ Estimated module conformance\n";
+	print "Conformance is degree to which module conforms with published ";
+	print "interfaces. Low number *good*, high number *bad*\n";
+	print TABLE(THR("Module","Conformance rating"), $conformanceReport);
+    }
+    
+    my $directivesReport = "";
+    foreach my $find (sort keys(%{$data{directives}})) {
+	$directivesReport .=
+	    TR(TD($find),TD(join(", ",sort(keys %{$data{directives}{$find}}))));
+    }
+    
+    if ($directivesReport ne "") {
+	print "---++ Directives apparently expanded by modules\n";
+	print TABLE(THR("Directive", "Module(s)"), $directivesReport);
+    }
 }
 
 # Find occurences of TWiki functions not from TWiki::Func in the module.
@@ -314,7 +402,7 @@ sub analyseCode {
 
 
       # search for probable directives %DIRECTIVE
-      my @finds = split(/\n/, `egrep '^.*s/%[^\/]*%' $r`);
+      @finds = split(/\n/, `egrep '^.*s/%[^\/]*%' $r`);
       foreach $find (@finds) {
 		if ($find !~ /^\s*\#/o) {
 		  if ($find =~ s/^.*s\/%(\w+)%.*$/$1/o) {
@@ -478,13 +566,13 @@ sub updateCVS {
 
   if (-d "repository/twikiplugins/$module") {
     print STDERR "--- Updating CVS $module\n";
-    `cd repository/twikiplugins/$module && cvs -d:pserver:anonymous\@cvs.TWiki.sourceforge.net:/cvsroot/twikiplugins update $quiet`;
+    `cd repository/twikiplugins/$module && cvs -d:pserver:anonymous\@cvs.sourceforge.net:/cvsroot/twikiplugins update $quiet`;
     if ($?) {
       print STDERR "CVS update on $module failed: $?\n";
     }
   } else {
     print "--- Checking out CVS $module\n";
-    `cd repository && cvs -d:pserver:anonymous\@cvs.TWiki.sourceforge.net:/cvsroot/twikiplugins checkout twikiplugins/$module $quiet`;
+    `cd repository && cvs -d:pserver:anonymous\@cvs.sourceforge.net:/cvsroot/twikiplugins checkout twikiplugins/$module $quiet`;
   }
 }
 
