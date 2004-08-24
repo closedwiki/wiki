270f46ba35a42c8dd1ab2e2f6de71131c7e938bf
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-24 18:05:16 +0000

    *** empty log message ***
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5131 a00a5322-12db-0310-a70b-8735589c885e

--- DistributionContrib/lib/TWiki/Contrib/DistributionContrib/FileFetcher.pm
+++ DistributionContrib/lib/TWiki/Contrib/DistributionContrib/FileFetcher.pm
@@ -17,20 +17,27 @@ Saves according to TWiki::Contrib::DistributionContrib::Config;
 In:
 | $file | name of filename, e.g. bin/view |
 | $distribution | name of distribution, e.g. TWiki20020201 |
+| $localFileSuffix | for the downloaded file, any suffix to be added to the filename |
 
 Out:
 | $location | where the local copy of the file has been placed |
 
+NB. the localFileSuffix is a way to allow multiple downloaded files to coexist at the same time.
+As the FileFetcher does not provide a way to delete the files (and TWiki does not automatically
+refresh its attachment table on the arrival of new attachments), I recommend you use unique names
+scarcely. e.g. keep using the same localFileSuffix "1", "2"
 
 =cut
 
 sub fetchDistributionFile {
-	my ( $file, $distribution ) = @_;
+	my ( $file, $distribution, $localFileSuffix ) = @_;
+	$localFileSuffix = "" unless ($localFileSuffix);
+	
 	$file =~ s!^twiki/!!;
 	my $fileUrl = $config{'serverUrl'}. "/" . $distribution . "/" . $file;
 	my $webTopicBodge = $config{'saveTopic'};
 	$webTopicBodge =~ s!\.!/!;
-	my $attachmentPath = TWiki::Func::getPubDir()."/".$webTopicBodge."/".$config{'saveTopicAttachmentName'};
+	my $attachmentPath = TWiki::Func::getPubDir()."/".$webTopicBodge."/".$config{'saveTopicAttachmentName'}.$localFileSuffix;
 	my $ans = $attachmentPath;
 	my $status = mirror($fileUrl, $attachmentPath);
 	if (is_error($status)) {
