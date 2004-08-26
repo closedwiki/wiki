ee6ef528ffa2b839bfaded1236dc140ebd2fc3c8
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-26 01:01:02 +0000

    *** empty log message ***
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5147 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/BuildContrib/lib/TWiki/Contrib/BuildContrib/TWikiCLI.pm
+++ twikiplugins/BuildContrib/lib/TWiki/Contrib/BuildContrib/TWikiCLI.pm
@@ -2,12 +2,14 @@ package TWiki::Contrib::BuildContrib::TWikiCLI;
 
 use Getopt::Long;
 
-my $verboseFlag = 0;
-GetOptions("verbose!"=>\$verboseFlag);
+sub dispatch {
+ my $verboseFlag = 0;
+ GetOptions( "verbose!" => \$verboseFlag );
 
-print "Unprocessed by Getopt::Long\n" if $ARGV[0];
-foreach (@ARGV) {
+ print "Unprocessed by Getopt::Long\n" if $ARGV[0];
+ foreach (@ARGV) {
   print "$_\n";
+ }
 }
 
 1;
