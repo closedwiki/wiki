80a520b5c1757019485df26c05f3d8bd11e5941c
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-26 16:09:47 +0000

    First cut of an install of plugins/addons/contribs
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5154 a00a5322-12db-0310-a70b-8735589c885e

--- BuildContrib/lib/TWiki/Contrib/BuildContrib/TWikiCLI/Extension.pm
+++ BuildContrib/lib/TWiki/Contrib/BuildContrib/TWikiCLI/Extension.pm
@@ -1,10 +1,73 @@
 package TWiki::Contrib::BuildContrib::TWikiCLI::Extension;
+use strict;
+use Cwd;
+
+# Ultimately we'd like to ask the Extension to handle this command 
+# itself. For now we just call build.pl
 
 sub install {
-    my (@args) = @_;
-    return "Yup @args\n";
+    my ($extension) = @_;
 
+    print "Installing $extension\n\n";
+    my $dir = getDirForExtension($extension);
+
+    my $buildDotPlDir = findBuildDotPlDir($dir);
+    my $oldPwd = cwd();
+    if ($buildDotPlDir) {
+	chdir ($buildDotPlDir) || die "Couldn't cd to $buildDotPlDir - $!";
+	print "From $buildDotPlDir...\n";
+	foreach my $home (getHomes()) {
+	    if ($home !~ m!^/!) {
+		$home = $ENV{"HOME"}."/".$home;
+	    }
+	    print "\tInstalling to $home\n";
+	    $ENV{"TWIKI_HOME"} = $home;
+
+	    my $ans = `perl build.pl install`;
+	}
+    } else {
+	print "Failed to find build.pl for $extension\n";
+    }
+    chdir ($oldPwd); # Not strictly necessary, but...
 }
 
+sub getHomes {
+    die "Set TWIKI_HOMES to : separated locations to install to" unless $ENV{"TWIKI_HOMES"};
+    return split / /, $ENV{"TWIKI_HOMES"};
+}
+
+
+sub findBuildDotPlDir {
+    my ($dir) = @_;
+    unless ($ENV{"TWIKI_LIBS"}) {
+	die "No TWIKI_LIBS set?";
+    }
+    my $result;
+    foreach my $libdir (split(/:/, $ENV{TWIKI_LIBS})) {
+	my $buildDotPlDir = "$libdir/TWiki/$dir";
+	if (-f $buildDotPlDir."/build.pl") {
+#	    print "Found it\n";
+	    return $buildDotPlDir;
+	} else {
+#	    print "Not $buildDotPlDir\n";
+	}
+    }
+    print "Didn't find it";
+    return "";
+}
+
+sub getDirForExtension {
+    my ($extension) = @_;
+    my $dir;
+    if ($extension =~ "Plugin") {
+	$dir = "Plugin/$extension";
+    } elsif ($extension =~ "AddOn") {
+	$dir = "Plugin/$extension";
+    } elsif ($extension =~ "Contrib") {
+	$dir = "Contrib/$extension";
+    }
+    return $dir;
+
+}
 
 1;
