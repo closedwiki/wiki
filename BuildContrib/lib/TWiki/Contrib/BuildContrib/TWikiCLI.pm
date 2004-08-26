1db258a1d4da8e7ff76abe4361052b54e0346ec0
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-26 01:04:59 +0000

    *** empty log message ***
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5148 a00a5322-12db-0310-a70b-8735589c885e

--- BuildContrib/lib/TWiki/Contrib/BuildContrib/TWikiCLI.pm
+++ BuildContrib/lib/TWiki/Contrib/BuildContrib/TWikiCLI.pm
@@ -6,10 +6,23 @@ sub dispatch {
  my $verboseFlag = 0;
  GetOptions( "verbose!" => \$verboseFlag );
 
+ my $subcommand = $ARGV[0];
+
+ if (defined &$subcommand) {
+  return &$subcommand;
+ } else {
+  return helpText();
+ }
+
  print "Unprocessed by Getopt::Long\n" if $ARGV[0];
  foreach (@ARGV) {
   print "$_\n";
  }
+}
+
+sub extension {
+ print "love ya sweety\n";
+
 }
 
 1;
