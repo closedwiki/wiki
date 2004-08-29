dcfa9d3b10a9cc9f78b6d1b5de9cb1d3987e6fb7
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-29 20:59:24 +0000

    *** empty log message ***
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5216 a00a5322-12db-0310-a70b-8735589c885e

--- AdvertsPlugin/lib/TWiki/Plugins/AdvertsPlugin.pm
+++ AdvertsPlugin/lib/TWiki/Plugins/AdvertsPlugin.pm
@@ -1,5 +1,6 @@
 # Plugin for TWiki Collaboration Platform, http://TWiki.org/
 #
+# Copyright (C) 2004 Martin Cleaver, Martin.Cleaver@BCS.org.uk
 # Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
 # Copyright (C) 2001-2004 Peter Thoeny, peter@thoeny.com
 #
@@ -11,293 +12,120 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
 #
-# This is an empty TWiki plugin. Use it as a template
-# for your own plugins; see TWiki.TWikiPlugins for details.
-#
-# Each plugin is a package that may contain these functions:        VERSION:
-#
-#   earlyInitPlugin         ( )                                     1.020
-#   initPlugin              ( $topic, $web, $user, $installWeb )    1.000
-#   initializeUserHandler   ( $loginName, $url, $pathInfo )         1.010
-#   registrationHandler     ( $web, $wikiName, $loginName )         1.010
-#   commonTagsHandler       ( $text, $topic, $web )                 1.000
-#   startRenderingHandler   ( $text, $web )                         1.000
-#   outsidePREHandler       ( $text )                               1.000
-#   insidePREHandler        ( $text )                               1.000
-#   endRenderingHandler     ( $text )                               1.000
-#   beforeEditHandler       ( $text, $topic, $web )                 1.010
-#   afterEditHandler        ( $text, $topic, $web )                 1.010
-#   beforeSaveHandler       ( $text, $topic, $web )                 1.010
-#   afterSaveHandler        ( $text, $topic, $web, $errors )        1.020
-#   writeHeaderHandler      ( $query )                              1.010  Use only in one Plugin
-#   redirectCgiQueryHandler ( $query, $url )                        1.010  Use only in one Plugin
-#   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
-#   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
-#
-# initPlugin is required, all other are optional. 
-# For increased performance, all handlers except initPlugin are
-# disabled. To enable a handler remove the leading DISABLE_ from
-# the function name. Remove disabled handlers you do not need.
-#
-# NOTE: To interact with TWiki use the official TWiki functions 
-# in the TWiki::Func module. Do not reference any functions or
-# variables elsewhere in TWiki!!
-
+# This is the Adverts TWiki plugin.
+# It interfaces to phpAdsNew (http://p
 
 # =========================
-package TWiki::Plugins::EmptyPlugin;    # change the package name and $pluginName!!!
+package TWiki::Plugins::AdvertsPlugin;
+
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $pluginName
-        $debug $exampleCfgVar
-    );
+  $web $topic $user $installWeb $VERSION $pluginName
+  $debug $phpAdsNewBase;
+);
 
-$VERSION = '1.021';
-$pluginName = 'EmptyPlugin';  # Name of this Plugin
+$VERSION    = '0.1';
+$pluginName = 'AdvertsPlugin';    # Name of this Plugin
 
 # =========================
-sub initPlugin
-{
-    ( $topic, $web, $user, $installWeb ) = @_;
+sub initPlugin {
+ ( $topic, $web, $user, $installWeb ) = @_;
 
-    # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.021 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
-        return 0;
-    }
+ # check for Plugins.pm versions
+ if ( $TWiki::Plugins::VERSION < 1.021 ) {
+  TWiki::Func::writeWarning(
+   "Version mismatch between $pluginName and Plugins.pm");
+  return 0;
+ }
 
-    # Get plugin debug flag
-    $debug = TWiki::Func::getPluginPreferencesFlag( "DEBUG" );
+ # Get plugin debug flag
+ $debug = TWiki::Func::getPluginPreferencesFlag("DEBUG");
 
-    # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
-    $exampleCfgVar = TWiki::Func::getPluginPreferencesValue( "EXAMPLE" ) || "default";
+ # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
+ $phpAdsNewBase = TWiki::Func::getPluginPreferencesValue("ADVERTSCRIPTBASE")
+   || "http://testwiki.mrjc.com/phpAdsNew-2.0/";
 
-    # Plugin correctly initialized
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
-    return 1;
+ # Plugin correctly initialized
+ TWiki::Func::writeDebug(
+  "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
+   if $debug;
+ return 1;
 }
 
 # =========================
-sub DISABLE_earlyInitPlugin
-{
-### Remove DISABLE_ for a plugin that requires early initialization, that is expects to have
-### initializeUserHandler called before initPlugin, giving the plugin a chance to set the user
-### See SessionPlugin for an example of this.
-    return 1;
-}
-
-
-# =========================
-sub DISABLE_initializeUserHandler
-{
-### my ( $loginName, $url, $pathInfo ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::initializeUserHandler( $_[0], $_[1] )" ) if $debug;
-
-    # Allows a plugin to set the username based on cookies. Called by TWiki::initialize.
-    # Return the user name, or "guest" if not logged in.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
-
-}
-
-# =========================
-sub DISABLE_registrationHandler
-{
-### my ( $web, $wikiName, $loginName ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::registrationHandler( $_[0], $_[1] )" ) if $debug;
-
-    # Allows a plugin to set a cookie at time of user registration.
-    # Called by the register script.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
-
-}
-
-# =========================
-sub DISABLE_commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+ TWiki::Func::writeDebug("- ${pluginName}::commonTagsHandler( $_[2].$_[1] )")
+   if $debug;
 
-    # This is the place to define customized tags and variables
-    # Called by sub handleCommonTags, after %INCLUDE:"..."%
+ # This is the place to define customized tags and variables
+ # Called by sub handleCommonTags, after %INCLUDE:"..."%
 
-    # do custom extension rule, like for example:
-    # $_[0] =~ s/%XYZ%/&handleXyz()/ge;
-    # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/ge;
+ # do custom extension rule, like for example:
+ $_[0] =~ s/%ADVERT%/&handleAdvert()/ge;
+
+ # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/ge;
 }
 
 # =========================
-sub DISABLE_startRenderingHandler
-{
-### my ( $text, $web ) = @_;   # do not uncomment, use $_[0], $_[1] instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::startRenderingHandler( $_[1] )" ) if $debug;
+=pod
+<script language='JavaScript' type='text/javascript'>
+<!--
+   if (!document.phpAds_used) document.phpAds_used = ',';
+   phpAds_random = new String (Math.random()); phpAds_random = phpAds_random.substring(2,11);
+   
+   document.write ("<" + "script language='JavaScript' type='text/javascript' src='");
+   document.write ("http://testwiki.mrjc.com/phpAdsNew-2.0/adjs.php?n=" + phpAds_random);
+   document.write ("&amp;what=Foobar&amp;clientid=2");
+   document.write ("&amp;exclude=" + document.phpAds_used);
+   if (document.referer)
+      document.write ("&amp;referer=" + escape(document.referer));
+   document.write ("'><" + "/script>");
+//-->
+</script><noscript><a href='http://testwiki.mrjc.com/phpAdsNew-2.0/adclick.php?n=ad13c123' target='_blank'><img src='http://testwiki.mrjc.com/phpAdsNew-2.0/adview.php?what=Foobar&amp;clientid=2&amp;n=ad13c123' border='0' alt=''></a></noscript>
 
-    # This handler is called by getRenderedVersion just before the line loop
+=cut
 
-    # do custom extension rule, like for example:
-    # $_[0] =~ s/old/new/g;
-}
+sub handleAdvert {
+ my $ans = getTemplateAdvert();
+ $what          = "Foobar&amp;clientid=2";
+ $random        = "ad13c123";
+ $serverUrlBase = "http://testwiki.mrjc.com/phpAdsNew-2.0/";
 
-# =========================
-sub DISABLE_outsidePREHandler
-{
-### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
-
-    ##TWiki::Func::writeDebug( "- ${pluginName}::outsidePREHandler( $renderingWeb.$topic )" ) if $debug;
-
-    # This handler is called by getRenderedVersion, once per line, before any changes,
-    # for lines outside <pre> and <verbatim> tags. 
-    # Use it to define customized rendering rules.
-    # Note: This is an expensive function to comment out.
-    # Consider startRenderingHandler instead
-
-    # do custom extension rule, like for example:
-    # $_[0] =~ s/old/new/g;
-}
-
-# =========================
-sub DISABLE_insidePREHandler
-{
-### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
-
-    ##TWiki::Func::writeDebug( "- ${pluginName}::insidePREHandler( $web.$topic )" ) if $debug;
-
-    # This handler is called by getRenderedVersion, once per line, before any changes,
-    # for lines inside <pre> and <verbatim> tags. 
-    # Use it to define customized rendering rules.
-    # Note: This is an expensive function to comment out.
-    # Consider startRenderingHandler instead
-
-    # do custom extension rule, like for example:
-    # $_[0] =~ s/old/new/g;
-}
-
-# =========================
-sub DISABLE_endRenderingHandler
-{
-### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::endRenderingHandler( $web.$topic )" ) if $debug;
-
-    # This handler is called by getRenderedVersion just after the line loop, that is,
-    # after almost all XHTML rendering of a topic. <nop> tags are removed after this.
+ $ans =~ s/%WHAT%/$what/g;
+ $ans =~ s/%RANDOM%/$random/g;
+ $ans =~ s/%SERVERURLBASE%/$serverUrlBase/g;
 
 }
 
-# =========================
-sub DISABLE_beforeEditHandler
-{
-### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::beforeEditHandler( $_[2].$_[1] )" ) if $debug;
-
-    # This handler is called by the edit script just before presenting the edit text
-    # in the edit box. Use it to process the text before editing.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
-
-}
-
-# =========================
-sub DISABLE_afterEditHandler
-{
-### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::afterEditHandler( $_[2].$_[1] )" ) if $debug;
-
-    # This handler is called by the preview script just before presenting the text.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
+sub getTemplateAdvert {
+ my $ans = <<EOM;
+<script language='JavaScript' type='text/javascript'>
+<!--
+   if (!document.phpAds_used) document.phpAds_used = ',';
+   phpAds_random = new String (Math.random()); phpAds_random = phpAds_random.substring(2,11);
+   
+   document.write ("<" + "script language='JavaScript' type='text/javascript' src='");
+   document.write ("%SERVERURLBASE/adjs.php%n=" + phpAds_random);
+   document.write ("&amp;what=%WHAT%");
+   document.write ("&amp;exclude=" + document.phpAds_used);
+   if (document.referer)
+      document.write ("&amp;referer=" + escape(document.referer));
+   document.write ("'><" + "/script>");
+//-->
+</script><noscript><a href='%SERVERURLBASE%?n=%RANDOM%' target='_blank'><img src='%SERVERURLDIR%/adview.php?what=%WHAT%;%RANDOM%' border='0' alt=''></a></noscript>
+   
+EOM
+ return $ans;
 
 }
-
-# =========================
-sub DISABLE_beforeSaveHandler
-{
-### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::beforeSaveHandler( $_[2].$_[1] )" ) if $debug;
-
-    # This handler is called by TWiki::Store::saveTopic just before the save action.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
-
-}
-
-# =========================
-sub DISABLE_afterSaveHandler
-{
-### my ( $text, $topic, $web, $error ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::afterSaveHandler( $_[2].$_[1] )" ) if $debug;
-
-    # This handler is called by TWiki::Store::saveTopic just after the save action.
-    # New hook in TWiki::Plugins $VERSION = '1.020'
-
-}
-
-# =========================
-sub DISABLE_writeHeaderHandler
-{
-### my ( $query ) = @_;   # do not uncomment, use $_[0] instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::writeHeaderHandler( query )" ) if $debug;
-
-    # This handler is called by TWiki::writeHeader, just prior to writing header. 
-    # Return a single result: A string containing HTTP headers, delimited by CR/LF
-    # and with no blank lines. Plugin generated headers may be modified by core
-    # code before they are output, to fix bugs or manage caching. Plugins should no
-    # longer write headers to standard output.
-    # Use only in one Plugin.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
-
-}
-
-# =========================
-sub DISABLE_redirectCgiQueryHandler
-{
-### my ( $query, $url ) = @_;   # do not uncomment, use $_[0], $_[1] instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::redirectCgiQueryHandler( query, $_[1] )" ) if $debug;
-
-    # This handler is called by TWiki::redirect. Use it to overload TWiki's internal redirect.
-    # Use only in one Plugin.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
-
-}
-
-# =========================
-sub DISABLE_getSessionValueHandler
-{
-### my ( $key ) = @_;   # do not uncomment, use $_[0] instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::getSessionValueHandler( $_[0] )" ) if $debug;
-
-    # This handler is called by TWiki::getSessionValue. Return the value of a key.
-    # Use only in one Plugin.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
-
-}
-
-# =========================
-sub DISABLE_setSessionValueHandler
-{
-### my ( $key, $value ) = @_;   # do not uncomment, use $_[0], $_[1] instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::setSessionValueHandler( $_[0], $_[1] )" ) if $debug;
-
-    # This handler is called by TWiki::setSessionValue. 
-    # Use only in one Plugin.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
-
-}
-
-# =========================
 
 1;
