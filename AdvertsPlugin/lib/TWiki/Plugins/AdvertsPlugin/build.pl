0325ea751839e39be7ef836a821f88d9ee31370f
mrjc <mrjc@a00a5322-12db-0310-a70b-8735589c885e>
2004-08-30 03:12:29 +0000

    Almost there - a few hardcoded things to remove
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5220 a00a5322-12db-0310-a70b-8735589c885e

--- AdvertsPlugin/lib/TWiki/Plugins/AdvertsPlugin/build.pl
+++ AdvertsPlugin/lib/TWiki/Plugins/AdvertsPlugin/build.pl
@@ -33,7 +33,7 @@ use TWiki::Contrib::Build;
 
   sub new {
     my $class = shift;
-    return bless( $class->SUPER::new( "AdvertsPlugin", "Build" ), $class );
+    return bless( $class->SUPER::new( "AdvertsPlugin", "AdvertsPlugin" ), $class );
   }
 
   # Example: Override the build target
