7578a488693bf5c33d886d0fdf02a7668e4d2063
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2004-10-22 00:06:53 +0000

    (WillNorris) relock cvs needs to be added (still commented out) from mac install.pl
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5301 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/post-twiki.sh
+++ twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/post-twiki.sh
@@ -4,3 +4,6 @@ mkdir -p htdocs/twiki
 mv cgi-bin/tmp/twiki/pub/* htdocs/twiki/
 mkdir twiki/templates
 cp cgi-bin/tmp/twiki/templates/* twiki/templates/
+
+# $account = /Users/(twiki)/Sites
+#echo `wget -O - http://localhost/~twiki/twiki/bin/manage?action=relockrcs | grep code | wc -l` topic(s) unlocked
