a23a4f4d5b2739bf17be645bfc9630c842ce2739
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2004-10-28 03:45:16 +0000

    (WillNorris) installs cpan modules specified on command line into a local cpan directory
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5325 a00a5322-12db-0310-a70b-8735589c885e

---
+++ lib/TWiki/Contrib/TWikiInstallerContrib/cpan/install-cpan.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/perl -w
+# $Id$
+# Copyright 2004 Will Norris.  All Rights Reserved.
+# License: GPL
+
+################################################################################
+
+use strict;
+++$|;
+open(STDERR,'>&STDOUT'); # redirect error to browser
+use Data::Dumper qw( Dumper );
+use CPAN;
+
+my $account = "twiki";
+
+# TODO: copied (and cropped) from install_twiki.cgi; share
+my $cgibin      = "/Users/$account/Sites/cgi-bin";
+my $lib		= $cgibin . '/lib';
+my $cpan        = "$lib/CPAN";
+
+################################################################################
+
+installLocalModules({
+    # TODO: update to use same output as =cpan/calc-twiki-deps.pl=
+    modules => [ @ARGV ? @ARGV : qw( 
+				     XML::Parser XML::Simple 
+				     List::Permutor File::Temp 
+				     Carp::Clan Bit::Vector Date::Calc 
+				     Error Class::Inner Devel::Symdump
+				     URI HTML::Parser HTML::Tagset
+				     Digest::MD5 Storable
+				     SOAP::Lite 
+				     Time::ParseDate Date::Handler Date::Parse 
+				     HTML::CalendarMonthSimple Image::LibRSVG
+				     Test::Unit 
+				     LWP::Simple 
+				     CGI::Session 
+				     ) ],
+#    Data::UUID Date::Handler Safe Language::Prolog GD XMLRPC::Transport::HTTP
+    dir => $cpan,
+});
+
+################################################################################
+################################################################################
+
+sub installLocalModules
+{
+    my $parm = shift;
+    my $cpan = $parm->{dir};
+
+    checkdir( $cpan );
+
+    $CPAN::Config->{'make'} = q[/usr/bin/make];
+    # some modules refuse to work if PREFIX is set, and some refuse to work if it is not. ???
+    $CPAN::Config->{'makepl_arg'} = "PREFIX=$cpan";
+    $CPAN::Config->{'make_arg'} = "";
+    $CPAN::Config->{'make_install_arg'} = "";
+    $CPAN::Config->{'makepl_arg'} = "";
+    # TODO: try other setup of config if install fails?  PREFIX seems like the preferred method, so try that first
+#    $CPAN::Config->{'make_arg'} = "-I$cpan/";
+#    $CPAN::Config->{'make_install_arg'} = "-I$cpan/";
+#    $CPAN::Config->{'makepl_arg'} = "LIB=$cpan/lib INSTALLMAN1DIR=$cpan/man/man1 INSTALLMAN3DIR=$cpan/man/man3";
+
+    $CPAN::Config->{'build_dir'} = "$cpan/.cpan/build";
+    $CPAN::Config->{'cpan_home'} = "$cpan/.cpan";
+
+#    'histfile' => q[/Users/wbniv/Sites/twiki/.cpan/histfile],
+    $CPAN::Config->{'histsize'} = 0;
+
+    $CPAN::Config->{'keep_source_where'} = "$cpan/.cpan/sources";
+
+    $CPAN::Config->{'prerequisites_policy'} = 'follow';
+
+#    $CPAN::Config->{'urllist'} = [ "file:/Users/wbniv/twiki/twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/cpan/MIRROR/MINICPAN/" ];
+    $CPAN::Config->{'urllist'} = [ "file:/Users/$account/Sites/cpan/MIRROR/TWIKI/" ];
+    $CPAN::Config->{'build_cache'} = q[0];
+
+    $CPAN::Config->{'ftp'} = q[/usr/bin/ftp];
+    $CPAN::Config->{'gzip'} = q[/sw/bin/gzip];
+    $CPAN::Config->{'pager'} = q[/usr/bin/less];
+    $CPAN::Config->{'shell'} = q[/bin/bash];
+    $CPAN::Config->{'tar'} = q[/sw/bin/tar];
+    $CPAN::Config->{'unzip'} = q[/sw/bin/unzip];
+    $CPAN::Config->{'wget'} = q[/sw/bin/wget];
+
+#?  'cache_metadata' => q[1],
+#?  'dontload_hash' => {  },
+#  'ftp_proxy' => q[],
+#?  'getcwd' => q[cwd],
+#  'gpg' => q[],
+#  'http_proxy' => q[],
+#  'inactivity_timeout' => q[0],
+#?  'index_expire' => q[1],
+#  'inhibit_startup_message' => q[0],
+#  'lynx' => q[],
+#  'ncftp' => q[],
+#  'ncftpget' => q[],
+#  'no_proxy' => q[],
+#?  'scan_cache' => q[atstart],
+#?  'term_is_latin' => q[1],
+
+#    print Data::Dumper::Dumper( $CPAN::Config );
+#    CPAN::Shell->reload( qw( index ) ); # or die $!;
+#    print Data::Dumper::Dumper( $CPAN::Config );
+
+    my @modules = @{$parm->{modules}};
+#    print Dumper( \@modules );
+    foreach my $module ( @modules )
+    {
+	my $obj = CPAN::Shell->expand( Module => $module ) or warn "$module: $!";
+	next unless $obj;
+#	$obj->force( 'install' ); # or warn "Error installing $module\n"; 
+	$obj->install; # or warn "Error installing $module\n"; 
+    }
+    
+#    print Dumper( $CPAN::Config );
+}
+
+################################################################################
+################################################################################
+
+sub mode {
+	my ($file) = @_;
+	my ($dev, $ino, $mode) = stat $file;
+	return $mode;
+}
+
+sub checkdir {
+	my ($dir) = @_;
+	unless (-d $dir) {
+	    print "Directory not found: $dir";
+	    exit 1;
+	}
+	unless (mode($dir) & 0x2) {
+	    print "Directory $dir is not world writable";
+	    exit 1;
+	}
+}
+
+################################################################################
