a902aa8afa737b0079a32b55a24c418ee369f22d
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2004-11-08 03:15:57 +0000

    (WillNorris) fixed various typos plugin -> contrib
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5359 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/bin/download-twiki-contribs.pl
+++ lib/TWiki/Contrib/TWikiInstallerContrib/bin/download-twiki-contribs.pl
@@ -1,10 +1,10 @@
-#! /usr/bin/perl -wT
+#! /usr/bin/perl -w
 ################################################################################
-# download-twiki-plugins.pl
+# download-twiki-contribs.pl
 # Copyright 2004 Will Norris.  All Rights Reserved.
 # License: GPL
 #
-# mirrors plugins locally from their distribution/topic pages
+# mirrors contribs locally from their distribution/topic pages
 # prints out a report (suitable for inclusion as a twiki page)
 #
 ################################################################################
@@ -12,10 +12,17 @@ use strict;
 use diagnostics;
 ++$|;
 
+BEGIN {
+    use Cwd qw( cwd getcwd );
+    use Config;
+    my $localLibBase = getcwd() . "/lib/CPAN/lib/site_perl/" . $Config{version};
+    unshift @INC, ( $localLibBase, "$localLibBase/$Config{archname}" );
+}
+
 ################################################################################
 # config
 my $Config = {
-    local_cache => 'downloads/contrib',
+    local_cache => 'downloads/contribs',
     twiki => {
 	pub => 'http://twiki.org/p/pub',
     },
@@ -29,60 +36,76 @@ use HTML::TokeParser;
 
 mkpath $Config->{local_cache} or die $! unless -d $Config->{local_cache};
 my @errors;
-my ( $nPlugins, $nDownloadedPlugins ) = ( 0, 0 );
-my @plugins = getPluginsCatalogList();
+my ( $nContribs, $nDownloadedContribs ) = ( 0, 0 );
+my @contribs = getContribsCatalogList();
 
-print "\n| *Plugin* | *Download Status* |";
-foreach my $plugin ( @plugins )
+print "\n| *Contrib* | *Download Status* |";
+foreach my $contribS ( @contribs )
 {
-    print "\n| TWiki:Plugins.$plugin ";
-    ++$nPlugins;
+    my $contrib = $contribS->{name} or die "no name?";
 
-    my $remote_uri = "$Config->{twiki}->{pub}/Plugins/$plugin/$plugin.zip";
-    # download plugin
-    my $status = mirror( $remote_uri, "$Config->{local_cache}/$plugin.zip" );
+    print "\n| TWiki:Plugins.$contrib ";
+    ++$nContribs;
+
+    my $remote_uri = "$Config->{twiki}->{pub}/Plugins/$contrib/$contrib.zip";
+    my $local_file = "$Config->{local_cache}/$contrib.zip";
+    # download contrib
+    my $status = mirror( $remote_uri, $local_file );
 
     if ($status == RC_OK) {
-	++$nDownloadedPlugins;
+	++$nDownloadedContribs;
 	print "| downloaded";
+	$contribS->{file} = $local_file;
     } elsif ($status != RC_NOT_MODIFIED) {
 	print "| $!: $remote_uri";
-	push @errors, $plugin;
+	push @errors, $contrib;
     } else {
-	++$nDownloadedPlugins;
+	++$nDownloadedContribs;
 	print "| updated";
+#	$contribS->{file} = $local_file;
     }
     print " |";
 }
 
 # print summary results (suitable for inclusion as a TWiki page)
-print "\n| *Plugins Processed* | $nDownloadedPlugins/$nPlugins |";
+print "\n| *Contribs Processed* | $nDownloadedContribs/$nContribs |";
 print "\n\n";
 local $, = "\n   * TWiki:Plugins.";
-print "Missing/Error plugin topics: ", @errors; 
+print "Missing/Error contrib topics: ", @errors; 
 print "\n";
+
+use XML::Simple;
+my $xs = new XML::Simple() or die $!;
+open( XML, ">$Config->{local_cache}/contribs.xml" ) or die $!;
+print XML $xs->XMLout( { contrib => [ @contribs ] }, NoAttr => 1 );
+close( XML ) or warn $!;
 
 ################################################################################
 
-sub getPluginsCatalogList
+sub getContribsCatalogList
 {
-    # get plugins catalog page
-    my $pluginsCatalogPage = LWP::Simple::get( qw( http://twiki.org/cgi-bin/search/Plugins/?scope=text&web=Plugins&order=topic&search=%5BT%5DopicClassification.*value%5C%3D%5C%22%5BC%5DontributedCode&casesensitive=on&regex=on&nosearch=on&nosummary=on&limit=all&skin=plain ) )
+    # get contribs catalog page
+    my $contribsCatalogPage = LWP::Simple::get( qw( http://twiki.org/cgi-bin/search/Plugins/?scope=text&web=Plugins&order=topic&search=%5BT%5DopicClassification.*value%5C%3D%5C%22%5BC%5DontributedCode&casesensitive=on&regex=on&nosearch=on&nosummary=on&limit=all&skin=plain ) )
+	|| LWP::Simple::get( "file:$Config->{local_cache}/TWikiContribs.html" )
 	or die $!;
 
-    # get list of plugins (from the links)
-    my @plugins = qw();
+    # get list of contribs (from the links)
+    my @contribs = qw();
 
-    my $stream = new HTML::TokeParser( \$pluginsCatalogPage ) or die $!;
+    my $stream = new HTML::TokeParser( \$contribsCatalogPage ) or die $!;
     while ( my $tag = $stream->get_tag('a'))
     {
 	next unless $tag->[1]{href} && $tag->[1]{href} =~ m|/view/Plugins/.+Contrib$|;
-	my ( $plugin ) = $tag->[1]{href} =~ m|.+/(.+Contrib)|;
-	next unless $plugin;
-	push @plugins, $plugin;
+	my ( $contrib ) = $tag->[1]{href} =~ m|.+/(.+Contrib)|;
+	next unless $contrib;
+	push @contribs, {
+	    name => $contrib,
+	    description => "$contrib description",
+	    homepage => "http://twiki.org/cgi-bin/view/Plugins/$contrib",
+	};
     }
 
-    return @plugins;
+    return @contribs;
 }
 
 ################################################################################
