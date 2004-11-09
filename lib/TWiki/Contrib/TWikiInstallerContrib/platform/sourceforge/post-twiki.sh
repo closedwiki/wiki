decd7ba0a764c5e845d4dfbdec11becc026e64e3
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2004-11-09 19:52:58 +0000

    (WillNorris) some cleanup, made darwin and sourceforge versions as similiar as possible
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5369 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/sourceforge/post-twiki.sh
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/sourceforge/post-twiki.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 # post-twiki.sh
-mkdir htdocs/twiki
+mkdir -p htdocs/twiki
 mv cgi-bin/tmp/twiki/pub/* htdocs/twiki/
 mkdir twiki/templates
 cp cgi-bin/tmp/twiki/templates/* twiki/templates/
+
+#rm -rf cgi-bin/install_twiki.cgi cgi-bin/tmp/
+
+# $account = /Users/(twiki)/Sites
+#echo `wget -O - http://localhost/~twiki/cgi-bin/twiki/manage?action=relockrcs | grep code | wc -l` topic(s) unlocked
+
 cat >htdocs/index.php <<'EOF'
 <?php 
 Header( "Location: http://" . $_SERVER[HTTP_HOST] . "/cgi-bin/twiki/view/" );
 ?>
 EOF
-rm -fr cgi-bin/TWiki20040901.tar.gz cgi-bin/install_twiki.cgi cgi-bin/tmp
 
--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/sourceforge/pre-twiki.sh
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/sourceforge/pre-twiki.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 # pre-twiki.sh
-cp downloads/releases/TWiki20040901.tar.gz install_twiki.cgi cgi-bin/
+echo "TWiki Installation (Step 1/4)"
+
+mkdir -p cgi-bin/
+cp install_twiki.cgi ./cgi-bin/
 chmod +x cgi-bin/install_twiki.cgi
 
-mkdir -p cgi-bin/tmp cgi-bin/tmp/twiki/pub cgi-bin/tmp/twiki/templates cgi-bin/tmp/install
-cp -R downloads cgi-bin/tmp/install
-cp -R cpan cgi-bin/tmp/install
-cp -R webs cgi-bin/tmp/install
-chmod -R 777 cgi-bin/tmp
+mkdir -p cgi-bin/tmp/ cgi-bin/tmp/twiki/pub/ cgi-bin/tmp/twiki/templates/ cgi-bin/tmp/install/
+cp -R downloads cgi-bin/tmp/install/
+cp -R cpan cgi-bin/tmp/install/
+cp -R webs cgi-bin/tmp/install/
+chmod -R 777 cgi-bin/tmp/
 
-mkdir -p cgi-bin/twiki ; chmod 777 cgi-bin/twiki
-mkdir -p cgi-bin/lib ; mkdir -p cgi-bin/lib/CPAN ; chmod -R 777 cgi-bin/lib
-mkdir -p twiki ; chmod -R 777 twiki
+mkdir -p cgi-bin/twiki/ ; chmod 777 cgi-bin/twiki/
+mkdir -p cgi-bin/lib/ ; chmod -R 777 cgi-bin/lib/
+mkdir -p twiki/ ; chmod -R 777 twiki/
+
+mkdir -p cgi-bin/lib/CPAN/ ; chmod -R 777 cgi-bin/lib/CPAN/
+perl cpan/install-cpan.pl 
+chmod -R 777 cgi-bin/lib/CPAN/
+
+echo "browse to http://`hostname`/~`whoami`/cgi-bin/install_twiki.cgi to continue installation"
