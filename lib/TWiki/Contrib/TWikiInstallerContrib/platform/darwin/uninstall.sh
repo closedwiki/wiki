b7eb65aa3f47ccfd87539d05a6dd787cc1a1119a
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2004-11-11 07:21:27 +0000

    (WillNorris) forgot to delete README file
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5382 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/uninstall.sh
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/uninstall.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-rm -rf bin cpan downloads *.cgi *.sh twiki-darwin.tar.bz2 cgi-bin/tmp
+rm -rf bin cpan downloads *.cgi *.sh twiki-darwin.tar.bz2 cgi-bin/tmp README
 rmdir cgi-bin htdocs
 rm uninstall.sh
