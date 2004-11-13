2bf890b7d46622d9503f3a79a5fe2ed047c8af84
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2004-11-13 22:19:43 +0000

    (WillNorris) explicitly deletes insallation script files, instead of wildcarding them
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5393 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/uninstall.sh
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/uninstall.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-rm -rf bin cpan downloads *.cgi *.sh twiki-darwin.tar.bz2 cgi-bin/tmp README install_twiki.cgi\?*
+rm -rf bin cpan downloads *.cgi pre-twiki.sh post-twiki.pl un-twiki.sh twiki-darwin.tar.bz2 cgi-bin/tmp README install_twiki.cgi\?*
 rmdir cgi-bin htdocs
 rm uninstall.sh
