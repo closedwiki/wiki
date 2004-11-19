47325ff241c65f6b6f9c8b6b22f5a356ce8d8398
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2004-11-19 13:42:42 +0000

    (WillNorris) more vigorous file cleanup
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5399 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/uninstall.sh
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/uninstall.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
-rm -rf bin cpan downloads *.cgi pre-twiki.sh post-twiki.pl un-twiki.sh twiki-darwin.tar.bz2 cgi-bin/tmp README install_twiki.cgi\?*
+rm -rf bin cpan downloads *.cgi pre-twiki.sh post-twiki.pl un-twiki.sh twiki-darwin.tar.bz2 cgi-bin/tmp README install_twiki.cgi\?* 
+# just in case cleanup
+rm -f TWikiInstallationReport.html
 rmdir cgi-bin htdocs
 rm uninstall.sh
