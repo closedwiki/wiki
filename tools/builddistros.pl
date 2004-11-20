bc4ab32498e6412907a353874ce8c02db6a572d9
SvenDowideit <SvenDowideit@a00a5322-12db-0310-a70b-8735589c885e>
2004-11-20 01:34:49 +0000

    DevelopBranch: build twiki kernel every 12 hours
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@3245 a00a5322-12db-0310-a70b-8735589c885e

--- tools/builddistros.pl
+++ tools/builddistros.pl
@@ -1,3 +1,32 @@
+#! /usr/bin/perl -w
+# Copyright 2004 Sven Dowideit.  All Rights Reserved.
+# License: GPL
+
+use strict;
+
+my $outputDir;
+
 #please do all your work in /tmp, and then move the resultant files (and error logs) into the directory specified on the command line
+if  ( $#ARGV == 0 ) {
+	$outputDir = $ARGV[0]; 
+} else {
+	print "please provide an outputDir\n";
+	exit(1);
+}
 
 print "building distros\n";
+print "results will be in $outputDir\n";
+
+execute ( "cd distro ; ./build-twiki-kernel.pl /tmp $outputDir" ) or die $!;
+
+exit 0;
+
+################################################################################
+################################################################################
+
+sub execute 
+{
+    my ($cmd) = @_;
+    chomp( my @output = `$cmd` );
+    print "$?: $cmd\n", join( "\n", @output );
+}
