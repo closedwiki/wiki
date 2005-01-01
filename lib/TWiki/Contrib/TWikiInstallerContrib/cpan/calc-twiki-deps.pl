ece60361deda74fb0e554f9dd73f4ca8d9aecce9
maphew <maphew@a00a5322-12db-0310-a70b-8735589c885e>
2005-01-01 22:32:49 +0000

    removed pieces of PublishAddOn which were mistakenly unpacked into the cvs root directory.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5454 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/cpan/calc-twiki-deps.pl
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/perl -w
-use strict;
-
-print <<'__MODULES__';
-^Data::UUID$ ^Date::Handler$ ^CGI::Session ^File::Temp$ ^List::Permutor$ ^Text::Diff$ ^Algorithm::Diff$ ^HTML::Diff$ ^Pod::Escapes$ ^Pod::Simple$ ^Clone$ ^Algorithm::Annotate$ ^Regexp::Shellish$ ^IO::Digest$ ^PerlIO::via::dynamic$ ^Data::Hierarchy$ ^PerlIO::via::symlink$ ^Time::ParseDate$ ^YAML ^Safe$ ^XML::Simple$ ^XML::Parser$ Prolog ^GD ^SOAP ^XMLRPC ^Date::Handler$ ^Date::Parse ^Date::Calc$ ^Bit::Vector$ ^Carp::Clan$ ^HTML::CalendarMonthSimple$ ^Image::LibRSVG$ ^Error$ ^Class::Inner$ ^Devel::Symdump$ ^URI$ ^HTML::Parser$ ^HTML::Tagset$ ^Digest::MD5$ ^Storable$ ^Test::Unit$ ^LWP::Simple$ ^Weather::Com$ ^File::Slurp ^Barcode::Code128$ WWW::Mechanize ^File::Find ^File::Slurp ^Text::Glob$ ^Number::Compare$ ^XML::SAX$ ^XML::NamespaceSupport$ ^XML::LibXML ^XML::LibXSLT$ ^Cache::Cache$ ^String::CRC$ ^Time::HiRes$ ^Archive:: ^IO::String$ ^File::AnySpec$ ^File::Package$ ^Data::Startup$ ^Tie::Gzip$ ^File::Where$ ^Class::Virtually::Abstract$ ^Carp::Assert$ ^Class::Data::Inheritable$ ^IO::Zlib$ ^LWP::UserAgent::TWiki ^HTML::TableExtract$
-__MODULES__
--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-rm -rf bin cpan downloads *.cgi pre-twiki.sh post-twiki.pl un-twiki.sh twiki-darwin.tar.bz2 cgi-bin/tmp README install_twiki.cgi\?* htdocs/index.php
-# just in case cleanup
-rm -f TWikiInstallationReport.html
-rmdir cgi-bin htdocs
-rm uninstall.sh
