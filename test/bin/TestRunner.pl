feb9f4d9926108fd92741972b76637e9aa6b2013
MartinCleaver <MartinCleaver@a00a5322-12db-0310-a70b-8735589c885e>
2005-01-17 17:26:51 +0000

    TestUnit: now checks and sets TWIKI_ASSERTS
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@3530 a00a5322-12db-0310-a70b-8735589c885e

--- test/bin/TestRunner.pl
+++ test/bin/TestRunner.pl
@@ -5,6 +5,17 @@ use strict;
 use Test::Unit::Debug qw(debug_pkgs);
 use Test::Unit::TestRunner;
 
+unless (defined $ENV{TWIKI_ASSERTS}) {
+  print "exporting TWIKI_ASSERTS=1 for extra checking; disable by exporting TWIKI_ASSERTS=0\n";
+  $ENV{TWIKI_ASSERTS} = 1;
+}
+
+if ($ENV{TWIKI_ASSERTS} == 1) {
+  print "Assert checking on\n";
+} else {
+  print "Assert checking off\n";
+}
+
 # Uncomment and edit to debug individual packages.
 #debug_pkgs(qw/Test::Unit::TestCase/);
 
