616dd75d32f79db4a3e3913bcfd8220bb0154a4c
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-02-01 16:09:28 +0000

    (WillNorris) initial import from 'template' of platform/darwin
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5473 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/uninstall.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+rm -rf bin cpan downloads *.cgi pre-twiki.sh post-twiki.pl un-twiki.sh twiki-darwin.tar.bz2 cgi-bin/tmp README install_twiki.cgi\?* htdocs/index.php
+# just in case cleanup
+rm -f TWikiInstallationReport.html
+rmdir cgi-bin htdocs
+rm uninstall.sh
