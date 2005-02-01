d1d4c694c117a5601dba31d42b095d1f34fb9eaf
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-02-01 22:49:51 +0000

    (WillNorris) sync with dreamhost; setup cpan mirror in a directory accessiable from a top-level $cpan directory
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5477 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/pre-twiki.sh
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/pre-twiki.sh
@@ -2,9 +2,7 @@
 # pre-twiki.sh
 echo "TWiki Installation (Step 1/4)"
 
-mkdir -p cgi-bin/
-cp install_twiki.cgi ./cgi-bin/
-chmod +x cgi-bin/install_twiki.cgi
+mkdir -p cgi-bin/ ; cp install_twiki.cgi ./cgi-bin/ ; chmod +x cgi-bin/install_twiki.cgi
 
 mkdir -p cgi-bin/tmp/ cgi-bin/tmp/twiki/pub/ cgi-bin/tmp/twiki/templates/ cgi-bin/tmp/install/
 cp -R downloads cgi-bin/tmp/install/
@@ -17,7 +15,12 @@ mkdir -p cgi-bin/lib/ ; chmod -R 777 cgi-bin/lib/
 mkdir -p twiki/ ; chmod -R 777 twiki/
 
 mkdir -p cgi-bin/lib/CPAN/ ; chmod -R 777 cgi-bin/lib/CPAN/
-perl cpan/install-cpan.pl 
+[ -e cgi-bin/tmp/install/cpan/MIRROR ] && mv cgi-bin/tmp/install/cpan/MIRROR/ cgi-bin/lib/CPAN/
+perl cpan/install-cpan.pl XML::Parser XML::Simple Text::Diff Algorithm::Diff HTML::Diff
+#perl cpan/install-cpan.pl
 chmod -R 777 cgi-bin/lib/CPAN/
 
-echo "browse to http://`hostname`/~`whoami`/cgi-bin/install_twiki.cgi to continue installation"
+find cgi-bin -print | xargs chmod go-w
+
+#open "http://`hostname`/~`whoami`/cgi-bin/install_twiki.cgi"
+open "http://`hostname`/~`whoami`/config/install.html"
