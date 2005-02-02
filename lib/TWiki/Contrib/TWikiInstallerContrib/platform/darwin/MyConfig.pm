3b89edf7e659d0b9cbe29ef93a561e03985d1ead
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-02-02 00:37:23 +0000

    (WillNorris) cpan configuration template starting point (for cpan install handled automatically from within the installer)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5479 a00a5322-12db-0310-a70b-8735589c885e

---
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/MyConfig.pm
@@ -0,0 +1,52 @@
+# this gets moved to ~/.cpan/CPAN
+
+sub mychomp { chomp $_[0]; $_[0] }
+
+my $cpan = "/Users/" . mychomp(`whoami`) . "/Sites/cgi-bin/lib/CPAN";
+
+$CPAN::Config = {
+  'build_cache' => q[10],
+  'build_dir' => "$cpan/.cpan/build",
+#  'build_dir' => q[/Users/wbniv/Sites/twiki/.cpan/build],
+  'cache_metadata' => q[1],
+  'cpan_home' => "$cpan/.cpan",
+#  'cpan_home' => q[/Users/wbniv/Sites/twiki/.cpan],
+  'ftp' => q[/usr/bin/ftp],
+  'ftp_proxy' => q[],
+  'getcwd' => q[cwd],
+  'gpg' => q[],
+  'gzip' => q[/sw/bin/gzip],
+  'histfile' => "$cpan/.cpan/histfile",
+#  'histfile' => q[/Users/wbniv/Sites/twiki/.cpan/histfile],
+  'histsize' => q[0],
+#  'histsize' => q[100],
+  'http_proxy' => q[],
+  'inactivity_timeout' => q[0],
+  'index_expire' => q[1],
+  'inhibit_startup_message' => q[0],
+  'keep_source_where' => "$cpan/.cpan/sources",
+#  'keep_source_where' => q[/Users/wbniv/Sites/twiki/.cpan/sources],
+  'lynx' => q[],
+  'make' => q[/usr/bin/make],
+  'make_arg' => "-I$cpan/",
+#  'make_arg' => q[],
+  'make_install_arg' => "-I$cpan/lib/",
+#  'make_install_arg' => q[],
+  'makepl_arg' => "LIB=$cpan/lib INSTALLMAN1DIR=$cpan/man/man1 INSTALLMAN3DIR=$cpan/man/man3",
+#???(sometimes?) $CPAN::Config->{'makepl_arg'} = "PREFIX=$cpan";
+#  'makepl_arg' => q[],
+  'ncftp' => q[],
+  'ncftpget' => q[],
+  'no_proxy' => q[],
+  'pager' => q[/usr/bin/less],
+  'prerequisites_policy' => q[follow],
+  'scan_cache' => q[atstart],
+  'shell' => q[/bin/bash],
+  'tar' => q[/sw/bin/tar],
+  'term_is_latin' => q[1],
+  'unzip' => q[/sw/bin/unzip],
+  'urllist' => ["file:$cpan/MIRROR/TWIKI/"],
+  'wget' => q[/sw/bin/wget],
+};
+1;
+__END__
