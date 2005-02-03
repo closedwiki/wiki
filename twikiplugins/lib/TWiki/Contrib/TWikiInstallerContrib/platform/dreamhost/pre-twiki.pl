70e1d8472749ebb177ad62ed7ed99cb2081c81cd
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-02-03 20:32:57 +0000

    (WillNorris) sync with darwin
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5490 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/pre-twiki.pl
+++ twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/pre-twiki.pl
@@ -9,13 +9,15 @@ print "TWiki Installation (Step 1/4)\n";
 my $whoami = mychomp(`whoami`);
 my $INSTALL = "http://" . mychomp(`hostname`) . "/~$whoami/config/install.html";
 
-if ( -e "twiki/" )
+#if ( -e "twiki/" )
+if ( 0 )
 {
     print "pre-twiki.pl already run; continue installatin at $INSTALL (you shouldn't have a ./twiki directory)\n";
     exit 0;
 }
 
-print `mkdir -p cgi-bin/ ; cp install_twiki.cgi ./cgi-bin/ ; chmod +x cgi-bin/install_twiki.cgi`;
+print `mkdir -p cgi-bin/` unless -d "cgi-bin";
+print `cp install_twiki.cgi cgi-bin/ ; chmod +x cgi-bin/install_twiki.cgi` unless -e "cgi-bin/install_twiki.cgi";
 
 print `mkdir -p cgi-bin/tmp/ cgi-bin/tmp/twiki/pub/ cgi-bin/tmp/twiki/templates/ cgi-bin/tmp/install/`;
 print `cp -R downloads cgi-bin/tmp/install/`;
@@ -28,11 +30,19 @@ print `mkdir -p cgi-bin/lib/ ; chmod -R 777 cgi-bin/lib/`;
 print `mkdir -p twiki/ ; chmod -R 777 twiki/`;
 
 print `mkdir -p cgi-bin/lib/CPAN/ ; chmod -R 777 cgi-bin/lib/CPAN/`;
-if ( -e ( my $mirrorOrigLoc = "cgi-bin/tmp/install/cpan/MIRROR/" ) ) { print `mv $mirrorOrigLoc/ cgi-bin/lib/CPAN/` }
-unless ( -e ( my $cpanConfig = "/Users/$whoami/.cpan/CPAN/MyConfig.pm" ) ) { print `mkdir -p /Users/$whoami/.cpan/CPAN; mv MyConfig.pm $cpanConfig` }
+if ( -e ( my $mirrorOrigLoc = "cgi-bin/tmp/install/cpan/MIRROR/" ) ) 
+{ 
+    print `mv $mirrorOrigLoc/ cgi-bin/lib/CPAN/`;
+}
+unless ( -e ( my $cpanConfig = "/Users/$whoami/.cpan/CPAN/MyConfig.pm" ) ) 
+{ 
+    print `mkdir -p /Users/$whoami/.cpan/CPAN; mv MyConfig.pm $cpanConfig`;
+}
 print `perl cpan/install-cpan.pl XML::Parser XML::Simple Text::Diff Algorithm::Diff HTML::Diff`;
 #print `perl cpan/install-cpan.pl'`;
-print `chmod -R 777 cgi-bin/lib/CPAN/`;
+
+print `chmod -R 777 cgi-bin/tmp/`;
+#print `chmod -R 777 cgi-bin/lib/CPAN/`;
 
 print `find cgi-bin -print | xargs chmod go-w`;
 
