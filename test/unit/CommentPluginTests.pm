9d982878be81404a95436e075e21cd14690360b0
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-02-23 14:56:54 +0000

    MovePluginsIntoSVN: Wnit tests for comment plugin
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@3686 a00a5322-12db-0310-a70b-8735589c885e

---
+++ test/unit/CommentPluginTests.pm
@@ -0,0 +1,319 @@
+use strict;
+
+package CommentPluginTests;
+
+use base qw(Test::Unit::TestCase);
+
+BEGIN {
+    unshift @INC, '../../bin';
+    require 'setlib.cfg';
+};
+
+use TWiki;
+
+use TWiki::Plugins::CommentPlugin;
+use TWiki::Plugins::CommentPlugin::Comment;
+use CGI;
+
+my $baseweb = "TestCommentPluginWeb";
+my $targetweb = "TestCommentPluginTargetWeb";
+my $basetopic = "TestCommentPluginTopic";
+my $targettopic = "TargetTopic";
+
+my $thePathInfo = "/$baseweb/$basetopic";
+my $twiki;
+
+sub new {
+    my $self = shift()->SUPER::new(@_);
+    return $self;
+}
+
+# Set up the test fixture
+sub set_up {
+    my $this = shift;
+
+    $twiki = new TWiki( $thePathInfo, "TestUser1", $basetopic, "" );
+
+    mkdir("$TWiki::cfg{DataDir}/$baseweb",0777) ||
+      die "$TWiki::cfg{DataDir}/$baseweb fixture setup failed: $!";
+    mkdir("$TWiki::cfg{DataDir}/$targetweb",0777) ||
+      die "$TWiki::cfg{DataDir}/$targetweb fixture setup failed: $!";
+
+    TWiki::Plugins::CommentPlugin::initPlugin();
+    $TWiki::Plugins::CommentPlugin::testing = 1;
+    $TWiki::Plugins::SESSION = $twiki;
+}
+
+sub tear_down {
+    `rm -rf $TWiki::cfg{DataDir}/$baseweb`;
+    print STDERR "tear_down $TWiki::cfg{DataDir}/$baseweb failed: $!\n" if $!;
+    `rm -rf $TWiki::cfg{DataDir}/$targetweb`;
+    print STDERR "tear_down $TWiki::cfg{DataDir}/$targetweb failed: $!\n" if $!;
+}
+
+sub writeTopic {
+    my( $web, $topic, $text ) = @_;
+    my $meta = new TWiki::Meta($twiki, $web, $topic);
+    $twiki->{store}->saveTopic( $twiki->{user}, $baseweb, $basetopic, $text,
+                                $meta );
+}
+
+sub trim {
+    my $s = shift;
+    $s =~ s/^\s*(.*?)\s*$/$1/sgo;
+    return $s;
+}
+
+# Not a test, a helper.
+sub inputTest {
+    my ($this, $type, $web, $topic, $anchor, $location) = @_;
+
+    my $eidx = 1;
+    my $sattrs = "";
+
+    $web = $baseweb unless $web;
+    $topic = $basetopic unless $topic;
+
+    if ($web ne $baseweb || $topic ne $basetopic || $anchor) {
+
+        $sattrs = "target=\"";
+
+        $sattrs .= "$web." unless ($web eq $baseweb);
+        $sattrs .= $topic unless ($topic eq $basetopic);
+
+        if ( $anchor) {
+            $anchor = "#$anchor";
+            $sattrs .= $anchor;
+        }
+        $sattrs .= "\"";
+    }
+
+    my $url = "$TWiki::cfg{DefaultUrlHost}$TWiki::cfg{ScriptUrlPath}/viewauth/%INTURLENCODE{$web}%/%INTURLENCODE{$topic}%";
+
+    if ( $location ) {
+        $sattrs .= " location=\"$location\"";
+    }
+
+    $type = "bottom" unless ($type);
+    $sattrs .= "type=\"$type\" ";
+
+    my $commentref = "%COMMENT{type=\"$type\" refmark=\"here\"}%";
+
+    # Build the target topic
+    my $sample = "TopOfTopic\n%COMMENT{type=\"$type\"}%\n";
+    $sample .= "BeforeAnchor\n$anchor\nAfterAnchor\n" if ($anchor);
+    $sample .= "BeforeLocation\nHereIsTheLocation\nAfterLocation";
+    $sample .= "$commentref\n";
+    $sample .= "BottomOfTopic\n";
+
+    writeTopic($web, $topic, $sample);
+    my $pidx = $eidx;
+    my $html =
+      TWiki::Plugins::CommentPlugin::Comment::_handleInput
+          ($sattrs,
+           $basetopic,
+           $baseweb,
+           \$pidx,
+           "The Message",
+           "",
+           "bottom");
+
+    $this->assert($pidx == $eidx + 1, $html);
+
+    $html =~ s/^<form(.*?)>//sio;
+    my $dattrs = $1;
+    $html =~ s/<\/form>\n$//sio;
+    $this->assert(scalar($dattrs =~ s/\s+name=\"(.*?)\"//), $dattrs);
+    $this->assert_str_equals("${type}$eidx", $1);
+    $this->assert(scalar($dattrs =~ s/\s+method\s*=\s*\"post\"//i), $dattrs);
+    $this->assert(scalar($dattrs =~ s/\s+action=\"(.*?)\"//), $dattrs);
+    $this->assert_str_equals("$url", $1);
+    $this->assert_str_equals("", trim($dattrs));
+
+    # no hiddens should be generated if disabled
+    $html =~ s/<input name=\"comment_type"(.*?)\s*\/>//i;
+    $dattrs = $1;
+    $this->assert(scalar($dattrs =~ s/\s*type=\"hidden\"//io), $dattrs);
+    $this->assert(scalar($dattrs =~ s/\s*value=\"$type\"//), $dattrs);
+    $this->assert_str_equals("", trim($dattrs));
+
+    if ( $anchor ) {
+        $this->assert_matches(qr/<input name=\"comment_anchor"(.*?)\s*\/>/i,$html);
+        $html =~ s/<input name=\"comment_anchor"(.*?)\s*\/>//i;
+        $dattrs = $1;
+        $this->assert(scalar($dattrs =~ s/\s*type=\"hidden\"//io), $dattrs);
+        $this->assert(scalar($dattrs =~ s/\s*value=\"(.*?)\"//o), $dattrs);
+        $this->assert_str_equals($anchor, $1);
+        $this->assert_str_equals("", trim($dattrs));
+        $this->assert_does_not_match(qr/<input name=\"comment_index/, $html);
+        $this->assert_does_not_match(qr/<input name=\"comment_location/, $html);
+    } elsif ( $location ) {
+        $this->assert_matches(qr/<input name=\"comment_location"(.*?)\s*\/>/i, $html);
+        $html =~ s/<input name=\"comment_location"(.*?)\s*\/>//i;
+        $dattrs = $1;
+        $this->assert(scalar($dattrs =~ s/\s*type=\"hidden\"//io), $dattrs);
+        $this->assert(scalar($dattrs =~ s/\s*value=\"(.*?)\"//o), $dattrs);
+        $this->assert_str_equals($location, $1);
+        $this->assert_str_equals("", trim($dattrs));
+        $this->assert_does_not_match(qr/<input name=\"comment_index/, $html);
+        $this->assert_does_not_match(qr/<input name=\"comment_anchor/, $html);
+    } else {
+        $this->assert_matches(qr/<input name=\"comment_index"(.*?)\s*\/>/i, $html);
+        $html =~ s/<input name=\"comment_index"(.*?)\s*\/>//i;
+        $dattrs = $1;
+        $this->assert(scalar($dattrs =~ s/\s*type=\"hidden\"//io), $dattrs);
+        $this->assert(scalar($dattrs =~ s/\s*value=\"(.*?)\"//io), $dattrs);
+        $this->assert_str_equals($eidx, $1);
+        $this->assert_str_equals("", trim($dattrs));
+        $this->assert_does_not_match(qr/<input name=\"comment_anchor/, $html);
+        $this->assert_does_not_match(qr/<input name=\"comment_location/, $html);
+    }
+
+    $this->assert_matches(qr/<input name=\"unlock\"(.*?)\s*\/>/, $html);
+    $html =~ s/<input name=\"unlock\"(.*?)\s*\/>//i;
+    $dattrs = $1;
+    $this->assert(scalar($dattrs =~ s/\s*type=\"hidden\"//io), $dattrs);
+    $this->assert(scalar($dattrs =~ s/\s*value=\"1\"//io), $dattrs);
+    $this->assert_str_equals("", trim($dattrs));
+
+    $this->assert_matches(qr/<input name=\"comment_action\"(.*?)\s*\/>/, $html);
+    $html =~ s/<input name=\"comment_action\"(.*?)\s*\/>//i;
+    $dattrs = $1;
+    $this->assert(scalar($dattrs =~ s/\s*type=\"hidden\"//io), $dattrs);
+    $this->assert(scalar($dattrs =~ s/\s*value=\"save\"//io), $dattrs);
+    $this->assert_str_equals("", trim($dattrs));
+
+    $html =~ s/<textarea  (.*?)>(.*?)<\/textarea>//i;
+    $dattrs = $1;
+    $this->assert_matches(qr/name=\"comment\"/, $dattrs);
+    my $mess = $2;
+    $this->assert_str_equals("The Message", $mess);
+    $this->assert_matches(qr/<input\s+\s*type="submit"\s*value=\".*?"\s*\/>/i,
+                          $html);
+
+    # Compose the query
+    my $comm = "This is the comment";
+    my $query = new CGI({'comment_action' => 'save',
+                         'comment_type' => $type,
+                         'comment' => $comm });
+    if ( $anchor ) {
+        $query->param(-name=>'comment_anchor', -value=>$anchor);
+    } elsif ( $location) {
+        $query->param(-name=>'comment_location', -value=>$location);
+    } else {
+        $query->param(-name=>'comment_index', -value=>$eidx);
+    }
+
+    $twiki->{cgiQuery} = $query;
+    my $text = "Ignore this text";
+    # invoke the save handler
+    TWiki::Plugins::CommentPlugin::commonTagsHandler($text, $topic, $web);
+
+    #$this->assert_str_equals("http://twiki/view.cgi/$web/$topic",
+    #BaseFixture::redirected());
+    $text = TWiki::Func::readTopicText($web, $topic);
+    $this->assert_matches(qr/$comm/, $text, "$web.$topic: $text");
+
+    my $refexpr;
+    if ($anchor) {
+        $refexpr = $anchor;
+    } elsif ($location) {
+        $refexpr = "HereIsTheLocation";
+    } else {
+        $refexpr = $commentref;
+    }
+
+    if( $topic eq $basetopic && $web eq $baseweb ) {
+        if ( $type eq "top") {
+            $this->assert_matches(qr/$comm.*TopOfTopic/s, $text);
+        } elsif ( $type eq "bottom" ) {
+            $this->assert_matches(qr/BottomOfTopic.*$comm/s, $text);
+        } elsif ( $type eq "above" ) {
+            $this->assert_matches(qr/TopOfTopic.*$comm.*$refexpr/s, $text);
+        } elsif ( $type eq "below" ) {
+            $this->assert_matches(qr/$refexpr.*$comm.*BottomOfTopic/s, $text);
+        }
+    }
+}
+
+sub test_above {
+    my $this = shift;
+    $this->inputTest("above", undef, undef, undef, undef, 0);
+}
+
+sub test_below {
+    my $this = shift;
+    $this->inputTest("below", undef, undef, undef, undef, 0);
+}
+
+sub test_targetTopic {
+    my $this = shift;
+    $this->inputTest("bottom", undef, $targettopic, undef, undef, 0);
+}
+
+sub test_targetWebTopic {
+    my $this = shift;
+    $this->inputTest("bottom", $targetweb, $targettopic, undef, undef, 0);
+}
+
+sub test_targetWebTopicAnchorTop {
+    my $this = shift;
+    $this->inputTest("top", $targetweb, $targettopic, "TargetAnchor", undef, 0);
+}
+
+sub test_targetWebTopicAnchorBottom {
+    my $this = shift;
+    $this->inputTest("bottom", $targetweb, $targettopic, "TargetAnchor", undef, 0);
+}
+
+sub test_location {
+    my $this = shift;
+    $this->inputTest("below", undef, undef, undef, "HereIsTheLocation", 0);
+}
+
+sub test_LocationRE {
+    my $this = shift;
+
+    $this->inputTest("above", undef, undef, undef, "^He.*on\$", 0);
+}
+
+sub test_reverseCompat {
+    my $this = shift;
+    # rows: Any number > 0 will set the rows of the text area (default is 5)
+    # cols: Any number > 10 will set the columns of the textarea (default is 70)
+    # mode: The word "after" tells Comment to put the posted data after the form in reverse chronological order (default = "normal" chronological order)
+    # button: This lets you change the text of the submit button (default is "Add Comment")
+    # id: This gives a unique name for a COMMENT, in case you have more than one COMMENT tag in a topic (mandatory with > 1 COMMENT)
+
+    my $pidx = 0;
+    my $html =
+      TWiki::Plugins::CommentPlugin::Comment::_handleInput
+          ("rows=99 cols=104 mode=after button=HoHo id=sausage",,
+           $basetopic,
+           $baseweb,
+           \$pidx,
+           "The Message",
+	 "",
+           "bottom");
+    $this->assert_matches(qr/form name=\"after0\"/, $html);
+    $this->assert_matches(qr/rows=\"99\"/, $html);
+    $this->assert_matches(qr/cols=\"104\"/, $html);
+    $this->assert_matches(qr/type=\"submit\" value=\"HoHo\"/, $html);
+}
+
+sub test_locationOverridesAnchor {
+    my $this = shift;
+    my $pidx = 0;
+    my $html =
+      TWiki::Plugins::CommentPlugin::Comment::_handleInput
+          ("target=\"$baseweb.ATopic#AAnchor\" location=\"AnRE\"",
+           $basetopic,
+           $baseweb,
+           \$pidx,
+           "The Message",
+	 "",
+           "bottom");
+    $this->assert_matches(qr/<input\s+name="comment_location"(.*?)\s*\/>/, $html);
+}
+
+1;
