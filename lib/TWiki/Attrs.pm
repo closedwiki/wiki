02b8911f00ba61f08a813b00241ff32f17acc045
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-03-05 19:35:52 +0000

    AcknowledgingTWikiContributors: executed
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@3747 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Attrs.pm
+++ lib/TWiki/Attrs.pm
@@ -1,20 +1,26 @@
+# TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2005 Crawford Currie - http://c-dot.co.uk
+# Copyright (C) 1999-2005 Peter Thoeny, peter@thoeny.com
+# and TWiki Contributors. All Rights Reserved. TWiki Contributors
+# are listed in the AUTHORS file in the root of this distribution.
+# NOTE: Please extend that file, not this notice.
 #
+# Additional copyrights apply to some or all of the code in this
+# file as follows:
 # Derived from Contrib::Attrs, which is
 # Copyright (C) 2001 Motorola - All rights reserved
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
-# http://www.gnu.org/copyleft/gpl.html
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
+# As per the GPL, removal of this notice is prohibited.
 
 =pod
 
--- lib/TWiki/Users/HtPasswdUser.pm
+++ lib/TWiki/Users/HtPasswdUser.pm
@@ -1,19 +1,24 @@
-# Module of TWiki Collaboration Platform, http://TWiki.org/
+# TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 1999-2004 Peter Thoeny, peter@thoeny.com
+# Copyright (C) 1999-2005 Peter Thoeny, peter@thoeny.com
+# and TWiki Contributors. All Rights Reserved. TWiki Contributors
+# are listed in the AUTHORS file in the root of this distribution.
+# NOTE: Please extend that file, not this notice.
 #
-# For licensing info read license.txt file in the TWiki root.
+# Additional copyrights apply to some or all of the code in this
+# file as follows:
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
-# http://www.gnu.org/copyleft/gpl.html
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
+# As per the GPL, removal of this notice is prohibited.
 
 =begin twiki
 
