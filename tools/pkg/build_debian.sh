fdd9f84fb9c7c895759f9df655e1bfe57e970ac9
sdowideit <sdowideit@a00a5322-12db-0310-a70b-8735589c885e>
2005-03-12 06:54:24 +0000

    TWikiOnDebian: added the script i use to build the package
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@3792 a00a5322-12db-0310-a70b-8735589c885e

---
+++ tools/pkg/build_debian.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd /tmp
+chmod -R 777 /tmp/twiki-20040902
+rm -r /tmp/twiki-20040902
+
+cp ~sven/build/twiki_20040902.orig.tar.gz .
+tar zxvf twiki_20040902.orig.tar.gz > /tmp/tar.log
+
+mv twiki twiki-20040902
+cd /tmp/twiki-20040902
+
+svn export http://ntwiki.ethermage.net:8181/svn/twiki/trunk/tools/pkg/debian > /tmp/svn_export.log
+
+fakeroot debian/rules patch
+
+debuild
