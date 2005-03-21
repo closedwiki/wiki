2829d0cf820c49804827a2a23188991ef4780b2f
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-03-21 09:47:29 +0000

    changed paths to dreamhost; more CPAN modules installation required
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5536 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/pre-twiki.pl
+++ twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/pre-twiki.pl
@@ -1,25 +1,30 @@
-#!/usr/bin/perl -w
+#! /home/wikihosting/packages/perl5.8.4/bin/perl -w
 # pre-twiki.pl
 use strict;
+use File::Path qw( mkpath );
 
 sub mychomp { chomp $_[0]; $_[0] }
 
 print "TWiki Installation (Step 1/4)\n";
 
 my $whoami = mychomp(`whoami`);
-my $INSTALL = "http://" . mychomp(`hostname`) . "/~$whoami/config/install.html";
+#my $INSTALL = "http://" . mychomp(`hostname`) . "/~$whoami/config/install.html";
+#my $INSTALL = "http://wbniv.wikihosting.com/config/install.html";
+my $INSTALL = "http://wbniv.wikihosting.com/cgi-bin/install_twiki.cgi";
 
-#if ( -e "twiki/" )
-if ( 0 )
+if ( -e "cgi-bin/install_twiki.cgi" )
 {
-    print "pre-twiki.pl already run; continue installatin at $INSTALL (you shouldn't have a ./twiki directory)\n";
+    print "pre-twiki.pl has already been run\n";
+    print "   (i checked for the existence of cgi-bin/install_twiki.cgi)\n";
+    print "   you can delete cgi-bin/install_twiki.cgi and rerun pre-twiki.pl to force ...\n";
+    print "\ncontinue installation at $INSTALL\n";
     exit 0;
 }
 
-print `mkdir -p cgi-bin/` unless -d "cgi-bin";
+-d "cgi-bin" || mkpath "cgi-bin";
 print `cp install_twiki.cgi cgi-bin/ ; chmod +x cgi-bin/install_twiki.cgi` unless -e "cgi-bin/install_twiki.cgi";
 
-print `mkdir -p cgi-bin/tmp/ cgi-bin/tmp/twiki/pub/ cgi-bin/tmp/twiki/templates/ cgi-bin/tmp/install/`;
+mkpath( [ qw( cgi-bin/tmp/ cgi-bin/tmp/twiki/pub/ cgi-bin/tmp/twiki/templates/ cgi-bin/tmp/install/ ) ] );
 print `cp -R downloads/ cgi-bin/tmp/install/downloads/`;
 print `cp -R cpan/ cgi-bin/tmp/install/cpan/`;
 print `cp -R webs/ cgi-bin/tmp/install/webs/`;
@@ -34,16 +39,20 @@ if ( -e ( my $mirrorOrigLoc = "cgi-bin/tmp/install/cpan/MIRROR/" ) )
 { 
     print `mv $mirrorOrigLoc/ cgi-bin/lib/CPAN/`;
 }
-unless ( -e ( my $cpanConfig = "/Users/$whoami/.cpan/CPAN/MyConfig.pm" ) ) 
+# TEMP: FIXME: !!! overwrites file FOR TESTING/DEVELOPMENT ONLY!!!
+my $cpanConfig = "/home/$whoami/.cpan/CPAN/MyConfig.pm";
+#unless ( -e ( my $cpanConfig = "/home/$whoami/.cpan/CPAN/MyConfig.pm" ) ) 
 { 
-    print `mkdir -p /Users/$whoami/.cpan/CPAN; mv MyConfig.pm $cpanConfig`;
+    print `mkdir -p /home/$whoami/.cpan/CPAN; cp MyConfig.pm $cpanConfig`;
 }
-print `perl cpan/install-cpan.pl XML::Parser XML::Simple Text::Diff Algorithm::Diff HTML::Diff`;
-#print `perl cpan/install-cpan.pl'`;
+print `~/bin/perl cpan/install-cpan.pl YAML Compress::Zlib IO::Zlib IO::String Archive::Tar ExtUtils::CBuilder ExtUtils::ParserXS Tree::DAG_Node </dev/null`;
+# Module::Build
+print `~/bin/perl cpan/install-cpan.pl Error URI HTML::Tagset HTML::Parser LWP XML::Parser XML::Simple Algorithm::Diff Text::Diff HTML::Diff </dev/null`;
+##print `~/bin/perl cpan/install-cpan.pl 
+print `~/bin/perl cpan/install-cpan.pl WWW::Mechanize HTML::TableExtract WWW::Mechanize::TWiki </dev/null`;
+# Net::SSLeay IO::Socket::SSL
+##print `~/bin/perl cpan/install-cpan.pl'`;
 
 print `chmod -R 777 cgi-bin/tmp/`;
-#print `chmod -R 777 cgi-bin/lib/CPAN/`;
 
-#print `find cgi-bin -print | xargs chmod go-w`;
-
-system( open => $INSTALL ) == 0 or print "continue installation at $INSTALL\n";
+system( links => $INSTALL ) == 0 or print "continue installation at $INSTALL\n";
