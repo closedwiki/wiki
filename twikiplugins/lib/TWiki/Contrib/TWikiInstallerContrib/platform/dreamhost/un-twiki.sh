c1660e95ad7ee5c923c4318625553b66259928ca
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-03-21 09:32:52 +0000

    cgi-bin/TWiki20040901.tar.gz no longer used or stored in cgi-bin
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5533 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/un-twiki.sh
+++ twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/un-twiki.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # un-twiki.sh
-rm -fr config/ cgi-bin/lib/ cgi-bin/tmp/ cgi-bin/twiki/ htdocs/twiki/ twiki/ cgi-bin/TWiki20040901.tar.gz cgi-bin/install_twiki.cgi
+rm -fr config/ cgi-bin/lib/ cgi-bin/tmp/ cgi-bin/twiki/ htdocs/twiki/ twiki/ cgi-bin/install_twiki.cgi
 
 # cleanup stuff that shouldn't still be here
 mkdir -p cgi-bin/tmp/del
