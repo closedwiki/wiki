a512192b024d508d2cc0c105a25aef1dabecca44
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-03-21 09:46:00 +0000

    changed paths to dreamhost; STARTED adding SCRIPT_SUFFIX support
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5535 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/post-twiki.pl
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/post-twiki.pl
@@ -1,23 +1,30 @@
-#!/usr/bin/perl -w
+#! /home/wikihosting/packages/perl5.8.4/bin/perl -w
 # post-twiki.sh
 use strict;
 use Data::Dumper qw( Dumper );
-use Cwd qw( cwd );
 use File::Basename qw( basename );
 
 # TODO notes:
 # * tighten up templates directory permissions (chmod -R o-w twiki/templates)
+# * use WWW::Mechanize::TWiki;
 
 sub mychomp { chomp $_[0]; $_[0] }
 
-my $startupTopic = "";
+my $opts = {
+    startupTopic => "",
+    scriptSuffix => '.cgi',
+};
 
 BEGIN {
     use Config;
-    my $localLibBase = cwd() . "/cgi-bin/lib/CPAN/lib/site_perl/" . $Config{version};
+    use FindBin;
+#    my $localLibBase = "$FindBin::Bin/cgi-bin/lib/CPAN/lib/site_perl/" . $Config{version};
+    my $localLibBase = "$FindBin::Bin/cgi-bin/lib/CPAN/lib";
     unshift @INC, ( $localLibBase, "$localLibBase/$Config{archname}" );
     # TODO: use setlib.cfg (along with TWiki:Codev.SetMultipleDirsInSetlibDotCfg)
 };
+
+#use WWW::Mechanize::TWiki;
 
 system( "mv cgi-bin/tmp/install/LocalSite.cfg cgi-bin/lib/" );
 system( "mkdir htdocs/" );
@@ -35,7 +42,7 @@ print `find cgi-bin/lib/ -print | xargs chmod go-w`;
 open( INDEX_PHP, ">htdocs/index.php" ) or die $!;
 print INDEX_PHP <<'EOF';
 <?php 
-Header( "Location: http://" . $_SERVER[HTTP_HOST] . "/cgi-bin/twiki/view/" );
+Header( "Location: http://" . $_SERVER[HTTP_HOST] . "/cgi-bin/twiki/view$opts->{scriptSuffix}/" );
 ?>
 EOF
 close( INDEX_PHP );
@@ -46,9 +53,18 @@ close( INDEX_PHP );
 ################################################################################
 ################################################################################
 
+# format: /Users/(account)/Sites/cgi-bin/...
+# format: /home/(drive)/(account)/account.wikihosting.com/...
+my $account = [ split( '/', $FindBin::Bin ) ]->[-2];
+print STDERR "account=[$account]\n";
+
+$ENV{SERVER_NAME} = "$account.wikihosting.com";
+chomp( my $hostname = $ENV{SERVER_NAME} || `hostname --long` || 'localhost' );
+die "hostname?" unless $hostname;
+
 my $agent = "TWikiInstaller: " . basename( $0 );
 my $mech = WWW::Mechanize::TWiki->new( agent => "$agent", autocheck => 1 ) or die $!;
-$mech->cgibin( "http://localhost/~twiki/cgi-bin/twiki" );
+$mech->cgibin( "http://$hostname/cgi-bin/twiki", { scriptSuffix => $opts->{scriptSuffix} } );
 
 # open up the system (could be locked down at the end)
 $mech->edit( "Main.TWikiAdminGroup" );
@@ -59,7 +75,7 @@ $mech->click_button( value => 'Save' );
 
 ################################################################################
 
-#rm -rf cgi-bin/install_twiki.cgi cgi-bin/tmp/
+#rm -rf cgi-bin/install_twiki$opts->{scriptSuffix} cgi-bin/tmp/
 
 # $account = /Users/(twiki)/Sites
 # echo `curl http://localhost/~twiki/cgi-bin/twiki/manage?action=relockrcs | grep code | wc -l` topic(s) unlocked
@@ -80,20 +96,21 @@ if ( -e "TWikiInstallationReport.html" )
     $mech->follow_link( text => 'Attach' );
     if ( $mech->submit_form(
 			    fields    => { 
-					filepath => cwd() . "/$topic.html",
+					filepath => "$FindBin::Bin/$topic.html",
 					filecomment => `date`,
 					hidefile => undef,
 			    },
 	) ) { #unlink "${topic}.html" 
 	}
     
-    $startupTopic = "TWiki.$topic";
+    $opts->{startupTopic} = "TWiki.$topic";
 }
 
 ################################################################################
 
-my $START = "http://" . mychomp(`hostname`) . "/~" . mychomp(`whoami`) . "/cgi-bin/twiki/view/$startupTopic";
-system( open => $START ) == 0 or print "start using your wiki at $START\n";
+my $START = "http://$hostname/cgi-bin/twiki/view$opts->{scriptSuffix}/$opts->{startupTopic}";
+print "$START\n";
+system( links => $START ) == 0 or print "start using your wiki at $START\n";
 
 ################################################################################
 ################################################################################
@@ -116,9 +133,12 @@ sub new {
 sub cgibin {
     my $self = shift;
     my $cgibin = shift;
-    my $args = @_;
+    my $args = shift;
 
     $self->{cgibin} = $cgibin;
+    $self->{scriptSuffix} = $args->{scriptSuffix};
+
+    print STDERR $self->{scriptSuffix}, "\n";
 
     return $self;
 }
@@ -135,7 +155,7 @@ sub AUTOLOAD {
     (my $action = $AUTOLOAD) =~ s/.*:://;
     *$AUTOLOAD = sub {
         my ($self, $topic, $args) = @_;
-        (my $url = URI->new( "$self->{cgibin}/$action/$topic" ))->query_form( $args );
+        (my $url = URI->new( "$self->{cgibin}/$action$self->{scriptSuffix}/$topic" ))->query_form( $args );
         return $self->get( $url );
     };
     goto &$AUTOLOAD;
