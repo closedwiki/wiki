c659a06f6840c255f8808005028230bf1e2200d6
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-03-24 12:50:03 +0000

    InternationalisationEnhancements: made link removal language-independent
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@3862 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/UI/View.pm
+++ lib/TWiki/UI/View.pm
@@ -190,7 +190,7 @@ sub view {
         # disable edit and attach
         # FIXME: won't work with non-default skins, see %EDITURL%
         $tmpl =~ s/%EDITTOPIC%/$mirrorLink.' | '.CGI::strike('Edit')/ge;
-        $tmpl =~ s/<a [^>]*?>(Attach)<\/a>/CGI::strike($1)/gei;
+        $tmpl =~ s/<a\s[^>]*href="[^"]*\/(attach|rename)\b[^>]*>(.*?)<\/a>/CGI::strike($2)/gei;
         if( $topicExists ) {
             # allow view to be indexed
             $indexableView = 1;
