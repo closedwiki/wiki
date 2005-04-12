7cb6dd84530b22e18689871b98316cc12106ffb1
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-04-12 02:16:17 +0000

    progressing to web-based install infrastructure/permissions
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5571 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/un-twiki.sh
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/un-twiki.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # un-twiki.sh
-rm -fr config/ cgi-bin/lib/ cgi-bin/tmp/ cgi-bin/twiki/ htdocs/twiki/ twiki/ cgi-bin/install_twiki.cgi
+rm -rf cgi-bin/ twiki/ htdocs/
+#lib/ cgi-bin/tmp/ cgi-bin/twiki/ htdocs/twiki/ twiki/ cgi-bin/install_twiki.cgi
 
 # cleanup stuff that shouldn't still be here
-mkdir -p cgi-bin/tmp/del
-mv twiki cgi-bin/tmp/del
-mv htdocs/twiki cgi-bin/tmp/del
+#mkdir -p cgi-bin/tmp/del
+#mv twiki cgi-bin/tmp/del
+#mv htdocs/twiki cgi-bin/tmp/del
