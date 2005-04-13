43fff04afa0fc8e75598959eb5be698db24ae9ca
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-04-13 13:33:45 +0000

    creates MyConfig.pm to run the CPAN installation process.  this has been being done by pre-twiki.pl, but other build scripts need to install cpan modules, too
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5576 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/cpan/install-cpan.pl
+++ twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/cpan/install-cpan.pl
@@ -89,6 +89,8 @@ sub installLocalModules
     my $parm = shift;
     my $cpan = $parm->{dir};
 
+    createMyConfigDotPm({ cpan => $cpan });
+
     $CPAN::Config->{'make'} = q[/usr/bin/make];
     # some modules refuse to work if PREFIX is set, and some refuse to work if it is not. ???
     $CPAN::Config->{'makepl_arg'} = "PREFIX=$cpan";
@@ -156,6 +158,71 @@ sub installLocalModules
     
 #    print Dumper( $CPAN::Config );
 }
+
+################################################################################
+
+sub createMyConfigDotPm
+{
+    my $parm = shift;
+    my $cpan = $parm->{dir};
+
+    my $cpanConfigDir = "~/.cpan/CPAN";
+    my $cpanConfig = "$cpanConfigDir/MyConfig.pm";
+
+    unless ( -e $cpanConfig )
+    { 
+	mkpath( $cpanConfigDir );
+	open( FH, ">$cpanConfig" ) or die "$!: Can't create $cpanConfig";
+	print FH <<__MYCONFIG_PM__
+$CPAN::Config = {
+  'build_cache' => q[0],
+  'build_dir' => "$cpan/.cpan/build",
+  'cache_metadata' => q[1],
+  'cpan_home' => "$cpan/.cpan",
+  'ftp' => q[/usr/bin/ftp],
+  'ftp_proxy' => q[],
+  'getcwd' => q[cwd],
+  'gpg' => q[],
+  'gzip' => q[/sw/bin/gzip],
+  'histfile' => "$cpan/.cpan/histfile",
+  'histsize' => q[0],
+  'http_proxy' => q[],
+  'inactivity_timeout' => q[0],
+  'index_expire' => q[1],
+  'inhibit_startup_message' => q[0],
+  'keep_source_where' => "$cpan/.cpan/sources",
+  'lynx' => q[],
+  'make' => q[/usr/bin/make],
+  'make_arg' => "-I$cpan/",
+#  'make_arg' => q[],
+  'make_install_arg' => "-I$cpan/lib/",
+#  'make_install_arg' => q[],
+  'makepl_arg' => "LIB=$cpan/lib INSTALLMAN1DIR=$cpan/man/man1 INSTALLMAN3DIR=$cpan/man/man3",
+#???(sometimes?) $CPAN::Config->{'makepl_arg'} = "PREFIX=$cpan";
+#  'makepl_arg' => q[],
+  'ncftp' => q[],
+  'ncftpget' => q[],
+  'no_proxy' => q[],
+  'pager' => q[/usr/bin/less],
+  'prerequisites_policy' => q[follow],
+  'scan_cache' => q[atstart],
+  'shell' => q[/bin/bash],
+  'tar' => q[/sw/bin/tar],
+  'term_is_latin' => q[1],
+  'unzip' => q[/sw/bin/unzip],
+#  ".../MIRROR/TWIKI/", ".../MIRROR/MINICPAN/",
+  'urllist' => ["file:$optsConfig->{mirror}",],
+  'wget' => q[/sw/bin/wget],
+};
+1;
+__END__
+__MYCONFIG_PM__
+}
+
+close FH;
+}
+
+################################################################################
 
 @defaultTWikiModules = qw(
 			  XML::Parser XML::Simple 
