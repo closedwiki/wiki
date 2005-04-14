21989860df9b95360108a8d603ea86924bbd3d93
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-04-14 15:36:35 +0000

    MyConfig.pm is gone, it gets created by pre-twiki.pl (including finding binaries like tar, gzip, etc.)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5591 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/uninstall.sh
+++ twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/uninstall.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-rm -rf bin/ cpan/ downloads/ *.cgi pre-twiki.* post-twiki.pl un-twiki.sh cgi-bin/tmp/ README install_twiki.cgi\?* htdocs/index.php MyConfig.pm
+rm -rf bin/ cpan/ downloads/ *.cgi pre-twiki.* post-twiki.pl un-twiki.sh cgi-bin/tmp/ README install_twiki.cgi\?* htdocs/index.php 
 # just in case cleanup
 rm -f TWikiInstallationReport.html
 rmdir cgi-bin htdocs
