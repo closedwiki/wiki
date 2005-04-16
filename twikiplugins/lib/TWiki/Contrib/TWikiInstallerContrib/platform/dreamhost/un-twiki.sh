f54fa82523910c4d09335cb197d8dabb82dfac95
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-04-16 17:26:45 +0000

    converted un-twiki (uninstall the installation) to simple pure perl
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5601 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/lib/TWiki/Contrib/TWikiInstallerContrib/platform/dreamhost/un-twiki.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# un-twiki.sh
-rm -fr config/ cgi-bin/lib/ cgi-bin/tmp/ cgi-bin/twiki/ htdocs/twiki/ twiki/ cgi-bin/install_twiki.cgi
-
-# cleanup stuff that shouldn't still be here
-mkdir -p cgi-bin/tmp/del
-mv twiki cgi-bin/tmp/del
-mv htdocs/twiki cgi-bin/tmp/del
