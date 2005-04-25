c6233d14228db5a2f431d0ba8599212be2f174a8
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-04-25 10:35:36 +0000

    DakarRelease: Will Norris pointed out that the settings of the checkboxes wasn't getting carried through preview - thanks Will
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@4112 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/UI/Preview.pm
+++ lib/TWiki/UI/Preview.pm
@@ -64,9 +64,17 @@ sub preview {
 
     # get view template, standard view or a view with a different skin
     $tmpl = $session->{templates}->readTemplate( 'preview', $skin );
-    $tmpl =~ s/%DONTNOTIFY%/$dontNotify/go;
-    my $forceNewRevision = $query->param( 'forcenewrevision' ) && 'checked="checked"' || '';
-    $tmpl =~ s/%FORCENEWREVISIONCHECKBOX%/$forceNewRevision/go;
+    if( $dontNotify ) {
+        $tmpl =~ s/%DONTNOTIFYCHECKBOX%/checked="checked"/go;
+    } else {
+        $tmpl =~ s/%DONTNOTIFYCHECKBOX%//go;
+    }
+    my $forceNewRevision = $query->param( 'forcenewrevision' ) || '';
+    if( $forceNewRevision ) {
+        $tmpl =~ s/%FORCENEWREVISIONCHECKBOX%/checked="checked"/go;
+    } else {
+        $tmpl =~ s/%FORCENEWREVISIONCHECKBOX%//go;
+    }
     if( $saveCmd ) {
         unless( $user->isAdmin()) {
             throw TWiki::OopsException( 'accessdenied', def => 'group',
