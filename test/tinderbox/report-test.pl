32c1f77748a6ce17b71c4b8966f18fe122430fa4
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2005-05-02 10:40:26 +0000

    DailyBuildAndSmokeTest: post the testrunner's report on http://ntwiki.ethermage.net/~develop/cgi-bin/view/Tinderbox/
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@4223 a00a5322-12db-0310-a70b-8735589c885e

---
+++ test/tinderbox/report-test.pl
@@ -0,0 +1,103 @@
+#! /usr/bin/perl -w
+# Copyright 2005 Will Norris.  All Rights Reserved.
+# License: GPL
+use strict;
+use Data::Dumper qw( Dumper );
+
+BEGIN {
+    my $cpan = '/home/wbniv/tinderbox.wbniv.wikihosting.com/cgi-bin/lib/CPAN/';
+    my @localLibs = ( "$cpan/lib", "$cpan/lib/arch" );
+    unshift @INC, @localLibs;
+}
+
+use Cwd qw( cwd );
+use Getopt::Long;
+use Pod::Usage;
+use File::Basename;
+use WWW::Mechanize;
+use WWW::Mechanize::TWiki 0.08;
+
+my $Config = {
+    svn => undef,
+    report => undef,
+# 
+    verbose => 0,
+    debug => 0,
+    help => 0,
+    man => 0,
+};
+
+my $result = GetOptions( $Config,
+#
+			 'svn=i', 'report=s',
+# miscellaneous/generic options
+			'agent=s', 'help', 'man', 'debug', 'verbose|v',
+			);
+pod2usage( 1 ) if $Config->{help};
+pod2usage({ -exitval => 1, -verbose => 2 }) if $Config->{man};
+print STDERR Dumper( $Config ) if $Config->{debug};
+	 
+die "svn is a required parameter" unless $Config->{svn};
+die "report is a required parameter" unless $Config->{report};
+die "no report '$Config->{report}'?" unless -e $Config->{report};
+
+################################################################################
+
+my $agent = "TWikiTestInfrastructure: " . File::Basename::basename( $0 );
+my $mech = WWW::Mechanize::TWiki->new( agent => "$agent", autocheck => 1 ) or die $!;
+$mech->cgibin( 'http://ntwiki.ethermage.net/~develop/cgi-bin' );
+
+my $topic = "Tinderbox.TestsReportSvn$Config->{svn}";
+
+$mech->edit( "$topic", { 
+    topicparent => 'Tinderbox.WebHome', 
+    templatetopic => 'TestReportTemplate',
+    formtemplate => 'TestReportForm',
+} );
+$mech->click_button( value => 'Save' );
+
+$mech->follow_link( text => 'Attach' );
+$mech->submit_form( fields => {
+    filepath => $Config->{report},
+    filecomment => `date`,
+    hidefile => undef,
+} );
+
+exit 0;
+
+################################################################################
+################################################################################
+
+__DATA__
+=head1 NAME
+
+report-test.pl - Codev.
+
+=head1 SYNOPSIS
+
+kernel-manifest.pl [options]
+
+Copyright 2004, 2005 Will Norris and Sven Dowideit.  All Rights Reserved.
+
+ Options:
+   -svn
+   -report
+   -verbose
+   -debug
+   -help			this documentation
+   -man				full docs
+
+=head1 OPTIONS
+
+=over 8
+
+=back
+
+=head1 DESCRIPTION
+
+
+=head2 SEE ALSO
+
+	http://twiki.org/cgi-bin/view/Codev/TWikiTestInfrastructure
+
+=cut
