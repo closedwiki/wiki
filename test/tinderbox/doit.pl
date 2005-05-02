577b235f9efd583ac92c20da71e2e9ffc37aa655
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2005-05-02 11:34:04 +0000

    DailyBuildAndSmokeTest: start of shell to run the complete sequence of testing a release
       * build a new twiki kernel
       * build a new distribution
       * install the distribution
       * run the tests
       * post the tests to tinderbox.wbniv.wikihosting.com
    
    
    
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@4224 a00a5322-12db-0310-a70b-8735589c885e

---
+++ test/tinderbox/doit.pl
@@ -0,0 +1,80 @@
+#! /usr/bin/perl -w
+# Copyright 2005 Will Norris.  All Rights Reserved.
+# License: GPL
+use strict;
+use Data::Dumper qw( Dumper );
+
+# build a new twiki kernel
+# build a new distribution
+# install the distribution
+# run the tests
+# post the tests to tinderbox.wbniv.wikihosting.com
+
+BEGIN {
+    my $cpan = '/home/wbniv/tinderbox.wbniv.wikihosting.com/cgi-bin/lib/CPAN/';
+    my @localLibs = ( "$cpan/lib", "$cpan/lib/arch" );
+    unshift @INC, @localLibs;
+}
+
+#use Cwd qw( cwd );
+use Getopt::Long;
+use Pod::Usage;
+use File::Basename;
+use WWW::Mechanize;
+use WWW::Mechanize::TWiki 0.08;
+
+my $Config = {
+# 
+    verbose => 0,
+    debug => 0,
+    help => 0,
+    man => 0,
+};
+
+my $result = GetOptions( $Config,
+# miscellaneous/generic options
+			'agent=s', 'help', 'man', 'debug', 'verbose|v',
+			);
+pod2usage( 1 ) if $Config->{help};
+pod2usage({ -exitval => 1, -verbose => 2 }) if $Config->{man};
+print STDERR Dumper( $Config ) if $Config->{debug};
+	 
+################################################################################
+
+
+exit 0;
+
+################################################################################
+################################################################################
+
+__DATA__
+=head1 NAME
+
+doit.pl - TWiki:Codev.DailyBuildAndSmokeTest
+
+=head1 SYNOPSIS
+
+doit.pl [options]
+
+Copyright 2004, 2005 Will Norris and Sven Dowideit.  All Rights Reserved.
+
+ Options:
+   -verbose
+   -debug
+   -help			this documentation
+   -man				full docs
+
+=head1 OPTIONS
+
+=over 8
+
+=back
+
+=head1 DESCRIPTION
+
+
+=head2 SEE ALSO
+
+	http://twiki.org/cgi-bin/view/Codev/DailyBuildAndSmokeTest
+
+=cut
