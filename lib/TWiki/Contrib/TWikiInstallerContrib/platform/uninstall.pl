4251bd7582bfcc32d57895ebbc8ab9c63a4cc142
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-05-26 09:40:13 +0000

    quieter uninstall
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@5721 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Contrib/TWikiInstallerContrib/platform/uninstall.pl
+++ lib/TWiki/Contrib/TWikiInstallerContrib/platform/uninstall.pl
@@ -4,7 +4,7 @@ use strict;
 use File::Path qw( rmtree );
 
 print `chmod -f -R a+rwx cgi-bin/lib/CPAN`;
-rmtree( [ qw( bin/ cpan/ downloads/ config/ cgi-bin/twiki/ cgi-bin/lib/ cgi-bin/tmp/ htdocs/twiki/ ) ], 1 );
+rmtree( [ qw( bin/ cpan/ downloads/ config/ cgi-bin/twiki/ cgi-bin/lib/ cgi-bin/tmp/ htdocs/twiki/ ) ] );
 unlink qw( install_twiki.cgi cgi-bin/install_twiki.cgi pre-twiki.pl post-twiki.pl un-twiki.pl );
 unlink qw( pre-twiki.log );
 unlink qw( README );
