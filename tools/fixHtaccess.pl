5143d06d1468032ef68c20dde85a5346b78992af
MartinCleaver <MartinCleaver@a00a5322-12db-0310-a70b-8735589c885e>
2005-06-25 15:14:20 +0000

    FixUpHtaccess: updated the script to write to the new format; minor mod to .htaccess.txt as names must be both preceded by 'user' and quoted
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@4429 a00a5322-12db-0310-a70b-8735589c885e

--- tools/fixHtaccess.pl
+++ tools/fixHtaccess.pl
@@ -1,11 +1,12 @@
 #! /usr/bin/perl -w
 # See http://twiki.org/cgi-bin/view/Codev/FixUpHtaccess
 
-my ($filePathToTwiki, $urlPathToTwiki) = getParams(@ARGV); 
-# e.g. ("/home/mrjc/wikiconsulting.com/twiki", "http://wikiconsulting.com/twiki");
+my ($filePathToTwiki, $urlPathToTwiki, $adminUsers) = getParams(@ARGV); 
+# e.g. ("/home/mrjc/wikiconsulting.com/twiki", "wikiconsulting.com/twiki");
 
 my %patterns = ("!FILE_path_to_TWiki!" => $filePathToTwiki,
-		"!URL_path_to_TWiki!" => $urlPathToTwiki);
+		"!URL_path_to_TWiki!" => $urlPathToTwiki,
+	        "!ADMIN_users!" => $adminUsers);
 
 unless (-d "bin") {
   die "This must be run in the top level bin directory";
@@ -21,7 +22,7 @@ close $htaccessTXTfh;
 
 foreach my $key (keys %patterns) {
     my $value = $patterns{$key};
-#    print "replacing $key with $value\n";
+    print "Replacing $key with $value\n";
     $file =~ s/$key/$value/g;
 }
 
@@ -33,34 +34,47 @@ close $htaccessFh;
 print `chmod og-w bin bin/*`;
 
 sub getParams {
-  my ($filePathToTwiki, $urlPathToTwiki)  = @_;
+  my ($filePathToTwiki, $urlPathToTwiki, $adminUsers)  = @_;
 
   unless ($filePathToTwiki) {
-    die "FILE missing ".usage();
+    die "ERROR: filePathToTwiki missing\n".usage();
   }
   unless ($urlPathToTwiki) {
-    die "URL missing: ".usage();
+    die "ERROR: urlPathToTwiki missing\n".usage();
   }
 
+  unless ($adminUsers) {
+    die "ERROR: adminUsers missing\n".usage();
+  } 
 
   unless (-d $filePathToTwiki) {
     die "The directory for filePathToTwiki '$filePathToTwiki' does not exist";
   }
 
-
   if ($urlPathToTwiki =~ m/^http/) {
     die "The URL for urlPathToTwiki '$urlPathToTwiki' must not start with http";
   }
 
-  return  ($filePathToTwiki, $urlPathToTwiki);
+  return  ($filePathToTwiki, $urlPathToTwiki, $adminUsers);
 }
 
 sub usage {
   my $ans = <<EOM;
-fixHtaccess.pl \$filePathToTwiki \$urlPathToTwiki
+Usage
+=====
+
+fixHtaccess.pl \$filePathToTwiki \$urlPathToTwiki \$adminUsers
+
+filePathToTwiki = location on the disk for TWiki root dir
+urlPathToTwiki  = base URL of TWiki when accessed by web 
+adminUsers      = which .htpasswd users can access configure
 
 e.g.
-fixHtaccess.pl /home/account/wikiconsulting.com/twiki http://wikiconsulting.com/twiki
+fixHtaccess.pl /home/account/wikiconsulting.com/twiki wikiconsulting.com/twiki YourAdminLoginName
+
+Note that you are responsible for putting an Admin LoginName into .htpasswd.
+On UNIX you can use the htpasswd tool for this.
+
 EOM
   return $ans.".";
 }
