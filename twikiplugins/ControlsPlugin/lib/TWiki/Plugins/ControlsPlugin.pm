dd333b5ef3d737a7398784ee0c406a733a86c0ba
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-06-27 02:41:29 +0000

    initial import from http://twiki.org/p/pub/Plugins/ControlsPlugin/ControlsPlugin.zip
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5750 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/ControlsPlugin/lib/TWiki/Plugins/ControlsPlugin.pm
@@ -0,0 +1,414 @@
+# Plugin for TWiki Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
+# Copyright (C) 2001-2004 Peter Thoeny, peter@thoeny.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
+#
+# =========================
+#
+#
+# Each plugin is a package that may contain these functions:        VERSION:
+#
+#   earlyInitPlugin         ( )                                     1.020
+#   initPlugin              ( $topic, $web, $user, $installWeb )    1.000
+#   initializeUserHandler   ( $loginName, $url, $pathInfo )         1.010
+#   registrationHandler     ( $web, $wikiName, $loginName )         1.010
+#   beforeCommonTagsHandler ( $text, $topic, $web )                 1.024
+#   commonTagsHandler       ( $text, $topic, $web )                 1.000
+#   afterCommonTagsHandler  ( $text, $topic, $web )                 1.024
+#   startRenderingHandler   ( $text, $web )                         1.000
+#   outsidePREHandler       ( $text )                               1.000
+#   insidePREHandler        ( $text )                               1.000
+#   endRenderingHandler     ( $text )                               1.000
+#   beforeEditHandler       ( $text, $topic, $web )                 1.010
+#   afterEditHandler        ( $text, $topic, $web )                 1.010
+#   beforeSaveHandler       ( $text, $topic, $web )                 1.010
+#   afterSaveHandler        ( $text, $topic, $web, $errors )        1.020
+#   renderFormFieldForEditHandler( $name, $type, $size, $value, $attributes, $possibleValues)
+#   writeHeaderHandler      ( $query )                              1.010  Use only in one Plugin
+#   redirectCgiQueryHandler ( $query, $url )                        1.010  Use only in one Plugin
+#   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
+#   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
+#
+# initPlugin is required, all other are optional. 
+# For increased performance, unused handlers are disabled. To
+# enable a handler remove the leading DISABLE_ from the function
+# name. Remove disabled handlers you do not need.
+#
+# NOTE: To interact with TWiki use the official TWiki functions 
+# in the TWiki::Func module. Do not reference any functions or
+# variables elsewhere in TWiki!!
+
+
+# =========================
+package TWiki::Plugins::ControlsPlugin;
+
+# =========================
+use vars qw(
+        $web $topic $user $installWeb $VERSION $pluginName
+        $debug
+    );
+
+$VERSION = '1.021';
+$pluginName = 'ControlsPlugin';  # Name of this Plugin
+
+# =========================
+sub initPlugin
+{
+    ( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $TWiki::Plugins::VERSION < 1.021 ) {
+        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+        return 0;
+    }
+
+    # Get plugin debug flag
+    $debug = TWiki::Func::getPluginPreferencesFlag( "CONTROLSPLUGIN_DEBUG" );
+
+    # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
+    #$exampleCfgVar = TWiki::Func::getPluginPreferencesValue( "EXAMPLE" ) || "default";
+
+    # Plugin correctly initialized
+    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+    return 1;
+}
+
+# =========================
+sub DISABLE_earlyInitPlugin
+{
+### Remove DISABLE_ for a plugin that requires early initialization, that is expects to have
+### initializeUserHandler called before initPlugin, giving the plugin a chance to set the user
+### See SessionPlugin for an example of this.
+    return 1;
+}
+
+
+# =========================
+sub DISABLE_initializeUserHandler
+{
+### my ( $loginName, $url, $pathInfo ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::initializeUserHandler( $_[0], $_[1] )" ) if $debug;
+
+    # Allows a plugin to set the username based on cookies. Called by TWiki::initialize.
+    # Return the user name, or "guest" if not logged in.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_registrationHandler
+{
+### my ( $web, $wikiName, $loginName ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::registrationHandler( $_[0], $_[1] )" ) if $debug;
+
+    # Allows a plugin to set a cookie at time of user registration.
+    # Called by the register script.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_beforeCommonTagsHandler
+{
+### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::beforeCommonTagsHandler( $_[2].$_[1] )" ) if $debug;
+
+    # Called at the beginning of TWiki::handleCommonTags (for cache Plugins use only)
+}
+
+# =========================
+sub commonTagsHandler
+{
+### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+
+    # This is the place to define customized tags and variables
+    # Called by TWiki::handleCommonTags, after %INCLUDE:"..."%
+
+    # do custom extension rule, like for example:
+    $_[0] =~ s/%CONTROL{(.*?)}%/handleControls( $_[2], $_[1], $1 )/ge;
+}
+
+# =========================
+sub DISABLE_afterCommonTagsHandler
+{
+### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::afterCommonTagsHandler( $_[2].$_[1] )" ) if $debug;
+
+    # Called at the end of TWiki::handleCommonTags (for cache Plugins use only)
+}
+
+# =========================
+sub DISABLE_startRenderingHandler
+{
+### my ( $text, $web ) = @_;   # do not uncomment, use $_[0], $_[1] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::startRenderingHandler( $_[1] )" ) if $debug;
+
+    # This handler is called by getRenderedVersion just before the line loop
+
+    # do custom extension rule, like for example:
+    # $_[0] =~ s/old/new/g;
+}
+
+# =========================
+sub DISABLE_outsidePREHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
+
+    ##TWiki::Func::writeDebug( "- ${pluginName}::outsidePREHandler( $renderingWeb.$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion, once per line, before any changes,
+    # for lines outside <pre> and <verbatim> tags. 
+    # Use it to define customized rendering rules.
+    # Note: This is an expensive function to comment out.
+    # Consider startRenderingHandler instead
+
+    # do custom extension rule, like for example:
+    # $_[0] =~ s/old/new/g;
+}
+
+# =========================
+sub DISABLE_insidePREHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
+
+    ##TWiki::Func::writeDebug( "- ${pluginName}::insidePREHandler( $web.$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion, once per line, before any changes,
+    # for lines inside <pre> and <verbatim> tags. 
+    # Use it to define customized rendering rules.
+    # Note: This is an expensive function to comment out.
+    # Consider startRenderingHandler instead
+
+    # do custom extension rule, like for example:
+    # $_[0] =~ s/old/new/g;
+}
+
+# =========================
+sub DISABLE_endRenderingHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::endRenderingHandler( $web.$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion just after the line loop, that is,
+    # after almost all XHTML rendering of a topic. <nop> tags are removed after this.
+
+}
+
+# =========================
+sub DISABLE_beforeEditHandler
+{
+### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::beforeEditHandler( $_[2].$_[1] )" ) if $debug;
+
+    # This handler is called by the edit script just before presenting the edit text
+    # in the edit box. Use it to process the text before editing.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_afterEditHandler
+{
+### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::afterEditHandler( $_[2].$_[1] )" ) if $debug;
+
+    # This handler is called by the preview script just before presenting the text.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_beforeSaveHandler
+{
+### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::beforeSaveHandler( $_[2].$_[1] )" ) if $debug;
+
+    # This handler is called by TWiki::Store::saveTopic just before the save action.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_afterSaveHandler
+{
+### my ( $text, $topic, $web, $error ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::afterSaveHandler( $_[2].$_[1] )" ) if $debug;
+
+    # This handler is called by TWiki::Store::saveTopic just after the save action.
+    # New hook in TWiki::Plugins $VERSION = '1.020'
+
+}
+
+# =========================
+sub DISABLE_writeHeaderHandler
+{
+### my ( $query ) = @_;   # do not uncomment, use $_[0] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::writeHeaderHandler( query )" ) if $debug;
+
+    # This handler is called by TWiki::writeHeader, just prior to writing header. 
+    # Return a single result: A string containing HTTP headers, delimited by CR/LF
+    # and with no blank lines. Plugin generated headers may be modified by core
+    # code before they are output, to fix bugs or manage caching. Plugins should no
+    # longer write headers to standard output.
+    # Use only in one Plugin.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_redirectCgiQueryHandler
+{
+### my ( $query, $url ) = @_;   # do not uncomment, use $_[0], $_[1] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::redirectCgiQueryHandler( query, $_[1] )" ) if $debug;
+
+    # This handler is called by TWiki::redirect. Use it to overload TWiki's internal redirect.
+    # Use only in one Plugin.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_getSessionValueHandler
+{
+### my ( $key ) = @_;   # do not uncomment, use $_[0] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::getSessionValueHandler( $_[0] )" ) if $debug;
+
+    # This handler is called by TWiki::getSessionValue. Return the value of a key.
+    # Use only in one Plugin.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_setSessionValueHandler
+{
+### my ( $key, $value ) = @_;   # do not uncomment, use $_[0], $_[1] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::setSessionValueHandler( $_[0], $_[1] )" ) if $debug;
+
+    # This handler is called by TWiki::setSessionValue. 
+    # Use only in one Plugin.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+=pod
+---++ sub handleControls ( $web, $topic, $parameterString ) ==> $value
+| $web | web and  |
+| $topic | topic to display the name for |
+| $parameterString | twiki parameter string  |
+
+=cut
+sub handleControls
+{
+    my( $theWeb, $theTopic, $theArgs ) = @_;
+
+    my %params = TWiki::extractParameters( $theArgs );
+
+    my $name   = $params{"_DEFAULT"} || $params{"name"} || "";
+    my $web    = $params{"web"}   || $theWeb;
+    my $topic  = $params{"topic"} || $theTopic;
+    my $size   = $params{"size"} || 1;
+    my $type   = $params{"type"} || "select";
+    my $url   = $params{"urlparam"} || "off";
+
+    return getListOfFieldValues($web, $topic, $name, $type, $size, $url );
+}
+
+# ============================
+# Get list of possible field values
+# If topic contains Web this overrides webName
+=pod
+
+---++ sub getListOfFieldValues (  $webName, $topic, $name, $type, $size  )
+
+Not yet documented.
+
+=cut
+
+sub getListOfFieldValues
+{
+    my( $webName, $topic, $name, $type, $size ) = @_;
+    
+    if( $topic =~ /^(.*)\.(.*)$/ ) {
+        $webName = $1;
+        $topic = $2;
+    }
+    my @posValues = ();
+
+    if( &TWiki::Store::topicExists( $webName, $topic ) ) {
+      my( $meta, $text ) = &TWiki::Store::readTopic( $webName, $topic );
+      # Processing of SEARCHES for Lists
+      $text =~ s/%SEARCH{(.*?)}%/&TWiki::handleSearchWeb($1)/geo;
+      @posValues = &TWiki::Form::getPossibleFieldValues( $text );
+    }
+
+    my $value = "";
+    $type = $type || "select";
+    $size = $size || 1;
+
+    if( $type eq "select" ) {
+      my $val = ($url eq "on")?"<option>%URLPARAM{\"$name\"}%</option>":"";
+      foreach my $item (@posValues) {
+	$item =~ s/<nop/&lt\;nop/go;
+	$val .= "   <option>$item</option>";
+      }
+      $value = "<select name=\"$name\" size=\"$size\">$val</select>";
+    } elsif( $type =~ "^checkbox" ) {
+      my $val ="<table  cellspacing=\"0\" cellpadding=\"0\"><tr>";
+      my $lines = 0;
+      foreach my $item ( @posValues ) {
+	my $expandedItem = &TWiki::handleCommonTags( $item, $topic );
+	$val .= "\n<td><input class=\"twikiEditFormCheckboxField\" type=\"checkbox\" name=\"$name$item\" />$expandedItem &nbsp;&nbsp;</td>";
+	if( $size > 0 && ($lines % $size == $size - 1 ) ) {
+	  $val .= "\n</tr><tr>";
+	}
+	$lines++;
+      }
+      $val =~ s/\n<\/tr><tr>$//;
+      $value = "$val\n</tr></table>\n";
+    } elsif( $type eq "radio" ) {
+      my $val = "<table  cellspacing=\"0\" cellpadding=\"0\"><tr>";
+      my $lines = 0;
+      foreach my $item ( @posValues ) {
+	my $expandedItem = &TWiki::handleCommonTags( $item, $topic );
+	$val .= "\n<td><input class=\"twikiEditFormRadioField twikiRadioButton\" type=\"radio\" name=\"$name\" value=\"$item\" />$expandedItem &nbsp;&nbsp;</td>";
+	if( $size > 0 && ($lines % $size == $size - 1 ) ) {
+	  $val .= "\n</tr><tr>";
+	}
+	$lines++;
+      }
+      $val =~ s/\n<\/tr><tr>$//;
+      $value = "$val\n</tr></table>\n";
+    }
+    return $value;
+}
+
+1;
