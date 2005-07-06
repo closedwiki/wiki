8fb1abee7e133ad1a79daa1a2631b81a8e330d4d
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2005-07-06 20:21:22 +0000

    TWikiSource: made autogeneration notes more generic (for not just on develop.twiki.org)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@4540 a00a5322-12db-0310-a70b-8735589c885e

--- tools/gendocs.pl
+++ tools/gendocs.pl
@@ -52,7 +52,7 @@ find( \&eachfile, ( $Config->{root} ));
 my $i = <<__TOPIC__;
 ---+!! TWiki Source Code Packages
 
-%X% This documentation is automatically generated from the =pod=, so always matches the code running on develop.twiki.org (which is the latest code to the nearest 5 minutes)
+%X% This documentation is automatically generated from the =pod=, so it always matches the running code
 
 Wherever you see a smell, your help is needed to get rid of it!
 
