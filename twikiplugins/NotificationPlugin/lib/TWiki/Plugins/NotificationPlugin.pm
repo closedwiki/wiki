a78d5b43b11d7f67f2e2e47d62e358c8b253e040
wbniv <wbniv@a00a5322-12db-0310-a70b-8735589c885e>
2005-07-19 05:37:25 +0000

    sync with http://twiki.org/p/pub/Plugins/NotificationPlugin/NotificationPlugin.zip (v1.14)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5836 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/NotificationPlugin/lib/TWiki/Plugins/NotificationPlugin.pm
+++ twikiplugins/NotificationPlugin/lib/TWiki/Plugins/NotificationPlugin.pm
@@ -56,7 +56,6 @@ use TWiki::Store;
 use vars qw(
         $web $topic $user $installWeb $VERSION $pluginName
         $sender @users $debug @sections
-	%paramDefaults $peopleWeb
     );
 
 $VERSION = '1.14';
@@ -73,7 +72,6 @@ sub initPlugin
         return 0;
     }
 
-    $peopleWeb = 'People'; #TWiki::Func::getMainWebname();
     @users = getUsers();
     @sections = (
       "(Topic) immediate notifications",
@@ -83,41 +81,137 @@ sub initPlugin
       "(Web) notifications",
       "(Regex) notifications"
     );
-
+    
     $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" ) || 0;
-    $sender = TWiki::Func::expandCommonVariables( TWiki::Func::getPreferencesValue( "\U$pluginName\E_SENDER" ),
-						  $topic, $web ) || "TWiki NotificationPlugin";
-
-    %paramDefaults = (
-		      popup       => 'on',
-		      style       => TWiki::Func::getPreferencesValue( "\U$pluginName\E_NTFSTYLE" ) || "buttons",
-		      buttonsep   => TWiki::Func::getPreferencesValue( "\U$pluginName\E_BUTTONSEP" ) || "&nbsp;",
-		      labelstyle  => 'default',
-		      value	  => 'text',
-		      border      => 1,
-		      cellspacing => 0,
-		      cellpadding => 1,
-		      align       => 'center',
-		      optional	  => '' # what's this for?
-		     );
-
+    $sender = TWiki::Func::getPreferencesValue( "\U$pluginName\E_SENDER" ) || "TWiki NotificationPlugin";
+    
     # Plugin correctly initialized
     TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
     return 1;
 }
 
+# =========================
+sub DISABLE_initializeUserHandler
+{
+### my ( $loginName, $url, $pathInfo ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::initializeUserHandler( $_[0], $_[1] )" ) if $debug;
+
+    # Allows a plugin to set the username based on cookies. Called by TWiki::initialize.
+    # Return the user name, or "guest" if not logged in.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_registrationHandler
+{
+### my ( $web, $wikiName, $loginName ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::registrationHandler( $_[0], $_[1] )" ) if $debug;
+
+    # Allows a plugin to set a cookie at time of user registration.
+    # Called by the register script.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
 sub commonTagsHandler
-  {
+{
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-  TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )"
- ) if $debug;
+    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
 
     # This is the place to define customized tags and variables
     # Called by sub handleCommonTags, after %INCLUDE:"..."%
 
     # do custom extension rule, like for example:
-    $_[0] =~ s/%NTF{(.*?)}%/&showNotifyButtons($1,$_[1],$_[2])/ge;
+    $_[0] =~ s/%NTF{(.*?)}%/&showNotifyButtons($1)/ge;
+}
+
+# =========================
+sub DISABLE_startRenderingHandler
+{
+### my ( $text, $web ) = @_;   # do not uncomment, use $_[0], $_[1] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::startRenderingHandler( $_[1] )" ) if $debug;
+
+    # This handler is called by getRenderedVersion just before the line loop
+
+    # do custom extension rule, like for example:
+    # $_[0] =~ s/old/new/g;
+}
+
+# =========================
+sub DISABLE_outsidePREHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
+
+    ##TWiki::Func::writeDebug( "- ${pluginName}::outsidePREHandler( $renderingWeb.$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion, once per line, before any changes,
+    # for lines outside <pre> and <verbatim> tags. 
+    # Use it to define customized rendering rules.
+    # Note: This is an expensive function to comment out.
+    # Consider startRenderingHandler instead
+
+    # do custom extension rule, like for example:
+    # $_[0] =~ s/old/new/g;
+}
+
+# =========================
+sub DISABLE_insidePREHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
+
+    ##TWiki::Func::writeDebug( "- ${pluginName}::insidePREHandler( $web.$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion, once per line, before any changes,
+    # for lines inside <pre> and <verbatim> tags. 
+    # Use it to define customized rendering rules.
+    # Note: This is an expensive function to comment out.
+    # Consider startRenderingHandler instead
+
+    # do custom extension rule, like for example:
+    # $_[0] =~ s/old/new/g;
+}
+
+# =========================
+sub DISABLE_endRenderingHandler
+{
+### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::endRenderingHandler( $web.$topic )" ) if $debug;
+
+    # This handler is called by getRenderedVersion just after the line loop, that is,
+    # after almost all XHTML rendering of a topic. <nop> tags are removed after this.
+
+}
+
+# =========================
+sub DISABLE_beforeEditHandler
+{
+### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::beforeEditHandler( $_[2].$_[1] )" ) if $debug;
+
+    # This handler is called by the edit script just before presenting the edit text
+    # in the edit box. Use it to process the text before editing.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_afterEditHandler
+{
+### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::afterEditHandler( $_[2].$_[1] )" ) if $debug;
+
+    # This handler is called by the preview script just before presenting the text.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
 }
 
 # =========================
@@ -135,17 +229,73 @@ sub beforeSaveHandler
   push( @notifyUsers, getUsersToNotify( $_[2], $_[1], 0 ) );
   push( @notifyUsers, getUsersToNotify( $_[2], $_[1], 1 ) );
   push( @notifyUsers, getUsersToNotify( $_[2], $_[1], 2 ) );
-#  &TWiki::Func::writeDebug( "COUNT = $#notifyUsers" );
+  #&TWiki::Func::writeDebug( "COUNT = $#notifyUsers" );
   my $subject = "Topic $_[2].$_[1] has been changed by $wikiUser.";
-  my $body = "Topic ".&TWiki::getViewUrl( $_[2], $_[1] )." has been changed by $wikiUser at " . TWiki::Func::formatTime( time(), '', "gmtime" ) . " GMT";
+  my $body = "Topic ".&TWiki::getScriptUrl( $_[2], $_[1], "view" )." has been changed by $wikiUser at " . &TWiki::formatGmTime( time() ) . " GMT";
   notifyUsers( \@notifyUsers, $subject, $body );
+}
+
+# =========================
+sub DISABLE_writeHeaderHandler
+{
+### my ( $query ) = @_;   # do not uncomment, use $_[0] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::writeHeaderHandler( query )" ) if $debug;
+
+    # This handler is called by TWiki::writeHeader, just prior to writing header. 
+    # Return a single result: A string containing HTTP headers, delimited by CR/LF
+    # and with no blank lines. Plugin generated headers may be modified by core
+    # code before they are output, to fix bugs or manage caching. Plugins should no
+    # longer write headers to standard output.
+    # Use only in one Plugin.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_redirectCgiQueryHandler
+{
+### my ( $query, $url ) = @_;   # do not uncomment, use $_[0], $_[1] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::redirectCgiQueryHandler( query, $_[1] )" ) if $debug;
+
+    # This handler is called by TWiki::redirect. Use it to overload TWiki's internal redirect.
+    # Use only in one Plugin.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_getSessionValueHandler
+{
+### my ( $key ) = @_;   # do not uncomment, use $_[0] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::getSessionValueHandler( $_[0] )" ) if $debug;
+
+    # This handler is called by TWiki::getSessionValue. Return the value of a key.
+    # Use only in one Plugin.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
+}
+
+# =========================
+sub DISABLE_setSessionValueHandler
+{
+### my ( $key, $value ) = @_;   # do not uncomment, use $_[0], $_[1] instead
+
+    TWiki::Func::writeDebug( "- ${pluginName}::setSessionValueHandler( $_[0], $_[1] )" ) if $debug;
+
+    # This handler is called by TWiki::setSessionValue. 
+    # Use only in one Plugin.
+    # New hook in TWiki::Plugins $VERSION = '1.010'
+
 }
 
 # =========================
 sub getUsers {
   my @result;
-  #&TWiki::Func::writeDebug( &TWiki::Func::getDataDir()."/".$peopleWeb );
-  if ( opendir( DIR, &TWiki::Func::getDataDir()."/".$peopleWeb ) ) {
+  #&TWiki::Func::writeDebug( &TWiki::Func::getDataDir()."/".&TWiki::Func::getMainWebname() );
+  if ( opendir( DIR, &TWiki::Func::getDataDir()."/".&TWiki::Func::getMainWebname() ) ) {
     my @topics = grep( /NotifyList.*txt$/, readdir( DIR ) );
     foreach my $name ( @topics ) {
       $name =~ /^(.*?)NotifyList/;
@@ -164,7 +314,7 @@ sub getUsersToNotify {
   #&TWiki::Func::writeDebug( "TYPE = $type" );
   foreach my $tmp ( @users ) {
     #&TWiki::Func::writeDebug( "TMP = $tmp" );
-    my $text = &TWiki::Store::readTopic( $peopleWeb, "$tmp"."NotifyList" );
+    my $text = &TWiki::Store::readTopic( &TWiki::Func::getMainWebname(), "$tmp"."NotifyList" );
     my $test = "";
     foreach my $line ( split( /\n/, $text ) ) {
       $line =~ s/\s+$//;
@@ -186,9 +336,7 @@ sub getUsersToNotify {
         }
       } elsif ( $test eq "Regex" ) {
         $line =~ /\s*\*\s(.*)/;
-	my $pat = eval{ qr($1) };
-	next if $@;
-        if ( "$tweb.$ttopic" =~ /$pat/ ) {
+        if ( "$tweb.$ttopic" =~ /$1/ ) {
           $result[++$#result] = $tmp;
           last;
       }
@@ -324,24 +472,24 @@ sub checkUserNotifyList {
   my $tmpText;
   my $tmpMeta;
   #&TWiki::Func::writeDebug( "NTF:checkUserNotifyList: WHO = $who" );
-  if ( !&TWiki::Store::topicExists( $peopleWeb, $who."NotifyList" ) ) {
+  if ( !&TWiki::Store::topicExists( "Main", $who."NotifyList" ) ) {
     &TWiki::Func::writeDebug( "TEST1" );
-    ( $tmpMeta, $tmpText ) = &TWiki::Store::readTopic( $peopleWeb, "NotificationPluginListTemplate" );
+    ( $tmpMeta, $tmpText ) = &TWiki::Store::readTopic( "Main", "NotificationPluginListTemplate" );
     $tmpMeta->put( "TOPICPARENT", ( "name" => $who ) );
     saveUserNotifyList( $who, $tmpMeta, $tmpText );
   } else {
-    ( $tmpMeta, $tmpText ) = &TWiki::Store::readTopic( $peopleWeb, $who."NotifyList" );
+    ( $tmpMeta, $tmpText ) = &TWiki::Store::readTopic( "Main", $who."NotifyList" );
   }
   return ( $tmpMeta, $tmpText );
 }
 
 sub saveUserNotifyList {
   my ( $who, $meta, $text ) = @_;
-  #&TWiki::Func::writeDebug( "NTF:saveUserNotifyList: Saving $peopleWeb.".$who."NotifyList topic..." );
+  #&TWiki::Func::writeDebug( "NTF:saveUserNotifyList: Saving Main.".$who."NotifyList topic..." );
   $text =~ s/   /\t/g;
   my $repRev = "repRev";
-  $repRev = "" if ( !&TWiki::Store::topicExists( $peopleWeb, $who."NotifyList" ) );
-  my $error = &TWiki::Store::saveTopic( $peopleWeb, $who."NotifyList", $text, $meta, $repRev, "checked", "checked" );
+  $repRev = "" if ( !&TWiki::Store::topicExists( "Main", $who."NotifyList" ) );
+  my $error = &TWiki::Store::saveTopic( "Main", $who."NotifyList", $text, $meta, $repRev, "checked", "checked" );
   if ( $error ) {
     my $url = &TWiki::getOopsUrl( $web, $topic, "oopssaveerr", $error );
     &TWiki::redirect( $query, $url );
@@ -362,178 +510,29 @@ sub isItemInSection {
 }
 
 sub showNotifyButtons {
-### my( $attr, $topic, $web ) = @_;     # do not uncomment, use $_[0], $_[1]... instead
-
-  return ""  if $TWiki::wikiName eq "TWikiGuest";
-
-  my %param = TWiki::Func::extractParameters( $_[0] );
-
-  foreach ( keys %paramDefaults )
-  {
-    $param{$_} = $paramDefaults{$_}
-      unless defined $param{$_};
-  }
-
-  my %flip = ( on => "OFF", off => "ON" );
-  my %icon = ( on  => '<img src="%PUBURL%/%TWIKIWEB%/TWikiDocGraphics/choice-yes.gif">',
-	       off => '<img src="%PUBURL%/%TWIKIWEB%/TWikiDocGraphics/choice-no.gif">',
-	     );
-
-
-  my @ufields = qw/ TIN WIN TN WN /;
-  my @fields  = qw/ tin win tn wn /;
-
-  my %attrs;
-
-  $attrs{uc($_)}{req} = $param{$_} || 'on' foreach @fields;
-
-  $attrs{TIN}{state} = isItemInSection( $TWiki::wikiName, "$web.$topic", 0 ) ? 'on' : 'off';
-  $attrs{WIN}{state} = isItemInSection( $TWiki::wikiName, "$web",        1 ) ? 'on' : 'off';
-  $attrs{TN}{state}  = isItemInSection( $TWiki::wikiName, "$web.$topic", 3 ) ? 'on' : 'off';
-  $attrs{WN}{state}  = isItemInSection( $TWiki::wikiName, "$web",        4 ) ? 'on' : 'off';
-
-  $attrs{T}{label} = "Topic";
-  $attrs{W}{label} = "Web";
-  $attrs{TIN}{label} = "Topic immediate";
-  $attrs{WIN}{label} = "Web immediate";
-  $attrs{TN}{label}  = "Topic";
-  $attrs{WN}{label}  = "Web";
-
-  if ( $param{labelstyle} eq 'short' )
-  {
-    $attrs{$_}{label} = $_ foreach  @ufields;
-  }
-
-  if ( $param{value} eq 'text' )
-  {
-    $attrs{$_}{value} = $attrs{$_}{state} foreach @ufields;
-  }
-
-  else
-  {
-    $attrs{$_}{value} = $icon{$attrs{$_}{state}} foreach @ufields;
-  }
-
-
-  my $tableattr = join(' ', map { "$_ = '$param{$_}'" } qw/ border cellspacing cellpadding align / );
-
-  my $changeURL = &TWiki::getScriptUrl( $web, $topic, "changenotify" );
-
+  my $attrs = shift;
+  my ( $tin, $win, $tn, $wn, $popup ) = ( "on", "on", "on", "on", "on" );
+  my ( $tinOn, $winOn, $tnOn, $wnOn ) = ( "on", "on", "on", "on" );
+  my $opt = "";
+  my %tmp = ( "on" => "OFF", "off" => "ON" );
+  $tin = $1 if ( $attrs =~ /tin=\"(.*?)\"/ );
+  $win = $1 if ( $attrs =~ /win=\"(.*?)\"/ ); 
+  $tn = $1 if ( $attrs =~ /tn=\"(.*?)\"/ );
+  $wn = $1 if ( $attrs =~ /wn=\"(.*?)\"/ );
+  $popup = $1 if ( $attrs =~ /popup=\"(.*?)\"/ );
+  $opt = $1 if ( $attrs =~ /optional=\"(.*?)\"/ );
   my $text = "";
-
-  if ( $param{style} eq 'buttons' ) {
-    my @text;
-
-    push @text,
-      "<input onClick='javascript:window.open(\"$changeURL?popup=on\");' type='button' value='Popup'>"
-	if ( $param{popup} eq "on" );
-
-    for my $what ( qw/ TIN WIN TN WN / )
-    {
-      next unless $attrs{$what}{req} eq 'on';
-
-      push @text, sprintf("<input onClick='javascript:location.href='%s?what=%s&action=%s&%s;' type='button' value='%s %s' title='%s notification: Click to set it !'>",
-			  $changeURL, $what, $flip{$attrs{$what}{state}}, $param{optional}, $what, 
-			  $attrs{$what}{value}, $attrs{$what}{label}, $flip{$attrs{$what}{state}} );
-    }
-
-    $text = join( $param{buttonsep}, @text );
+  if ( $TWiki::wikiName ne "TWikiGuest" ) {
+    $tinOn = "off" if ( !isItemInSection( $TWiki::wikiName, "$web.$topic", 0 ) );
+    $winOn = "off" if ( !isItemInSection( $TWiki::wikiName, "$web", 1 ) );
+    $tnOn = "off" if ( !isItemInSection( $TWiki::wikiName, "$web.$topic", 3 ) );
+    $wnOn = "off" if ( !isItemInSection( $TWiki::wikiName, "$web", 4 ) );
+    $text .= "<input onClick='javascript:window.open(\"".&TWiki::getScriptUrl( $web, $topic, "changenotify" )."?popup=on\");' type='button' value='Popup'>&nbsp;" if ( $popup eq "on" );
+    $text .= "<input onClick='javascript:location.href(\"".&TWiki::getScriptUrl( $web, $topic, "changenotify" )."?what=TIN&action=$tmp{$tinOn}&$opt\");' type='button' value='TIN $tinOn' title='Topic immediate notifications! Click to set it $tmp{$tinOn}!'>&nbsp;" if ( $tin eq "on" );
+    $text .= "<input onClick='javascript:location.href(\"".&TWiki::getScriptUrl( $web, $topic, "changenotify" )."?what=WIN&action=$tmp{$winOn}&$opt\");' type='button' value='WIN $winOn' title='Web immediate notifications! Click to set it $tmp{$winOn}!'>&nbsp;" if ( $win eq "on" );
+    $text .= "<input onClick='javascript:location.href(\"".&TWiki::getScriptUrl( $web, $topic, "changenotify" )."?what=TN&action=$tmp{$tnOn}&$opt\");' type='button' value='TN $tnOn' title='Topic notifications! Click to set it $tmp{$tnOn}!'>&nbsp;" if ( $tn eq "on" );
+    $text .= "<input onClick='javascript:location.href(\"".&TWiki::getScriptUrl( $web, $topic, "changenotify" )."?what=WN&action=$tmp{$wnOn}&$opt\");' type='button' value='WN $wnOn' title='Web notifications! Click to set it $tmp{$wnOn}!'>&nbsp;" if ( $wn eq "on" );
   }
-
-  elsif ( $param{style} eq 'twikitable' ) {
-
-    $text = "| *Notification* | *State* |\n";
-    for my $what ( qw/ TIN WIN TN WN / )
-    {
-      next unless $attrs{$what}{req} eq 'on';
-
-      $text .= sprintf("| *%s* | [[%s?what=%s&action=%s&%s'][%s]] |\n",
-			  $attrs{$what}{label}, $changeURL, $what, $flip{$attrs{$what}{state}}, $param{optional}, $attrs{$what}{value} );
-    }
-    $text = TWiki::Func::renderText($text, $_[2] );
-  }
-
-  elsif ( $param{style} eq 'twikitable2' ) {
-
-    $text = "|  | *IN* | *N* |\n";
-    for my $what ( qw/ T W / )
-    {
-      $text .= "| *$attrs{$what}{label}* | ";
-
-      for my $where ( qw/ IN N / )
-      {
-	my $ww = $what . $where;
-	$text .= sprintf("[[%s?what=%s&action=%s&%s][%s]] |",
-			 $changeURL, $ww, $flip{$attrs{$ww}{state}}, $param{optional}, $attrs{$ww}{value} );
-      }
-      $text .= "\n";
-    }
-  }
-
-  elsif ( $param{style} eq 'table' ) {
-
-    $text = "<table $tableattr>";
-
-    for my $what ( qw/ TIN WIN TN WN / )
-    {
-      next unless $attrs{$what}{req} eq 'on';
-
-      $text .= sprintf("<tr><td><strong>%s</strong></td><td><a href='%s?what=%s&action=%s&%s'>%s</a></td></tr>",
-			  $attrs{$what}{label}, $changeURL, $what, $flip{$attrs{$what}{state}}, $param{optional}, $attrs{$what}{value} );
-
-    }
-    $text .= "</table>\n";
-  }
-
-  elsif ( $param{style} eq 'compacttable' ) {
-    $text = "<table $tableattr>";
-
-    my $idx = 0;
-    for my $what ( qw/ TIN WIN TN WN / )
-    {
-      next unless $attrs{$what}{req} eq 'on';
-      $idx++;
-
-      $text .= '<tr>' if $idx%2 ;
-      $text .= sprintf("<td><strong>%s</strong></td><td><a href='%s?what=%s&action=%s&%s'>%s</a></td>",
-			  $attrs{$what}{label}, $changeURL, $what, $flip{$attrs{$what}{state}}, $param{optional}, $attrs{$what}{value} );
-      $text .= '</tr>' unless $idx%2;
-
-    }
-    $text .= "</table>\n";
-  }
-
-  elsif ( $param{style} eq 'wide' ) {
-    $text = "<table $tableattr><tr>";
-
-    for my $what ( qw/ TIN WIN TN WN / )
-    {
-      next unless $attrs{$what}{req} eq 'on';
-      $text .= sprintf("<td><strong>%s</strong></td><td><a href='%s?what=%s&action=%s&%s'>%s</a></td>",
-			  $attrs{$what}{label}, $changeURL, $what, $flip{$attrs{$what}{state}}, $param{optional}, $attrs{$what}{value} );
-    }
-    $text .= "</tr></table>\n";
-  }
-
-  elsif ( $param{style} eq 'compacttable2' ) {
-
-    $text = "<table $tableattr> <tr><th></th><th><strong>IN</strong></th><th><strong>N</strong></th></tr>";
-    for my $what ( qw/ T W / )
-    {
-      $text .= "<tr><td><strong>$attrs{$what}{label}</strong></td>";
-
-      for my $where ( qw/ IN N / )
-      {
-	my $ww = $what . $where;
-	$text .= sprintf("<td><a href='%s?what=%s&action=%s&%s'>%s</a></td>",
-			 $changeURL, $ww, $flip{$attrs{$ww}{state}}, $param{optional}, $attrs{$ww}{value} );
-      }
-      $text .= "</tr>";
-    }
-    $text .= "</table>";
-  }
-
-  $text = TWiki::Func::renderText($text, $_[2] );
   return $text;
 }
 
