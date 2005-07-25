a6688dd604add81557d3795c13c9e7542d6cf68d
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-07-25 06:38:23 +0000

    Item134: improvements to the script, so it handles differences to existing files in the core tree better
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@5907 a00a5322-12db-0310-a70b-8735589c885e

--- mklinks.sh
+++ mklinks.sh
@@ -1,30 +1,36 @@
 #!/bin/sh
-here=`pwd`/
-for dir in twikiplugins/*Plugin; do
-    plugin=`basename dir`
-    if [ -d twikiplugins/$plugin/pub/TWiki/$plugin ]; then
-        link=pub/TWiki/$plugin
-        if [ -L pub/TWiki/$plugin ]; then
+shopt -s nullglob
+function mklink () {
+    link=`echo $1 | sed -e 's#twikiplugins/[A-Za-z0-9]*Plugin/##'`
+    if [ -L $link ]; then
+        rm $link
+    fi
+    if [ -e $link ]; then
+        x=`diff -q $1 $link`
+        if [ "$x" = "" ]; then
             rm $link
+        else
+            echo "$1 and $link differ - Keeping $link intact"
         fi
-        ln -s $heretwikiplugins/$plugin/pub/TWiki/$plugin $link
+    else
+        ln -s `pwd`/$1 $link
+    fi
+}
+
+for dir in twikiplugins/*Plugin; do
+    plugin=`basename $dir`
+    if [ -d twikiplugins/$plugin/pub/TWiki/$plugin ]; then
+        mklink twikiplugins/$plugin/pub/TWiki/$plugin
     fi
     if [ -d twikiplugins/$plugin/data/TWiki ]; then
         for txt in twikiplugins/$plugin/data/TWiki/*.txt; do
-            link=data/TWiki/`basename $txt`
-            if [ -L $link ]; then
-                rm $link
-            fi
-            ln -s $here/$txt $link
+            mklink $txt
         done
     fi
     if [ -d twikiplugins/$plugin/templates ]; then
         for tmpl in twikiplugins/$plugin/templates/*.tmpl; do
-            link=templates/`basename $tmpl`
-            if [ -L $link ]; then
-                rm $link
-            fi
-            ln -s $here$tmpl $link
+            mklink $tmpl
         done
     fi
 done
+
