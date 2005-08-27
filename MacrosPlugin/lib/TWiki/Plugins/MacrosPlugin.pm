77996e6d74287157f8a64bb8fa55b7cae2b932c0
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-08-27 01:52:17 +0000

    Item196: updates for MacrosPlugin
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@6173 a00a5322-12db-0310-a70b-8735589c885e

--- MacrosPlugin/lib/TWiki/Plugins/MacrosPlugin.pm
+++ MacrosPlugin/lib/TWiki/Plugins/MacrosPlugin.pm
@@ -6,9 +6,7 @@ use TWiki::Attrs;
 
 $VERSION = '1.010';
 
-my $pluginName = 'MacrosPlugin';  # Name of this Plugin
-my %macros;
-my %macro_times; # TimSlidel 1/4/04
+my $pluginName = 'MacrosPlugin';
 
 sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
@@ -23,33 +21,33 @@ sub initPlugin {
 }
 
 sub commonTagsHandler {
-  ### my ( $text, $topic, $web ) = @_;
+    ### my ( $text, $topic, $web ) = @_;
 
-  # First expand all macros and macro parameters
-  $_[0] =~ s/%CALLMACRO{(.*?)}%/&_callMacro($1,$_[2],$_[1])/ge;
+    # First expand all macros and macro parameters
+    $_[0] =~ s/%CALLMACRO{(.*?)}%/&_callMacro($1,$_[2],$_[1])/ge;
 
-  return unless ( $_[0] =~ m/%SET\s+/mo );
+    return unless ( $_[0] =~ m/%SET\s+/mo );
 
-  # Now process in order to ensure correct SET ordering
-  my %sets; # scope of this topic only
-  my $res;
+    # Now process in order to ensure correct SET ordering
+    my %sets; # scope of this topic only
+    my $res;
 
-  foreach my $block ( split( /\n%SET\s+/, "\n$_[0]" )) {
-    foreach my $set ( keys %sets ) {
-      $block =~ s/\%$set\%/$sets{$set}/g;
+    foreach my $block ( split( /\n%SET\s+/, "\n$_[0]" )) {
+        foreach my $set ( keys %sets ) {
+            $block =~ s/\%$set\%/$sets{$set}/g;
+        }
+
+        if ( $block =~ s/^(\w+)[ \t]*=[ \t]*([^\r\n]*)\r*\n//o ) {
+            my $setname = $1;
+            my $setval = $2;
+            $setval = TWiki::Func::expandCommonVariables( $setval, $topic, $web );
+            $sets{$setname} = $setval;
+            $block =~ s/\%$setname\%/$setval/g;
+        }
+        $res .= $block;
     }
-
-    if ( $block =~ s/^(\w+)[ \t]*=[ \t]*([^\r\n]*)\r*\n//o ) {
-      my $setname = $1;
-      my $setval = $2;
-      $setval = TWiki::Func::expandCommonVariables( $setval, $topic, $web );
-      $sets{$setname} = $setval;
-      $block =~ s/\%$setname\%/$setval/g;
-    }
-	$res .= $block;
-  }
-  $res =~ s/^\n//o;
-  $_[0] = $res;
+    $res =~ s/^\n//o;
+    $_[0] = $res;
 }
 
 # Expand a macro. The macro is identified by the 'topic' parameter
@@ -57,42 +55,31 @@ sub commonTagsHandler {
 # their values replaced into the macro and the expanded macro body
 # is returned.
 sub _callMacro {
-  my ( $params, $web, $topic ) = @_;
-  my $attrs = new TWiki::Attrs( $params );
-  my $mtop = $attrs->get( "topic" );
-  my $mweb = $web;
+    my ( $params, $web, $topic ) = @_;
+    my $attrs = new TWiki::Attrs( $params, 1 );
+    my $mtop = $attrs->{topic} || $attrs->{_DEFAULT};
+    my $mweb = $web;
 
-  if ( $mtop =~ s/^(.*)[\.\/](.*)$/$2/o ) {
-	$mweb = $1;
-  }
-  my $dataDir = TWiki::Func::getDataDir() . "/$mweb"; # TimSlidel 1/4/04
-  my $filename = "$dataDir/$mtop.txt"; # TimSlidel 1/4/04
+    return unless $mtop;
 
-  my @sinfo = stat( $filename );
-  my $time = $sinfo[9];
+    ( $mweb, $mtop ) = TWiki::Func::normalizeWebTopicName( $web, $mtop );
 
-  if ( !defined( $macros{$mtop} ) || $macro_times{$mtop} < $time ) {
-	if ( !TWiki::Func::topicExists( $mweb, $mtop )) {
-	  return " <font color=red> No such macro $mtop in CALLMACRO\{$params\} </font> ";
+    if ( !TWiki::Func::topicExists( $mweb, $mtop )) {
+        return " <font color=red> No such macro $mtop in CALLMACRO\{$params\} </font> ";
 	}
 	my ($meta, $text ) = TWiki::Func::readTopic( $mweb, $mtop );
-	$macro_times{$mtop} = $time;
-	$macros{$mtop} = $text;
-  }
 
-  my $m = $macros{$mtop};
+    foreach my $vbl ( keys %$attrs ) {
+        my $val = $attrs->get( $vbl );
+        $text =~ s/%$vbl%/$val/g;
+    }
 
-  foreach my $vbl ( keys %$attrs ) {
-	my $val = $attrs->get( $vbl );
-	$m =~ s/%$vbl%/$val/g;
-  }
+    $text =~ s/[\r\n]+//go if ( $text =~ s/%STRIP%//go );
 
-  $m =~ s/[\r\n]+//go if ( $m =~ s/%STRIP%//go );
+    # Recursive expansion
+    $text =~ s/%CALLMACRO{(.+?)}%/&_callMacro($1,$web,$topic)/geo;
 
-  # Recursive expansion
-  $m =~ s/%CALLMACRO{(.+?)}%/&_callMacro($1,$web,$topic)/geo;
-
-  return $m;
+    return $text;
 }
 
 1;
