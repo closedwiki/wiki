30320a80c32449cc6b4624377628ebf536875a97
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2005-08-29 20:49:33 +0000

    Item253: twikiEmulatedLink css properties could not be overwritten
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@6190 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/UI/Preview.pm
+++ lib/TWiki/UI/Preview.pm
@@ -90,8 +90,8 @@ sub preview {
     $dispText = $session->{renderer}->getRenderedVersion( $dispText, $web, $topic );
 
     # Disable links and inputs in the text
-    $dispText =~ s(<a\s[^>]*>(.*?)</a>)
-      (<span style="text-decoration:underline;color:blue"><span class="twikiEmulatedLink">$1</span></span>)gis;
+    $dispText =~ s(<a\s[^>]*>(.+?)</a>)
+      (<span class="twikiEmulatedLink">$1</span>)gis;
     $dispText =~ s/<(input|button|textarea) /<$1 disabled="disabled"/gis;
     $dispText =~ s(</?form(|\s.*?)>)()gis;
     $dispText =~ s/(<[^>]*\bon[A-Za-z]+=)('[^']*'|"[^"]*")/$1''/gis;
