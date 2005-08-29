c25ffc74df8507f731faa6c5964b9bc8e743192b
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2005-08-29 18:21:04 +0000

    Item298: added debug,verbose options; added documentation
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@6189 a00a5322-12db-0310-a70b-8735589c885e

--- test/tinderbox/rebuild-deploy-test-if-new.pl
+++ test/tinderbox/rebuild-deploy-test-if-new.pl
@@ -25,18 +25,29 @@ use FindBin;
 use Cwd;
 use Data::Dumper;
 use Getopt::Long;
+use Pod::Usage;
 
 use constant BUILD_LOCK => '.build';
 use constant LAST_BUILD => '.last_build';
 
 my $Config = {
     force => 0,
+# 
+    verbose => 0,
+    debug => 0,
+    help => 0,
+    man => 0,
 };
 
 my $result = GetOptions( $Config,
 #
 			 'force|f',
+# miscellaneous/generic options
+			'help', 'man', 'debug', 'verbose|v',
 			);
+pod2usage( 1 ) if $Config->{help};
+pod2usage({ -exitval => 1, -verbose => 2 }) if $Config->{man};
+print STDERR Dumper( $Config ) if $Config->{debug};
 
 # make easy to work as a crontab job
 chdir( $FindBin::Bin );
@@ -78,3 +89,49 @@ if ( $Config->{force} || $newVersionAvailable )
     # mark build complete
     rename BUILD_LOCK, LAST_BUILD;
 }
+
+exit 0;
+
+################################################################################
+################################################################################
+
+__DATA__
+=head1 NAME
+
+rebuild-deploy-test-if-new.pl - 
+
+=head1 SYNOPSIS
+
+rebuild-deploy-test-if-new.pl [options]
+
+crontab-compatible script used to perform the following:
+
+   * run the (unit) tests
+   * build a new twiki kernel
+   * build a new distribution
+   * publish the distribution
+   * install the distribution
+   * run the (golden html) tests
+   * post the test results to tinderbox.wbniv.wikihosting.com
+
+Copyright 2005 Will Norris.  All Rights Reserved.
+
+ Options:
+   -force                       force a new build, test, and install procedure even if current
+   -verbose
+   -debug
+   -help			this documentation
+   -man				full docs
+
+=head1 OPTIONS
+
+=over 8
+
+=back
+
+=head1 DESCRIPTION
+
+
+=head2 SEE ALSO
+
+=cut
