c661550bbab2d2e74b7d8dbbea69f003c54fe04c
RafaelAlvarez <RafaelAlvarez@a00a5322-12db-0310-a70b-8735589c885e>
2005-09-12 23:33:54 +0000

    Item196: First release of TwistyContrib and TwistyPlugin
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@6311 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/TwistyPlugin/lib/TWiki/Plugins/TwistyPlugin.pm
@@ -0,0 +1,103 @@
+# Plugin for TWiki Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
+# Copyright (C) 2001-2004 Peter Thoeny, peter@thoeny.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
+#
+
+=pod
+
+---+ package TwistyPlugin
+
+Convenience plugin for TWiki:Plugins.TwistyContrib.
+It has two major features:
+   * When active, the Twisty javascript library is included in every topic.
+   * Provides a convenience sintax to define twisty areas.
+
+
+=cut
+
+package TWiki::Plugins::TwistyPlugin;    # change the package name and $pluginName!!!
+
+use strict;
+
+use vars qw( $VERSION $pluginName $debug @modes);
+
+$VERSION = '1.000';
+$pluginName = 'TwistyPlugin';  # Name of this Plugin
+
+#there is no need to document this.
+sub initPlugin {
+    my( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $TWiki::Plugins::VERSION < 1.1 ) {
+        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+        return 0;
+    }
+
+    # Get plugin preferences, variables defined by:
+    #   * Set EXAMPLE = ...
+    $debug = TWiki::Func::getPluginPreferencesFlag( "DEBUG" );
+    my $header=<<'EOF'; 
+<style type="text/css" media="all">
+@import url("%PUBURL%/TWiki/TwistyContrib/twist.css");
+</style>
+
+<script type="text/javascript" src="%PUBURL%/TWiki/TwistyContrib/twist.js"></script>
+EOF
+
+    TWiki::Func::addToHEAD('TWISTYPLUGIN_TWISTY',$header);
+    TWiki::Func::registerTagHandler( 'EXAMPLETAG', \&_EXAMPLETAG );
+    TWiki::Func::registerTagHandler('TWISTYSHOW',\&_TWISTYSHOW);
+    TWiki::Func::registerTagHandler('TWISTYHIDE',\&_TWISTYHIDE);
+    TWiki::Func::registerTagHandler('TWISTYTOGGLE',\&_TWISTYTOGGLE);
+    TWiki::Func::registerTagHandler('ENDTWISTYTOGGLE',\&_ENDTWISTYTOGGLE);
+
+    return 1;
+}
+
+
+sub _TWISTYSHOW {
+    my($session, $params, $theTopic, $theWeb) = @_;
+    my $id=$params->{'id'}||'';
+    my $link=$params->{'link'}||'';
+    my $mode=$params->{'mode'}||'span';
+    return '<'.$mode.' id="'.$id.'show" class="twikiTwistMakeVisible"><a href="#" class="twikiTwistTrigger">'.$link.'</a></'.$mode.'>';
+}
+
+sub _TWISTYHIDE {
+    my($session, $params, $theTopic, $theWeb) = @_;
+    my $id=$params->{'id'}||'';
+    my $link=$params->{'link'}||'';
+    my $mode=$params->{'mode'}||'span';
+
+    return '<'.$mode.' id="'.$id.'hide" class="twikiTwistHidden twikiTwistMakeVisible"><a href="#" class="twikiTwistTrigger">'.$link.'</a></'.$mode.'>';
+}
+
+sub _TWISTYTOGGLE {
+    my($session, $params, $theTopic, $theWeb) = @_;
+    my $id=$params->{'id'}||'';
+    my $mode=$params->{'mode'}||'span';
+    unshift @modes,$mode;
+    return '<'.$mode.' id="'.$id.'toggle" class="twikiTwistMakeHidden">';
+}
+
+sub _ENDTWISTYTOGGLE {
+    my($session, $params, $theTopic, $theWeb) = @_;
+    my $mode=shift @modes;
+    return '</'.$mode.'>';
+}
+
+
+1;
