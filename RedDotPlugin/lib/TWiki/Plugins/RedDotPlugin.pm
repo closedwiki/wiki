4040272a5de25e3e3c547c131a48f9b1ad190bf6
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2005-09-16 15:28:27 +0000

    Item0: added redirect feature to jump back to origin after edit
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@6390 a00a5322-12db-0310-a70b-8735589c885e

--- RedDotPlugin/lib/TWiki/Plugins/RedDotPlugin.pm
+++ RedDotPlugin/lib/TWiki/Plugins/RedDotPlugin.pm
@@ -19,10 +19,10 @@ package TWiki::Plugins::RedDotPlugin;
 ###############################################################################
 use vars qw(
         $web $topic $user $installWeb $VERSION
-        $debug $styleLink $doneHeader
+        $debug $styleLink $doneHeader $hasInitRedirector
     );
 
-$VERSION = '0.91';
+$VERSION = '1.01';
 
 ###############################################################################
 sub writeDebug {
@@ -42,12 +42,16 @@ sub initPlugin {
     '" type="text/css" media="all" />';
     
   $doneHeader = 0;
+  $hasInitRedirector = 0;
   
   return 1;
 }
 
 ###############################################################################
 sub commonTagsHandler {
+
+  &initRedirector();
+
   $_[0] =~ s/%REDDOT%/&renderRedDot()/geo;
   $_[0] =~ s/%REDDOT{(.*?)}%/&renderRedDot($1)/geo;
 
@@ -55,6 +59,77 @@ sub commonTagsHandler {
     $_[0] =~ s/<head>(.*?[\r\n]+)/<head>$1$styleLink\n/) {
     $doneHeader = 1;
   }
+
+
+}
+
+###############################################################################
+sub initRedirector {
+
+  return if $hasInitRedirector;
+  $hasInitRedirector = 1;
+
+  writeDebug("called initRedirector");
+
+  my $query = &TWiki::Func::getCgiQuery();
+  return unless $query;
+
+  my $theAction = $query->url(-relative=>1); 
+  writeDebug("theAction=$theAction");
+
+  my $sessionKey = "REDDOT_REDIRECT_$web.$topic";
+
+  # init redirect
+  if ($theAction =~ /^edit/) {
+    writeDebug("found edit");
+    my $theRedirect = $query->param('redirect');
+    writeDebug("found theRedirect=$theRedirect") if $theRedirect;
+    if ($theRedirect && 
+	($theRedirect =~ /([A-Z]+[A-Za-z]+)\.([A-Za-z0-9.-]+)/ ||
+	$theRedirect =~ /([A-Za-z0-9.-]+)/)) {
+      &TWiki::Func::setSessionValue($sessionKey, $theRedirect);
+      writeDebug("init redirect to $theRedirect");
+    }
+  }
+
+  # execute redirect
+  elsif ($theAction =~ /^view/) { # only on view
+    writeDebug("found view");
+    my $theRedirect = &TWiki::Func::getSessionValue($sessionKey);
+    if ($theRedirect && $theRedirect ne '') {
+      &clearSessionValue($sessionKey);
+      &writeDebug("found theRedirect=$theRedirect in session");
+      my $redirectUrl; # get target
+      if ($theRedirect =~ /([A-Z]+[A-Za-z]+)\.([A-Za-z0-9.-]+)/) {
+	$redirectUrl = &TWiki::Func::getViewUrl($1,$2);
+      } elsif ($theRedirect =~ /([A-Za-z0-9.-]+)/) {
+	$redirectUrl = &TWiki::Func::getViewUrl($web,$1);
+      }
+      if ($redirectUrl && $redirectUrl ne &TWiki::Func::getViewUrl($web,$topic)) {
+	&writeDebug("redirecting to $redirectUrl");
+	&TWiki::Func::redirectCgiQuery($query,$redirectUrl);
+      }
+    }
+  }
+}
+
+###############################################################################
+# wrapper
+sub clearSessionValue {
+  my $key = shift;
+
+  # using dakar's client 
+  if (defined &TWiki::Client::clearSessionValue) {
+    return $TWiki::Plugins::SESSION->{client}->clearSessionValue($key);
+  }
+  
+  # using the SessionPlugin
+  if (defined &TWiki::Plugins::SessionPlugin::clearSessionValueHandler) {
+    return &TWiki::Plugins::SessionPlugin::clearSessionValueHandler($key);
+  }
+
+  # last resort
+  return &TWiki::Func::setSessionValue($key, undef);
 }
 
 ###############################################################################
@@ -68,6 +143,8 @@ sub renderRedDot {
   } else {
     $theWebTopics = "$web.$topic";
   }
+  my $theRedirect = &TWiki::Func::extractNameValuePair($args, 'redirect') 
+    || "$web.$topic";
 
   # find the first webtopic that we have access to
   my $theWeb;
@@ -105,7 +182,8 @@ sub renderRedDot {
   my $result = 
     '<span class="redDot"><a href="%SCRIPTURLPATH%/edit%SCRIPTSUFFIX%/' .
     "$theWeb/$theTopic" .
-    '?t=' . time() . '" ' .
+    '?t=' . time() . 
+    "&redirect=$theRedirect" . '" ' .
     "title=\"Edit&nbsp;<nop>$theWeb.$theTopic\" " .
     "alt=\"Edit&nbsp;<nop>$theWeb.$theTopic\"" .
     '>.</a></span>';
