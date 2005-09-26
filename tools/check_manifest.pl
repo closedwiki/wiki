dc9e0d5e042abed53d9229a160b32da092a0a8a1
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-09-26 17:11:24 +0000

    Item458: updated MANIFEST and added script that checks it for missing files
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@6609 a00a5322-12db-0310-a70b-8735589c885e

---
+++ tools/check_manifest.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+print <<END;
+This script must be run from the tools directory. It will scan the MANIFEST
+and compare the contents with what is checked in under subversion. Any#
+differences are reported.
+
+The test, tools and twikiplugins directories are *not* scanned.
+END
+my %man;
+
+map{ s/ .*//; $man{$_} = 1; } grep { !/^!include/  } split(/\n/, `cat MANIFEST` );
+
+my @lost;
+
+foreach my $dir( grep { -d "../$_" }
+                   split(/\n/, `svn ls ..`) ) {
+    next if $dir =~ /^(test|tools|twikiplugins)/;
+    print "--------------------- Examining $dir\n";
+    push( @lost,
+          grep { !$man{$_} && !/\/TestCases\// && ! -d "../$_" }
+            map{ "$dir$_" }
+              split(/\n/, `cd .. && svn ls -R $dir`));
+}
+print "The following files were found in subversion, but are not in MANIFEST\n";
+print join("\n", @lost ),"\n";
