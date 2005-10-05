8da1514126a03af59254161d207657458e356cad
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-10-05 11:58:09 +0000

    Item470: fixed, documented and released rewriteshebang script
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@6771 a00a5322-12db-0310-a70b-8735589c885e

--- tools/rewriteshbang.pl
+++ tools/rewriteshbang.pl
@@ -1,24 +1,42 @@
 #! perl -w
 
-# This script would not be necessary if there was a CommonFrontEndCgiScript
+# Rewrite "/usr/bin/perl" shebang lines to "perl"
+# SMELL: This script would not be necessary if there was a CommonFrontEndCgiScript
 # i.e. use of the bin/twiki script
 
 use FileHandle;
 
 my $old = "/usr/bin/perl";
 my $new = "perl";
-# FIXME: @files should be generated from what actually exists in bin
-my @files = qw(edit preview resetpasswd view attach geturl manage rdiff rest statistics  viewfile changes login oops register save twiki configure logon passwd rename search upload);
+
+print <<END;
+
+Rewrite #!/usr/bin/perl shebang lines to your local installation of perl.
+This script will rewrite the first lines of all your TWiki cgi scripts so
+they use a different shebang line. Use it if your perl is in a non-standard
+location, or you want to use a different interpreter (such as 'speedy')
+
+END
 
 unless (-d "bin") {
-  die "This must be run in the top level bin directory";
+  die "This must be run in the top level of your TWiki installation";
 }
 
 chdir "bin" || die "Can't cd into the bin dir";
 
-foreach my $file (@files) {
+while (1) {
+    print "Enter path to perl executable [hit enter to choose '$new']: ";
+    my $n = <>;
+    chomp $n;
+    last if( !$n );
+    $new = $n;
+};
+
+opendir(D, ".") || die "Can't open bin dir";;
+foreach my $file (grep { /^\w+$/ } readdir D) {
    replaceLine($file, $old, $new);
 }
+closedir(D);
 
 sub replaceLine {
   my ($file, $old, $new) = @_;
@@ -31,9 +49,9 @@ sub replaceLine {
   $replacementMade = ($contents =~ s/$old/$new/);
 
   if ($replacementMade) {
-    my $fh = new FileHandle(">$file") || die "Can't open $file for writing";
-    print $fh $contents;
-    close $fh;   
+ #   my $fh = new FileHandle(">$file") || die "Can't open $file for writing";
+ #   print $fh $contents;
+ #   close $fh;   
     print "$file modified\n";
   } else {
     print "$file unmodified\n";
