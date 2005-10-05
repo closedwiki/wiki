fb63433615b801d444b5b5ff104c55b6735a034d
MartinCleaver <MartinCleaver@a00a5322-12db-0310-a70b-8735589c885e>
2005-10-05 20:56:45 +0000

    Item584: this is better than the one in SVN. Item541 is related. Too busy to really look
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@6775 a00a5322-12db-0310-a70b-8735589c885e

--- tools/fixHtaccess.pl
+++ tools/fixHtaccess.pl
@@ -1,12 +1,22 @@
 #! /usr/bin/perl -w
 # See http://twiki.org/cgi-bin/view/Codev/FixUpHtaccess
 
-my ($filePathToTwiki, $urlPathToTwiki, $adminUsers) = getParams(@ARGV); 
+my ($dataDir, $defaultUrlHost, $scriptUrlPath, $adminUsers) = getParams(@ARGV); 
 # e.g. ("/home/mrjc/wikiconsulting.com/twiki", "wikiconsulting.com/twiki");
 
-my %patterns = ("!FILE_path_to_TWiki!" => $filePathToTwiki,
-		"!URL_path_to_TWiki!" => $urlPathToTwiki,
-	        "!ADMIN_users!" => $adminUsers);
+# {DataDir}
+#    Get the value from =configure=
+# {DefaultUrlHost}
+#    Get the value from =configure=
+# {ScriptUrlPath}
+#    Get the value from =configure=
+# {Administrators}
+
+
+my %patterns = ("{DataDir}" => $dataDir,
+		"{DefaultUrlHost}" => $defaultUrlHost,
+		"{ScriptUrlPath}" => $scriptUrlPath,
+	        "{Administrators}" => $adminUsers);
 
 unless (-d "bin") {
   die "This must be run in the top level bin directory";
@@ -22,8 +32,9 @@ close $htaccessTXTfh;
 
 foreach my $key (keys %patterns) {
     my $value = $patterns{$key};
-    print "Replacing $key with $value\n";
-    $file =~ s/$key/$value/g;
+    print "Replaced $key with $value ";
+    my $count = $file =~ s/$key/$value/g;
+    print "$count times\n";
 }
 
 my $htaccessFh = new FileHandle("> bin/.htaccess");
@@ -34,28 +45,29 @@ close $htaccessFh;
 print `chmod og-w bin bin/*`;
 
 sub getParams {
-  my ($filePathToTwiki, $urlPathToTwiki, $adminUsers)  = @_;
+    my ($dataDir, $defaultUrlHost, $scriptUrlPath, $adminUsers) = @_;
 
-  unless ($filePathToTwiki) {
-    die "ERROR: filePathToTwiki missing\n".usage();
+  unless ($dataDir) {
+    die "ERROR: dataDir missing\n".usage();
   }
-  unless ($urlPathToTwiki) {
-    die "ERROR: urlPathToTwiki missing\n".usage();
+  unless ($defaultUrlHost) {
+    die "ERROR: defaultUrlHost missing\n".usage();
   }
 
-  unless ($adminUsers) {
-    die "ERROR: adminUsers missing\n".usage();
+  unless ($scriptUrlPath) {
+    die "ERROR: scriptUrlPath missing\n".usage();
   } 
 
-  unless (-d $filePathToTwiki) {
-    die "The directory for filePathToTwiki '$filePathToTwiki' does not exist";
-  }
+   unless ($adminUsers) {
+	die "ERROR: adminUsers missing\n".usage();
+    }
 
-  if ($urlPathToTwiki =~ m/^http/) {
-    die "The URL for urlPathToTwiki '$urlPathToTwiki' must not start with http";
-  }
 
-  return  ($filePathToTwiki, $urlPathToTwiki, $adminUsers);
+  unless (-d $dataDir) {
+    die "The directory for dataDir '$dataDir' does not exist";
+  }
+  return ($dataDir, $defaultUrlHost, $scriptUrlPath, $adminUsers)
+
 }
 
 sub usage {
@@ -63,14 +75,15 @@ sub usage {
 Usage
 =====
 
-fixHtaccess.pl \$filePathToTwiki \$urlPathToTwiki \$adminUsers
+fixHtaccess.pl \$dataDir, \$defaultUrlHost, \$scriptUrlPath, \$adminUsers
 
-filePathToTwiki = location on the disk for TWiki root dir
-urlPathToTwiki  = base URL of TWiki when accessed by web 
+dataDir = location on the disk for TWiki root dir
+defaultUrlHost = hostname TWiki is running on
+scriptUrlPath  = base URL of TWiki when accessed by web 
 adminUsers      = which .htpasswd users can access configure
 
 e.g.
-fixHtaccess.pl /home/account/wikiconsulting.com/twiki wikiconsulting.com/twiki YourAdminLoginName
+fixHtaccess.pl /home/account/wikiconsulting.com/twiki wikiconsulting.com /twiki YourAdminLoginName
 
 Note that you are responsible for putting an Admin LoginName into .htpasswd.
 On UNIX you can use the htpasswd tool for this.
