d491e4259e9d9d1c708bd6721c3d91b53b11a79d
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2005-10-09 07:10:14 +0000

    Item638: fixed RELEASE overwrite
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@6897 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/RedDotPlugin/lib/TWiki/Plugins/RedDotPlugin.pm
+++ twikiplugins/RedDotPlugin/lib/TWiki/Plugins/RedDotPlugin.pm
@@ -26,12 +26,11 @@ use vars qw(
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
 $VERSION = '$Rev$';
-$RELEASE = '1.25';
 
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in ACTIVATED_PLUGINS.
-$RELEASE = 'Dakar';
+$RELEASE = '1.25';
 
 ###############################################################################
 sub writeDebug {
