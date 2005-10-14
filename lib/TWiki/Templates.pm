3cec7669287aa2abe91c0b3782b1eacca9f55dc4
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2005-10-14 07:07:37 +0000

    Item314: fixed typo in i18n comment
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@7043 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Templates.pm
+++ lib/TWiki/Templates.pm
@@ -188,7 +188,7 @@ Translates a string to the current language. Supports the following formats:
 Inside the { ... }, there should be a comma-separated list of double-quoted
 strings, like:
 
-<code>"Translate this [_1] for [_2", "parameter one", "parameter two".</code>
+<code>"Translate this [_1] for [_2]", "parameter one", "parameter two".</code>
 
 Return value: (eventually) translated text
 
