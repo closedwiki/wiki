fac07fd67bff7c66b1ea35fd609f7569f7b056c5
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2005-10-22 09:12:50 +0000

    Item0: more infrastructure: DEPENDENCIES, MANIFEST, lib/TWiki/Plugins/$PluginName.txt
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@7115 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/create-new-plugin.pl
+++ twikiplugins/create-new-plugin.pl
@@ -3,7 +3,6 @@ use strict;
 
 use FindBin;
 use File::Path qw( mkpath );
-use File::Copy qw( cp );
 use Cwd;
 
 ################################################################################
@@ -20,14 +19,54 @@ chdir $FindBin::Bin;
 #print STDERR "$name already exists; will not overwrite (use --f/--force to override)\n";
 -e $name && die "$name already exists; will not overwrite\n";
 
-eval { mkpath [ "$name/lib/TWiki/Plugins/$name", "$name/data/TWiki/$name.txt" ] };
+eval { mkpath [ "$name/lib/TWiki/Plugins/$name/", "$name/data/TWiki/" ] };
 $@ && die $@;
 
-# BuildContrib
-cp "BuildContrib/lib/TWiki/Contrib/BuildContrib/build.pl", "$name/lib/TWiki/Plugins/$name/build.pl" or die $!;
+{ # BuildContrib
+#cp "BuildContrib/lib/TWiki/Contrib/BuildContrib/build.pl", "$name/lib/TWiki/Plugins/$name/build.pl" or die $!;
+    open( BUILD, '<', "BuildContrib/lib/TWiki/Contrib/BuildContrib/build.pl" ) or die $!;
+    local $/ = undef;
+    my $build = <BUILD>;
+    close BUILD;
 
-# PluginSkeleton
-cp "../lib/TWiki/Plugins/EmptyPlugin.pm", "$name/lib/TWiki/Plugins/$name.pm" or die $!;
+    $build =~ s/BuildContrib/$name/g;
+
+    open( BUILD, '>', "$name/lib/TWiki/Plugins/$name/build.pl" ) or die $!;
+    print BUILD $build;
+    close BUILD;
+}
+
+{ # PluginSkeleton
+#    cp "../lib/TWiki/Plugins/EmptyPlugin.pm", "$name/lib/TWiki/Plugins/$name.pm" or die $!;
+    open( PLUGIN, '<', "../lib/TWiki/Plugins/EmptyPlugin.pm" ) or die $!;
+    local $/ = undef;
+    my $lib = <PLUGIN>;
+    close PLUGIN;
+
+    $lib =~ s/EmptyPlugin/$name/g;
+
+    open( PLUGIN, '>', "$name/lib/TWiki/Plugins/$name.pm" ) or die $!;
+    print PLUGIN $lib;
+    close PLUGIN;
+
+    open( MANIFEST, '>', "$name/lib/TWiki/Plugins/$name/MANIFEST" ) or die $!;
+    print MANIFEST <<__MANIFEST__;
+data/TWiki/$name.txt  Plugin doc page
+lib/TWiki/Plugins/$name.pm  Plugin Perl module 
+__MANIFEST__
+    close MANIFEST;
+
+    open( DEPENDENCIES, '>', "$name/lib/TWiki/Plugins/$name/DEPENDENCIES" ) or die $!;
+    close DEPENDENCIES;
+
+    open( TOPIC, '>', "$name/data/TWiki/$name.txt" ) or die $!;
+    print TOPIC "xxx";
+    close TOPIC;
+
+    # TODO: make unit tests
+}
+
+# replace-string EmptyPlugin $name
 
 ################################################################################
 
