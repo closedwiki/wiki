013622985d045f7de2e51965c7de38b77bf5da0c
MartinCleaver <MartinCleaver@a00a5322-12db-0310-a70b-8735589c885e>
2005-10-29 13:47:01 +0000

    Item437: build.pl reverted - sorry people, it seems this broke the build :(
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@7214 a00a5322-12db-0310-a70b-8735589c885e

--- tools/build.pl
+++ tools/build.pl
@@ -45,17 +45,14 @@ sub target_build {
     $this->SUPER::target_build();
 
     # generate the POD documentation
-    print "Building documentation in $this->{basedir}....\n";
-    print "  Building TWiki.SourceCode tree docs...\n";
-    $this->sys_action("perl gendocs.pl -root $this->{basedir}");
-
-    print "  Generating AUTHORS...\n";
+    print "Building documentation....\n";
+    print `perl gendocs.pl -root $this->{basedir}`;
     $this->cp( $this->{basedir}.'/AUTHORS',
                $this->{basedir}.'/pub/Main/TWikiContributor/AUTHORS' );
-    print "  Generating HTML static docs...\n";
-    $this->sys_action("cd ../bin ; ./view TWiki.TWikiDocumentation skin plain | ../tools/fix_local_links.pl > ../TWikiDocumentation.html ");
-    $this->sys_action("cd ../bin ; ./view TWiki.TWikiHistory skin plain > ../TWikiHistory.html ");
-    $this->sys_action("cd ../bin ; ./view TWiki.DakarReleaseNotes skin plain > ../DakarReleaseNotes.html ");
+
+    print `cd ../bin ; ./view TWiki.TWikiDocumentation skin plain | ../tools/fix_local_links.pl > ../TWikiDocumentation.html 2> /dev/null`;
+    print `cd ../bin ; ./view TWiki.TWikiHistory skin plain > ../TWikiHistory.html 2> /dev/null`;
+    print `cd ../bin ; ./view TWiki.DakarReleaseNotes skin plain > ../DakarReleaseNotes.html 2> /dev/null`;
 
     print "Documentation built\n";
 }
@@ -67,8 +64,7 @@ sub target_stage {
 
     #use a Cairo install to create new ,v files for the data, and pub
     #WARNING: I don't know how to get the 'last' release, so i'm hardcoding Cairo
-    my $lastRelease = "http://svn.twiki.org:8181/svn/twiki/tags/twiki-20040902-release/";
-    $this->stage_rcsfiles($lastRelease);
+    $this->stage_rcsfiles();
 }
 
 # check in a single file to RCS
@@ -78,13 +74,13 @@ sub _checkInFile {
     return if ( shift =~ /\,v$/ ); #lets not check in ,v files
 
     my $currentRevision = 0;
-    print "Checking in $new/$file\n" if ($this->{-v});
+    print "Checking in $new/$file\n";
     if ( -e $old.'/'.$file.',v' ) {
         $this->cp($old.'/'.$file.',v', $new.'/'.$file.',v');
         #force unlock
-        $this->sys_action("rcs -u -M $new/$file,v");
+        `rcs -u -M $new/$file,v`;
         #lock to this user
-        $this->sys_action("rcs -l $new/$file");
+        `rcs -l $new/$file`;
 
         #try to get current revision number
         my ($rcsInfo) = "rlog -r $new/$file";
@@ -103,9 +99,9 @@ sub _checkInFile {
     `$cmd`;
 
     #check in TODO: this commits as the current user, not TWikiContributor
-    $this->sys_action("ci -mbuildrelease -t-new-topic $new/$file");
+    `ci -mbuildrelease -t-new-topic $new/$file`;
     #get a copy of the latest revsion, no lock
-    $this->sys_action("co -u -M $new/$file");
+    `co -u -M $new/$file`;
 }
 
 # recursively check in files to RCS
@@ -114,7 +110,7 @@ sub _checkInDir {
     my $dir;
 
     opendir( $dir, "$new/$root" ) || die "Failed to open $root: $!";
-    print "Scanning $new/$root...\n" if ($this->{-v});
+    print "Scanning $new/$root...\n";
     foreach my $content ( grep { !/^\./ } readdir($dir)) {
         my $sub = "$root/$content";
         if( -d "$new/$sub" ) {
@@ -128,30 +124,24 @@ sub _checkInDir {
 
 sub stage_rcsfiles() {
     my $this = shift;
-    my $lastRelease = shift;
-
-    $lastRelease or die "lastRelease not specified";
 
     # svn co cairo to a new dir
     #foreach file in data|pub in tmpDir, cp ,v file from svnCo 
     #do a ci
     #if there was no existing ,v file, make one and ci
 
-    # TODO: make _getTmpDir public.
-    my $lastReleaseDir = $this->_getTmpDir("LastRelease");
+    my $lastReleaseDir = '/tmp/lastRel'.($$ +1);
 
     $this->makepath($lastReleaseDir);
     $this->cd($lastReleaseDir);
-    print 'Checking out last release to ', $lastReleaseDir, "\n";
-    $this->sys_action("svn co $lastRelease .");
-
-    print '  Unlocking ,v files', "\n";
+    print 'Checking out last release to '.$lastReleaseDir."\n";
+    `svn co http://svn.twiki.org:8181/svn/twiki/tags/twiki-20040902-release/ .`;
+    print "Creating ,v files.\n";
     $this->_checkInDir( $lastReleaseDir, $this->{tmpDir}, 'data',
                        sub { return shift =~ /\.txt$/ } );
 
     $this->_checkInDir( $lastReleaseDir, $this->{tmpDir}, 'pub',
                        sub { return -f shift; } );
-    print '  Done', "\n";
 }
 
 # Create the build object
