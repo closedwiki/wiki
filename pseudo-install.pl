b4a6489d5430c7d02eb8ca0db58c6e4622c27b36
MartinCleaver <MartinCleaver@a00a5322-12db-0310-a70b-8735589c885e>
2005-11-01 16:35:26 +0000

    Item145: extended psuedo-install so you can add a list of named plugins instead of having to take them all
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@7261 a00a5322-12db-0310-a70b-8735589c885e

--- pseudo-install.pl
+++ pseudo-install.pl
@@ -1,35 +1,77 @@
 #!perl
-# pseduo-install plugins in a SVN checkout tree
-# This is done by a simple copy of the files listed in the MANIFEST for each
-# plugin and contrib. There is no linking, and no attempt to be clever. It should
-# be equivalent to a zip - unzip of the plugin over the dev tree.
 #
 # Intended for use on PC where softlinks (as done by mklinks.sh) are not
 # available
 use strict;
 use File::Path;
 use File::Copy;
+my %cfg;
+$cfg{-v} = 0;
 
-opendir(D, "twikiplugins") || die "Must be run from root of installation";
-foreach my $module ( grep { /(Plugin|Contrib)$/ } readdir( D )) {
-    print "Processing $module\n";
-    my $subdir = 'Plugins';
-    $subdir = 'Contrib' if $module =~ /Contrib$/;
-    if( -e "twikiplugins/$module/lib/TWiki/$subdir/$module/MANIFEST" ) {
-        open( F, "<twikiplugins/$module/lib/TWiki/$subdir/$module/MANIFEST" ) || die $!;
+sub usage {
+    print <<EOM;
+ pseduo-install plugins in a SVN checkout tree
+ This is done by a simple copy of the files listed in the MANIFEST for each
+ plugin and contrib. There is no linking, and no attempt to be clever. It should
+ be equivalent to a zip - unzip of the plugin over the dev tree.
+
+ either:
+    * pseudo-install all
+  or:
+    * pseudo-install FirstPlugin SomeContrib ...
+EOM
+
+}
+
+my @modules = @ARGV;
+
+unless (@modules) {
+    usage();
+    exit 1;
+}
+
+if ($modules[0] eq "all") {
+  opendir(D, "twikiplugins") || die "Must be run from root of installation";
+  @modules = ( grep { /(Plugin|Contrib)$/ } readdir( D )); 
+  closedir( D );
+}
+
+print "Installing modules: ".join(",", @modules).":\n";
+
+foreach my $module (@modules) {
+    installModule($module);
+}
+print "Don't forget to enable them using configure\n";
+
+# SMELL - this probably duplicates something in BuildContrib
+sub installModule {
+  my $module = shift;
+  print "Processing $module\n";
+  my $subdir = 'Plugins';
+  $subdir = 'Contrib' if $module =~ /Contrib$/;
+  my $moduleDir = "twikiplugins/$module/";
+#lib/TWiki/$subdir/$module/
+  unless (-d $moduleDir) {
+      print STDERR "---> No such $moduleDir\n";
+      return;
+  }
+
+  my $manifest = $moduleDir."lib/TWiki/$subdir/$module/".'/MANIFEST';
+    if( -e "$manifest" ) {
+        open( F, "<$manifest" ) || die $!;
         foreach my $file ( <F> ) {
             chomp( $file );
             next unless $file =~ /^\w+/;
             $file =~ s/\s.*$//;
-            next if -d "twikiplugins/$module/$file";
+            next if -d "$moduleDir/$file";
             my $dir = $file;
             $dir =~ s/\/[^\/]*$//;
             File::Path::mkpath( $dir );
-            File::Copy::copy( "twikiplugins/$module/$file", $file );
+            File::Copy::copy( "$moduleDir/$file", $file ) || die "Couldn't install $file";
+	    print "  installed $file\n" if $cfg{-v};
         }
         close(F);
     } else {
-        print STDERR "No MANIFEST in $module\n";
+        print STDERR "---> No MANIFEST in $module\n";
     }
 }
-closedir( D );
