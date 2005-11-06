dfaa7ed2cc93571f5ff7bb22ed690b3197089172
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-11-06 12:30:39 +0000

    Item818: pseudo-install now does link and uninstall as well
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@7335 a00a5322-12db-0310-a70b-8735589c885e

--- pseudo-install.pl
+++ pseudo-install.pl
@@ -1,68 +1,43 @@
 #!perl
-#
-# Intended for use on PC where softlinks (as done by mklinks.sh) are not
-# available
-# SMELL this will become unnecessary once twikishell is bundled.
 use strict;
+
 use File::Path;
 use File::Copy;
-my %cfg;
-$cfg{-v} = 0;
+
+my $install;
 
 sub usage {
     print <<EOM;
- pseduo-install plugins in a SVN checkout tree
- This is done by a simple copy of the files listed in the MANIFEST for each
- plugin and contrib. There is no linking, and no attempt to be clever. It should
- be equivalent to a zip - unzip of the plugin over the dev tree.
+ Must be run from the root of a SVN checkout tree
 
- either:
-    * pseudo-install all
-  or:
-    * pseudo-install FirstPlugin SomeContrib ...
+ pseudo-install optional modules in a SVN checkout tree
+ This is done by a simple copy or link of the files listed in the MANIFEST
+ for each module. The installer script is not called.
+ It should be equivalent to a tar zx of the packaged module over the dev tree.
+
+ Usage: pseudo-install.pl [-link] [all|default] <module>...
+    -link - link instead of copy (using ln -s)
+    -uninstall - self explanatory
+    all - install all modules found in twikiplugins
+    default - install modules listed in tools/MANIFEST
+    <module>... one or more modules to install e.g. FirstPlugin SomeContrib ...
 EOM
 
 }
 
-my @modules = @ARGV;
-
-unless (@modules) {
-    usage();
-    exit 1;
-}
-
-if ($modules[0] eq "all") {
-  opendir(D, "twikiplugins") || die "Must be run from root of installation";
-  @modules = ( grep { /(Plugin|Contrib)$/ } readdir( D )); 
-  closedir( D );
-}
-
-
-if ($modules[0] eq "default") {
-    @modules = qw(CommentPlugin EditTablePlugin InterwikiPlugin PreferencesPlugin SpreadSheetPlugin SmiliesPlugin TablePlugin WysiwygPlugin TipsContrib);
-}
-
-print "Installing modules: ".join(",", @modules).":\n";
-
-foreach my $module (@modules) {
-    installModule($module);
-}
-print "Don't forget to enable them using configure\n";
-
-# SMELL - this probably duplicates something in BuildContrib
 sub installModule {
-  my $module = shift;
-  print "Processing $module\n";
-  my $subdir = 'Plugins';
-  $subdir = 'Contrib' if $module =~ /Contrib$/;
-  my $moduleDir = "twikiplugins/$module/";
-#lib/TWiki/$subdir/$module/
-  unless (-d $moduleDir) {
-      print STDERR "---> No such $moduleDir\n";
-      return;
-  }
+    my $module = shift;
+    print "Processing $module\n";
+    my $subdir = 'Plugins';
+    $subdir = 'Contrib' if $module =~ /Contrib$/;
+    my $moduleDir = "twikiplugins/$module/";
 
-  my $manifest = $moduleDir."lib/TWiki/$subdir/$module/".'/MANIFEST';
+    unless (-d $moduleDir) {
+        print STDERR "---> No such $moduleDir\n";
+        return;
+    }
+
+    my $manifest = $moduleDir."lib/TWiki/$subdir/$module/".'/MANIFEST';
     if( -e "$manifest" ) {
         open( F, "<$manifest" ) || die $!;
         foreach my $file ( <F> ) {
@@ -72,12 +47,71 @@ sub installModule {
             next if -d "$moduleDir/$file";
             my $dir = $file;
             $dir =~ s/\/[^\/]*$//;
-            File::Path::mkpath( $dir );
-            File::Copy::copy( "$moduleDir/$file", $file ) || die "Couldn't install $file";
-	    print "  installed $file\n" if $cfg{-v};
+            &$install( $moduleDir, $dir, $file );
         }
         close(F);
     } else {
         print STDERR "---> No MANIFEST in $module\n";
     }
 }
+
+sub copy_in {
+    my( $moduleDir, $dir, $file ) = @_;
+    File::Path::mkpath( $dir );
+    File::Copy::copy( "$moduleDir/$file", $file ) ||
+        die "Couldn't install $file";
+    print "Copied $file\n";
+}
+
+sub just_link {
+    my( $moduleDir, $dir, $file ) = @_;
+    File::Path::mkpath( $dir );
+    my $argh = `ln -s $moduleDir/$file $file`;
+    die "$argh $@" if ( $argh || $@ );
+    print "Lunk $file\n";
+}
+
+sub uninstall {
+    my( $moduleDir, $dir, $file ) = @_;
+    unlink $file;
+    print "Unlunk $file\n";
+}
+
+unless (@ARGV) {
+    usage();
+    exit 1;
+}
+
+if ($ARGV[0] eq '-link') {
+    shift(@ARGV);
+    $install = \&just_link;
+} elsif ($ARGV[0] eq '-uninstall') {
+    shift(@ARGV);
+    $install = \&uninstall;
+} else {
+    $install = \&copy_in;
+}
+
+my @modules;
+
+if ($ARGV[0] eq "all") {
+  opendir(D, "twikiplugins") || die "Must be run from root of installation";
+  @modules = ( grep { /(Plugin|Contrib)$/ } readdir( D ));
+  closedir( D );
+} elsif ($ARGV[0] eq "default") {
+    open(F, "<", "tools/MANIFEST") || die "Could not open MANIFEST: $!";
+    local $/ = "\n";
+    @modules =
+      map { /(\w+)$/; $1 }
+        grep { /^!include/ } <F>;
+    close(F);
+} else {
+    @modules = @ARGV;
+}
+
+print "Installing modules: ".join(",", @modules).":\n";
+
+foreach my $module (@modules) {
+    installModule($module);
+}
+print "Don't forget to enable plugins using configure\n";
