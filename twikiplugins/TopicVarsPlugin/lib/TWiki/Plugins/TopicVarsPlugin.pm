d193118bf21a36bc04242b6cff19d12ea7395af5
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2005-12-01 14:49:01 +0000

    Item998: fixed so it doesn't assume tabs but can use 3 spaces for sets
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@7692 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/TopicVarsPlugin/lib/TWiki/Plugins/TopicVarsPlugin.pm
+++ twikiplugins/TopicVarsPlugin/lib/TWiki/Plugins/TopicVarsPlugin.pm
@@ -23,14 +23,10 @@
 # the same web, but may expand to include cross-web, and possibly
 # even InterWiki references in the future.
 
+package TWiki::Plugins::TopicVarsPlugin;
 
-
-# =========================
-package TWiki::Plugins::TopicVarsPlugin; 	# change the package name!!!
-
-# =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $debug
+        $currWeb $currTopic $user $installWeb $VERSION $RELEASE
         %vars
     );
 
@@ -44,139 +40,97 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-
-# =========================
-sub initPlugin
-{
-    ( $topic, $web, $user, $installWeb ) = @_;
+sub initPlugin {
+    ( $currTopic, $currWeb, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
     if( $TWiki::Plugins::VERSION < 1 ) {
-        &TWiki::Func::writeWarning( "Version mismatch between TopicVarsPlugin and Plugins.pm" );
+        TWiki::Func::writeWarning( "Version mismatch between TopicVarsPlugin and Plugins.pm" );
         return 0;
     }
 
-    # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
-    #$exampleCfgVar = &TWiki::Func::getPreferencesValue( "TOPICVARSPLUGIN_EXAMPLE" ) || "default";
-
-    # Get plugin debug flag
-    $debug = &TWiki::Func::getPreferencesFlag( "TOPICVARSPLUGIN_DEBUG" );
-
-    # Plugin correctly initialized
-    &TWiki::Func::writeDebug( "- TWiki::Plugins::TopicVarsPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
-
     return 1;
 }
-
 
 ## Stolen from TWiki::Prefs::getPrefsFromTopic because I don't
 ## (offhand) know of an easy way to override that to make it do what I
 ## want. I suppose I might be able to work some kind of black magic
 ## with the symbol table, but I'm not up to that right now.
-
+#
+# SMELL: with Dakar, you can do it, but it would require calling internals.
+# Perhaps some day, when the object model is properly done.
+#
 sub getVarsFromTopic {
-	my ( $theWeb, $theTopic ) = @_;
-	my $theKeyPrefix = "";
+	my ( $web, $topic ) = @_;
 
+	my( $meta, $text ) = TWiki::Func::readTopic( $web, $topic );
 
-	&TWiki::Func::writeDebug( "- TWiki::Plugins::TopicVarsPlugin::getVarsFromTopic( $theWeb.$theTopic )" ) if $debug;
-
-	my( $meta, $text ) = &TWiki::Func::readTopic( $theWeb, $theTopic );
-	$text =~ s/\r/\n/go;
-	$text =~ s/\n+/\n/go;
-
-	my $keyPrefix = $theKeyPrefix || "";  # prefix is for plugin prefs
-	my $key = "";
-	my $value ="";
+	my $key = '';
+	my $value = '';
 	my $isKey = 0;
-	foreach( split( /\n/, $text ) ) {
-		if( /^\t+\*\sSet\s([a-zA-Z0-9_]+)\s\=\s*(.*)/ ) {
+	foreach( split( /\r?\n/, $text ) ) {
+		if( /^(?:\t|   )+\*\sSet\s(\w+)\s\=\s*(.*)$/ ) {
 			if( $isKey ) {
-				$vars{$theWeb}{$theTopic}{$key} = $value;
+				$vars{$web}{$topic}{$key} = $value;
 			}
-			$key = "$keyPrefix$1";
-			$value = defined $2 ? $2 : "";
+			$key = $1;
+			$value = $2;
+            $value = '' unless defined $value;
 			$isKey = 1;
 		} elsif ( $isKey ) {
-			if( ( /^\t+/ ) && ( ! /^\t+\*/ ) ) {
+			if( ( /^(\t|   )+/ ) && ( ! /^(\t|   )+\*/ ) ) {
 				# follow up line, extending value
 				$value .= "\n$_";
 			} else {
-				$vars{$theWeb}{$theTopic}{$key} = $value;
+				$vars{$web}{$topic}{$key} = $value;
 				$isKey = 0;
 			}
 		}
 	}
 	if( $isKey ) {
-		$vars{$theWeb}{$theTopic}{$key} = $value;
+		$vars{$web}{$topic}{$key} = $value;
 	}
 }
 
+sub get_var {
+    my ( $web, $topic, $varname ) = @_;
 
+    my $id = '';
 
-
-
-sub do_var_from_wiki {
-### my ( $web, $topic, $varname ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-	my $theWeb = $_[0];
-    if(defined($TWiki::securityFilter)) {
-        $theWeb =~ s/$TWiki::securityFilter//go;
+    if( $web ) {
+        $id = $web.'.';
+        if(defined($TWiki::securityFilter)) {
+            $web =~ s/$TWiki::securityFilter//go;
+        } else {
+            $web =~ s/$TWiki::cfg{NameFilter}//go;
+        }
     } else {
-        $theWeb =~ s/$TWiki::cfg{NameFilter}//go;
+        $web = $currWeb;
     }
-	getVarsFromTopic( $theWeb, $_[1] ) unless $vars{$theWeb}{$_[1]};
-	my $var = $vars{$theWeb}{$_[1]}{$_[2]};
-	$var = defined $var ? $var : "\%$theWeb.$_[1].$_[2]\%";
-	if ($debug) {
-		&TWiki::Func::writeDebug( "- TopicVarsPlugin::do_var_from_wiki $theWeb.$_[1].$_[2]: [$var]" );
-	}
-	return $var;
-}
-sub do_var_from_web {
-### my ( $topic, $varname ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-	getVarsFromTopic( $web, $_[0] ) unless $vars{$web}{$_[0]};
-	my $var = $vars{$web}{$_[0]}{$_[1]};
-	$var = defined $var ? $var : return "\%$_[0].$_[1]\%";
-	if ($debug) {
-		&TWiki::Func::writeDebug( "- TopicVarsPlugin::do_var_from_web $_[0].$_[1]: [$var]" );
-	}
-	return $var;
-}
-sub do_var_from_topic {
-### my ( $varname ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+
+    if( $topic ) {
+        $id .= $topic.'.';
+    } else {
+        $topic = $currTopic;
+    }
+
 	getVarsFromTopic( $web, $topic ) unless $vars{$web}{$topic};
-	my $var = $vars{$web}{$topic}{$_[0]};
-	$var = defined $var ? $var : "\%$_[0]\%";
-	if ($debug) {
-		&TWiki::Func::writeDebug( "- TopicVarsPlugin::do_var_from_topic $_[0]: [$var]" );
-	}
+	my $var = $vars{$web}{$topic}{$varname};
+	$var = defined $var ? $var : "%$id$varname%";
 	return $var;
 }
 
+sub commonTagsHandler {
+    ### my ( $text, $topic, $web ) = @_;
 
+    ## Handle unqualified var references (will look on this page)
+    $_[0] =~ s/%(\w+)%/get_var(undef, undef, $1)/geo;
 
-# =========================
-sub commonTagsHandler
-{
-### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
+    ## Handle topic-qualified var references (will look at topics on this web)
+    $_[0] =~ s/%([A-Z]+[a-z]+[A-Z]+[A-Za-z0-9]*)\.([A-Za-z0-9_]+)%/get_var(undef, $1, $2)/geo;
 
-    &TWiki::Func::writeDebug( "- TopicVarsPlugin::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
-
-    # This is the place to define customized tags and variables
-    # Called by sub handleCommonTags, after %INCLUDE:"..."%
-
-    # do custom extension rule, like for example:
-    # $_[0] =~ s/%XYZ%/&handleXyz()/geo;
-    # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/geo;
-
-		## Handle unqualified var references (will look on this page)
-		$_[0] =~ s/%([a-zA-Z0-9_]+)%/&do_var_from_topic($1)/geo;
-
-		## Handle topic-qualified var references (will look at topics on this web)
-		$_[0] =~ s/%([A-Z]+[a-z]+[A-Z]+[A-Za-z0-9]*)\.([A-Za-z0-9_]+)%/&do_var_from_web($1, $2)/geo;
-
-		## Handle fully-qualified var references (will look at webs/topics on this wiki)
-		$_[0] =~ s/%([A-Z][^%]*)\.([A-Z]+[a-z]+[A-Z]+[A-Za-z0-9_]*)\.([a-zA-Z0-9_]+)%/&do_var_from_wiki($1, $2, $3)/geo;
+    ## Handle fully-qualified var references (will look at webs/topics on this wiki)
+    $_[0] =~ s/%([A-Z][^%]*)\.([A-Z]+[a-z]+[A-Z]+\w*)\.(\w+)%/get_var($1, $2, $3)/geo;
 }
 
 1;
