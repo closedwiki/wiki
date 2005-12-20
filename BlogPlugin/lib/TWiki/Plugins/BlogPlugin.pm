47eb60ef6e2820de127d3be6bb41774efe009789
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2005-12-20 19:23:57 +0000

    Item840:    * implemented blog slicing; SubjectCategories are of type FrontPage now    * better Textiles using the new AliasPlugin features    * w3c validateion fixes in RenderRecentComments (duh, dont use twiki lists)    *
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@7885 a00a5322-12db-0310-a70b-8735589c885e

--- BlogPlugin/lib/TWiki/Plugins/BlogPlugin.pm
+++ BlogPlugin/lib/TWiki/Plugins/BlogPlugin.pm
@@ -24,7 +24,7 @@ use vars qw(
 use TWiki::Plugins::BlogPlugin::WebDB; # must be compiled in advance
 
 $VERSION = '$Rev$';
-$RELEASE = '0.42';
+$RELEASE = '0.43';
 
 ###############################################################################
 sub initPlugin {
