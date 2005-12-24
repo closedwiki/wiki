4074d0051cde97c376eb7168f7f26eccadd6d9f9
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2005-12-24 00:03:17 +0000

    Item1223: removing old cruft
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@7910 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/TWikiInstallerContrib/lib/TWiki/Contrib/TWikiInstallerContrib/platform/darwin/un-twiki.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# un-twiki.sh
-rm -rf cgi-bin/ twiki/ htdocs/
-#lib/ cgi-bin/tmp/ cgi-bin/twiki/ htdocs/twiki/ twiki/ cgi-bin/install_twiki.cgi
-
-# cleanup stuff that shouldn't still be here
-#mkdir -p cgi-bin/tmp/del
-#mv twiki cgi-bin/tmp/del
-#mv htdocs/twiki cgi-bin/tmp/del
