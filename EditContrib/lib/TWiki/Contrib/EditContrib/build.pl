3b368a9e835f12dbf8d008bf2c3b8c73d2ac8424
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2005-12-28 02:38:49 +0000

    Item1238: fixed BuildContrib support
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@8009 a00a5322-12db-0310-a70b-8735589c885e

--- EditContrib/lib/TWiki/Contrib/EditContrib/build.pl
+++ EditContrib/lib/TWiki/Contrib/EditContrib/build.pl
@@ -33,7 +33,7 @@ use TWiki::Contrib::Build;
 
   sub new {
     my $class = shift;
-    return bless( $class->SUPER::new( "EditContrib", "Build" ), $class );
+    return bless( $class->SUPER::new( "EditContrib" ), $class );
   }
 
   # Example: Override the build target
