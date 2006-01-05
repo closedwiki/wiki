8989b20a8ed2170a48e23e2023b9389cd57cab4d
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-05 22:43:54 +0000

    Item1330: initial import of TWikiPluginInstallerContrib (later, rename to TWikiExtensionInstallerContrib?)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@8127 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/TWikiPluginInstallerContrib/test/unit/TWikiPluginInstallerContrib/TWikiPluginInstallerContribSuite.pm
@@ -0,0 +1,9 @@
+package TWikiPluginInstallerContribSuite;
+
+use base qw(Test::Unit::TestSuite);
+
+sub name { 'TWikiPluginInstallerContribSuite' };
+
+sub include_tests { qw(TWikiPluginInstallerContribTests) };
+
+1;
