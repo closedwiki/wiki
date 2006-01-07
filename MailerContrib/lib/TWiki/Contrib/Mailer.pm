4c5ac0b48e06f47fe6befeb1a8198155922c60de
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-07 05:41:07 +0000

    Item1343: Fix copyright notice for 2006 (only comment changes, no code changes)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@8154 a00a5322-12db-0310-a70b-8735589c885e

--- MailerContrib/lib/TWiki/Contrib/Mailer.pm
+++ MailerContrib/lib/TWiki/Contrib/Mailer.pm
@@ -1,20 +1,23 @@
-#
-# TWiki Collaboration Platform, http://TWiki.org/
+# Module of TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2004 Wind River Systems Inc.
+# Copyright (C) 1999-2006 Peter Thoeny, peter@thoeny.org
+# and TWiki Contributors. All Rights Reserved. TWiki Contributors
+# are listed in the AUTHORS file in the root of this distribution.
+# NOTE: Please extend that file, not this notice.
 #
-# For licensing info read license.txt file in the TWiki root.
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
-# http://www.gnu.org/copyleft/gpl.html
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
+# As per the GPL, removal of this notice is prohibited.
+
 use strict;
 use TWiki;
 
