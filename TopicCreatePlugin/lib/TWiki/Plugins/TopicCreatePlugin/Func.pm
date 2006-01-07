5f26f2f203815abc27cc0fa68378782c96b4a71b
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-07 02:46:50 +0000

    Item0: DOS 2 Unix fix of file
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@8153 a00a5322-12db-0310-a70b-8735589c885e

--- TopicCreatePlugin/lib/TWiki/Plugins/TopicCreatePlugin/Func.pm
+++ TopicCreatePlugin/lib/TWiki/Plugins/TopicCreatePlugin/Func.pm
@@ -1,8 +1,8 @@
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# TWiki WikiClone ($wikiversion has version info)
+# Copyright (C) 2005-2006 Peter Thoeny, peter@thoeny.org
 #
-# Copyright (C) 2001-2005 Peter Thoeny, Peter@Thoeny.com
-#
+# For licensing info read LICENSE file in the TWiki root.
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
@@ -13,6 +13,8 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
+#
+# As per the GPL, removal of this notice is prohibited.
 #
 # =========================
 #
