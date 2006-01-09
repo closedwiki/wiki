d4e3a2c9ec0dfe11c7f3997ba2287538a94acdac
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-09 17:16:10 +0000

    Item1335: cron script now removes expired leases
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@8184 a00a5322-12db-0310-a70b-8735589c885e

--- tools/expire_sessions.pl
+++ tools/expire_sessions.pl
@@ -2,7 +2,10 @@
 #
 # TWiki Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2005 TWiki Contributors
+# Copyright (C) 2005-2006 TWiki Contributors.
+# All Rights Reserved. TWiki Contributors are listed in
+# the AUTHORS file in the root of this distribution.
+# NOTE: Please extend that file, not this notice.
 #
 # For licensing info read license.txt file in the TWiki root.
 # This program is free software; you can redistribute it and/or
@@ -20,17 +23,41 @@
 # This is so it can pick up the right TWiki environment from
 # setlib.cfg.
 #
-# It will expire sessions that have not been used for
-# |{Sessions}{ExpireAfter}| seconds i.e. if you set {Sessions}{ExpireAfter}
-# to -36000 or 36000 it will expire sessions that have not been used for
-# more than 100 hours,
+# It executes a number of non-essential regular administration
+# tasks that will help keep your TWiki healthy and happy.
+#
+# It is intended to be run as a cron job (remember it has to be run
+# by a user who can write files created by the webserver user!)
+# For example,
+#
+# 0 0 * * 0 cd /usr/twiki/bin && perl ../tools/tick_twiki.pl
 #
 BEGIN {
     unshift @INC, '.';
     require 'setlib.cfg';
 }
 
-use TWiki::Client;
+# This will expire sessions that have not been used for
+# |{Sessions}{ExpireAfter}| seconds i.e. if you set {Sessions}{ExpireAfter}
+# to -36000 or 36000 it will expire sessions that have not been used for
+# more than 100 hours,
 
+use TWiki::Client;
 TWiki::Client::expireDeadSessions();
 
+# This will remove topic leases that have expired. Topic leases may be
+# left behind when users edit a topic and then navigate away without
+# cancelling the edit.
+
+use TWiki;
+my $twiki = new TWiki();
+my $store = $twiki->{store};
+my $now = time();
+foreach my $web ( $store->getListOfWebs()) {
+    foreach my $topic ( $store->getTopicNames( $web )) {
+        my $lease = $store->getLease( $web, $topic );
+        if( $lease && $lease->{expires} < $now) {
+            $store->clearLease( $web, $topic );
+        }
+    }
+}
