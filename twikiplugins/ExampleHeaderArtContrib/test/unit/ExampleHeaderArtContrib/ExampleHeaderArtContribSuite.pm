c23a46a5afb6e059811fbe4e283f3be000f49cf7
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-11 01:17:06 +0000

    Item1333: splitting out former graphics temporarily in PatternSkin into its own contrib
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@8220 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/ExampleHeaderArtContrib/test/unit/ExampleHeaderArtContrib/ExampleHeaderArtContribSuite.pm
@@ -0,0 +1,9 @@
+package ExampleHeaderArtContribSuite;
+
+use base qw(Test::Unit::TestSuite);
+
+sub name { 'ExampleHeaderArtContribSuite' };
+
+sub include_tests { qw(ExampleHeaderArtContribTests) };
+
+1;
