882c5f7b5f11d11cf69ea434c8fdecc7695593ca
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-11 01:12:11 +0000

    Item1333: finally, OO-ified (and, added support for HeaderArtContrib in addition to existing Plugin and Contrib support)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@8218 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/create-new-plugin.pl
+++ twikiplugins/create-new-plugin.pl
@@ -1,16 +1,15 @@
 #! /usr/bin/perl -w
 use strict;
+use Data::Dumper qw( Dumper );
 
 use FindBin;
-use File::Path qw( mkpath );
 use Cwd;
 
 ################################################################################
 
 my ( $name ) = ( @ARGV, '' );
-my @types = qw( Plugin Contrib );
-#print "[$name]\n";
-( my $type = $name ) =~ s/^.*?(Plugin|Contrib)?$/$1/;
+my @types = qw( Plugin Contrib HeaderArtContrib );
+( my $type = $name ) =~ s/^.*?(Plugin|(HeaderArt)?Contrib)?$/$1/;
 die "'$name' must end in one of: " . join( ', ', @types ) . "\n" unless $type;
 
 usage(), exit 0 unless $name;
@@ -20,62 +19,110 @@ usage(), exit 0 unless $name;
 chdir $FindBin::Bin;
 
 #print STDERR "$name already exists; will not overwrite (use --f/--force to override)\n";
+# SMELL: should be -d?
 -e $name && die "$name already exists; will not overwrite\n";
 
-my $twiki_lib_dir = '';
 # TOOD: object
-if ( $type =~ /^Plugin/ ) {
-    $twiki_lib_dir = "lib/TWiki/Plugins";
-} elsif ( $type =~ /^Contrib/ ) {
-    $twiki_lib_dir = "lib/TWiki/Contrib";
-}
-my $lib_base_dir = "$name/$twiki_lib_dir";
-eval { mkpath "$lib_base_dir/$name" };
-$@ && die $@;
+my $extension = TWiki::Builder::Extension::Factory::new( $type, $name );
+#print Dumper( $extension );
 
-{ # BuildContrib
-    # TODO: object
-    my $build_pl_dir;
-    if ( $type =~ /^Plugin/ ) {
-	$build_pl_dir = "BuildContrib/lib/TWiki/Contrib/BuildContrib";
-    } elsif ( $type =~ /^Contrib/ ) {
-	$build_pl_dir = "EmptyContrib/lib/TWiki/Contrib/EmptyContrib";
-    }
-    open( BUILD, '<', "$build_pl_dir/build.pl" ) or die $!;
-    local $/ = undef;
-    my $build = <BUILD>;
-    close BUILD;
+# could have command-line switches to optionally not generate various bits
+$extension->generateMinimalExtension();
+$extension->generateBuildContrib();
+$extension->generateSample();
+$extension->generateUnitTests();
 
-    $build =~ s/(Build|Empty)Contrib/$name/g;
+################################################################################
 
-    open( BUILD, '>', "$lib_base_dir/$name/build.pl" ) or die $!;
-    print BUILD $build;
-    close BUILD;
+sub usage
+{
+    print STDERR <<__USAGE__;
+Usage: create-new-extension.pl ExtensionName
+__USAGE__
 }
 
-{ # PluginSkeleton
+################################################################################
+
+package TWiki::Builder::Extension;
+
+use File::Path qw( mkpath );
+
+################################################################################
+
+sub generateMinimalExtension
+{
+    my $self = shift or die;
+
+    my $lib_base_dir = $self->{lib_base_dir} or die "lib_base_dir?";
+    my $name = $self->{name} or die "name?";
+
+    # Plugin.pm
     my $pm = '';
-    # TOOD: object
-    if ( $type =~ /^Plugin/ )
-    {
-	open( PM, '<', "../lib/TWiki/Plugins/EmptyPlugin.pm" ) or die $!;
-	local $/ = undef;
-	$pm = <PM>;
-	close PM;
-    } elsif ( $type =~ /^Contrib/ )
-    {
-	open( CONTRIB, '<', "EmptyContrib/lib/TWiki/Contrib/EmptyContrib.pm" ) or die $!;
-	local $/ = undef;
-	$pm = <CONTRIB>;
-	close CONTRIB;
-    }
+
+    open( PM, '<', $self->{module_template} ) or die $!;
+    local $/ = undef;
+    $pm = <PM>;
+    close PM;
 
     $pm =~ s/Empty$type/$name/g;
+#    $pm =~ s/EmptyContrib/$name/g;
+#    $pm =~ s/EmptyPlugin/$name/g;
 
     open( PM, '>', "$lib_base_dir/$name.pm" ) or die $!;
     print PM $pm;
     close PM;
 
+    # Plugin.txt
+    eval { mkpath "$name/data/TWiki/" };
+    $@ && die $@;
+
+    my $topic = '';
+    open( TOPIC, '<', $self->{extension_topic_template} ) or die $!;
+    local $/ = undef;
+    $topic = <TOPIC>;
+    close TOPIC;
+
+    $topic =~ s/EmptyPlugin/$name/g;
+    $topic =~ s/EmptyContrib/$name/g;
+
+    # adjust variables examples to use the actual topic name
+    $topic =~ s/EMPTYPLUGIN/\U$name/g;
+    $topic =~ s/EMPTYCONTRIB/\U$name/g;
+
+    # stick in the username as the author (hey, it's better than nothing)
+    chop( my $whoami = `whoami` );
+#    $topic =~ s/(\|\s*.*Author:\s*\|).*?(\|)/$1 TWiki:Main.$whoami $2/gs;
+    $topic =~ s/%USERNAME%/$whoami/gs;
+
+    open( TOPIC, '>', "$name/data/TWiki/$name.txt" ) or die $!;
+    # TODO: get page from twiki.org
+    print TOPIC $topic;
+    close TOPIC;
+}
+
+################################################################################
+
+sub generateBuildContrib
+{
+    my $self = shift or die;
+
+    my $lib_base_dir = $self->{lib_base_dir} or die "lib_base_dir?";
+    my $name = $self->{name} or die "name?";
+    my $twiki_lib_dir = $self->{twiki_lib_dir} or die "twiki_lib_dir?";
+
+    # build.pl
+    open( BUILD, '<', "$self->{build_pl_dir}/build.pl" ) or die $!;
+    local $/ = undef;
+    my $build = <BUILD>;
+    close BUILD;
+
+    $build =~ s/(Build|Empty)(HeaderArt)?Contrib/$name/g;
+
+    open( BUILD, '>', "$self->{lib_base_dir}/${name}/build.pl" ) or die $!;
+    print BUILD $build;
+    close BUILD;
+
+    # MANIFEST
     open( MANIFEST, '>', "$lib_base_dir/$name/MANIFEST" ) or die $!;
     print MANIFEST <<__MANIFEST__;
 data/TWiki/$name.txt  Plugin doc page
@@ -83,53 +130,56 @@ $twiki_lib_dir/$name.pm  Plugin Perl module
 __MANIFEST__
     close MANIFEST;
 
+    # DEPENDENCIES
     open( DEPENDENCIES, '>', "$lib_base_dir/$name/DEPENDENCIES" ) or die $!;
     close DEPENDENCIES;
+}
 
-    eval { mkpath "$name/data/TWiki/" };
-    $@ && die $@;
+################################################################################
 
-    # TODO: object
-    my $topic = '';
-    if ( $type =~ /^Plugin/ ) {
-	open( TOPIC, '<', "../data/TWiki/EmptyPlugin.txt" ) or die $!;
+sub generateSample
+{
+    my $self = shift or die;
+
+    my $name = $self->{name} or die "name?";
+    my $lib_base_dir = $self->{lib_base_dir} or die "lib_base_dir?";
+
+    # Sandbox example topic
+    eval { mkpath "$name/data/Sandbox/" };
+    {
+	my $template_file = "EmptyContrib/data/Sandbox/PluginTestEmptyContrib.txt";
+	open( TEST, '<', $template_file ) or die $!;
 	local $/ = undef;
-	$topic = <TOPIC>;
-	close TOPIC;
+	my $test = <TEST>;
+	close TEST;
 
-	$topic =~ s/EmptyPlugin/$name/g;
-	# adjust examples to use the actual topic name
-	$topic =~ s/EMPTYPLUGIN/\U$name/g;
+	$test =~ s/EmptyContrib/$name/g;
+	( my $output_file = $template_file ) =~ s/EmptyContrib/$name/g;
 
-	# stick in the username as the author (hey, it's better than nothing)
-	chop( my $whoami = `whoami` );
-	$topic =~ s/(\|\s*Plugin\s+Author:\s*\|).*?(\|)/$1 TWiki:Main.$whoami $2/gs;
-	$topic =~ s/(\-\-\-\+ )Empty TWiki Plugin.*?(\-\-\-\+)/$1$name\n\nDescribe the plugin\n\n$2/gs;
+	open( TEST, '>', $output_file ) or die $!;
+	print TEST $test;
+	close TEST;
 
-    } elsif ( $type =~ /^Contrib/ ) {
-	open( TOPIC, '<', "EmptyContrib/data/TWiki/EmptyContrib.txt" ) or die $!;
-	local $/ = undef;
-	$topic = <TOPIC>;
-	close TOPIC;
-
-	$topic =~ s/EmptyContrib/$name/g;
+	# update MANIFEST
+	open( MANIFEST, '>>', "$lib_base_dir/$name/MANIFEST" ) or die $!;
+	print MANIFEST <<__MANIFEST__;
+data/Sandbox/PluginTest$name.txt  Plugin examples
+__MANIFEST__
+	close MANIFEST;
     }
+}
 
-    open( TOPIC, '>', "$name/data/TWiki/$name.txt" ) or die $!;
-    # TODO: get page from twiki.org
-    print TOPIC $topic;
-    close TOPIC;
+################################################################################
 
-    ################################################################################
-    # make unit tests
-    ################################################################################
+sub generateUnitTests
+{
+    my $self = shift or die;
+
+    my $name = $self->{name} or die "name?";
+    my $unit_test_dir = $self->{unit_test_template_dir} or die "unit_test_template_dir?";
+
+    # yes, these all say EmptyContrib -- that's ok (replacements are purely based on the extension name)
     eval { mkpath "$name/test/unit/$name/" };
-    my $unit_test_dir;
-    if ( $type =~ /^Plugin/ ) {
-	$unit_test_dir = "EmptyContrib/test/unit/EmptyContrib";
-    } elsif ( $type =~ /^Contrib/ ) {
-	$unit_test_dir = "EmptyContrib/test/unit/EmptyContrib";
-    }
     # *Tests.pm
     {
 	my $template_file = "$unit_test_dir/EmptyContribTests.pm";
@@ -161,50 +211,154 @@ __MANIFEST__
 	print TEST $test;
 	close TEST;
     }
-    
-    # Sandbox example topic
-    eval { mkpath "$name/data/Sandbox/" };
-    {
-	my $template_file = "EmptyContrib/data/Sandbox/PluginTestEmptyContrib.txt";
-	open( TEST, '<', $template_file ) or die $!;
-	local $/ = undef;
-	my $test = <TEST>;
-	close TEST;
-
-	$test =~ s/EmptyContrib/$name/g;
-	( my $output_file = $template_file ) =~ s/EmptyContrib/$name/g;
-
-	open( TEST, '>', $output_file ) or die $!;
-	print TEST $test;
-	close TEST;
-
-	# update MANIFEST
-	open( MANIFEST, '>>', "$lib_base_dir/$name/MANIFEST" ) or die $!;
-	print MANIFEST <<__MANIFEST__;
-data/Sandbox/PluginTest$name.txt  Plugin examples
-__MANIFEST__
-	close MANIFEST;
-    }
-}
-
-# replace-string EmptyPlugin $name
+}    
 
 ################################################################################
 
-sub usage
+package TWiki::Builder::Extension;
+
+sub _init
 {
-    print STDERR <<__USAGE__;
-Usage: create-new-extension.pl ExtensionName
-__USAGE__
+    my $self = shift or die;
+
+    $self->{unit_test_template_dir} = 'EmptyContrib/test/unit/EmptyContrib';
+#    $self->{lib_base_dir} = "$self->{name}/$self->{twiki_lib_dir}";
 }
 
 ################################################################################
 
-__DATA__
+package TWiki::Builder::Extension::Plugin;
+use base( qw( TWiki::Builder::Extension ) );
 
-# create build.pl
-plugins: BuildContrib
-contribs: EmptyContrib
+use File::Path qw( mkpath );
+use Data::Dumper qw( Dumper );
 
-twikiplugins/EmptyContrib
-lib/TWiki/Plugins/EmptyPlugin.pm
+sub _init
+{
+    my $self = shift or die;
+
+    $self->SUPER::_init();
+
+    $self->{name} = $name;
+    $self->{twiki_lib_dir} = 'lib/TWiki/Plugins';
+    $self->{build_pl_dir} = 'BuildContrib/lib/TWiki/Contrib/BuildContrib';
+    $self->{module_template} = '../lib/TWiki/Plugins/EmptyPlugin.pm';
+    $self->{extension_topic_template} = '../data/TWiki/EmptyPlugin.txt';
+    $self->{lib_base_dir} = "$self->{name}/$self->{twiki_lib_dir}";
+}
+
+sub new
+{
+#    print "new Plugin:", Dumper( \@_ );
+    my $class = shift;
+    my $name = shift;
+    my $self  = {};
+    bless( $self, $class );
+    $self->_init();
+
+    eval { mkpath "$self->{lib_base_dir}/$self->{name}" };
+    $@ && die $@;
+
+    return $self;
+}
+
+################################################################################
+
+package TWiki::Builder::Extension::Contrib;
+use base( qw( TWiki::Builder::Extension ) );
+
+use File::Path qw( mkpath );
+
+sub _init
+{
+    my $self = shift;
+
+    $self->SUPER::_init();
+
+    $self->{name} = $name;
+    $self->{twiki_lib_dir} = 'lib/TWiki/Contrib';
+    $self->{build_pl_dir} = 'EmptyContrib/lib/TWiki/Contrib/EmptyContrib';
+    $self->{module_template} = 'EmptyContrib/lib/TWiki/Contrib/EmptyContrib.pm';
+    $self->{extension_topic_template} = 'EmptyContrib/data/TWiki/EmptyContrib.txt';
+    $self->{lib_base_dir} = "$self->{name}/$self->{twiki_lib_dir}";
+
+    eval { mkpath "$self->{lib_base_dir}/$self->{name}" };
+    $@ && die $@;
+}
+
+sub new
+{
+    my $class = shift;
+    my $name = shift;
+    my $self  = {};
+    bless( $self, $class );
+    $self->_init();
+
+    return $self;
+}
+
+################################################################################
+
+package TWiki::Builder::Extension::Contrib::HeaderArt;
+use base( qw( TWiki::Builder::Extension::Contrib ) );
+
+use File::Path qw( mkpath );
+
+sub _init
+{
+    my $self = shift;
+
+    $self->SUPER::_init();
+
+    $self->{name} = $name;
+    $self->{twiki_lib_dir} = 'lib/TWiki/Contrib';
+    $self->{build_pl_dir} = 'EmptyHeaderArtContrib/lib/TWiki/Contrib/EmptyHeaderArtContrib';
+    $self->{module_template} = 'EmptyHeaderArtContrib/lib/TWiki/Contrib/EmptyHeaderArtContrib.pm';
+    $self->{extension_topic_template} = 'EmptyHeaderArtContrib/data/TWiki/EmptyHeaderArtContrib.txt';
+    $self->{lib_base_dir} = "$self->{name}/$self->{twiki_lib_dir}";
+
+    eval { mkpath "$self->{lib_base_dir}/$self->{name}" };
+    $@ && die $@;
+}
+
+sub new
+{
+#    die "not implemented yet; will inherit from TWiki::Builder::Contrib";
+    my $class = shift;
+    my $self  = {};
+    bless( $self, $class );
+    $self->_init();
+    return $self;
+}
+
+sub generateMinimalExtension
+{
+    my $self = shift or die;
+    my $name = $self->{name} or die "name?";
+
+    $self->SUPER::generateMinimalExtension();
+
+    mkpath( "$name/pub/TWiki/$name" );
+}
+
+################################################################################
+
+package TWiki::Builder::Extension::Factory;
+
+sub new
+{
+    my ( $type, $name ) = @_;
+    die "name?" unless $name;
+
+    # may need to massage type into a class-compatable name
+    my $class = $type;
+    if ( my ( $subclass, $baseclass ) = $class =~ /(HeaderArt)(Contrib|Plugin)$/ )
+    {
+	$class = "$2::$1";
+    }
+
+    $class = "TWiki::Builder::Extension::$class";
+    return $class->new( $name );
+}
+
+# replace-string EmptyPlugin $name (what does that mean?)
