e1c4a8b05747b14cd7dcfb70d9df8c2cc32753d5
WillNorris <WillNorris@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-13 01:38:08 +0000

    Item741: primitive build caching system so that all libraries aren't built from scratch everything (this brings the build time down on my system from about 1.5 hours to about a minute)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@8260 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/CpanContrib/lib/TWiki/Contrib/CpanContrib/build.pl
+++ twikiplugins/CpanContrib/lib/TWiki/Contrib/CpanContrib/build.pl
@@ -15,7 +15,6 @@ BEGIN {
   unshift @INC, split( /:/, $ENV{TWIKI_LIBS} );
 }
 
-use File::Find;
 use TWiki::Contrib::Build;
 
 # Declare our build package
@@ -24,6 +23,7 @@ use base qw( TWiki::Contrib::Build );
 
 use File::Path qw( mkpath rmtree );
 use FindBin;
+use Data::Dumper qw( Dumper );		# not (just) for debugging, but for loading and saving the build cache
 
 sub new {
     my $class = shift;
@@ -70,35 +70,96 @@ sub target_build {
 	}
     }
 
-    # Do other build stuff here
+    # Do other build stuff here (?)
     # get cpan minimirror up-to-date
     # create twiki mini-minicpan mirror (also publish this)
-    # only rebuild if CPAN module is newer than the already-built files! (*big* timesaver!)
 
     use Cwd;
     my $base_lib_dir = getcwd . "/../../../../lib/CPAN";
-    -e $base_lib_dir && rmtree $base_lib_dir;
-    mkpath $base_lib_dir or die $!;
+    # to keep "old" builds so that everything doesn't need to be built from scratch (for reasonable build times)
+#    -e $base_lib_dir && rmtree $base_lib_dir;
+    -e $base_lib_dir || mkpath $base_lib_dir or die $!;
     chdir 'bin' or die $!;
     ++$|;
 
-    foreach my $module ()
-#    foreach my $module ( @CpanContrib::Modules::CPAN )
+    my $build_cache_filename = '.build_cache';
+
+    my $build_cache;
+    if ( open( BUILD_CACHE, '<', $build_cache_filename ) )
+    {   # read module build cache
+	local $/ = undef;
+	$file = <BUILD_CACHE>;
+	close BUILD_CACHE;
+
+	$build_cache = eval $file;
+    }
+
+    foreach my $module ( @CpanContrib::Modules::CPAN )
     {
 	# clean out old build stuff (in particular, ExtUtils::MakeMaker leaves bad stuff lying around)
 	my $dirCpanBuild = "$base_lib_dir/.cpan/build/";
 	# SMELL: fixed unix-specific chmod shell call
 	system( chmod => '-R' => 'a+rwx' => $dirCpanBuild ), rmtree $dirCpanBuild if -d $dirCpanBuild;
 
-	print "Installing $module\n";
-	print "-" x 80, "\n";
-	`perl install-cpan.pl --mirror=../../../../../../../../MIRROR/MINICPAN/ --baselibdir=$base_lib_dir $module </dev/null`;
+	if ( _upToDate( $build_cache, $module ) )
+	{
+	    print "Skipping $module (already built)\n";
+	    print "-" x 80, "\n";
+	}
+	else
+	{
+	    print "Installing $module\n";
+	    print "-" x 80, "\n";
+	    my $mirror = '../../../../../../../../MIRROR/MINICPAN/';
+	    $build_cache->{$module}->{timebuilt} = time;	# earlier timestamp is better than later
+	    my $INSTALL_CPAN = `perl install-cpan.pl --mirror=$mirror --baselibdir=$base_lib_dir $module </dev/null`;
+	    # fill in the cache information
+	    ( my $CPAN_FILE ) = $INSTALL_CPAN =~ /.*CPAN\.pm: Going to build (.*)/;
+	    $build_cache->{$module}->{CPAN_FILE} = "$mirror/authors/id/$CPAN_FILE";
+
+	    # save module build cache
+	    open( BUILD_CACHE, '>', $build_cache_filename );
+	    $Data::Dumper::Sortkeys = sub { my ( $hash ) = @_; return [ sort keys %$hash ] };
+	    print BUILD_CACHE Dumper( $build_cache );
+	    close BUILD_CACHE;
+	}
     }
 
     # cleanup the intermediate CPAN build directories
     `chmod -R 777 $base_lib_dir/.cpan`;
     rmtree "$base_lib_dir/.cpan";
 }
+
+################################################################################
+
+# SMELL: doesn't really handle modules that didn't build correctly
+# (eg, XML::LibXML, XML::LibXSLT, SVG, etc.), but i don't have a way to build them now anyway...
+# SMELL: also, doesn't handle if you manually delete the built library files without also deleting the cache
+# (*maybe* i can check the return code, but i'm doing force installs, so that _might_ not work)
+# (or maybe i can do more parsing of the output to see if it copied any files anywhere)
+sub _upToDate
+{
+    my $cache = shift;
+    # no cache information; it "can't" be up-to-date
+    return 0 unless $cache;
+
+    my $module = shift or die "module?";
+
+    my $module_cache = $cache->{$module};
+
+    # if cache doesn't know anything about it, we can't say it's up-to-date
+    return 0 unless $module_cache->{timebuilt};
+
+    # newer versions of the module won't (quite) have the same name and therefore, won't be found
+    return 0 unless -e $module_cache->{CPAN_FILE};
+
+    # simply, if the source file is newer, rebuild
+    return 0 if (stat( $module_cache->{CPAN_FILE} ))[9] > $module_cache->{timebuilt};
+
+    return 1;
+}
+
+################################################################################
 
 package main;
 # Create the build object
