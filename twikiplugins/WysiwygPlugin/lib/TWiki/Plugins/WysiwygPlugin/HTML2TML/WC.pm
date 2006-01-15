193e7b9bd91fac36c3949dd1985e18e4ab1543bb
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-15 13:28:24 +0000

    Item1231: added %SPAN% to indicate a spanned-over cell in the editor. Improved handling of HTML in verbatim tags by inserting line breaks is the tag type calls for it, before removing the HTML.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@8300 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/WysiwygPlugin/lib/TWiki/Plugins/WysiwygPlugin/HTML2TML/WC.pm
+++ twikiplugins/WysiwygPlugin/lib/TWiki/Plugins/WysiwygPlugin/HTML2TML/WC.pm
@@ -19,7 +19,7 @@
 
 ---+ package WC
 
-Base class of Node and Leaf
+Constants, and base class of Node and Leaf
 
 _generate
 _cleanAttrs
@@ -42,10 +42,12 @@ package WC;
 
 use vars qw( $NO_TML $NO_BLOCK_TML $NOP_ALL $BLOCK_TML );
 
-$NO_TML = 1 << 0;
-$NO_BLOCK_TML = 1 << 1;
-$NOP_ALL = 1 << 2;
-$BLOCK_TML = $NO_BLOCK_TML;
+$NO_HTML      = 1 << 0;
+$NO_TML       = 1 << 1;
+$NO_BLOCK_TML = 1 << 2;
+$NOP_ALL      = 1 << 3;
+
+$BLOCK_TML    = $NO_BLOCK_TML;
 
 =pod
 
@@ -89,6 +91,105 @@ use vars qw( $STARTWW $ENDWW $PROTOCOL );
 $STARTWW = qr/^|(?<=[ \t\n\(\!])/om;
 $ENDWW = qr/$|(?=[ \t\n\,\.\;\:\!\?\)])/om;
 $PROTOCOL = qr/^(file|ftp|gopher|http|https|irc|news|nntp|telnet|mailto):/;
+
+# Table of HTML tags that says whether they should have a newline before
+# them when they are encountered in a preformatted (verbatim) block. This
+# supports very crude formatting during removal of HTML tags.
+use vars qw( %BREAK_BEFORE );
+
+%BREAK_BEFORE = (
+    A => 0,
+    ABBR => 0,
+    ACRONYM => 0,
+    ADDRESS => 0,
+    APPLET => 1,
+    AREA => 0,
+    B => 0,
+    BASE => 0,
+    BASEFONT => 0,
+    BDO => 0,
+    BIG => 0,
+    BLOCKQUOTE => 1,
+    BODY => 1,
+    BR => 1,
+    BUTTON => 0,
+    CAPTION => 0,
+    CENTER => 1,
+    CITE => 0,
+    CODE => 0,
+    COL => 0,
+    COLGROUP => 0,
+    DD => 0,
+    DEL => 0,
+    DFN => 0,
+    DIR => 1,
+    DIV => 1,
+    DL => 1,
+    DT => 1,
+    EM => 0,
+    FIELDSET => 1,
+    FONT => 0,
+    FORM => 1,
+    FRAME => 1,
+    FRAMESET => 1,
+    H1 => 1,
+    H2 => 1,
+    H3 => 1,
+    H4 => 1,
+    H5 => 1,
+    H6 => 1,
+    HEAD => 1,
+    HR => 1,
+    HTML => 1,
+    I => 0,
+    IFRAME => 0,
+    IMG => 0,
+    INPUT => 0,
+    INS => 0,
+    ISINDEX => 1,
+    KBD => 0,
+    LABEL => 0,
+    LEGEND => 1,
+    LI => 1,
+    LINK => 1,
+    MAP => 1,
+    MENU => 1,
+    META => 1,
+    NOFRAMES => 0,
+    NOSCRIPT => 1,
+    OBJECT => 1,
+    OL => 1,
+    OPTGROUP => 0,
+    OPTION => 0,
+    P => 1,
+    PARAM => 1,
+    PRE => 1,
+    Q => 0,
+    S => 0,
+    SAMP => 0,
+    SCRIPT => 1,
+    SELECT => 0,
+    SMALL => 0,
+    SPAN => 0,
+    STRIKE => 0,
+    STRONG => 0,
+    STYLE => 1,
+    SUB => 0,
+    SUP => 0,
+    TABLE => 1,
+    TBODY => 1,
+    TD => 0,
+    TEXTAREA => 1,
+    TFOOT => 1,
+    TH => 0,
+    THEAD => 1,
+    TITLE => 1,
+    TR => 1,
+    TT => 0,
+    U => 0,
+    UL => 1,
+    VAR => 0,
+   );
 
 # pure virtual
 sub generate {
