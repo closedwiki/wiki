ee247dad7059abb0a88167b8a9cac258f64b9a1e
SteffenPoulsen <SteffenPoulsen@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-21 20:23:16 +0000

    Item1461: Moving registration e-mail address from form to topic text, naive obfuscation performed
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP@8415 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/UI/Register.pm
+++ lib/TWiki/UI/Register.pm
@@ -885,6 +885,10 @@ sub _getKeyValuePairsAsTopicForm {
         # from no key at all.
         if ( $name eq 'Comment' ) {
             $leftoverText .= "\t* $name: $value\n"; #SMELL - tab not 3 spaces
+        } elsif ( $name eq 'Email' ) {
+            $value =~ s/(^.*?)@(.*?$)/$1 at $2/go;     # Naive approach on obfuscating the e-mail address
+            $value =~ s/\./ dot /go;                   # http://www.cdt.org/speech/spam/030319spamreport.shtml says it'll work
+            $leftoverText .= "\t* E-mail: " . TWiki::entityEncode( $value ) . "\n";
         } else {
             $meta->putKeyed( 'FIELD',
                         { name => $name, value => $value, title => $title } );
