134701c25c23bf8b809aa73dca8b5635bce4a0cf
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2006-01-30 16:17:35 +0000

    Item840:    * replaced NOPs with STARSECTIONs    * fixed customized search view
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/DEVELOP/twikiplugins@8591 a00a5322-12db-0310-a70b-8735589c885e

--- BlogPlugin/lib/TWiki/Plugins/BlogPlugin.pm
+++ BlogPlugin/lib/TWiki/Plugins/BlogPlugin.pm
@@ -24,7 +24,7 @@ use vars qw(
 use TWiki::Plugins::BlogPlugin::WebDB; # must be compiled in advance
 
 $VERSION = '$Rev$';
-$RELEASE = '0.46';
+$RELEASE = '0.47';
 
 ###############################################################################
 sub initPlugin {
