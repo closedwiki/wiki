10359d20b1c747e7b1cb055cedb76533a2b238f8
SvenDowideit <SvenDowideit@a00a5322-12db-0310-a70b-8735589c885e>
2006-02-07 15:05:26 +0000

    Item000: BUILD TWiki-4.0.1 at Tue Feb  7 15:07:18 2006 GMT
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@8739 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki.pm
+++ lib/TWiki.pm
@@ -155,7 +155,7 @@ BEGIN {
     # DO NOT CHANGE THE FORMAT OF $VERSION
     # automatically expanded on checkin of this module
     $VERSION = '$Date$ $Rev$ ';
-    $RELEASE = 'TWiki-4.0.0';
+    $RELEASE = 'TWiki-4.0.1';
     $VERSION =~ s/^.*?\((.*)\).*: (\d+) .*?$/$1 build $2/;
 
     # Default handlers for different %TAGS%
