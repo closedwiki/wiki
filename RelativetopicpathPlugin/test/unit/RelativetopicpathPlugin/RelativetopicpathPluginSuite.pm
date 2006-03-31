b22561713e123905e76533fb08481de76c85b26b
MeredithLesly <MeredithLesly@a00a5322-12db-0310-a70b-8735589c885e>
2006-03-31 15:25:37 +0000

    Item1999: Plugin that implements the functionality of the TWiki.pm variable (atm)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00/twikiplugins@9617 a00a5322-12db-0310-a70b-8735589c885e

---
+++ RelativetopicpathPlugin/test/unit/RelativetopicpathPlugin/RelativetopicpathPluginSuite.pm
@@ -0,0 +1,9 @@
+package RelativetopicpathPluginSuite;
+
+use base qw(Test::Unit::TestSuite);
+
+sub name { 'RelativetopicpathPluginSuite' };
+
+sub include_tests { qw(RelativetopicpathPluginTests) };
+
+1;
