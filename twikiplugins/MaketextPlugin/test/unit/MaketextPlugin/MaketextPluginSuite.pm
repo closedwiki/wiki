a3cc3161ebd7cb78aa1257180055fde3e589b264
SteffenPoulsen <SteffenPoulsen@a00a5322-12db-0310-a70b-8735589c885e>
2006-04-02 19:00:17 +0000

    Reverting 9613-9617, now lives in DrusillaBranch
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@9656 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/MaketextPlugin/test/unit/MaketextPlugin/MaketextPluginSuite.pm
+++
@@ -1,9 +0,0 @@
-package MaketextPluginSuite;
-
-use base qw(Test::Unit::TestSuite);
-
-sub name { 'MaketextPluginSuite' };
-
-sub include_tests { qw(MaketextPluginTests) };
-
-1;
--- twikiplugins/TopiclistPlugin/test/unit/TopiclistPlugin/TopiclistPluginSuite.pm
+++
@@ -1,9 +0,0 @@
-package TopiclistPluginSuite;
-
-use base qw(Test::Unit::TestSuite);
-
-sub name { 'TopiclistPluginSuite' };
-
-sub include_tests { qw(TopiclistPluginTests) };
-
-1;
