87f4c9e91d29f3cf81c9813ab4eba226976ade3c
SteffenPoulsen <SteffenPoulsen@a00a5322-12db-0310-a70b-8735589c885e>
2006-04-02 19:00:17 +0000

    Reverting 9613-9617, now lives in DrusillaBranch
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00/twikiplugins@9656 a00a5322-12db-0310-a70b-8735589c885e

--- RelativetopicpathPlugin/test/unit/RelativetopicpathPlugin/RelativetopicpathPluginSuite.pm
+++
@@ -1,9 +0,0 @@
-package RelativetopicpathPluginSuite;
-
-use base qw(Test::Unit::TestSuite);
-
-sub name { 'RelativetopicpathPluginSuite' };
-
-sub include_tests { qw(RelativetopicpathPluginTests) };
-
-1;
--- WeblistPlugin/test/unit/WeblistPlugin/WeblistPluginSuite.pm
+++
@@ -1,9 +0,0 @@
-package WeblistPluginSuite;
-
-use base qw(Test::Unit::TestSuite);
-
-sub name { 'WeblistPluginSuite' };
-
-sub include_tests { qw(WeblistPluginTests) };
-
-1;
