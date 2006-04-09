45ca11ede62706146db3b7821e4f8690267e957e
SteffenPoulsen <SteffenPoulsen@a00a5322-12db-0310-a70b-8735589c885e>
2006-04-09 12:20:53 +0000

    Item2064: CompareRevisionAddOn: Added MANIFEST
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00/twikiplugins@9754 a00a5322-12db-0310-a70b-8735589c885e

--- CompareRevisionsAddOn/lib/TWiki/Contrib/CompareRevisionsAddOn/build.pl
+++ CompareRevisionsAddOn/lib/TWiki/Contrib/CompareRevisionsAddOn/build.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# Build for CompareRevisionsAddonPlugin
+# Build for CompareRevisionsAddOn
 #
 BEGIN {
   foreach my $pc (split(/:/, $ENV{TWIKI_LIBS})) {
@@ -11,7 +11,7 @@ BEGIN {
 use TWiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build( 'CompareRevisionsAddonPlugin' );
+$build = new TWiki::Contrib::Build( 'CompareRevisionsAddOn' );
 
 # Build the target on the command line, or the default target
 $build->build($build->{target});
