6c2320431271b71cb033d6c6baed96aa804c32f2
SteffenPoulsen <SteffenPoulsen@a00a5322-12db-0310-a70b-8735589c885e>
2006-04-10 15:57:27 +0000

    Item2073: FindElsewherePlugin: Dakar update
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@9777 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/FindElsewherePlugin/lib/TWiki/Plugins/FindElsewherePlugin.pm
+++ twikiplugins/FindElsewherePlugin/lib/TWiki/Plugins/FindElsewherePlugin.pm
@@ -14,12 +14,31 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at
+# GNU General Public License for more details, published at 
 # http://www.gnu.org/copyleft/gpl.html
 #
-# This is the FindElsewhere TWiki plugin,
+# =========================
+#
+# This is the FindElsewhere TWiki plugin, 
 # see http://twiki.org/cgi-bin/view/Plugins/FindElsewherePlugin for details.
 #
+# Each plugin is a package that contains the subs:
+#
+#   initPlugin           ( $topic, $web, $user, $installWeb )
+#   commonTagsHandler    ( $text, $topic, $web )
+#   startRenderingHandler( $text, $web )
+#   outsidePREHandler    ( $text )
+#   insidePREHandler     ( $text )
+#   endRenderingHandler  ( $text )
+#
+# initPlugin is required, all other are optional. 
+# For increased performance, all handlers except initPlugin are
+# disabled. To enable a handler remove the leading DISABLE_ from
+# the function name.
+# 
+# NOTE: To interact with TWiki use the official TWiki functions
+# in the &TWiki::Func module. Do not reference any functions or
+# variables elsewhere in TWiki!!
 
 ## Changelog
 # 29-Jan-2002:	Mike Barton
@@ -34,204 +53,245 @@
 # 12-Feb-2004:	Matt Wilkie 
 #		- put all of above into twikiplugins cvs, 
 #		- removed "/o"'s as there may be issues with modperl (Codev.ModPerl)
-# 31-Mar-2005:  SteffenPoulsen
+# 31-Mar-2005: SteffenPoulsen
 #		- updated plugin to be I18N-aware
-# 02-Apr-2005:  SteffenPoulsen
+# 02-Apr-2005: SteffenPoulsen
 #		- fixed problems with WikiWordAsWebName.WikiWord
-# 12-Aug-2005 - Crawford Currie
-#       - improved conformance to standards, incremented version #
-# 15-Dec-2005 - SteffenPoulsen
-#       - improved handling of the special case [[Main.ABBR][Some desc with a found ABBRTWO]]
-#       - is now ignored correctly, instead of breaking the link
-# 14-Mar-2006 - MichaeDaum
-#       - repsects <noautolink> ... </noautolink> blocks as well as the
-#         NOAUTOLINK preference flag
+# 04-Apr-2005: SteffenPoulsen
+#		- made plugin less greedy - now leaves [[WikiWord][long link with ACRONYM or WikiWord]] alone
+# 05-Apr-2005: SteffenPoulsen
+#		- bugfix: Preambles for ACRONYMS were doubled
+# 08-Apr-2005: SteffenPoulsen
+#		- negated vars "DISABLELOOKELSEWHERE" and "DISABLEPLURALTOSINGULAR" can now be set per topic or web (WebPreferences)
+# 07-Apr-2006: ScottHunter
+#		- replaced direct usage of %regex with TWiki::Func::getRegularExpression()
+#		- replaced some implicit scalar references with explicit $ notation
+#
 
+# =========================
 package TWiki::Plugins::FindElsewherePlugin;
 
+# =========================
 use vars qw(
-            $web $topic $user $installWeb $VERSION $RELEASE
-            $plural2SingularEnabled
-            $wnre
-            $wwre
-            $manre
-            $abbre
-	    $noAutolink
-           );
-use vars qw( %TWikiCompatibility );
+	    $web $topic $user $installWeb $VERSION $debug
+	    $disabledFlag $disablePluralToSingular
+	    $webNameRegex $wikiWordRegex $abbrevRegex $singleMixedAlphaNumRegex
+	    );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
-$VERSION = '$Rev$';
 
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
-
-sub initPlugin {
-    ( $topic, $web, $user, $installWeb ) = @_;
-
-    # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        TWiki::Func::writeWarning( "Version mismatch between FindElsewherePlugin and Plugins.pm" );
-        return 0;
+# ===========================
+# Read the configuration file at compile time in order to set locale
+BEGIN {
+    # Do a dynamic 'use locale' for this module
+    if( $TWiki::useLocale ) {
+        require locale;
+        import locale ();
     }
+} 
 
-    my $pp = 'FINDELSEWHEREPLUGIN_';
-    $lookElsewhereDisabled =
-      TWiki::Func::getPreferencesFlag( 'DISABLELOOKELSEWHERE' ) ||
-          TWiki::Func::getPreferencesFlag( $pp.'DISABLELOOKELSEWHERE' );
+$VERSION = '1.000';
 
-    $otherWebs =
-      TWiki::Func::getPreferencesValue( $pp.'LOOKELSEWHEREWEBS' ) || '';
-    @webList = split( /[\,\s]+/, $otherWebs );
+# =========================
+sub initPlugin
+{
+   ( $topic, $web, $user, $installWeb ) = @_;
+   
+   # check for Plugins.pm versions
+   if( $TWiki::Plugins::VERSION < 1 ) {
+      &TWiki::Func::writeWarning( "Version mismatch between FindElsewherePlugin and Plugins.pm" );
+      return 0;
+   }
 
-    $plural2SingularEnabled =
-      !TWiki::Func::getPreferencesFlag( 'DISABLEPLURALTOSINGULAR' ) &&
-        !TWiki::Func::getPreferencesFlag( $pp.'DISABLEPLURALTOSINGULAR' );
+   # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
+   $debug = &TWiki::Func::getPreferencesFlag( "FINDELSEWHEREPLUGIN_DEBUG" );
 
-    $wnre = TWiki::Func::getRegularExpression('webNameRegex');
-    $wwre = TWiki::Func::getRegularExpression('wikiWordRegex');
-    $manre = TWiki::Func::getRegularExpression('mixedAlphaNum');
-    $abbre = TWiki::Func::getRegularExpression('abbrevRegex');
+   $disabledFlag = &TWiki::Func::getPreferencesFlag( "DISABLELOOKELSEWHERE" ) || 
+          &TWiki::Func::getPreferencesFlag( "FINDELSEWHEREPLUGIN_DISABLELOOKELSEWHERE" ) || 
+          0 ;
 
-    $noAutolink = TWiki::Func::getPreferencesFlag('NOAUTOLINK');
-    # Plugin correctly initialized
-    return 1;
+   $otherWebs = &TWiki::Func::getPreferencesValue( "LOOKELSEWHEREWEBS" ) || 
+          &TWiki::Func::getPreferencesValue( "FINDELSEWHEREPLUGIN_LOOKELSEWHEREWEBS" ) || 
+          "" ;
+
+   $disablePluralToSingular = &TWiki::Func::getPreferencesFlag( "DISABLEPLURALTOSINGULAR" ) || 
+          &TWiki::Func::getPluginPreferencesFlag( "DISABLEPLURALTOSINGULAR" ) || 
+          0 ;
+
+   @webList = split( /[\,\s]+/, $otherWebs );
+
+   $webNameRegex = TWiki::Func::getRegularExpression('webNameRegex');
+   $wikiWordRegex = TWiki::Func::getRegularExpression('wikiWordRegex');
+   $abbrevRegex = TWiki::Func::getRegularExpression('abbrevRegex');
+   
+   my $upperAlphaRegex = TWiki::Func::getRegularExpression(upperAlpha);
+   my $lowerAlphaRegex = TWiki::Func::getRegularExpression(lowerAlpha);
+   my $numericRegex = TWiki::Func::getRegularExpression(numeric);
+   $singleMixedAlphaNumRegex = qr/[$upperAlphaRegex$lowerAlphaRegex$numericRegex]/;
+
+   # Plugin correctly initialized
+   &TWiki::Func::writeDebug( "- TWiki::Plugins::FindElsewherePlugin::initPlugin( $web.$topic ) is OK ($disabledFlag/'disabledFlag', otherWebs/'$otherWebs', disablePluralToSingular/'$disablePluralToSingular', &TWiki::Func::getPreferencesFlag( \"DISABLEPLURALTOSINGULAR\" ))".&TWiki::Func::getPreferencesFlag( "DISABLEPLURALTOSINGULAR" )."(" ) if $debug;
+   return 1;
 }
 
-sub preRenderingHandler {
-    #my ( $text, \%map ) = @_;
-    # SMELL: skips PRE blocks
-    _handle( @_ );
+
+# =========================
+sub startRenderingHandler
+{
+# This handler is called by getRenderedVersion just before the line loop
+### my ( $text, $web ) = @_;   # do not uncomment, use $_[0], $_[1] instead
+
+   &TWiki::Func::writeDebug( "- FindElsewherePlugin::startRenderingHandler( $_[1].$topic )" ) if $debug;
+
+   if ( $disabledFlag ) {
+      &TWiki::Func::writeDebug( "- FindElsewherePlugin::startRenderingHandler( Plugin is disabled, returning $_[1].$topic untouched! )" ) if $debug;
+      return;
+   }
+
+   # Find instances of WikiWords not in this web, but in the otherWeb(s)
+   # If the WikiWord is found in theWeb, put the word back unchanged
+   # If the WikiWord is found in the otherWeb, link to it via [[otherWeb.WikiWord]]
+   # If it isn't found there either, put the word back unchnaged
+
+   # Debug incoming topic text if uncommented
+   # &TWiki::Func::writeDebug( "- FindElsewherePlugin::startRenderingHandler( Incoming text: $_[0] )" ) if $debug;
+
+   # Match 
+   # 0) (Allowed preambles: "\s" and "(")
+   # 1) [[something]] - (including [[something][something]], but non-greedy),
+   # 2) WikiWordAsWebName.WikiWord,
+   # 3) WikiWords, and 
+   # 4) WIK IWO RDS
+   
+   $_[0] =~ s/([\s\(])(\[\[.*?\]\]|$webNameRegex\.$wikiWordRegex|$wikiWordRegex|$abbrevRegex)/&findTopicElsewhere($_[1],$1,$2)/geo;
+
+   # Debug outgoing topic text if uncommented
+   # &TWiki::Func::writeDebug( "- FindElsewherePlugin::startRenderingHandler( Outgoing text: $_[0] )" ) if $debug;
 }
 
-$TWikiCompatibility{startRenderingHandler} = 1.1;
-sub startRenderingHandler {
-    #my ( $text, \%map ) = @_;
-    # SMELL: why is there another call th _handle() here
-    _handle( @_ );
+sub makeTopicLink
+{
+  ##my($otherWeb, $theTopic) = @_;
+  return "[[$_[0].$_[1]][$_[0]]]";
 }
 
-sub _handle {
-    return if $lookElsewhereDisabled && $noAutolink;
+# =========================
+sub findTopicElsewhere
+{
+   # This was copied and pruned from TWiki::internalLink
 
-    # Find instances of WikiWords not in this web, but in the otherWeb(s)
-    # If the WikiWord is found in theWeb, put the word back unchanged
-    # If the WikiWord is found in the otherWeb, link to it via
-    # [[otherWeb.WikiWord]]
-    # If it isn't found there either, put the word back unchnaged
+   my( $theWeb, $thePreamble, $theTopic ) = @_;
+   
+   my $text = $thePreamble;
+   
+   # If we got ourselves something like [[something][something]] or WebName.WikiWord 
+   # the links is absolute. We're done - return untouched info
+   if ($theTopic =~ /(\[\[.*\]\[.*\]\]|$webNameRegex\.$wikiWordRegex)/o) {
+      &TWiki::Func::writeDebug( "FindElsewherePlugin::findTopicElsewhere: \"$theTopic\" is an absolute link, leaving it untouched." ) if $debug;
+      $text .= "$theTopic";
+      return $text;
+   }
+   
+   # preserve link style formatting
+   my $oldTheTopic = $theTopic;
 
-    my $text = $_[0];
-    my $removed = {};
+   # Turn spaced-out names into WikiWords - upper case first letter of
+   # whole link, and first of each word.
+   $theTopic =~ s/^(.)/\U$1/o;
+   $theTopic =~ s/\s($singleMixedAlphaNumRegex)/\U$1/go;
+   $theTopic =~ s/\[\[($singleMixedAlphaNumRegex)(.*)\]\]/\u$1$2/o;
 
-    # SMELL: taking out blocks should be in Func.pm
-    my $renderer = $TWiki::Plugins::SESSION->{renderer};
-    $text = $renderer->takeOutBlocks( $text, 'noautolink', $removed );
+   # Look in the current web, return when found
+   &TWiki::Func::writeDebug( "FindElsewherePlugin::findTopicElsewhere: Looking for \"$theTopic\" elsewhere .." ) if $debug;
+   my $exist = &TWiki::Func::topicExists( $theWeb, $theTopic );
+   if ( ! $exist ) {
+      if ( ( !$disablePluralToSingular ) && ( $theTopic =~ /s$/ ) ) {
+         my $theTopicSingular = &makeSingular( $theTopic );
+         if( &TWiki::Func::topicExists( $theWeb, $theTopicSingular ) ) {
+            &TWiki::Func::writeDebug( "FindElsewherePlugin::findTopicElsewhere: - $theTopicSingular was found in $theWeb" ) if $debug;
+            $text .= "$oldTheTopic"; # leave it as we found it
+            return $text;
+         }
+      }
+   }
+   else  {
+      &TWiki::Func::writeDebug( "FindElsewherePlugin::findTopicElsewhere: - $theTopic was found in $theWeb" ) if $debug;
+      $text .= "$oldTheTopic"; # leave it as we found it
+      return $text;
+   }
+   
+   # Look in the other webs, return when found
+   my @topicLinks;
+   foreach ( @webList ) {
+      my $otherWeb = $_;
 
-    # Match WikiWordAsWebName.WikiWord, WikiWords, [[wiki words]] and
-    # WIK IWO RDS
-    $text =~ s/(^|[\s\(])(\[\[$wnre\.($wwre|$abbre)\]\[[$manre\s]+\]\]|\[\[[$manre\s]+\]\[[$manre\s]+\]\]|$wnre\.$wwre|$wwre|\[\[[$manre\s]+\]\]|$abbre)/$1._findTopicElsewhere($2)/geo;
+      # For systems running WebNameAsWikiName 
+      # If the $theTopic is a reference to a the name of 
+      # otherWeb, point at otherWeb.WebHome - MRJC
+      if ($otherWeb eq $theTopic) {
+         &TWiki::Func::writeDebug( "- $theTopic is the name of another web $otherWeb." );# if $debug;
+         $text .= "[[$otherWeb.WebHome][$otherWeb]]";
+         return $text;
+      }
 
-    $renderer->putBackBlocks( \$text, $removed, 'noautolink' );
-    $_[0] = $text;
-}
-
-sub _makeTopicLink {
-    #my($otherWeb, $theTopic) = @_;
-    return "";
-}
-
-sub _findTopicElsewhere {
-    my $link = shift;
-
-    # If we got ourselves a WikiWordAsWebName.WikiWord, we're done - return untouched info
-    if ($link =~ /$wnre\.$wwre/o) {
-        return $link;
-    }
-
-    # If we got ourselves an explicit [[SOMETHING][SOMETHING]] link, let's leave it untouched
-    if ($link =~ /\[\[[$manre\s]+\]\[[$manre\s]+\]\]/o) {
-        return $link;
-    }
-
-    # preserve link style formatting
-    my $original = $link;
-
-    # Turn spaced-out names into WikiWords - upper case first letter of
-    # whole link, and first of each word.
-    $link =~ s/^(.)/\U$1/o;
-    $link =~ s/\s(\w)/\U$1/go;
-    $link =~ s/\[\[(\w)(.*)\]\]/\u$1$2/o;
-
-    my $text = '';
-
-    my @topicLinks;
-
-    # Look in the current web
-    my $exist = TWiki::Func::topicExists( $web, $link );
-    return $original if $exist;
-
-    if( $plural2SingularEnabled && $link =~ /s$/ ) {
-        my $linkSingular = _makeSingular( $link );
-        if( TWiki::Func::topicExists( $web, $linkSingular ) ) {
-            # $linkSingular was found in $web.
-            return $original; # leave it as we found it
-        }
-    }
-
-    # Look in the other webs, return when found
-    foreach my $otherWeb ( @webList ) {
-
-        # If the $link is a reference to a the name of
-        # otherWeb, point at otherWeb.WebHome
-        if( $otherWeb eq $link ) {
-            return "[[$otherWeb.WebHome][$otherWeb]]";
-        }
-
-        my $exist = TWiki::Func::topicExists( $otherWeb, $link );
-        if( $exist ) {
-            # $link was found in $otherWeb.
-            push(@topicLinks, [ $otherWeb, $link ]);
-        } elsif ( ( $plural2SingularEnabled ) && ( $link =~ /s$/ ) ) {
-            my $linkSingular = _makeSingular( $link );
-            if( TWiki::Func::topicExists( $otherWeb, $linkSingular )) {
-                # $linkSingular was found in $otherWeb.
-                push(@topicLinks, [ $otherWeb, $link ]);
+      my $exist = &TWiki::Func::topicExists( $otherWeb, $theTopic );
+      if ( ! $exist ) {
+         if ( ( !$disablePluralToSingular ) && ( $theTopic =~ /s$/ ) ) {
+            my $theTopicSingular = &makeSingular( $theTopic );
+            if( &TWiki::Func::topicExists( $otherWeb, $theTopicSingular ) ) {
+               &TWiki::Func::writeDebug( "FindElsewherePlugin::findTopicElsewhere: - $theTopicSingular was found in $otherWeb" ) if $debug;
+               push(@topicLinks, makeTopicLink($otherWeb,$theTopic));
             }
-        }
-    }
+         }
+      }
+      else  {
+         &TWiki::Func::writeDebug( "FindElsewherePlugin::findTopicElsewhere: - $theTopic was found in $otherWeb" ) if $debug;
+         push(@topicLinks, makeTopicLink($otherWeb,$theTopic));
+      }
+   }
 
-    if( scalar @topicLinks > 0 ) {
-        # If link text [[was in this form]] then <em> it
-        $original =~ s/\[\[(.*)\]\]/<em>$1<\/em>/g;
-        # Prepend WikiWords with <nop>, preventing double links
-        $original =~ s/([\s\(])($wwre)/$1<nop>$2/go;
-        if( scalar(@topicLinks) > 1 ) {
-            return "<nop>$original<sup>(".
-              join( ',', map { "[[$_->[0].$_->[1]][$_->[0]]]" }
-                      @topicLinks ).")</sup>" ;
-        } else {
-            my $l = $topicLinks[0];
-            return "[[$l->[0].$l->[1]][$l->[1]]]";
-        }
-    #} else {
-    #    print STDERR "OK $link (",join(',',@webList),"\n";
-    }
-    # $link is not in any of these webs
-    return $original;
+   if (@topicLinks > 0)
+   { 
+      if (@topicLinks == 1)
+      {
+	# Topic found elsewhere
+	# If link text [[was in this form]], free it
+	$oldTheTopic =~ s/\[\[(.*)\]\]/$1/o;
+
+        # Link to topic
+     	$topicLinks[0] =~ s/(\[\[.*?\]\[)(.*?)(\]\])/$1$oldTheTopic$3/o;
+     	$text .= $topicLinks[0] ;
+      } else {
+	# topic found elsewhere
+	# If link text [[was in this form]] <em> it
+	$oldTheTopic =~ s/\[\[(.*)\]\]/<em>$1<\/em>/go;
+
+	# If $oldTheTopic is a WikiWord, prepend with <nop> (prevent double links)
+	$oldTheTopic =~ s/($wikiWordRegex)/<nop\/>$1/go;
+	$text .= "$oldTheTopic<sup>(".join(",", @topicLinks ).")</sup>" ;
+      }
+   }
+   else
+   {
+     &TWiki::Func::writeDebug( "FindElsewherePlugin::findTopicElsewhere: - $theTopic was not in any of these webs: @webList" ) if $debug;
+     $text .= "$oldTheTopic"; 
+   }
+
+   return $text;
 }
 
-sub _makeSingular {
-    my ($theWord) = @_;
+# =========================
+sub makeSingular 
+{
+   my ($theWord) = @_;
 
-    $theWord =~ s/ies$/y/o;       # plurals like policy / policies
-    $theWord =~ s/sses$/ss/o;     # plurals like address / addresses
-    $theWord =~ s/([Xx])es$/$1/o; # plurals like box / boxes
-    $theWord =~ s/([A-Za-rt-z])s$/$1/o; # others, excluding ending ss like address(es)
-    return $theWord;
+   $theWord =~ s/ies$/y/o;       # plurals like policy / policies
+   $theWord =~ s/sses$/ss/o;     # plurals like address / addresses
+   $theWord =~ s/([Xx])es$/$1/o; # plurals like box / boxes
+   $theWord =~ s/([A-Za-rt-z])s$/$1/o; # others, excluding ending ss like address(es)
+   return $theWord;
 }
+
+
+# =========================
 
 1;
