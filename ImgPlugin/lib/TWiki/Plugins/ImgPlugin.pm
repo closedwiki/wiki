d6fb88a1a71d61e8ebdc4aef7b0a892b48852241
SteffenPoulsen <SteffenPoulsen@a00a5322-12db-0310-a70b-8735589c885e>
2006-05-09 04:52:53 +0000

    Item2248: ImgPlugin: New version by CraigMeyer
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00/twikiplugins@10122 a00a5322-12db-0310-a70b-8735589c885e

--- ImgPlugin/lib/TWiki/Plugins/ImgPlugin.pm
+++ ImgPlugin/lib/TWiki/Plugins/ImgPlugin.pm
@@ -24,6 +24,7 @@
 
 This is a pretty winky-dink plugin that allows people to use %IMG{"foo.gif"}% instead
 of <img src="%ATTACHURL%/foo.gif" />. It allows specification of the standard attributes 
+
 as well as an optional web=<web> and/or topic=<topic>.
 
 Another small step in the eradication of html in TWiki!
@@ -80,13 +81,39 @@ and highly dangerous!
 
 =cut
 
+    use vars qw(
+		$ConvertProg
+		$IdentifyProg
+		$Default_Thumb
+		$MAGNIFY_FORMAT
+		$SIMPLE_FORMAT
+		$NO_CAPTION_FORMAT
+		$CAPTION_FORMAT
+		$ImgPlugin_Style
+		$Debug
+		);
+
+    # href, caption, img, caption, width, height, href
+    $SIMPLE_FORMAT = qq[<a href="%s" class="image" title="%s"><img border="0" align="absmiddle" src="%s" alt="%s" width="%d" height="%d" longdesc="%s" /></a>];
+
+    # href
+    $MAGNIFY_FORMAT = qq[<div class="magnify" style="float:right"><a href="%s" class="internal" title="Enlarge"><img border="0" align="absmiddle" src="/tw/templates/magnify-clip.png" width="15" height="11" alt="Enlarge" /></a></div>];
+
+    $NO_CAPTION_FORMAT = qq[<div class="float%s"><div class="floatnone"><span><a href="%s" class="image" title=""><img border="0" align="absmiddle" src="%s" width="%d" height="%d" longdesc="%s" /></a></span></div></div>];
+
+    $CAPTION_FORMAT = qq[<div class="thumb t%s"><div style="width:%dpx;"><a href="%s" class="internal" title="%s"><img border="0" align="absmiddle" src="%s" alt="%s" width="%d" height="%d" longdesc="%s" /></a><div class="thumbcaption">%s</div></div></div>];
+
+
+
 sub initPlugin {
     my( $topic, $web, $user, $installWeb ) = @_;
+    my($error);
 
+    $error = 0;
     # check for Plugins.pm versions
     if( $TWiki::Plugins::VERSION < 1.026 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
-        return 0;
+      TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+      return( 0 );
     }
 
     # Get plugin preferences, variables defined by:
@@ -98,20 +125,101 @@ sub initPlugin {
 
     #$exampleCfgVar ||= 'default'; # make sure it has a value
 
+    $Debug = TWiki::Func::getPluginPreferencesValue( 'DEBUG' ) || 0;
+
+    $ConvertProg  = TWiki::Func::getPluginPreferencesValue( 'CONVERT_PROG' ) ||
+	'/usr/local/bin/convert';
+
+    if( ! (-e $ConvertProg || -x $ConvertProg) ){
+        TWiki::Func::writeWarning( qq[ImgPlugin: CONVERT_PROG does not exist!] );
+	$error++;
+    }
+
+    $IdentifyProg = TWiki::Func::getPluginPreferencesValue( 'IDENTIFY_PROG' ) ||
+	'/usr/local/bin/identify';
+
+    if( ! (-e $IdentifyProg || -x $ConvertProg) ){
+        TWiki::Func::writeWarning( qq[ImgPlugin: IDENTITY_PROG does not exist!] );
+	$error++;
+    }
+
+    $Default_Thumb   = TWiki::Func::getPluginPreferencesValue( 'THUMBNAIL_SIZE' ) || 180;
+
+    $ImgPlugin_Style = TWiki::Func::getPluginPreferencesValue( 'IMGPLUGIN_STYLE' ) ||
+      join('/', TWiki::Func::getPubUrlPath(), 'TWiki', 'ImgPlugin', 'ImgPlugin.css');
+
+    TWiki::Func::writeWarning( "Initialize (ImgPlugin) error=($error): [$ConvertProg][$IdentifyProg][$Default_Thumb][$ImgPlugin_Style]" );
+
+    if( $error > 0 ){
+	return( 0 );
+    }
     # register the _IMGfunction to handle %IMG{...}%
     TWiki::Func::registerTagHandler( 'IMG', \&_IMG);
+    TWiki::Func::registerTagHandler( 'IMAGE', \&_IMAGE);
 
     # Allow a sub to be called from the REST interface 
     # using the provided alias
     TWiki::Func::registerRESTHandler('example', \&restExample);
 
     # Plugin correctly initialized
-    return 1;
-}
+    return(1);
 
-# The function used to handle the %IMG{...}% tag
-# You would have one of these for each tag you want to process.
-sub _IMG{
+} # initPlugin
+
+
+
+sub get_size {
+    my($dir_path, $file) = @_;
+    my($result, @info);
+
+    $result = `$IdentifyProg '$dir_path/$file'`;
+    chomp($result);
+
+    (@info) = split(/\s+/, $result);
+    TWiki::Func::writeDebug("get_size($file): ", join(' | ', @info), "\n") if( $Debug > 0 );
+
+    if( defined($info[2]) && $info[2] =~ m/(\d+)x(\d+)/ ){
+	return( {
+	    DIR  => $dir_path,
+	    FILE => $file,
+	    X => $1,
+	    Y => $2, } );
+    } else {
+	return( {
+	    DIR  => $dir_path,
+	    FILE => $file,
+	    X => '',
+	    Y => '', } );
+    }
+} # get_size
+
+
+sub resize {
+    my($src_path, $dst_path, $file, $size) = @_;
+    my($result, $cmd, $new);
+
+    if( $size ne '' ){
+	$new = join('px-', $size, $file);
+	if( ! -f "$dst_path/$new" ){
+	    $cmd = qq[$ConvertProg -resize $size '$src_path/$file' '$dst_path/$new'];
+	    TWiki::Func::writeDebug( "resize($file): [$cmd]\n" ) if( $Debug > 0 );
+	    $result = `$cmd`;
+	}
+	return( get_size($dst_path, $new) );
+    } else {
+	return( get_size($src_path, $file) );
+    }
+} # resize
+
+
+sub _CLEAR {
+    my($session, $params, $theTopic, $theWeb) = @_;
+
+    return( qq[<br clear="all" />] );
+} # _CLEAR
+
+
+sub _IMAGE {
     my($session, $params, $theTopic, $theWeb) = @_;
     # $session  - a reference to the TWiki session object (if you don't know
     #             what this is, just ignore it)
@@ -124,18 +232,174 @@ sub _IMG{
     # Return: the result of processing the tag
 
     # For example, %IMG{'foo.gif' topic="SomeTopic"}%
-    # $params->{_DEFAULT} will be 'hamburger'
-    # $params->{sideorder} will be 'onions'
+    # $params->{_DEFAULT} will be 'foo.gif'
+    # $params->{topic} will be 'SomeTopic'
 
-    my $imgName = $params->{_DEFAULT};
-    my $path = TWiki::Func::getPubUrlPath();
-    my $imgTopic = $params->{topic} || $theTopic;
-    my $imgWeb = $params->{web} || $theWeb;
-    my $altTag = $params->{alt} || '';
+    my($str, @args, $file, $align, $size, $frame, $caption, $thumb,
+       $result, $link_only, $tmp, $height);
 
-    my @attrs = ('align', 'border', 'height', 'width', 'id', 'class');
+    $str = $params->{_DEFAULT};
+    if( $str =~ m/clr|clear/i ){
+	return( qq[<br clear="all" />] );
+    }
 
-    my $txt = "<img src='$path/$imgWeb/$imgTopic/$imgName' ";
+    
+    foreach ( qw(type align caption) ){
+	$tmp = $params->{$_};
+	if( defined($tmp) ){
+	    $str = join('|', $str, $params->{$_});
+	}
+    }
+
+    $tmp = $params->{size};
+    if( defined($tmp) && $tmp =~ m/(\d+)(px)?x?(\d+)?(px)?/ ){
+	$str = join('|', $str, defined($3) ? qq[$1x$3px] : qq[$1px] );
+    }
+
+    TWiki::Func::writeDebug("_IMAGE START: [$str]\n" ) if( $Debug > 0);
+
+    $str =~ s/^\[\[//;
+    $str =~ s/\]\]$//;		 
+    $str .= ' ';
+
+    ($file, @args) = split(/\|/, $str);
+
+    if( $file =~ s/^:// ){
+	$link_only = 1;
+    }
+    $file =~ s/^image://i;
+
+    TWiki::Func::writeDebug("$file, size:", scalar(@args), " ", join(", ", @args), "\n" ) if( $Debug > 2);
+
+    $size = '';
+    $caption = '';
+
+    foreach ( @args ){
+	if( $_ =~ m/^\s*(right|left|center|none)\s*$/i ){
+	    $align = $1;
+
+	} elsif( $_ =~ m/^\s*(\d+)px\s*$/i ){
+	    $size = $1;
+
+	} elsif( $_ =~ m/^\s*frame\s*$/i ){
+	    $frame = 1;
+
+	} elsif( $_ =~ m/^\s*(thumb|thumbnail)\s*$/i ){
+	    $thumb = 1;
+	    $size = $Default_Thumb;
+
+	} elsif( $_ =~ m/^\s*h(\d+)px\s*$/i ){
+	    $height = $1;
+	    
+	} else {
+	    $caption = $_;
+	}
+    }
+
+    TWiki::Func::writeDebug("[$file] [$align] [$size] frame[$frame] caption[$caption]\n" ) if( $Debug > 1);
+
+    #
+    # Do resize here as needed
+    # check if allready resized (before)
+    # get EXIF info from file
+    # Do smart convert, size is final width, adjust height accordingly
+    #
+
+    my($src_web, $src_topic, $src_path, $web_path, $dst_path, $view_img,
+       $pinfo, $x, $y);
+
+    $src_web   = $params->{web}   || $theWeb;
+    $src_topic = $params->{topic} || $theTopic;
+    $src_path  = join('/', TWiki::Func::getPubDir(), $src_web, $src_topic);
+    $dst_path = join('/', TWiki::Func::getPubDir(),     $theWeb, $theTopic);
+
+    $pinfo = resize($src_path, $dst_path, $file, $size);
+    $x = $pinfo->{X};
+    $y = $pinfo->{Y};
+    if( $pinfo->{DIR} eq $dst_path ){
+	$web_path = join('/', TWiki::Func::getPubUrlPath(), $theWeb, $theTopic);
+	$view_img  = TWiki::Func::getScriptUrl($theWeb, $theTopic, 'viewfile') .
+	    qq[?filename=$file];
+    } else {
+	$web_path = join('/', TWiki::Func::getPubUrlPath(), $src_web, $src_topic);
+	$view_img  = TWiki::Func::getScriptUrl($src_web, $src_topic, 'viewfile') .
+	    qq[?filename=$file];
+    }
+
+    TWiki::Func::writeDebug("Img => [$pinfo->{DIR}/$pinfo->{FILE}]\n", "Size is ($x, $y)\n" ) if( $Debug > 0 );
+
+    $tmp = join('/', $web_path, $pinfo->{FILE});
+
+    $caption =~ s/ $//;
+
+    if( defined($link_only) ){
+	$result = qq[<a href="$view_img" title="$caption">$file</a>];
+
+   } elsif( (scalar(@args) == 1 && $caption ne '') ||
+	(!defined($thumb) && !defined($frame) && !defined($align)) ){ # Special case for single option
+	$result = sprintf($SIMPLE_FORMAT, $view_img, $caption, $tmp, $caption, $x, $y, $view_img);
+
+    } elsif( defined($frame) ){
+	if( !defined($align) ){
+	    $align = 'right';
+	}
+	$result = sprintf($CAPTION_FORMAT, $align, $x+2, $view_img, $caption, $tmp,
+			  $caption, $x, $y, $view_img, $caption);
+
+    } elsif( $caption eq '' && !defined($thumb) ) {
+	$result = sprintf($NO_CAPTION_FORMAT, $align, $view_img, $tmp, $x, $y, $view_img);
+
+    } else {
+	my($enlarge);
+	$enlarge = sprintf($MAGNIFY_FORMAT, $view_img) . $caption;
+	if( !defined($align) ){
+	    $align = 'right';
+	}
+	$result = sprintf($CAPTION_FORMAT, $align, $x+2, $view_img, $caption, $tmp,
+			  $caption, $x, $y, $view_img, $enlarge);
+    }
+
+    writeDebug("[$result]\n" ) if( $Debug > 1);
+
+    TWiki::Func::addToHEAD("IMGPLUGIN_STYLE",
+			   qq[<link id="ImgPluginCss" rel="stylesheet"  type="text/css" href="$ImgPlugin_Style" media="all" />]);
+
+    return( $result ); 
+} # _IMAGE
+
+
+
+# The function used to handle the %IMG{...}% tag
+# You would have one of these for each tag you want to process.
+
+sub _IMG {
+    my($session, $params, $theTopic, $theWeb) = @_;
+    # $session  - a reference to the TWiki session object (if you don't know
+    #             what this is, just ignore it)
+    # $params=  - a reference to a TWiki::Attrs object containing parameters.
+    #             This can be used as a simple hash that maps parameter names
+    #             to values, with _DEFAULT being the name for the default
+    #             parameter.
+    # $theTopic - name of the topic in the query
+    # $theWeb   - name of the web in the query
+    # Return: the result of processing the tag
+
+    # For example, %IMG{'foo.gif' topic="SomeTopic"}%
+    # $params->{_DEFAULT} will be 'foo.gif'
+    # $params->{topic} will be 'SomeTopic'
+
+    my($imgName, $path, $imgTopic, $imgWeb, $altTag, @attrs, $txt);
+
+    $imgName = $params->{_DEFAULT};
+    $path = TWiki::Func::getPubUrlPath();
+    $imgTopic = $params->{topic} || $theTopic;
+    $imgWeb = $params->{web} || $theWeb;
+    $altTag = $params->{alt} || '';
+
+    @attrs = qw(align border height width id class);
+
+    $txt = "<img src='$path/$imgWeb/$imgTopic/$imgName' ";
+
     $txt .= " alt='$altTag'";
     while (my $key = shift @attrs) {
 	if (my $val = $params->{$key}) {
@@ -144,6 +408,7 @@ sub _IMG{
     }
     $txt .= " />";
 
-    return $txt;
-}
+    return( $txt );
+} #_IMG
+
 return 1;
