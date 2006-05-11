ec6a06bb4565c655040330205af4f48d3a9ffed8
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2006-05-11 07:20:24 +0000

    Item2267: normalized META info
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00/twikiplugins@10150 a00a5322-12db-0310-a70b-8735589c885e

--- BlogPlugin/lib/TWiki/Plugins/BlogPlugin/WebDB.pm
+++ BlogPlugin/lib/TWiki/Plugins/BlogPlugin/WebDB.pm
@@ -1,6 +1,6 @@
 # Plugin for TWiki Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2005-2006 Michael Daum <micha@nats.informatik.uni-hamburg.de>
+# Copyright (C) 2005-2006 MichaelDaum@WikiRing.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
