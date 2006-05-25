c12c876aa7bd90629e003cf3f74c911ed54701ec
ArthurClemens <ArthurClemens@a00a5322-12db-0310-a70b-8735589c885e>
2006-05-25 17:32:51 +0000

    Item2214: changed special chars to Unicode escape sequence (\uXXXX); better capitalize function
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@10336 a00a5322-12db-0310-a70b-8735589c885e

--- pub/TWiki/TWikiJavascripts/twiki.js
+++ pub/TWiki/TWikiJavascripts/twiki.js
@@ -13,8 +13,14 @@ var ns4 = (document.layers) ? true : false;
 var ie4 = (document.all) ? true : false;
 var dom = (document.getElementById) ? true : false;
 
-var UPPER_ALPHA_CHARS    	= "ÀÈÌÒÙÁÉÍÓÚİÂÊÎÔÛÃÑÕÄËÏÖÜŸŸÇŒØÅÆşĞA-Z";
-var LOWER_ALPHA_CHARS		= "àèìòùáéíóúıâêîôûãñõäëïöüœßøŞåæğça-z";
+// Unicode conversion tools:
+// Convert text to hexadecimal Unicode escape sequence (\uXXXX)
+// http://www.hot-tips.co.uk/useful/unicode_converter.HTML
+// Convert hexadecimal Unicode escape sequence (\uXXXX) to text
+// http://www.hot-tips.co.uk/useful/unicode_convert_back.HTML
+
+var UPPER_ALPHA_CHARS    	= "\u00c0\u00c8\u00cc\u00d2\u00d9\u00c1\u00c9\u00cd\u00d3\u00da\u00dd\u00c2\u00ca\u00ce\u00d4\u00db\u00c3\u00d1\u00d5\u00c4\u00cb\u00cf\u00d6\u00dc\u0178\u0178\u00c7\u0152\u00d8\u00c5\u00c6\u00fe\u00d0A-Z";
+var LOWER_ALPHA_CHARS		= "\u00e0\u00e8\u00ec\u00f2\u00f9\u00e1\u00e9\u00ed\u00f3\u00fa\u00fd\u00e2\u00ea\u00ee\u00f4\u00fb\u00e3\u00f1\u00f5\u00e4\u00eb\u00ef\u00f6\u00fc\u0153\u00df\u00f8\u00de\u00e5\u00e6\u00f0\u00e7a-z";
 var NUMERIC_CHARS			= "\\d";
 var MIXED_ALPHA_CHARS		= UPPER_ALPHA_CHARS + LOWER_ALPHA_CHARS;
 var MIXED_ALPHANUM_CHARS	= MIXED_ALPHA_CHARS + NUMERIC_CHARS;
@@ -158,7 +164,8 @@ function isWikiWord(inValue) {
 Capitalizes words in the string. For example: "A handy dictionary" becomes "A Handy Dictionary".
 */
 String.prototype.capitalize = function() {
-    return this.replace(/[ÀÈÌÒÙÁÉÍÓÚİÂÊÎÔÛÃÑÕÄËÏÖÜŸŸÇŒØÅÆşĞA-ZàèìòùáéíóúıâêîôûãñõäëïöüœßøŞåæğça-z]+/g, function(a) {
+	var re = new RegExp("[" + MIXED_ALPHANUM_CHARS + "]+", "g");
+    return this.replace(re, function(a) {
         return a.charAt(0).toUpperCase() + a.substr(1);
     });
 };
