c558a47a81925100e96a824894afecad9692fe23
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2006-05-26 08:51:38 +0000

    Item2356: no, a change is classified twikiDiffDelete* for the first part and then twikiDiffAdd* for the second part; twikiDiffChanged* isn't used anywhere.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@10347 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/UI/RDiff.pm
+++ lib/TWiki/UI/RDiff.pm
@@ -270,13 +270,13 @@ sub _renderSequential
         $result .=
           _sequentialRow( '#D0FFD0',
                           ($session->{i18n}->maketext('Changed')),
-                          'Changed',
+                          'Deleted',
                           _renderCellData( $session, $left, $web, $topic ),
                           '-', '&lt;' );
         $result .=
           _sequentialRow( undef,
                           ($session->{i18n}->maketext('Changed')),
-                          'Changed',
+                          'Added',
                           _renderCellData( $session, $right, $web, $topic ),
                           '+', '&gt;' );
     } elsif ( $diffType eq 'l' && $left ne '' && $right ne '' ) {
