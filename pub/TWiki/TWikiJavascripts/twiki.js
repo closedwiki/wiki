593ba365316c749ff87b445f8892018651f9eaef
ArthurClemens <ArthurClemens@a00a5322-12db-0310-a70b-8735589c885e>
2006-06-04 16:42:35 +0000

    Item2214: more unicode characters - in a separate file unicode_chars.js.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@10458 a00a5322-12db-0310-a70b-8735589c885e

--- pub/TWiki/TWikiJavascripts/twiki.js
+++ pub/TWiki/TWikiJavascripts/twiki.js
@@ -19,14 +19,23 @@ var dom = (document.getElementById) ? true : false;
 // Convert hexadecimal Unicode escape sequence (\uXXXX) to text
 // http://www.hot-tips.co.uk/useful/unicode_convert_back.HTML
 
-var UPPER_ALPHA_CHARS    	= "\u00c0\u00c8\u00cc\u00d2\u00d9\u00c1\u00c9\u00cd\u00d3\u00da\u00dd\u00c2\u00ca\u00ce\u00d4\u00db\u00c3\u00d1\u00d5\u00c4\u00cb\u00cf\u00d6\u00dc\u0178\u0178\u00c7\u0152\u00d8\u00c5\u00c6\u00fe\u00d0A-Z";
-var LOWER_ALPHA_CHARS		= "\u00e0\u00e8\u00ec\u00f2\u00f9\u00e1\u00e9\u00ed\u00f3\u00fa\u00fd\u00e2\u00ea\u00ee\u00f4\u00fb\u00e3\u00f1\u00f5\u00e4\u00eb\u00ef\u00f6\u00fc\u0153\u00df\u00f8\u00de\u00e5\u00e6\u00f0\u00e7a-z";
+// More international characters in unicode_chars.js
+// Import file when international support is needed:
+// <script type="text/javascript" src="%PUBURLPATH%/%TWIKIWEB%/TWikiJavascripts/unicode_chars.js"></script>
+// unicode_chars.js will overwrite the regexes below
+
+// Info on unicode: http://www.fileformat.info/info/unicode/
+
+var UPPER_ALPHA_CHARS    	= "A-Z";
+var LOWER_ALPHA_CHARS		= "a-z";
 var NUMERIC_CHARS			= "\\d";
 var MIXED_ALPHA_CHARS		= UPPER_ALPHA_CHARS + LOWER_ALPHA_CHARS;
 var MIXED_ALPHANUM_CHARS	= MIXED_ALPHA_CHARS + NUMERIC_CHARS;
 var WIKIWORD_REGEX = "^" + "[" + UPPER_ALPHA_CHARS + "]" + "+" + "[" + LOWER_ALPHA_CHARS + "]" + "+" + "[" + UPPER_ALPHA_CHARS + "]" + "+" + "[" + MIXED_ALPHANUM_CHARS + "]" + "*";
 var ALLOWED_URL_CHARS = MIXED_ALPHANUM_CHARS + "-_^";
 
+// TWiki namespace
+var TWiki = {};
 
 // Chain a new load handler onto the existing handler chain
 // http://simon.incutio.com/archive/2004/05/26/addLoadEvent
@@ -166,7 +175,7 @@ Capitalizes words in the string. For example: "A handy dictionary" becomes "A Ha
 String.prototype.capitalize = function() {
 	var re = new RegExp("[" + MIXED_ALPHANUM_CHARS + "]+", "g");
     return this.replace(re, function(a) {
-        return a.charAt(0).toUpperCase() + a.substr(1);
+        return a.charAt(0).toLocaleUpperCase() + a.substr(1);
     });
 };
 
