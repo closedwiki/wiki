505dda94a16de51e446ad5f6b4cdf8f8c0ae31ac
VernonLyon <VernonLyon@a00a5322-12db-0310-a70b-8735589c885e>
2006-07-13 14:20:16 +0000

    Item2623: new version
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@11013 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/QuickMenuSkin/pub/TWiki/QuickMenuSkin/qmenu.js
+++ twikiplugins/QuickMenuSkin/pub/TWiki/QuickMenuSkin/qmenu.js
@@ -1,48 +1,102 @@
 /***********************************
-QuickMenu Class v0.9
-
-A more simplified version of XWebMenu:
-Jeremy McPeak http://www.wdonline.com/dhtml/xwebmenu/
-
-(c) 2006  Vernon Lyon  vlyon@hotmail.com
+QuickMenu Class v1.0
+(c) 2006 Vernon Lyon
 ***********************************/
 
 var QuickMenu = {
   Set : {
-    ShowOnHover : 1
+    ShowOnHover : 1,
+    HideTimeout : 1000
   },
   User : {
   },
   Timer : null,
   ShownMenu : null,
-  MenuButn : function (menu, parent, text, action, tip) {
-    if (text) {
-      this.Button = document.createElement("SPAN");
+  Menu : function (menubar, text, action, icon, tip) {
+    if (text || icon) {
+      if (!menubar.QuickMenuTR) {
+        menubar.appendChild(x = document.createElement("TABLE"));
+        x.cellSpacing = 0;
+        x.appendChild(x = document.createElement("TBODY"));
+        x.appendChild(menubar.QuickMenuTR = document.createElement("TR"));
+      }
+      this.Button = document.createElement("TD");
       this.Button.className = "qmenu-menu";
-      this.Button.appendChild(document.createTextNode(text));
-      parent.appendChild(this.Button);
+      menubar.QuickMenuTR.appendChild(this.Button);
+      if (icon) {
+        this.Icon = document.createElement("IMG");
+        this.Icon.src = icon;
+        this.Button.appendChild(this.Icon);
+        this.Button.vAlign = "middle";
+        if (text) {
+          this.Icon.style.paddingRight = "3px";
+        }
+      }
+      if (text) {
+        this.Button.appendChild(document.createTextNode(text));
+      }
     } else {
-      this.Button = parent;
+      this.Button = menubar;
       if (!this.Button.className) this.Button.className = "qmenu-menu-free";
     }
-    this.Button.Menu = menu;
-    this.Button.MenuBar = parent;
-    if (parent.ShowOnHover == null) parent.ShowOnHover = QuickMenu.Set.ShowOnHover;
+    this.MenuBar = this.Button.MenuBar = menubar;
+    if (menubar.ShowOnHover == null) menubar.ShowOnHover = QuickMenu.Set.ShowOnHover;
     this.Button.defaultClass = this.Button.className;
     if (tip) this.Button.title = tip;
 
-  // Dot for IE
-    if (QuickMenu.User.Ie) {
-      this.Button.Dot = document.createElement("SPAN");
-      this.Button.Dot.appendChild(document.createTextNode(" "));
-      this.Button.appendChild(this.Button.Dot);
-      this.Button.Dot.style.left = "0px";
-      this.Button.Dot.style.backgroundColor = "#315ea3";
-      this.Button.Dot.style.visibility = "hidden";
-      this.Button.Dot.style.position = "absolute";
-      this.Button.Dot.style.width = "1px";
-      this.Button.Dot.style.height = "1px";
-      this.Button.Dot.style.fontSize = "0px";
+    this.Button.onmouseover = function () {
+      if (this.MenuList && this.MenuList.Shown) {
+        clearTimeout(QuickMenu.Timer);
+        QuickMenu.Timer = null;
+        return;
+      }
+      if (this.MenuList && (this.MenuBar.ShowOnHover ||
+          (QuickMenu.ShownMenu && (QuickMenu.ShownMenu.MenuBar == this.MenuBar)))) {
+        clearTimeout(QuickMenu.Timer);
+        QuickMenu.HideAll();
+        this.className = "qmenu-menu-click";
+        if (this.Dot) {
+          pos = findAbsPos(this);
+          this.Dot.style.top = pos.y + this.offsetHeight - 1 + "px";
+          this.Dot.style.left = pos.x + "px"
+          this.Dot.style.visibility = "visible";
+        }
+        this.MenuList.Show();
+      } else {
+        if (QuickMenu.ShownMenu) {
+          clearTimeout(QuickMenu.Timer);
+          QuickMenu.HideAll();
+        }
+        this.className = "qmenu-menu-over";
+      }
+    }
+
+    this.Button.onmouseout = function () {
+      if (this.className == "qmenu-menu-over") this.className = this.defaultClass;
+      if (this.MenuList && this.MenuList.Shown && !QuickMenu.Timer)
+        QuickMenu.Timer = setTimeout("QuickMenu.HideAll()", QuickMenu.Set.HideTimeout);
+    }
+
+    this.Button.onclick = function () {
+      if (this.Link) {
+        window.location = this.Link;
+      } else if (this.Action) {
+        eval (this.Action);
+      } else if (this.MenuList.Shown && !this.MenuBar.ShowOnHover) {
+        this.MenuList.Hide();
+        if (this.Dot) this.Dot.style.visibility = "hidden";
+        this.className = "qmenu-menu-over";
+      } else {
+        QuickMenu.HideAll();
+        this.className = "qmenu-menu-click";
+        if (this.Dot) {
+          pos = findAbsPos(this);
+          this.Dot.style.top = pos.y + this.offsetHeight - 1 + "px";
+          this.Dot.style.left = pos.x + "px"
+          this.Dot.style.visibility = "visible";
+        }
+        this.MenuList.Show();
+      }
     }
 
     if (action) {
@@ -51,67 +105,37 @@ var QuickMenu = {
       } else {
         this.Button.Link = action;
       }
+      return this;
     }
 
-    this.MouseOver = function () {
-      if (this.Menu.Shown) {
-        clearTimeout(QuickMenu.Timer);
-        QuickMenu.Timer = null;
-        return;
-      }
-      if (this.MenuBar.ShowOnHover ||
-          (QuickMenu.ShownMenu && (QuickMenu.ShownMenu.MenuBar == this.Menu.MenuBar))) {
-        clearTimeout(QuickMenu.Timer);
-        QuickMenu.HideAll();
-        if (this.Menu.Items.length) {
-          this.className = "qmenu-menu-click";
-          if (this.Dot) {
-            this.Dot.style.top = (this.offsetHeight - 1) + "px";
-            this.Dot.style.visibility = "visible";
-          }
-          this.Menu.Show();
-        } else {
-          this.className = "qmenu-menu-over";
-        }
-      } else {
-        this.className = "qmenu-menu-over";
-      }
-    }
+    this.Button.MenuList = new QuickMenu.MenuList(this, menubar);
+    this.Items = this.Button.MenuList.Items;
 
-    this.MouseOut = function () {
-      if (this.className == "qmenu-menu-over") this.className = this.defaultClass;
-      if (!QuickMenu.Timer && this.Menu.Shown)
-        QuickMenu.Timer = setTimeout("QuickMenu.HideAll()", 1000);
+  // Dot for IE
+    if (QuickMenu.User.Ie) {
+      this.Button.Dot = document.createElement("SPAN");
+      this.Button.Dot.appendChild(document.createTextNode(" "));
+      this.Button.Dot.style.left = "0px";
+      this.Button.Dot.style.backgroundColor = "#315ea3";
+      this.Button.Dot.style.visibility = "hidden";
+      this.Button.Dot.style.position = "absolute";
+      this.Button.Dot.style.width = "1px";
+      this.Button.Dot.style.height = "1px";
+      this.Button.Dot.style.fontSize = "0px";
+      this.Button.Dot.style.zIndex = 3;
+      pos = findAbsPos(this.Button);
+      this.Button.Dot.style.top = pos.y + this.Button.offsetHeight - 1 + "px";
+      this.Button.Dot.style.left = pos.x + "px"
+      document.body.appendChild(this.Button.Dot);
     }
-
-    this.MouseClick = function () {
-      if (this.Link) {
-        window.location = this.Link;
-      } else if (this.Action) {
-        eval (this.Action);
-      } else if (this.Menu.Shown && !this.MenuBar.ShowOnHover) {
-        this.Menu.Hide();
-        if (this.Dot) this.Dot.style.visibility = "hidden";
-        this.className = "qmenu-menu-over";
-      } else {
-        QuickMenu.HideAll();
-        this.className = "qmenu-menu-click";
-        if (this.Dot) {
-          this.Dot.style.top = (this.offsetHeight - 1) + "px";
-          this.Dot.style.visibility = "visible";
-        }
-        this.Menu.Show();
-      }
-    }
-
-    this.Button.onmouseover = this.MouseOver;
-    this.Button.onmouseout = this.MouseOut;
-    this.Button.onclick = this.MouseClick;
   },
-  Menu : function (parent, text, action, tip) {
+  MenuList : function (parent, menubar) {
+    this.Parent = parent;
+    this.MenuBar = menubar;
     this.Items = [];
     this.OuterDiv = document.createElement("DIV");
     this.OuterDiv.className = "qmenu-menulist-outer";
+    this.OuterDiv.style.left = this.OuterDiv.style.top = 0;
     this.Shadow = document.createElement("DIV");
     this.Shadow.className = "qmenu-menulist-shadow";
     this.InnerDiv = document.createElement("DIV");
@@ -123,19 +147,10 @@ var QuickMenu = {
     this.Table.cellSpacing = 0;
     this.Table.cellPadding = 0;
     this.TBody = document.createElement("TBODY");
-    if (action == ":") {
-      this.Parent = parent;
-      this.MenuBar = parent.Parent.MenuBar;
-      pos = findAbsPos(this.Parent.TRow);
-      this.OuterDiv.style.left = pos.x + this.Parent.TRow.offsetWidth + "px";
-      this.OuterDiv.style.top = pos.y - 1 + "px";
-    } else {
-      this.Parent = new QuickMenu.MenuButn(this, parent, text, action, tip);
-      this.MenuBar = parent;
-      pos = findAbsPos(this.Parent.Button);
-      this.OuterDiv.style.left = pos.x + "px";
-      this.OuterDiv.style.top = pos.y + this.Parent.Button.offsetHeight - 1 + "px";
-    }
+/*
+    this.Place();
+*/
+    this.OuterDiv.style.left = this.OuterDiv.style.top = "0px";
 
     this.Table.appendChild(this.TBody);
     this.Container.appendChild(this.Table);
@@ -144,11 +159,10 @@ var QuickMenu = {
     this.OuterDiv.appendChild(this.InnerDiv);
     document.body.appendChild(this.OuterDiv);
   },
-  MenuItem : function (menu, text, action, icon, tip) {
-    this.Parent = menu;
+  MenuItem : function (parent, text, action, icon, tip) {
+    this.Parent = parent;
     this.TRow = document.createElement("TR");
-    this.TRow.className = "qmenu-menuitem";
-    this.TRow.ParentMenu = menu;
+    this.TRow.ParentMenu = parent;
 
     this.LeftCell = document.createElement("TD");
     if (text) {
@@ -156,10 +170,20 @@ var QuickMenu = {
         if (action.substring(0,3) == "js:") {
           this.TRow.Action = action.substring(3);
         } else if (action.substring(0,1) == ":") {
-          this.TRow.Menu = menu[action.substring(1)] = new QuickMenu.Menu(this, "", ":");
+          this.TRow.MenuList = parent.Parent[action.substring(1)] = new QuickMenu.MenuList(this, parent.MenuBar);
         } else {
           this.TRow.Link = action;
         }
+
+        this.TRow.onclick = function () {
+          if (!this.MenuList) QuickMenu.HideAll();
+          if (this.Link) {
+            window.location = this.Link;
+          } else if (this.Action) {
+            eval (this.Action);
+          }
+        }
+        this.TRow.className = "qmenu-menuitem";
       } else {
         this.TRow.className = "qmenu-menuitem-disabled";
       }
@@ -177,56 +201,47 @@ var QuickMenu = {
       this.MiddleCell.className = "qmenu-menuitem-middle";
       this.MiddleCell.appendChild(document.createTextNode(text));
       this.RightCell = document.createElement("TD");
-      this.RightCell.className = "qmenu-menuitem-" + (this.TRow.Menu ? "arrow" : "right");
+      this.RightCell.className = "qmenu-menuitem-" + (this.TRow.MenuList ? "arrow" : "right");
       this.RightCell.appendChild(document.createTextNode(" "));
       if (tip) this.TRow.title = tip;
       this.TRow.appendChild(this.LeftCell);
       this.TRow.appendChild(this.MiddleCell);
       this.TRow.appendChild(this.RightCell);
-
-      this.MouseOver = function () {
-        clearTimeout(QuickMenu.Timer);
-        QuickMenu.Timer = null;
-        if (this.cells[0].className == "qmenu-menuitem-left-over") return;
-        while (QuickMenu.ShownMenu != this.ParentMenu) QuickMenu.ShownMenu.Hide();
-        this.cells[0].className = "qmenu-menuitem-left-over";
-        this.cells[1].className = "qmenu-menuitem-middle-over";
-        if (this.Menu) {
-          this.cells[2].className = "qmenu-menuitem-arrow-over";
-          this.Menu.Show();
-        } else {
-          this.cells[2].className = "qmenu-menuitem-right-over";
-        }
-      }
-      this.MouseOut = function () {
-        if (!this.Menu) {
-          this.cells[0].className = "qmenu-menuitem-left";
-          this.cells[1].className = "qmenu-menuitem-middle";
-          this.cells[2].className = "qmenu-menuitem-right";
-        }
-        if (!QuickMenu.Timer && QuickMenu.ShownMenu)
-          QuickMenu.Timer = setTimeout("QuickMenu.HideAll()", 1000);
-      }
-      this.MouseClick = function () {
-        if (!this.Menu) QuickMenu.HideAll();
-        if (this.Link) {
-          window.location = this.Link;
-        } else if (this.Action) {
-          eval (this.Action);
-        }
-      }
-      this.TRow.onmouseover = this.MouseOver;
-      this.TRow.onmouseout = this.MouseOut;
-      this.TRow.onclick = this.MouseClick;
     } else {
       this.Separator = document.createElement("TD");
       this.Separator.className = "qmenu-menuitem-separator";
       this.Separator.colSpan = 2;
       this.Separator.appendChild(document.createElement("DIV"));
+      this.TRow.className = "qmenu-menuitem";
       this.TRow.appendChild(this.LeftCell);
       this.TRow.appendChild(this.Separator);
     }
-    menu.TBody.appendChild(this.TRow);
+
+    this.TRow.onmouseover = function () {
+      clearTimeout(QuickMenu.Timer);
+      QuickMenu.Timer = null;
+      if (!text || !action || this.cells[0].className == "qmenu-menuitem-left-over")
+        return;
+      while (QuickMenu.ShownMenu != this.ParentMenu) QuickMenu.ShownMenu.Hide();
+      this.cells[0].className = "qmenu-menuitem-left-over";
+      this.cells[1].className = "qmenu-menuitem-middle-over";
+      if (this.MenuList) {
+        this.cells[2].className = "qmenu-menuitem-arrow-over";
+        this.MenuList.Show();
+      } else {
+        this.cells[2].className = "qmenu-menuitem-right-over";
+      }
+    }
+    this.TRow.onmouseout = function () {
+      if (text && !this.MenuList) {
+        this.cells[0].className = "qmenu-menuitem-left";
+        this.cells[1].className = "qmenu-menuitem-middle";
+        this.cells[2].className = "qmenu-menuitem-right";
+      }
+      if (!QuickMenu.Timer && QuickMenu.ShownMenu)
+        QuickMenu.Timer = setTimeout("QuickMenu.HideAll()", QuickMenu.Set.HideTimeout);
+    }
+    parent.TBody.appendChild(this.TRow);
   },
   HideAll : function () {
     QuickMenu.Timer = "HideAll"; // Ensure that no timer gets set while hiding
@@ -255,11 +270,15 @@ function findAbsPos(obj) {
 }
 
 QuickMenu.Menu.prototype.Add = function (text, action, icon, tip) {
+  this.Button.MenuList.Add(text, action, icon, tip);
+};
+
+QuickMenu.MenuList.prototype.Add = function (text, action, icon, tip) {
   var i = this.Items.length;
   this.Items[i] = new QuickMenu.MenuItem(this, text, action, icon, tip);
 };
 
-QuickMenu.Menu.prototype.Hide = function () {
+QuickMenu.MenuList.prototype.Hide = function () {
   this.Shadow.style.visibility = "hidden";
   this.OuterDiv.style.visibility = "hidden";
   this.Shown = false;
@@ -268,22 +287,52 @@ QuickMenu.Menu.prototype.Hide = function () {
   } else {
     this.Parent.LeftCell.className = "qmenu-menuitem-left";
     this.Parent.MiddleCell.className = "qmenu-menuitem-middle";
-    this.Parent.RightCell.className = "qmenu-menuitem-" + (this.Parent.TRow.Menu ? "arrow" : "right");
+    this.Parent.RightCell.className = "qmenu-menuitem-" +
+      (this.Parent.TRow.MenuList ? "arrow" : "right");
     QuickMenu.ShownMenu = this.Parent.Parent;
   }
 };
 
-QuickMenu.Menu.prototype.Show = function () {
-  QuickMenu.ShownMenu = this;
+QuickMenu.MenuList.prototype.Place = function () {
+  h = (QuickMenu.User.Moz) ? innerHeight : (QuickMenu.User.cssCompat) ?
+    document.documentElement.clientHeight : document.body.clientHeight;
+  w = (QuickMenu.User.Moz) ? innerWidth : (QuickMenu.User.cssCompat) ?
+    document.documentElement.clientWidth : document.body.clientWidth;
   if (this.Parent.Button) {
     pos = findAbsPos(this.Parent.Button);
-    this.OuterDiv.style.left = pos.x + "px";
+    max = w - this.OuterDiv.offsetWidth - (QuickMenu.User.Ie ? 5 : 3);
+    if (max < 0) max = 0;
+    this.OuterDiv.style.left = (pos.x > max) ?  max + "px" : pos.x + "px";
     this.OuterDiv.style.top = pos.y + this.Parent.Button.offsetHeight - 1 + "px";
   } else {
-    pos = findAbsPos(this.Parent.TRow);
-    this.OuterDiv.style.left = pos.x + this.Parent.TRow.offsetWidth + "px";
-    this.OuterDiv.style.top = pos.y - 1 + "px";
+    // Find left, right & top
+    var l, r, t;
+    if (QuickMenu.User.Ie) {
+      pos = findAbsPos(this.Parent.TRow);
+      l = pos.x + 1;
+      r = pos.x + this.Parent.TRow.offsetWidth - 1;
+      t = pos.y;
+    } else if (QuickMenu.User.Safari) {
+      pos = findAbsPos(this.Parent.LeftCell);
+      l = pos.x + 1;
+      pos = findAbsPos(this.Parent.RightCell);
+      r = pos.x + this.Parent.RightCell.offsetWidth - 1;
+      pos = findAbsPos(this.Parent.MiddleCell);
+      t = pos.y - 1;
+    } else {
+      pos = findAbsPos(this.Parent.TRow);
+      l = pos.x + 2;
+      r = pos.x + this.Parent.TRow.offsetWidth;
+      t = pos.y;
+    }
+    this.OuterDiv.style.left = (r + this.OuterDiv.offsetWidth + (QuickMenu.User.Ie ? 5 : 3) > w) ? l - this.OuterDiv.offsetWidth + "px" : r + "px";
+    this.OuterDiv.style.top = t - (QuickMenu.User.Ie ? 1 : 0) + "px";
   }
+};
+
+QuickMenu.MenuList.prototype.Show = function () {
+  QuickMenu.ShownMenu = this;
+  this.Place();
   this.OuterDiv.style.visibility = "visible";
   this.Shadow.style.width = this.OuterDiv.offsetWidth + "px";
   this.Shadow.style.height = this.OuterDiv.offsetHeight + "px";
@@ -294,6 +343,7 @@ QuickMenu.Menu.prototype.Show = function () {
 QuickMenu.User.v = navigator.userAgent.toLowerCase();
 QuickMenu.User.Dom = document.getElementById ? 1 : 0;
 QuickMenu.User.Ie = (QuickMenu.User.v.indexOf("msie 6") > -1 && QuickMenu.User.Dom) ? 1 : 0;
+QuickMenu.User.cssCompat = (QuickMenu.User.Ie && document.compatMode == "CSS1Compat") ? 1 : 0;
 QuickMenu.User.Gecko = (QuickMenu.User.v.indexOf("gecko") > -1 && QuickMenu.User.Dom) ? 1 : 0;
 QuickMenu.User.Safari = (QuickMenu.User.v.indexOf("safari") > -1 && QuickMenu.User.Dom) ? 1 : 0;
 QuickMenu.User.Moz = (QuickMenu.User.Gecko && parseInt(navigator.productSub) > 20020512) ? 1 : 0;
