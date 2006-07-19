c8293a9aeba0d8bf74eb1fa6d2bce7fa0cd7ae3e
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2006-07-19 15:37:26 +0000

    Item2645: new ldap suite
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@11062 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/LdapNgPlugin/lib/TWiki/Plugins/LdapNgPlugin.pm
@@ -0,0 +1,57 @@
+# Plugin for TWiki Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) MichaelDaum@WikiRing.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+package TWiki::Plugins::LdapNgPlugin;
+
+use strict;
+use vars qw($VERSION $RELEASE $isInitialized);
+
+$VERSION = '$Rev$';
+$RELEASE = 'v0.01';
+
+###############################################################################
+sub initPlugin { 
+  $isInitialized = 0;
+  return 1; 
+}
+
+###############################################################################
+sub commonTagsHandler {
+### my ( $text, $topic, $web ) = @_;
+
+  $_[0] =~ s/%LDAP{(.*?)}%/&handleLdap($_[2], $_[1], $1)/ge;
+}
+
+###############################################################################
+sub afterCommonTagsHandler {
+  return unless $isInitialized;
+  return TWiki::Plugins::LdapNgPlugin::Core::afterCommonTagsHandler(@_);
+}
+
+###############################################################################
+sub handleLdap {
+
+  unless ($isInitialized) {
+    eval 'use TWiki::Plugins::LdapNgPlugin::Core;';
+    die $@ if $@;
+    $isInitialized = 1;
+  }
+
+  return TWiki::Plugins::LdapNgPlugin::Core::handleLdap(@_);
+}
+
+
+
+
+1;
