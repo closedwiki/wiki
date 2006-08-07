bfa23bb1925bd3d8db0ecc661c7abb53321e6ff2
KennethLavrsen <KennethLavrsen@a00a5322-12db-0310-a70b-8735589c885e>
2006-08-07 07:48:53 +0000

    Item2737: ControlsPlugin: Updating SVN to 04 Aug 2006 version Updated code by Thomas Weigert
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@11273 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/ControlsPlugin/lib/TWiki/Plugins/ControlsPlugin.pm
+++ twikiplugins/ControlsPlugin/lib/TWiki/Plugins/ControlsPlugin.pm
@@ -17,37 +17,13 @@
 # =========================
 #
 #
-# Each plugin is a package that may contain these functions:        VERSION:
-#
-#   earlyInitPlugin         ( )                                     1.020
-#   initPlugin              ( $topic, $web, $user, $installWeb )    1.000
-#   initializeUserHandler   ( $loginName, $url, $pathInfo )         1.010
-#   registrationHandler     ( $web, $wikiName, $loginName )         1.010
-#   beforeCommonTagsHandler ( $text, $topic, $web )                 1.024
-#   commonTagsHandler       ( $text, $topic, $web )                 1.000
-#   afterCommonTagsHandler  ( $text, $topic, $web )                 1.024
-#   startRenderingHandler   ( $text, $web )                         1.000
-#   outsidePREHandler       ( $text )                               1.000
-#   insidePREHandler        ( $text )                               1.000
-#   endRenderingHandler     ( $text )                               1.000
-#   beforeEditHandler       ( $text, $topic, $web )                 1.010
-#   afterEditHandler        ( $text, $topic, $web )                 1.010
-#   beforeSaveHandler       ( $text, $topic, $web )                 1.010
-#   afterSaveHandler        ( $text, $topic, $web, $errors )        1.020
-#   renderFormFieldForEditHandler( $name, $type, $size, $value, $attributes, $possibleValues)
-#   writeHeaderHandler      ( $query )                              1.010  Use only in one Plugin
-#   redirectCgiQueryHandler ( $query, $url )                        1.010  Use only in one Plugin
-#   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
-#   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
-#
-# initPlugin is required, all other are optional. 
-# For increased performance, unused handlers are disabled. To
-# enable a handler remove the leading DISABLE_ from the function
-# name. Remove disabled handlers you do not need.
-#
-# NOTE: To interact with TWiki use the official TWiki functions 
-# in the TWiki::Func module. Do not reference any functions or
-# variables elsewhere in TWiki!!
+## Bug: If we allow the [[][]] notation to distinguish between value and
+## display, we should take care of this for URL parameters also.
+## Bug: If selection is "", then a hierarchical control is generated but
+## may be in a context where it does not make sense. Figure out how to 
+## force an empty menu in that case (or some other default solution).
+## Idea: If selector="", list all options, rather than none. Use none only
+## if unknown selection is given.
 
 
 # =========================
@@ -57,18 +33,11 @@ package TWiki::Plugins::ControlsPlugin;
 use vars qw(
         $web $topic $user $installWeb $VERSION $RELEASE $pluginName
         $debug
+	$maxlevel @menuStack
     );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
-$VERSION = '$Rev$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
+$VERSION = '$Rev: 9598$';
 $RELEASE = 'Dakar';
-
 $pluginName = 'ControlsPlugin';  # Name of this Plugin
 
 # =========================
@@ -77,92 +46,73 @@ sub initPlugin
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.021 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if( $TWiki::Plugins::VERSION < 1.1 ) {
+        TWiki::Func::writeWarning( "This version of $pluginName works only with TWiki 4 and greater." );
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = TWiki::Func::getPluginPreferencesFlag( "CONTROLSPLUGIN_DEBUG" );
+    $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
 
-    # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
-    #$exampleCfgVar = TWiki::Func::getPluginPreferencesValue( "EXAMPLE" ) || "default";
-
-
-    TWiki::Func::registerTagHandler( 'CONTROL', \&_CONTROL );
-    TWiki::Func::registerTagHandler( 'CONTROLS', \&_CONTROLS );
+    TWiki::Func::registerTagHandler( 'CONTROL', \&handleControls,
+                                     'context-free' );
 
     # Plugin correctly initialized
     TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+
     return 1;
 }
 
 # =========================
-sub commonTagsHandler
-{
-### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
-
-    # This is the place to define customized tags and variables
-    # Called by TWiki::handleCommonTags, after %INCLUDE:"..."%
-
-    # do custom extension rule, like for example:
-#    $_[0] =~ s/%CONTROL{(.*?)}%/handleControls( $_[2], $_[1], $1 )/ge;
-}
-
 =pod
-
----++ sub handleGenSearch ( $theWeb, $theTopic, $theArgs )
-
-Not yet documented.
-
-=cut
-
-sub _CONTROLS {
-    my($session, $params, $theTopic, $theWeb) = @_;
-    
-    my $form    = $params->{"form"} || "$theTopic.$theWeb";
-    my $createtopic    = $params->{"createtopic"};
-    
-   my $twikiForm = new TWiki::Form($session, $theWeb, $form );
-   return "" unless $twikiForm;
-   my $meta = new TWiki::Meta($session, $theWeb, $form );
-   
-   my $formEdit = $twikiForm->renderForEdit( $theWeb, $theTopic, $meta );
-   if (defined($createtopic)) {
-   	$formEdit = '<form name="newTopic" action="%SCRIPTURL{edit}%/%WEB%/">'.
-	'<input type="hidden" name="formtemplate" value="'.$form.'" />'.
-	'<input type="hidden" name="templatetopic" value="'.$form.'" />'.
-	'<input type="hidden" name="action" value="form" />'.
-	'<input type="hidden" name="topic" value="CalendarEntryXXXXXXXXXXX" />'.
-	$formEdit.
-	'<input type="submit" value="  Create topic  "> </form>';
-   }
-   return $formEdit;
-}
-
-=pod
-
----++ sub handleControls ( $web, $topic, $parameterString ) ==> $value
+---++ sub handleControls ( $session, $params, $web, $topic ) ==> $value
+| $session | twiki session |
+| $params | twiki parameter string  |
 | $web | web and  |
 | $topic | topic to display the name for |
-| $parameterString | twiki parameter string  |
 
 =cut
-
-sub _CONTROL
+sub handleControls
 {
-    my($session, $params, $theTopic, $theWeb) = @_;
+    my( $session, $params, $theTopic, $theWeb ) = @_;
 
-    my $name   = $params->{"_DEFAULT"} || $params{"name"} || "";
-    my $web    = $params->{"web"}   || $theWeb;
-    my $topic  = $params->{"topic"} || $theTopic;
-    my $size   = $params->{"size"} || 1;
-    my $type   = $params->{"type"} || "select";
-    my $url   = $params->{"urlparam"} || "off";
+    my $name   = $params->{"_DEFAULT"} || $params->{name} || "";
+    my $web    = $params->{web}   || $theWeb;
+    my $topic  = $params->{topic} || $theTopic;
+    my $size   = $params->{size} || 1;
+    my $type   = $params->{type} || "select";
+    my $url    = $params->{urlparam} || "off";
+    my $sep    = $params->{separator} || " ";
+    my $form   = $params->{form} || '';
+    my $sel    = $params->{selection};
+    my $msg1   = $params->{initmessage} || 'Select previous level first';
+    my $msg2   = $params->{nextmessage} || '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+    $maxlevel = 0;
+    @menuStack = ();
 
-    return getListOfFieldValues($web, $topic, $name, $type, $size, $url );
+    my $menu = getListOfFieldValues( $session, $web, $topic, $name, $type, $size, $sep, $url, $msg1, $msg2, $sel );
+
+    if ( $maxlevel > 0 ) {
+      # hierarchical menu present
+      TWiki::Func::addToHEAD('control', "<SCRIPT language=JavaScript src=\"%PUBURLPATH%/$installWeb/$pluginName/cascade_select.js\" type=text/javascript></SCRIPT>");
+    
+
+      my $tag = "new DepSelectOne(document.$form.elements[\'${name}0\']";
+      for (my $idx=1; $idx<=$maxlevel; $idx++) {
+	$tag .= ", \'$name$idx\'";
+      }
+      $tag .= ')';
+      TWiki::Func::addToHEAD("$name$form", "<SCRIPT type=\"text/javascript\">
+//<![CDATA[
+   addLoadEvent(initControl$name$form);
+   function initControl$name$form () {
+      $tag;
+   }
+//]]>
+</SCRIPT>");
+    }
+    
+    return $menu;
 }
 
 # ============================
@@ -170,7 +120,7 @@ sub _CONTROL
 # If topic contains Web this overrides webName
 =pod
 
----++ sub getListOfFieldValues (  $webName, $topic, $name, $type, $size  )
+---++ sub getListOfFieldValues ( $session, $webName, $topic, $name, $type, $size, $url, $msg1, $msg2, $sel )
 
 Not yet documented.
 
@@ -178,44 +128,107 @@ Not yet documented.
 
 sub getListOfFieldValues
 {
-    my( $webName, $topic, $name, $type, $size ) = @_;
+    my( $session, $webName, $topic, $name, $type, $size, $sep, $url, $msg1, $msg2, $selection ) = @_;
+    # Bug: Due to the onLoad that configures the hierarchical menu,
+    # the fancy features do not work unless this is in a form. Outside a
+    # form this operates in fallback mode.
+    #
+    # Needs error checking if items are not "select" in hierarchical menu
+    # i.e., if not select, we need to pull out a right option even if it has
+    # a [[][]] name
+    #
+    # Requires that the menu hierarchy is complete (i.e., all branches
+    # have the same depth).
+
+    $maxlevel = 0;
+    $menuStack[0] = '';
     
-    #SMELL: shouldn't this be normalize?
     if( $topic =~ /^(.*)\.(.*)$/ ) {
         $webName = $1;
         $topic = $2;
     }
     my @posValues = ();
+
     if( &TWiki::Func::topicExists( $webName, $topic ) ) {
       my( $meta, $text ) = &TWiki::Func::readTopic( $webName, $topic );
       # Processing of SEARCHES for Lists
-      $text =~ s/%SEARCH{(.*?)}%/&TWiki::handleSearchWeb($1)/geo;
+      $text =~ s/%SEARCH{(.*?)}%/&TWiki::_SEARCH($session, $1, $webName, $topic)/geo;
       @posValues = &TWiki::Form::_getPossibleFieldValues( $text );
+    } else {
+      # Need to deal with error case.
     }
 
     my $value = "";
+    my $script = '';
     $type = $type || "select";
     $size = $size || 1;
 
     if( $type eq "select" ) {
-    
-    
-      my $val = 
-#        ($url eq "on")
-#        ?
-        "<option>%URLPARAM{\"$name\"}%</option>"
-#        :""
-;
+      my $idx = 0;
+      my $val = '';
+      $val = ($url eq "on")?"<option value=\"%URLPARAM{\"$name\"}%\">%URLPARAM{\"$name\"}%</option>":"";
       foreach my $item (@posValues) {
-	$item =~ s/<nop/&lt\;nop/go;
-	$val .= "   <option>$item</option>";
+	#next if ($item =~ m/^\s*$/o);
+	my $web = $webName;
+	$item =~ s/%WEB%/$web/o;
+	my $subtopic = $item;
+	if( $item =~ /\[\[(.+)\]\[(.+)\]\]/ ) {
+	  # hierarchical expansion
+	  $item = $2;
+	  $subtopic = $1;
+	  if( $subtopic =~ /^(.*)\.(.*)$/ ) {
+	    # alternate web
+	    $web = $1;
+	    $subtopic = $2;
+	  }
+	} elsif( $item =~ /^(.*)\.(.*)$/ ) {
+	  # alternate web
+	  $web = $1;
+	  $subtopic = $2;
+	  $item = $2;
+	}
+	if ( $selection ) {
+	  # only consider $selection, unless it is empty (in that case, use all)
+	  next unless ($item eq $selection);
+	}
+	if ( $subtopic && TWiki::Func::topicExists( $web, $subtopic ) ) {
+	  # hierarchical control
+	  getHierarchicalMenu( $session, $web, $subtopic, $name, $type, $size, $sep, $url, 1, $item, $msg1, $msg2 );
+	}
+## If we want to be able to set [[value][title]] use the following, but
+## it will not work if we have subtopics
+##	$val .= "   <option value=\"$subtopic\">$item</option>";
+	$val .= "   <option value=\"$item\">$item</option>";
+        $idx++;
       }
-      $value = "<select name=\"$name\" size=\"$size\">$val</select>";
+      my $menudepth = $#menuStack;  # could use $maxlevel
+      if ( defined $selection && ( $menudepth > 0 ) ) {
+	for (my $idy=1; $idy<=$menudepth; $idy++) {
+	  $menuStack[$idy-1] = $menuStack[$idy];
+	}
+	pop @menuStack;
+	$maxlevel--;
+	$val = $menuStack[0];
+	unless ( $maxlevel ) {
+	  $val =~ s/<option value="choose">$msg2<\/option>//o;
+	  $val = "<option value=\"%URLPARAM{\"$name\"}%\">%URLPARAM{\"$name\"}%</option>$val" if ($url eq "on");
+	}
+      }
+      if ( defined $selection && ! $maxlevel ) {
+	$maxlevel = 0;
+	$value = '';
+      } elsif ( $maxlevel ) {
+	$menuStack[0] = $val;
+	$value = generateHierarchicalMenu($name, $sep, $msg1);
+      } else {
+	$value = "<select name=\"$name\" size=\"$size\">";
+	$value .= "$val</select>";
+      }
     } elsif( $type =~ "^checkbox" ) {
       my $val ="<table  cellspacing=\"0\" cellpadding=\"0\"><tr>";
       my $lines = 0;
       foreach my $item ( @posValues ) {
-	my $expandedItem = &TWiki::Func::expandCommonVariables( $item, $topic );
+	my $expandedItem = $session->handleCommonTags( $item, $webName, $topic );
 	$val .= "\n<td><input class=\"twikiEditFormCheckboxField\" type=\"checkbox\" name=\"$name$item\" />$expandedItem &nbsp;&nbsp;</td>";
 	if( $size > 0 && ($lines % $size == $size - 1 ) ) {
 	  $val .= "\n</tr><tr>";
@@ -228,7 +241,7 @@ sub getListOfFieldValues
       my $val = "<table  cellspacing=\"0\" cellpadding=\"0\"><tr>";
       my $lines = 0;
       foreach my $item ( @posValues ) {
-	my $expandedItem = &TWiki::Func::expandCommonVariables( $item, $topic );
+	my $expandedItem = $session->handleCommonTags( $item, $webName, $topic );
 	$val .= "\n<td><input class=\"twikiEditFormRadioField twikiRadioButton\" type=\"radio\" name=\"$name\" value=\"$item\" />$expandedItem &nbsp;&nbsp;</td>";
 	if( $size > 0 && ($lines % $size == $size - 1 ) ) {
 	  $val .= "\n</tr><tr>";
@@ -241,4 +254,107 @@ sub getListOfFieldValues
     return $value;
 }
 
+=pod
+
+---++ sub getHierarchicalMenu ( $session, $webName, $topic, $name, $type, $size, $url, $level, $upper, $msg1, $msg2  )
+
+Not yet documented.
+
+=cut
+
+sub getHierarchicalMenu
+{
+    my( $session, $webName, $topic, $name, $type, $size, $sep, $url, $level, $upper, $msg1, $msg2 ) = @_;
+    # Due to the onLoad that configures the hierarchical menu,
+    # the fancy features do not work unless this is in a form. Outside a
+    # form this operates in fallback mode.
+    #
+    # Requires that the menu hierarchy is complete (i.e., all branches
+    # have the same depth).
+
+    $level = 0 unless $level;
+    $maxlevel = $level if ($level > $maxlevel);
+    my $hierarchicalmenu = '';
+    $menuStack[$level] = '' unless $menuStack[$level];
+    
+    if( $topic =~ /^(.*)\.(.*)$/ ) {
+        $webName = $1;
+        $topic = $2;
+    }
+    my @posValues = ();
+
+    if( &TWiki::Func::topicExists( $webName, $topic ) ) {
+      my( $meta, $text ) = &TWiki::Func::readTopic( $webName, $topic );
+      # Processing of SEARCHES for Lists
+      $text =~ s/%SEARCH{(.*?)}%/&TWiki::_SEARCH($session, $1, $webName, $topic)/geo;
+      @posValues = &TWiki::Form::_getPossibleFieldValues( $text );
+    } else {
+      # Need to deal with error case.
+    }
+
+    unless ( @posValues ) {
+      # Was not hierarchical after all...
+      pop @menuStack;
+      $maxlevel--;
+      return;
+    }
+
+    my $value = "";
+    my $script = '';
+    $type = $type || "select";
+    $size = $size || 1;
+
+    # type must be select
+      my $web = $webName;
+      my $idx = 0;
+      my $val = '';
+      $val .= "<option value=\"choose\">$msg2</option>";
+## Cannot select from the URL parameter
+#      $val .= ($url eq "on")?"<option value=\"%URLPARAM{\"$name$level\"}%\">%URLPARAM{\"$name$level\"}%</option>":"";
+      foreach my $item (@posValues) {
+	#next if ($item =~ m/^\s*$/o);
+	if( $item =~ /\[\[(.+)\]\[(.+)\]\]/ ) {
+	  # hierarchical expansion
+	  $item = $2;
+	  $subtopic = $1;
+	  if( $subtopic =~ /^(.*)\.(.*)$/ ) {
+	    # alternate web
+	    $web = $1;
+	    $subtopic = $2;
+	  }
+	} elsif( $item =~ /^(.*)\.(.*)$/ ) {
+	  # alternate web
+	  $web = $1;
+	  $subtopic = $2;
+	  $item = $2;
+	} else {
+	  $subtopic = $item;
+	}
+	if ( $subtopic && TWiki::Func::topicExists( $web, $subtopic ) ) {
+	  # hierarchical control
+	  getHierarchicalMenu( $session, $web, $subtopic, $name, $type, $size, $sep, $url, $level + 1, $item, $msg1, $msg2 );
+	}
+	$val .= "   <option value=\"$subtopic\">$item</option>";
+        $idx++;
+      }
+
+      $menuStack[$level] .= $val;
+}
+
+sub generateHierarchicalMenu {
+  my ( $name, $sep, $msg1 ) = @_;
+  my $menu .= "<select name=\"${name}0\">";
+  $menu .= "<option value=\"choose\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</option>";
+  $menu .= $menuStack[0];
+  $menu .= "</select>";
+  for ( my $idx=1; $idx<=$maxlevel; $idx++ ) {
+    $menu .= $sep;
+    $menu .= "<select name=\"$name$idx\">";
+    $menu .= "<option value=\"choose\">$msg1</option>";
+    $menu .= $menuStack[$idx];
+    $menu .= "</select>";
+  }
+  return $menu;
+
+}
 1;
