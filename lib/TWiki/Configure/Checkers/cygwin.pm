1638dbc604349b2ff886d69bc2ab28fce28c1924
RafaelAlvarez <RafaelAlvarez@a00a5322-12db-0310-a70b-8735589c885e>
2006-08-27 00:11:03 +0000

    Item2819: Added missing quote. The item is still open, though
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@11376 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Configure/Checkers/cygwin.pm
+++ lib/TWiki/Configure/Checkers/cygwin.pm
@@ -1,6 +1,6 @@
 sub check {
     # Get Cygwin perl's package version number
-    my $pkg = `perl -v; 
+    my $pkg = `perl -v`; 
     if ($?) { 
         return TWiki::Configure::Checker::WARN(<<HERE);
 Cannot identify perl package version - cygcheck or grep not installed
