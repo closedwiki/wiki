9ada31205c751bfb6fcb958fcf3d2435225e374b
AntonioTerceiro <AntonioTerceiro@a00a5322-12db-0310-a70b-8735589c885e>
2006-09-04 12:27:42 +0000

    Item2844: TopicTranslationsPlugin's new version:
       * applied patch from TWiki:Main/BeomsuChang for different methods osf language detection
       * fixed annoying warnings in logs
       * reworked perl code indentation
    
    
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00@11418 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/TopicTranslationsPlugin/lib/TWiki/Plugins/TopicTranslationsPlugin.pm
+++ twikiplugins/TopicTranslationsPlugin/lib/TWiki/Plugins/TopicTranslationsPlugin.pm
@@ -18,12 +18,14 @@ package TWiki::Plugins::TopicTranslationsPlugin;
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-        $debug
-        @translations
-        $defaultLanguage
-        $acceptor
-    );
+    $web $topic $user $installWeb $VERSION $RELEASE $pluginName
+    $debug
+    @translations
+    $defaultLanguage
+    $redirectMethod
+    $userLanguage
+    $acceptor
+);
 
 use I18N::AcceptLanguage;
 
@@ -40,8 +42,7 @@ $RELEASE = 'Dakar';
 $pluginName = 'TopicTranslationsPlugin';  # Name of this Plugin
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
@@ -57,15 +58,19 @@ sub initPlugin
     # corresponding plugin setting (or "en" if someone messes with it)
     my $trans = TWiki::Func::getPreferencesValue("TOPICTRANSLATIONS") || TWiki::Func::getPluginPreferencesValue("TOPICTRANSLATIONS") || "en";
     @translations = split(/,\s*/,$trans);
+    $redirectMethod = TWiki::Func::getPreferencesValue("REDIRECTMETHOD") || TWiki::Func::getPluginPreferencesValue("REDIRECTMETHOD") || "http";
+    $userLanguage = TWiki::Func::getPreferencesValue("LANGUAGE") || "en";    
 
     # first listed language is the default one:
     $defaultLanguage = $translations[0];
 
     # create a language acceptor for later use:
-    $acceptor = I18N::AcceptLanguage->new(
-      strict => 0,
-      defaultLanguage => $defaultLanguage
-      );
+    if($redirectMethod eq "http") {
+        $acceptor = I18N::AcceptLanguage->new (
+            strict => 0,
+            defaultLanguage => $defaultLanguage
+        );
+    }
 
     # must I redirect to the best available translation?
     my $mustRedirect = (! TWiki::Func::getPluginPreferencesFlag("DISABLE_AUTOMATIC_REDIRECTION"));
@@ -77,8 +82,7 @@ sub initPlugin
 }
 
 # =========================
-sub beforeCommonTagsHandler
-{
+sub beforeCommonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
     TWiki::Func::writeDebug( "- ${pluginName}::beforeCommonTagsHandler( $_[2].$_[1] )" ) if $debug;
@@ -88,8 +92,7 @@ sub beforeCommonTagsHandler
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
     TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
@@ -107,234 +110,231 @@ sub commonTagsHandler
 #   pt-br -> Ptbr     EN -> En
 #   pt_BR -> Ptbr     Pt -> Pt
 #   EN-US -> Enus     pt -> Pt
-sub normalizeLanguageName
-{
-  my $lang = shift;
-  $lang =~ s/[_-]//g;
-  $lang =~ s/^(.)(.*)$/\u$1\L$2/;
-  return $lang;
+sub normalizeLanguageName {
+    my $lang = shift;
+    $lang =~ s/[_-]//g;
+    $lang =~ s/^(.)(.*)$/\u$1\L$2/;
+    return $lang;
 }
 
 # finds the base topic name, i.e., the topic name without any language suffix.
 # If no topic is passed as argument, uses $topic (the topic from which the
 # plugin is being called).
-sub findBaseTopicName
-{
-  my $base = shift || $topic;
-  foreach $lang (@translations) {
-    $norm = normalizeLanguageName($lang);
-    if ($base =~ m/$norm$/) {
-      $base =~ s/$norm$//;
+sub findBaseTopicName {
+    my $base = shift || $topic;
+    foreach $lang (@translations) {
+        $norm = normalizeLanguageName($lang);
+        if ($base =~ m/$norm$/) {
+            $base =~ s/$norm$//;
+        }
     }
-  }
-  return $base;
+    return $base;
 }
 
 # finds the language that must be used for anything within the current topic,
 # based on its suffix
-sub currentLanguage
-{
-  my $norm;
-  foreach $lang (@translations) {
-    $norm = normalizeLanguageName($lang);
-    if ($topic =~ m/$norm$/) {
-      return $lang;
+sub currentLanguage {
+    my $norm;
+    foreach $lang (@translations) {
+        $norm = normalizeLanguageName($lang);
+        if ($topic =~ m/$norm$/) {
+            return $lang;
+        }
     }
-  }
-  return $defaultLanguage;
+    return $defaultLanguage;
 }
 
 # list the translations of the current topic (or to that one passed as an
 # argument). Depending on the arguments to the %TRANSLATIONS% tag, many options
 # can apply.
-sub handleTranslations
-{
-  my $params = shift;
+sub handleTranslations {
+    my $params = shift;
 
-  my $result = "";
-  my $separator = "";
-  my $norm;
+    my $result = "";
+    my $separator = "";
+    my $norm;
 
-  # format for the items:
-  my $format = TWiki::Func::extractNameValuePair($params, "format") || "[[\$web.\$translation][\$language]]";
-  my $missingFormat = TWiki::Func::extractNameValuePair($params, "missingformat") || $format;
-  
-  # other stuff:
-  my $userSeparator = TWiki::Func::extractNameValuePair($params, "separator") || " ";
+    # format for the items:
+    my $format = TWiki::Func::extractNameValuePair($params, "format") || "[[\$web.\$translation][\$language]]";
+    my $missingFormat = TWiki::Func::extractNameValuePair($params, "missingformat") || $format;
+    
+    # other stuff:
+    my $userSeparator = TWiki::Func::extractNameValuePair($params, "separator") || " ";
 
-  # 
-  my $theTopic = TWiki::Func::extractNameValuePair($params) || TWiki::Func::extractNameValuePair($params,"topic") || $topic;
-  my $theWeb;
-  if ($theTopic =~ m/^([^.]+)\.([^.]+)/) {
-    # topic="Web.MyTopic"
-    $theWeb = $1;
-    $theTopic = $2;
-  } else {
-    $theWeb = $web;
-  }
-  my $baseTopicName = findBaseTopicName($theTopic);
+    # 
+    my $theTopic = TWiki::Func::extractNameValuePair($params) || TWiki::Func::extractNameValuePair($params,"topic") || $topic;
+    my $theWeb;
+    if ($theTopic =~ m/^([^.]+)\.([^.]+)/) {
+        # topic="Web.MyTopic"
+        $theWeb = $1;
+        $theTopic = $2;
+    } else {
+        $theWeb = $web;
+    }
+    my $baseTopicName = findBaseTopicName($theTopic);
 
-  # find out which translations we must list:
-  my $which = TWiki::Func::extractNameValuePair($params, "which") || "all";
-  my @whichTranslations;
-  if ($which eq "available") {
-    @whichTranslations = findAvailableTranslations($theTopic);
-  } elsif ($which eq "missing") {
-    @whichTranslations = findMissingTranslations($theTopic);
-  } else {
-    @whichTranslations = @translations;
-  }
+    # find out which translations we must list:
+    my $which = TWiki::Func::extractNameValuePair($params, "which") || "all";
+    my @whichTranslations;
+    if ($which eq "available") {
+        @whichTranslations = findAvailableTranslations($theTopic);
+    } elsif ($which eq "missing") {
+        @whichTranslations = findMissingTranslations($theTopic);
+    } else {
+        @whichTranslations = @translations;
+    }
  
-  # list translations
-  foreach $lang (@whichTranslations) {
-    $norm = ($lang eq $defaultLanguage)?'':normalizeLanguageName($lang);
-    $result .= $separator;
-    $separator = $userSeparator;
-    $result .= formatTranslationEntry($baseTopicName, $theWeb, $baseTopicName . $norm, $lang, $format, $missingFormat);
-  }
+    # list translations
+    foreach $lang (@whichTranslations) {
+        $norm = ($lang eq $defaultLanguage)?'':normalizeLanguageName($lang);
+        $result .= $separator;
+        $separator = $userSeparator;
+        $result .= formatTranslationEntry($baseTopicName, $theWeb, $baseTopicName . $norm, $lang, $format, $missingFormat);
+    }
 
-  return $result;
+    return $result;
 }
 
 # shows the item using the given format
-sub formatTranslationEntry
-{
-  my ($theTopic, $theWeb, $translationTopic, $lang, $format, $missingFormat) = @_;
+sub formatTranslationEntry {
+    my ($theTopic, $theWeb, $translationTopic, $lang, $format, $missingFormat) = @_;
 
-  # wheter to use the format for available translations or for missing ones:
-  my $result = (TWiki::Func::topicExists($web, $translationTopic))?($format):($missingFormat);
+    # wheter to use the format for available translations or for missing ones:
+    my $result = (TWiki::Func::topicExists($web, $translationTopic))?($format):($missingFormat);
 
-  # substitute the variables:
-  $result =~ s/\$web/$theWeb/g;
-  $result =~ s/\$topic/$theTopic/g;
-  $result =~ s/\$translation/$translationTopic/g;
-  $result =~ s/\$language/$lang/g;
+    # substitute the variables:
+    $result =~ s/\$web/$theWeb/g;
+    $result =~ s/\$topic/$theTopic/g;
+    $result =~ s/\$translation/$translationTopic/g;
+    $result =~ s/\$language/$lang/g;
 
-  return $result;
+    return $result;
 }
 
 # include the translation of the given topic that corresponds to our current
 # language
-sub handleIncludeTranslation
-{
-  my $params = shift;
+sub handleIncludeTranslation {
+    my $params = shift;
 
-  my $theLang = currentLanguage();
-  
-  my $theTopic = TWiki::Func::extractNameValuePair($params);
-  my $theWeb;
-  if ($theTopic =~ m/^([^.]+)\.([^.]+)/) {
-    # topic="Web.MyTopic"
-    $theWeb = $1;
-    $theTopic = $2;
-  } else {
-    $theWeb = $web;
-  }
-  $theTopic = findBaseTopicName($theTopic);
+    my $theLang = currentLanguage();
+    
+    my $theTopic = TWiki::Func::extractNameValuePair($params);
+    my $theWeb;
+    if ($theTopic =~ m/^([^.]+)\.([^.]+)/) {
+        # topic="Web.MyTopic"
+        $theWeb = $1;
+        $theTopic = $2;
+    } else {
+        $theWeb = $web;
+    }
+    $theTopic = findBaseTopicName($theTopic);
 
-  if ($theLang ne $defaultLanguage) {
-    $theTopic .= normalizeLanguageName($theLang);
-  }
+    if ($theLang ne $defaultLanguage) {
+        $theTopic .= normalizeLanguageName($theLang);
+    }
 
-  # undef is ok, meaning current revision:
-  my $theRev = TWiki::Func::extractNameValuePair($params, "rev");
+    # undef is ok, meaning current revision:
+    my $theRev = TWiki::Func::extractNameValuePair($params, "rev");
 
-  my $args = "\"$theWeb.$theTopic\"";
-  $args .= " rev=\"$theRev\"" if $theRev;
+    my $args = "\"$theWeb.$theTopic\"";
+    $args .= " rev=\"$theRev\"" if $theRev;
 
-  return '%INCLUDE{' . $args . '}%';
+    return '%INCLUDE{' . $args . '}%';
 }
 
 # finds the best suitable translation to the current topic (or, alternatively,
 # to the topic passsed as the first parameter)
-sub findBestTranslation
-{
-  my $theTopic = shift || $topic;
-  my @alternatives = findAvailableTranslations($theTopic);
-  return $acceptor->accepts($ENV{HTTP_ACCEPT_LANGUAGE}, \@alternatives);
+sub findBestTranslation {
+    my $theTopic = shift || $topic;
+    my @alternatives = findAvailableTranslations($theTopic);
+    my $best=$defaultLanguage;
+    if ($redirectMethod eq "user"){
+        foreach $lang (@alternatives){
+            $best=$lang if $userLanguage eq $lang;
+        }
+    } else { # $redirectMethod is http or anything else
+        $best=$acceptor->accepts($ENV{HTTP_ACCEPT_LANGUAGE}, \@alternatives);
+    }
+    return $best;
 }
 
 # check if a redirection is needed, possible, and do that if it's the case
-sub checkRedirection
-{
-  # we only want to be redirected in view or viewauth, and when there is no
-  # extra parameters to the request:
-  if (($ENV{SCRIPT_NAME} =~ m#/view(auth)?$#) and (! $ENV{QUERY_STRING})) {
-    my $query = TWiki::Func::getCgiQuery();
-  
-    # several checks
-    my $baseTopicName = findBaseTopicName();
-    my $baseUrl = TWiki::Func::getViewUrl($web, $baseTopicName);
-    my $editUrl = TWiki::Func::getScriptUrl($web, $baseTopicName, 'edit');
-    my $origin = $query->referer() || '';
+sub checkRedirection {
+    # we only want to be redirected in view or viewauth, and when there is no
+    # extra parameters to the request:
+    if (($ENV{SCRIPT_NAME} =~ m#/view(auth)?$#) and (! $ENV{QUERY_STRING})) {
+        my $query = TWiki::Func::getCgiQuery();
     
-    # we don't want to redirect if the user came from another translation of
-    # this same topic, or from an edit
-    if ( (!($origin =~ /^$baseUrl/)) and (!($origin =~ /^$editUrl/))) {
+        # several checks
+        my $baseTopicName = findBaseTopicName();
+        my $baseUrl = TWiki::Func::getViewUrl($web, $baseTopicName);
+        my $editUrl = TWiki::Func::getScriptUrl($web, $baseTopicName, 'edit');
+        my $origin = $query->referer() || '';
+        
+        # we don't want to redirect if the user came from another translation of
+        # this same topic, or from an edit
+        if ( (!($origin =~ /^$baseUrl/)) and (!($origin =~ /^$editUrl/))) {
 
-      # check where we are:
-      my $current = currentLanguage();
-      my $best = findBestTranslation(); # for the current topic, indeed
+            # check where we are:
+            my $current = currentLanguage();
+            my $best = findBestTranslation(); # for the current topic, indeed
 
-      # we don't need to redirect if we are already in the best translation:
-      if (($current ne $best)) {
-        # actually do the redirect:
-        my $bestTranslationTopic = findBaseTopicName() . (($best eq $defaultLanguage)?'':(normalizeLanguageName($best)));
-        my $url = TWiki::Func::getViewUrl($web,$bestTranslationTopic);
-        TWiki::Func::redirectCgiQuery($query, $url);
-      }
+            # we don't need to redirect if we are already in the best translation:
+            if (($current ne $best)) {
+              # actually do the redirect:
+              my $bestTranslationTopic = findBaseTopicName() . (($best eq $defaultLanguage)?'':(normalizeLanguageName($best)));
+              my $url = TWiki::Func::getViewUrl($web,$bestTranslationTopic);
+              TWiki::Func::redirectCgiQuery($query, $url);
+            }
+        }
     }
-  }
 }
 
 # find the translations that already exist for the given topic, if any,
 # or for $topic, if no topic is informed.
-sub findAvailableTranslations
-{
-  return findTranslations(1, (shift || $topic));
+sub findAvailableTranslations {
+    return findTranslations(1, (shift || $topic));
 }
 
 # find the translations that doesnt' exist yet for the given topic, if any, or
 # for $topic, if no topic is informed.
-sub findMissingTranslations
-{
-  return findTranslations(0, (shift || $topic));
+sub findMissingTranslations {
+    return findTranslations(0, (shift || $topic));
 }
 
 # find translations that exists or are missing, depending on the first
 # parameter (call it $existance):
 # * if $existance evaluates to TRUE, find translations that do exist
 # * if $existance evaluates to FALSE, find translations that DON'T exit
-sub findTranslations
-{
-  my $existance = shift;
+sub findTranslations {
+    my $existance = shift;
 
-  $theTopic = shift || $topic;
-  $theTopic = findBaseTopicName($theTopic);
+    $theTopic = shift || $topic;
+    $theTopic = findBaseTopicName($theTopic);
 
-  my ($norm, $exists);
-  my @items;
+    my ($norm, $exists);
+    my @items;
 
-  foreach $lang (@translations) {
-    # the suffix is empty in the case of the default language:
-    $norm = ($lang eq $defaultLanguage)?(""):(normalizeLanguageName($lang));
-    
-    # is that translation available?
-    $exists = TWiki::Func::topicExists($web, $theTopic . $norm);
+    foreach $lang (@translations) {
+        # the suffix is empty in the case of the default language:
+        $norm = ($lang eq $defaultLanguage)?(""):(normalizeLanguageName($lang));
+        
+        # is that translation available?
+        $exists = TWiki::Func::topicExists($web, $theTopic . $norm);
 
-    # what kind (available or not) are we looking for?
-    if (($existance and $exists) or ((!$existance) and (!$exists))) {
-      push(@items, $lang);
+        # what kind (available or not) are we looking for?
+        if (($existance and $exists) or ((!$existance) and (!$exists))) {
+            push(@items, $lang);
+        }
     }
-  }
-  
-  return @items;
+    
+    return @items;
 }
 
 sub handleBaseTranslation {
-  my $params = shift;
-  my $myTopic = TWiki::Func::extractNameValuePair($params, "topic") || $topic;
-  return findBaseTopicName($myTopic);
+    my $params = shift;
+    my $myTopic = TWiki::Func::extractNameValuePair($params, "topic") || $topic;
+    return findBaseTopicName($myTopic);
 }
 
 # =========================
