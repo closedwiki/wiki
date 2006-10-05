eda1298275f05b2bf63616d0f371472318f54f19
SvenDowideit <SvenDowideit@a00a5322-12db-0310-a70b-8735589c885e>
2006-10-05 18:24:56 +0000

    Item2403: working towards more browser compatibility, while giving the user affordances (like an edit button)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x00/twikiplugins@11668 a00a5322-12db-0310-a70b-8735589c885e

--- JSPopupPlugin/pub/TWiki/JSPopupPlugin/JSPopupPlugin.js
+++ JSPopupPlugin/pub/TWiki/JSPopupPlugin/JSPopupPlugin.js
@@ -206,6 +206,32 @@ TWiki.JSPopupPlugin.ajaxCall = function(event, popupUrl, popupParams) {
 /***********************************************************
 more generic tools - need to share at some stage
 */
+/* http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html#more */
+function addEvent( obj, type, fn )
+{
+	if (obj.addEventListener)
+		obj.addEventListener( type, fn, false );
+	else if (obj.attachEvent)
+	{
+		obj["e"+type+fn] = fn;
+		obj[type+fn] = function() { obj["e"+type+fn]( window.event ); }
+		obj.attachEvent( "on"+type, obj[type+fn] );
+	}
+}
+function removeEvent( obj, type, fn )
+{
+	if (obj.removeEventListener)
+		obj.removeEventListener( type, fn, false );
+	else if (obj.detachEvent)
+	{
+		obj.detachEvent( "on"+type, obj[type+fn] );
+		obj[type+fn] = null;
+		obj["e"+type+fn] = null;
+	}
+}
+
+
+
 //from http://weblogs.asp.net/asmith/archive/2003/10/06/30744.aspx
 //add an event handler so they chain, and cross browser
 function XBrowserAddHandler(target,eventName,handlerName) { 
