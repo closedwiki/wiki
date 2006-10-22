5a584012b41e48da173242fe91a2db622c39ec21
SvenDowideit <SvenDowideit@a00a5322-12db-0310-a70b-8735589c885e>
2006-10-22 13:07:53 +0000

    Item2403: commit YUI 0.11.4 so i can DEPEND in it :)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@11796 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/YahooUserInterfaceContrib/test/unit/YahooUserInterfaceContrib/YahooUserInterfaceContribSuite.pm
@@ -0,0 +1,9 @@
+package YahooUserInterfaceContribSuite;
+
+use base qw(Test::Unit::TestSuite);
+
+sub name { 'YahooUserInterfaceContribSuite' };
+
+sub include_tests { qw(YahooUserInterfaceContribTests) };
+
+1;
