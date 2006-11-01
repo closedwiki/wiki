3de0d9daabb925fe640e5bd9a7b4bd5513c10cf1
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2006-11-01 15:44:20 +0000

    Item3080: fix SELECT
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@11864 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Configure/Types/SELECT.pm
+++ lib/TWiki/Configure/Types/SELECT.pm
@@ -26,6 +26,7 @@ sub prompt {
     my( $this, $id, $opts, $value ) = @_;
     $opts =~ s/^\s+//;
     $opts =~ s/\s+$//;
+    $opts =~ s/(\b|^)EXPERT(\b|$)//i;
     my $sopts = '';
     if ( defined($value) ) {
     	$sopts .= '<option selected="selected">'.$value.'</option>';
