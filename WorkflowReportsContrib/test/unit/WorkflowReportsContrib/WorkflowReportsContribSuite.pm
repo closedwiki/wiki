afc82394bc86655695e4999683795b4cb17449bb
PaulHoesli <PaulHoesli@a00a5322-12db-0310-a70b-8735589c885e>
2006-11-04 19:36:04 +0000

    Item3099: WorkflowReportsContrib initial check in
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@11894 a00a5322-12db-0310-a70b-8735589c885e

---
+++ WorkflowReportsContrib/test/unit/WorkflowReportsContrib/WorkflowReportsContribSuite.pm
@@ -0,0 +1,9 @@
+package WorkflowReportsContribSuite;
+
+use base qw(Test::Unit::TestSuite);
+
+sub name { 'WorkflowReportsContribSuite' };
+
+sub include_tests { qw(WorkflowReportsContribTests) };
+
+1;
