34ebc58dbd1ce514dbeaa2d0ab4109ac80b0d20b
ThomasWeigert <ThomasWeigert@a00a5322-12db-0310-a70b-8735589c885e>
2006-11-12 04:31:33 +0000

    Item2870: Updated RedirectPlugin.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@11968 a00a5322-12db-0310-a70b-8735589c885e

--- RedirectPlugin/lib/TWiki/Plugins/RedirectPlugin.pm
+++ RedirectPlugin/lib/TWiki/Plugins/RedirectPlugin.pm
@@ -1,5 +1,6 @@
 # TWiki RedirectPlugin
 #
+# Copyright (C) 2006 Motorola, thomas.weigert@motorola.com
 # Copyright (C) 2006 Meredith Lesly, msnomer@spamcop.net
 # Copyright (C) 2003 Steve Mokris, smokris@softpixel.com
 #
@@ -11,41 +12,45 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at
+# GNU General Public License for more details, published at 
 # http://www.gnu.org/copyleft/gpl.html
+
 
 # =========================
 package TWiki::Plugins::RedirectPlugin;
 
 # =========================
-use vars qw( $VERSION $RELEASE $debug);
+use vars qw( $VERSION $RELEASE $debug $pluginName );
+use strict;
 
 $VERSION = '$Rev$';
 $RELEASE = 'Dakar';
+$pluginName = 'RedirectPlugin';
 
 # =========================
-sub initPlugin {
-    #my ( $topic, $web, $user, $installWeb ) = @_;
+sub initPlugin
+{
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if ($TWiki::Plugins::VERSION < 1.1) {
-        TWiki::Func::writeWarning("Version mismatch between RedirectPlugin and Plugins.pm");
+    if( $TWiki::Plugins::VERSION < 1.1 ) {
+        TWiki::Func::writeWarning( "This version of $pluginName works only with TWiki 4 and greater." );
         return 0;
     }
 
-    my $query = TWiki::Func::getCgiQuery();
-
     # this doesn't really have any meaning if we aren't being called as a CGI
+    my $query=&TWiki::Func::getCgiQuery();
     return 0 unless $query;
 
     # Get plugin debug flag
-    $debug = TWiki::Func::getPreferencesFlag("REDIRECTPLUGIN_DEBUG");
+    $debug = &TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
 
     TWiki::Func::registerTagHandler('REDIRECT', \&REDIRECT);
+
+    # Plugin correctly initialized
+    &TWiki::Func::writeDebug( "- TWiki::Plugins::$pluginName::initPlugin( $web.$topic ) is OK" ) if $debug;
     return 1;
 }
-
-
 
 # =========================
 sub REDIRECT {
@@ -55,40 +60,61 @@ sub REDIRECT {
     my $newWeb = $web;
     my $newTopic;
     my $anchor = '';
+    my $urlparams = '';
     my $dest = $params->{'newtopic'} || $params->{_DEFAULT};
 
-    #
     # Redirect only on view.
-    #
     if ($context->{'view'} && $dest) {
         my $query = TWiki::Func::getCgiQuery();
-        $dest = TWiki::Func::expandCommonVariables($dest);
+        $dest = TWiki::Func::expandCommonVariables($dest, $topic, $web);
 
         my $webNameRegex = TWiki::Func::getRegularExpression('webNameRegex');
         my $wikiWordRegex = TWiki::Func::getRegularExpression('wikiWordRegex');
         my $anchorRegex = TWiki::Func::getRegularExpression('anchorRegex');
 
+	# redirect to URL
+	if ($dest =~ m/^http/) {
+
+	  return "<br>%RED%Cannot redirect to current topic%ENDCOLOR%"
+	    if ( $dest eq TWiki::Func::getViewUrl($web, $topic) );
+	  TWiki::Func::redirectCgiQuery($query, $dest);
+	  return '';
+	}
+
+	# redirect within this site
         if ($dest =~ s/^($webNameRegex)\.// ) {
             $newWeb = $1;
         }
 
         if ($dest =~ s/^($wikiWordRegex)//) {
             $newTopic = $1;
-        } elsif ($dest =~ s/^\.([a-zA-Z0-9]+)//) {
-            $newTopic = $1;
         }
-        if ($dest =~ /^($anchorRegex)$/) {
+
+        if ($newWeb eq $web && $newTopic eq $topic) {
+            return "<br>%RED%Cannot redirect to current topic%ENDCOLOR%";
+        }
+
+        if ($dest =~ s/^($anchorRegex)$//) {
             $anchor = $1;
         }
 
-        if ($newWeb eq $web && $newtopic eq $topic) {
-            return "%RED%Can't redirect to same topic%ENDCOLOR%";
-        } else {
-            TWiki::Func::redirectCgiQuery($query, TWiki::Func::getViewUrl($newWeb, $newTopic) . $anchor);
+	if ($dest =~ /^\?/) {
+	    $urlparams = $dest;
+	    $dest = '';
+	}
+	
+	unless ($dest) {
+            TWiki::Func::redirectCgiQuery($query, TWiki::Func::getViewUrl($newWeb, $newTopic) . $anchor . $urlparams);
         }
-        return "%RED%Destination $dest not found";
+
+        return "%RED%Cannot redirect to $dest";
+
     }
-    return "";
+
+    return '';
+
 }
+
+# =========================
 
 1;
