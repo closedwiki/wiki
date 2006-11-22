98f7105b2f739b67a5b513b7702cd4e9bf6c4add
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2006-11-22 17:53:53 +0000

    Item3178: just occurred to me why the precommit isn't blocking checkins correctly
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@12035 a00a5322-12db-0310-a70b-8735589c885e

--- tools/develop/pre-commit.pl
+++ tools/develop/pre-commit.pl
@@ -12,12 +12,6 @@ my $REPOS = $ARGV[0];
 my $TXN = $ARGV[1];
 my $BRANCH = $ARGV[2];
 
-# See if this checkin is changing our branch
-my $paths = `/usr/bin/svnlook changed -t $TXN $REPOS`;
-exit 0 unless( $paths =~ m#\stwiki/branches/$BRANCH/#s );
-
-my $logmsg = `/usr/bin/svnlook log -t $TXN $REPOS`;
-
 sub fail {
     my $message = shift;
     print STDERR <<EOF;
@@ -35,6 +29,12 @@ EOF
 }
 
 fail("$BRANCH is disabled for checkins") if $BRANCH =~ /^(TWikiRelease04x00|DEVELOP)$/;
+
+# See if this checkin is changing our branch
+my $paths = `/usr/bin/svnlook changed -t $TXN $REPOS`;
+exit 0 unless( $paths =~ m#\stwiki/branches/$BRANCH/#s );
+
+my $logmsg = `/usr/bin/svnlook log -t $TXN $REPOS`;
 
 local $/ = undef;
 fail("No Bug item in log message")
