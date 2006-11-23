407a085b74902c19513a7238f38dc1f9e8ad25ac
ThomasWeigert <ThomasWeigert@a00a5322-12db-0310-a70b-8735589c885e>
2006-11-23 04:08:36 +0000

    Item2870: Implement new features in SignaturePlugin.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@12039 a00a5322-12db-0310-a70b-8735589c885e

--- SignaturePlugin/lib/TWiki/Plugins/SignaturePlugin.pm
+++ SignaturePlugin/lib/TWiki/Plugins/SignaturePlugin.pm
@@ -1,10 +1,6 @@
 # Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
-# Copyright (C) 2001-2006 Peter Thoeny, peter@thoeny.org
-# and TWiki Contributors. All Rights Reserved. TWiki Contributors
-# are listed in the AUTHORS file in the root of this distribution.
-# NOTE: Please extend that file, not this notice.
+# Copyright (C) 2006 Motorola, thomas.weigert@motorola.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -67,95 +63,14 @@ sub preRenderingHandler
     # Only bother with this plugin if viewing (i.e. not searching, etc)
     return unless ($0 =~ m/view|viewauth|render/o);
 
-    # Get rid of CRs (we only want to deal with LFs)
     my $cnt;
-    $_[0] =~ s/%SIGNATURE%/&handleSignature($cnt++)/geo;
+    $_[0] =~ s/%SIGNATURE(?:{(.*)})?%/&handleSignature($cnt++, $1)/geo;
 
 }
 
 sub handleSignature {
-  my ( $cnt ) = @_;
-  my $session = $TWiki::Plugins::SESSION;
-  my $fmt = TWiki::Func::getPreferencesValue( "\U$pluginName\E_SIGNATURELABEL" ) || 'Sign';
-
-  return "<form action=\"" . &TWiki::Func::getScriptUrl($session->{webName}, $session->{topicName}, 'digisign') . "\" /><input type=\"hidden\" name=\"nr\" value=\"$cnt\" /><input type=\"submit\" value=\"$fmt\" /></form>";
-
-}
-
-sub sign {
-  my $session = shift;
-  $TWiki::Plugins::SESSION = $session;
-  my $query = $session->{cgiQuery};
-  return unless ( $query );
-
-  my $cnt = $query->param( 'nr' );
-
-  my $webName = $session->{webName};
-  my $topic = $session->{topicName};
-  my $user = $session->{user};
-  return unless ( &doEnableEdit ($webName, $topic, $user, $query, 'editTableRow') );
-
-  my ( $meta, $text ) = &TWiki::Func::readTopic( $webName, $topic );
-  $text =~ s/%SIGNATURE%/&replaceSignature($cnt--, $user)/geo;
-
-  my $error = &TWiki::Func::saveTopicText( $webName, $topic, $text, 1 );
-  TWiki::Func::setTopicEditLock( $webName, $topic, 0 );  # unlock Topic
-  if( $error ) {
-    TWiki::Func::redirectCgiQuery( $query, $error );
-    return 0;
-  } else {
-    # and finally display topic
-    TWiki::Func::redirectCgiQuery( $query, &TWiki::Func::getViewUrl( $webName, $topic ) );
-  }
-  
-}
-
-sub replaceSignature {
-  my ( $dont, $user ) = @_;
-
-  return '%SIGNATURE%' if $dont;
-  
-  my $fmt = TWiki::Func::getPreferencesValue( "\U$pluginName\E_SIGNATUREFORMAT" ) || '$wikiusername - $date';
-
-  my @months = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
-  my ($d, $m, $y) = (localtime)[3, 4, 5];
-  $y += 1900;
-  my $ourDate = sprintf('%02d %s %d', $d, $months[$m], $y);
-
-  $fmt =~ s/\$quot/\"/go;
-  $fmt =~ s/\$wikiusername/$user->webDotWikiName()/geo;
-  $fmt =~ s/\$wikiname/$user->wikiName()/geo;
-  $fmt =~ s/\$username/$user->login()/geo;
-  $fmt =~ s/\$date/$ourDate/geo;
-
-  return $fmt;
-
-}
-
-sub doEnableEdit
-{
-    my ( $theWeb, $theTopic, $user, $query ) = @_;
-
-    if( ! &TWiki::Func::checkAccessPermission( "change", $user, "", $theTopic, $theWeb ) ) {
-        # user does not have permission to change the topic
-        throw TWiki::OopsException( 'accessdenied',
-                                    def => 'topic_access',
-                                    web => $_[2],
-                                    topic => $_[1],
-				    params => [ 'Edit topic', 'You are not permitted to edit this topic' ] );
-	return 0;
-    }
-
-    my( $oopsUrl, $lockUser ) = &TWiki::Func::checkTopicEditLock( $theWeb, $theTopic, 'edit' );
-    if( $lockUser && ! ( $lockUser eq $user->login ) ) {
-      # warn user that other person is editing this topic
-      &TWiki::Func::redirectCgiQuery( $query, $oopsUrl );
-      return 0;
-    }
-    TWiki::Func::setTopicEditLock( $theWeb, $theTopic, 1 );
-
-    return 1;
-
+  require TWiki::Plugins::SignaturePlugin::Signature;
+  return TWiki::Plugins::SignaturePlugin::Signature::handleSignature(@_);
 }
 
 1;
