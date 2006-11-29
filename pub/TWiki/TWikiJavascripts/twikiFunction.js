51df567895561e598e59ab2526deee4e97317033
ArthurClemens <ArthurClemens@a00a5322-12db-0310-a70b-8735589c885e>
2006-11-29 11:50:57 +0000

    Item3064: extendsFrom prototype refactored to static twiki.Function function
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@12088 a00a5322-12db-0310-a70b-8735589c885e

--- pub/TWiki/TWikiJavascripts/twikiFunction.js
+++ pub/TWiki/TWikiJavascripts/twikiFunction.js
@@ -1,19 +1,26 @@
-/**
-Easy inheritance, see http://blogger.xs4all.nl/peterned/archive/2006/01/12/73948.aspx
-@use
-<pre>
-	function BThing(lorem, ipsum, dolor) {
+
+twiki.Function = {
+
+	/**
+	Easy inheritance, see http://blogger.xs4all.nl/peterned/archive/2006/01/12/73948.aspx
+	@param inClass : (Function) Function object to extend
+	@param inSuperClass : (Function) Function object to extend from
+	@return The extended inClass Function object.
+	@use
+	<pre>
+		function AThing() {}
+		function BThing(lorem, ipsum, dolor) {}
+		BThing = twiki.twikiFunction.extendClass(BThing, AThing);
+		// BThing prototype functions here...
+	</pre>
+	*/
+	extendClass:function(inClass, inSuperClass) {
+		var Self = inClass;
+		var Func = function() {
+			inSuperClass.apply(inClass, arguments);
+			Self.apply(inClass, arguments);
+		};
+		Func.prototype = new inSuperClass();
+		return Func;
 	}
-	BThing = BThing.extendsFrom(AThing);
-	// prototype functions here...
-</pre>
-*/
-Function.prototype.extendsFrom = function(Super) {
-	var Self = this;
-	var Func = function() {
-		Super.apply(this, arguments);
-		Self.apply(this, arguments);
-	};
-	Func.prototype = new Super();
-	return Func;
-}
+};
