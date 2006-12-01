a233cddc2570c40ed488299d7f24201687d6ee1d
HaraldJoerg <HaraldJoerg@a00a5322-12db-0310-a70b-8735589c885e>
2006-12-01 22:05:45 +0000

    Item3219: Remove ViewParamSectionTest.pm dependency on CliRunnerContrib
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@12123 a00a5322-12db-0310-a70b-8735589c885e

--- test/unit/ViewParamSectionTests.pm
+++ test/unit/ViewParamSectionTests.pm
@@ -6,9 +6,9 @@ use base qw(TWikiTestCase);
 
 use strict;
 
-use TWiki::Contrib::CliRunnerContrib;
+use TWiki::UI::View;
 
-my $runner; # will be passed from set_up to the tests
+my $twiki;
 
 sub new {
     my $self = shift()->SUPER::new(@_);
@@ -20,22 +20,38 @@ sub set_up {
     my $this = shift;
 
     $this->SUPER::set_up();
-    $runner  =  TWiki::Contrib::CliRunnerContrib->new;
-    $runner->addScriptOptions(skin => 'text', user => 'TWikiGuest');
-    $runner->twikiCfg(LogFileName => '/dev/null');
-    $runner->topic('TestCases.IncludeFixtures');
+    $twiki = TWiki->new();
 }
 
 sub tear_down {
     my $this = shift;
+
+    $twiki->finish();
     $this->SUPER::tear_down();
 }
+
+sub _viewSection {
+    my ($this,$section) = @_;
+
+    $twiki->{webName} = 'TestCases';
+    $twiki->{topicName} = 'IncludeFixtures';
+
+    my $query = $twiki->{cgiQuery};
+    $query->param('-name'=>'skin','-value'=>'text');
+    $query->path_info('TestCases/IncludeFixtures');
+
+    $query->param('-name'=>'section','-value'=>$section);
+    my ($text,$result)  =  $this->capture( \&TWiki::UI::View::view, $twiki);
+    $text =~ s/(.*?)\r?\n\r?\n//s;
+
+    return ($text);
+}
+
 
 # ----------------------------------------------------------------------
 # General:  All tests assume that formatting parameters (especially
 #           skin) are applied correctly after the section has been 
 #           extracted from the topic
-# General:  Log file entries are discarded.
 
 
 # ----------------------------------------------------------------------
@@ -43,8 +59,8 @@ sub tear_down {
 # Verifies: with parameter section=first returns text of first section
 sub test_sectionFirst {
     my $this = shift;
-    $runner->addScriptOptions(section => 'first');
-    my $result  =  $runner->run;
+
+    my $result  =  $this->_viewSection('first');
     $this->assert_matches(qr(^\s?This is the first section\s?$)s,$result);
 }
 
@@ -54,8 +70,8 @@ sub test_sectionFirst {
 #           outer and inner
 sub test_sectionOuter {
     my $this = shift;
-    $runner->addScriptOptions(section => 'outer');
-    my $result  =  $runner->run;
+
+    my $result  =  $this->_viewSection('outer');
     $this->assert_matches(qr(^\s?This is the start of the outer section)s,$result);
     $this->assert_matches(qr(This is the whole content of the inner section)s,$result);
     $this->assert_matches(qr(This is the end of the outer section\s?$)s,$result);
@@ -66,8 +82,8 @@ sub test_sectionOuter {
 # Verifies: with parameter section=inner returns only the inner part
 sub test_sectionInner {
     my $this = shift;
-    $runner->addScriptOptions(section => 'inner');
-    my $result  =  $runner->run;
+
+    my $result  =  $this->_viewSection('inner');
     $this->assert_matches(qr(^\s?This is the whole content of the inner section\s?$)s,$result);
 }
 
@@ -76,8 +92,8 @@ sub test_sectionInner {
 # Verifies: with parameter section=notExisting returns nothing
 sub test_sectionNotExisting {
     my $this = shift;
-    $runner->addScriptOptions(section => 'notExisting');
-    my $result  =  $runner->run;
+
+    my $result  =  $this->_viewSection('notExisting');
     $this->assert_equals('',$result);
 }
 
