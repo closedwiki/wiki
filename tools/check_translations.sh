07d44e435907dd10fd02127c54c7737101d34242
AntonioTerceiro <AntonioTerceiro@a00a5322-12db-0310-a70b-8735589c885e>
2006-12-09 20:56:13 +0000

    Item3273: new tool for generating TWiki translation status; removing old check_translations.sh
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@12199 a00a5322-12db-0310-a70b-8735589c885e

--- tools/check_translations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-[ -d 'tools' ] || (echo "You must run this utility from the root of TWiki sources directory"; exit 1)
-
-for each in `ls locale/*.po`; do
-  echo -n "$each: "
-  msgfmt --output=/dev/null --statistics $each
-done
-
