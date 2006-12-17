c02135af0388a1b00692285b31ebbff5ab8cb951
ArthurClemens <ArthurClemens@a00a5322-12db-0310-a70b-8735589c885e>
2006-12-17 01:27:49 +0000

    Item3311: version 0.8: reworked for TWiki 4.1 and optimized; activated =filter= parameter, added topic view permission check.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@12285 a00a5322-12db-0310-a70b-8735589c885e

--- FileListPlugin/lib/TWiki/Plugins/FileListPlugin.pm
+++ FileListPlugin/lib/TWiki/Plugins/FileListPlugin.pm
@@ -1,7 +1,8 @@
 # Plugin for TWiki Collaboration Platform, http://TWiki.org/
 #
-# TWiki:Main.VinodKulkarni, Apr 2005
-# v0.6
+# v0.6 initial release - TWiki:Main.VinodKulkarni, Apr 2005
+# v0.7 reworked for TWiki 4 - TWiki:Main.SopanShewale, Feb 2006
+# v0.8 reworked for TWiki 4.1 and optimized - TWiki:Main.ArthurClemens, Dec 2006
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -11,54 +12,18 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
-# =========================
-#
-# This is the default TWiki plugin. Use EmptyPlugin.pm as a template
-# for your own plugins; see TWiki.TWikiPlugins for details.
-#
-# Each plugin is a package that may contain these functions:        VERSION:
-#
-#   earlyInitPlugin         ( )                                     1.020
-#   initPlugin              ( $topic, $web, $user, $installWeb )    1.000
-#   initializeUserHandler   ( $loginName, $url, $pathInfo )         1.010
-#   registrationHandler     ( $web, $wikiName, $loginName )         1.010
-#   beforeCommonTagsHandler ( $text, $topic, $web )                 1.024
-#   commonTagsHandler       ( $text, $topic, $web )                 1.000
-#   afterCommonTagsHandler  ( $text, $topic, $web )                 1.024
-#   startRenderingHandler   ( $text, $web )                         1.000
-#   outsidePREHandler       ( $text )                               1.000
-#   insidePREHandler        ( $text )                               1.000
-#   endRenderingHandler     ( $text )                               1.000
-#   beforeEditHandler       ( $text, $topic, $web )                 1.010
-#   afterEditHandler        ( $text, $topic, $web )                 1.010
-#   beforeSaveHandler       ( $text, $topic, $web )                 1.010
-#   afterSaveHandler        ( $text, $topic, $web, $errors )        1.020
-#   renderFormFieldForEditHandler( $name, $type, $size, $value, $attributes, $possibleValues)
-#   writeHeaderHandler      ( $query )                              1.010  Use only in one Plugin
-#   redirectCgiQueryHandler ( $query, $url )                        1.010  Use only in one Plugin
-#   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
-#   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
-#
-# initPlugin is required, all other are optional. 
-# For increased performance, unused handlers are disabled. To
-# enable a handler remove the leading DISABLE_ from the function
-# name. Remove disabled handlers you do not need.
-#
-# NOTE: To interact with TWiki use the official TWiki functions 
-# in the TWiki::Func module. Do not reference any functions or
-# variables elsewhere in TWiki!!
 
 
-# =========================
 package TWiki::Plugins::FileListPlugin;
 
-# =========================
-use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-        $debug $doOldInclude $renderingWeb
+use strict;
+
+
+use vars qw($VERSION $RELEASE $web $topic $user $installWeb $pluginName
+        $debug $renderingWeb $defaultFormat
     );
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
@@ -69,173 +34,138 @@ $VERSION = '$Rev$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$RELEASE = '0.8';
 
 $pluginName = 'FileListPlugin';  # Name of this Plugin
 
-# =========================
+
 sub initPlugin
 {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.021 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
-        return 0;
+    if( $TWiki::Plugins::VERSION < 1.026 ) {
+		TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+		return 0;
     }
 
+	my $notSpecifiedFormat = '   * [[$fileUrl][$fileName]] $fileComment';
+	
     # Get plugin preferences
-    $defaultFormat = TWiki::Func::getPluginPreferencesFlag( "FORMAT" ) || "";
+    $defaultFormat = TWiki::Func::getPreferencesValue( 'FORMAT' ) || TWiki::Func::getPluginPreferencesValue( 'FORMAT' ) || $notSpecifiedFormat;
 
     # Get plugin debug flag
     $debug = TWiki::Func::getPluginPreferencesFlag( "DEBUG" );
-
-    $renderingWeb = $web;
 
     # Plugin correctly initialized
     TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
     return 1;
 }
 
+
 sub _handleFileList 
 {
-  my ($args, $theWeb, $theTopic) = @_;
+	my ($args, $theWeb, $theTopic) = @_;
+	my %params = TWiki::Func::extractParameters($args);
+	
+	my $thisWeb = $params{'web'} || $theWeb || '';
+	my $thisTopic = $params{'topic'} || $theTopic || '';
+	
+	my $format = $params{'format'} || $defaultFormat;
+	
+	my $filter = $params{"filter"}; # "abc, def" syntax. Substring match will be used
+	
+	 # check if the user has permissions to view the topic
+	my $user = TWiki::Func::getWikiName();
+	my $wikiUserName = TWiki::Func::userToWikiName($user, 1);
+	if ( ! TWiki::Func::checkAccessPermission( 'VIEW', $wikiUserName, undef, $thisTopic, $thisWeb ) ) {
+		return '';
+	}
 
-  use TWiki::Func;
-  my %params = TWiki::Func::extractParameters($args);
+	my ($meta, $text) = TWiki::Func::readTopic($thisWeb, $thisTopic);
+	my $outtext=""; 
 
+	# Make sure filter string is valid.
+	if ( $filter ) {
+		# Convert it into regexp to search files against.
+		# "abc, bcd" => (abc)|(bcd)
+		$filter =~ s/\s*([\w\._\-\+\s]*)\s*,/($1)|/g  ;
+		$filter =~ s/\s*([\w\._\-\+\s]*)\s*$/($1)/;
+	}
+	# store once for reuse in loop
+	my $pubUrl = TWiki::Func::getUrlHost() . TWiki::Func::getPubUrlPath();
 
-  my $thisWeb = $theWeb;
-  my $thisTopic = $theTopic;
-  if ( !($params{"topic"} eq "")) {
-      $thisTopic = $params{"topic"};
-      if (!($params{"web"} eq "")) {
-         $thisWeb = $params{"web"};
-      }
-  }
-  
-  my $format = $params{"format"} || $params{"_DEFAULT"} || ('	* [[$fileUrl][$fileName]]: $fileComment'. "\n" ) ;
-  # my $flags = $params{"flags"}; # Later use
-  my $selection = $params{"selection"}; # "abc, def" syntax. Substring match will be used
+	my @attachments = $meta->find( "FILEATTACHMENT" );
+	
+	foreach my $attachment ( @attachments ) {
+		my $file=$attachment->{name};
+		next if ( $filter && ! ($file =~ m/$filter/) );
+		
+		my $attrSize = $attachment->{size};
+		my $attrUser = $attachment->{user};
+		my $attrComment = $attachment->{comment};
+		my $attrAttr = $attachment->{attr};
+		
+		# I18N: To support attachments via UTF-8 URLs to attachment
+		# directories/files that use non-UTF-8 character sets, go through viewfile. 
+		# If using %PUBURL%, must URL-encode explicitly to site character set.
+		
+		# Go direct to file where possible, for efficiency
+		# TODO: more flexible size formatting
+		# also take MB into account
+		my $attrSizeStr;
+		$attrSizeStr = $attrSize.'b' if ( $attrSize < 100 );
+		$attrSizeStr = sprintf( "%1.1fK", $attrSize / 1024 ) if ( $attrSize >= 100 );
+		$attrComment = $attrComment || "";
+		my $s = "$format";
+		$s =~ s/\$fileName/$file/g;
+    
+		if ( $s=~/fileIcon/ ) {
+			## To find the File Extention..
+			my @bits = (split(/\./, $file));
+			my $ext =  lc $bits[$#bits];  
+			my $fileIcon = '%ICON{"'.$ext.'"}%';
+			$s =~ s/\$fileIcon/$fileIcon/g; 
+		}
+		$s =~ s/\$fileSize/$attrSizeStr/g; 
+		$s =~ s/\$fileComment/$attrComment/g;
+		if ( $s=~/fileDate/ ) {
+			my $attrDate = TWiki::Time::formatTime($attachment->{"date"});
+			$s =~ s/\$fileDate/$attrDate/g;
+		}
+		$s =~ s/\$fileUser/$attrUser/g; 
+		$s =~ s/\$n/\n/g; 
+		$s =~ s/\$br/\<br \/\>/g; 
 
-  my ($meta, $text) = TWiki::Func::readTopic($thisWeb, $thisTopic); # Third parameter: check permissions.
-  my $outtext=""; 
-
-  # Make sure selection string is valid.
-  if ( $selection ) {
-    # Convert it into regexp to search files against.
-    # "abc, bcd" => (abc)|(bcd)
-    $selection =~ s/\s*([\w\._\-\+\s]*)\s*,/($1)|/g  ;
-    $selection =~ s/\s*([\w\._\-\+\s]*)\s*$/($1)/;
-  }
-
-  my @attachments = $meta->find( "FILEATTACHMENT" );
-  
-  foreach my $attachment ( @attachments ) {
-    my $file=$attachment->{name};
-    my $attrVersion=$attachment->{Version};
-    my $attrDate=TWiki::formatTime($attachment->{"date"});
-    my $attrSize=$attachment->{size};
-    my $attrUser=$attachment->{user};
-    my $attrComment=$attachment->{comment};
-    my $attrAttr=$attachment->{attr};
-
-    my $fileIcon = TWiki::Attach::filenameToIcon( $file );
-
-    # I18N: To support attachments via UTF-8 URLs to attachment
-    # directories/files that use non-UTF-8 character sets, go through viewfile. 
-    # If using %PUBURL%, must URL-encode explicitly to site character set.
-
-    # Go direct to file where possible, for efficiency
-    $attrSize = $attrSize.'b' if( $attrSize < 100 );
-    $attrSize = sprintf( "%1.1fK", $attrSize / 1024 );
-    $attrComment = $attrComment || "&nbsp;";
-    my $s = "$format";
-    next if ( $selection && ! ($file =~ m/$selection/));
-    $s =~ s/\$fileName/$file/g; 
-    $s =~ s/\$fileIcon/$fileIcon/g; 
-    $s =~ s/\$fileSize/$attrSize/g; 
-    $s =~ s/\$fileComment/$attrComment/g; 
-    $s =~ s/\$fileDate/$attrDate/g; 
-    $s =~ s/\$fileUser/$attrUser/g; 
-    $s =~ s/\$n/\n/g; 
-    $s =~ s/\$br/\<br\/\>/g; 
-
-    my $fileActionUrl =  TWiki::handleNativeUrlEncode(TWiki::Func::getScriptUrl($thisWeb, $thisTopic, "attach") . "?filename=$file&revInfo=1");
-    $s =~ s/\$fileActionUrl/$fileActionUrl/; 
-
-    my $viewfileUrl = TWiki::handleNativeUrlEncode(TWiki::Func::getScriptUrl($thisWeb, $thisTopic, "viewfile") . "?rev=$attrVersion&filename=$file");
-    $s =~ s/\$viewfileUrl/$viewfileUrl/; 
-
-    my $fileUrl=TWiki::handleNativeUrlEncode( TWiki::Func::getUrlHost() . TWiki::Func::getPubUrlPath() . "/$thisWeb/$thisTopic/$file", 1);
-    $s =~ s/\$fileUrl/$fileUrl/; 
-
-    $outtext .= $s;
-  }
-  return $outtext;
+		if ( $s=~/fileActionUrl/ ) {
+			my $fileActionUrl = TWiki::Func::getScriptUrl($thisWeb, $thisTopic, "attach") . "?filename=$file&revInfo=1";
+			$s =~ s/\$fileActionUrl/$fileActionUrl/; 
+		}
+	
+		if ( $s=~/viewfileUrl/ ) {
+			my $attrVersion=$attachment->{Version};
+			my $viewfileUrl = TWiki::Func::getScriptUrl($thisWeb, $thisTopic, "viewfile") . "?rev=$attrVersion&filename=$file";
+			$s =~ s/\$viewfileUrl/$viewfileUrl/; 
+		}
+	
+		my $fileUrl = $pubUrl . "/$thisWeb/$thisTopic/$file";
+		$s =~ s/\$fileUrl/$fileUrl/;
+    
+		$outtext .= $s . "\n";
+	}
+	return $outtext;
 }
 
 
-# =========================
 sub commonTagsHandler
 {
-### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
-
-    # This is the place to define customized tags and variables
-    # Called by TWiki::handleCommonTags, after %INCLUDE:"..."%
-
-    # do custom extension rule, like for example:
-    $_[0] =~ s/%FILELIST%/&_handleFileList($defaultFormat, $web, $topic)/ge;
-    $_[0] =~ s/%FILELIST{(.*?)}%/&_handleFileList($1, $web, $topic)/ge;
-}
-
-# =========================
-sub startRenderingHandler
-{
-### my ( $text, $web ) = @_;   # do not uncomment, use $_[0], $_[1] instead
-
-    TWiki::Func::writeDebug( "- ${pluginName}::startRenderingHandler( $_[1] )" ) if $debug;
-
-    # This handler is called by getRenderedVersion just before the line loop
-
-    $renderingWeb = $_[1];
-}
-
-# =========================
-sub outsidePREHandler
-{
-### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
-
-    ##TWiki::Func::writeDebug( "- ${pluginName}::outsidePREHandler( $renderingWeb.$topic )" ) if $debug;
-
-    # This handler is called by getRenderedVersion, once per line, before any changes,
-    # for lines outside <pre> and <verbatim> tags. 
-    # Use it to define customized rendering rules
-
-    # do custom extension rule, like for example:
-    # $_[0] =~ s/old/new/go;
-
-    # render deprecated *_text_* as "bold italic" text:
-    $_[0] =~ s/(^|\s)\*_([^\s].*?[^\s])_\*(\s|$)/$1<strong><em>$2<\/em><\/strong>$3/go;
-
-    # Use alternate %Web:WikiName% syntax (versus the standard Web.WikiName).
-    # This is an old JosWiki render option. (Uncomment for JosWiki compatibility)
-#   $_[0] =~ s/(^|\s|\()\%([^\s].*?[^\s]):([^\s].*?[^\s])\%/&TWiki::Render::internalLink($2,$3,"$2:$3",$1,1)/geo;
-
-    # Use "forced" non-WikiName links (i.e. %Linkname%)
-    # This is an old JosWiki render option. (Uncomment for JosWiki compatibility)
-#   $_[0] =~ s/(^|\s|\()\%([^\s].*?[^\s])\%/&TWiki::Render::internalLink($web,$2,$2,$1,1)/geo;
-
-    # Use "forced" non-WikiName links (i.e. %Web.Linkname%)
-    # This is an old JosWiki render option combined with the new Web.LinkName notation
-    # (Uncomment for JosWiki compatibility)
-#   $_[0] =~ s/(^|\s|\()\%([a-zA-Z0-9]+)\.(.*?[^\s])\%(\s|\)|$)/&TWiki::Render::internalLink($2,$3,$3,$1,1)/geo;
-
-    # Use <link>....</link> links
-    # This is an old JosWiki render option. (Uncomment for JosWiki compatibility)
-#   $_[0] =~ s/<link>(.*?)<\/link>/&TWiki::internalLink("",$web,$1,$1,"",1)/geo;
+	TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+	
+	# This is the place to define customized tags and variables
+	# Called by TWiki::handleCommonTags, after %INCLUDE:"..."%
+	
+	$_[0] =~ s/%FILELIST%/&_handleFileList($defaultFormat, $web, $topic)/ge;
+	$_[0] =~ s/%FILELIST{(.*?)}%/&_handleFileList($1, $web, $topic)/ge;
 }
 
 1;
