5e1fbaf803b000332b51f640df08759211b1a2a0
DanielRohde <DanielRohde@a00a5322-12db-0310-a70b-8735589c885e>
2007-01-16 15:10:07 +0000

    Item3455: improved performance and error handling (AJAX);
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@12573 a00a5322-12db-0310-a70b-8735589c885e

--- ChecklistPlugin/pub/TWiki/ChecklistPlugin/itemstatechange.js
+++ ChecklistPlugin/pub/TWiki/ChecklistPlugin/itemstatechange.js
@@ -1,5 +1,4 @@
 var clpStateChangeObjectArray = new Array(); // queue
-
 function ClpStateChangeObject(url, stateChangeRequest) {
 	this.stateChangeRequest = stateChangeRequest;
 	this.url = url;
@@ -10,18 +9,16 @@ function ClpStateChangeObject(url, stateChangeRequest) {
 	this.changesNew = new Array();
 }
 function clpInit() {
-	try {
-		// Firefox, Opera 8.0+, Safari
+	try { // Firefox, Opera 8.0+, Safari
 		this.stateChangeRequest = new XMLHttpRequest();
-	} catch (e) {
-		// Internet Explorer
+	} catch (e) { // Internet Explorer
 		try {
 			this.stateChangeRequest = new ActiveXObject("Msxml2.XMLHTTP");
 		} catch (e) {
 			try {
 				this.stateChangeRequest=new ActiveXObject("Microsoft.XMLHTTP");
 			} catch (e) {
-				//alert("Your browser does not support AJAX!");
+				alert("Your browser does not support AJAX!\nPlease disable AJAX (e.g. use attribute: useajax=\"off\") ");
 			}
 		}
 	}
@@ -41,10 +38,55 @@ function clpHandleNextObject(self) {
 		var n = clpGetStateChangeObject(oldUrl);
 		if (n) n.url=newUrl;
 	}
-	if (clpStateChangeObjectArray.length>0) {
-		var newStateChangeObject = clpStateChangeObjectArray.shift();
-		newStateChangeObject.clpDoIt();
+	if (clpStateChangeObjectArray.length>0) clpStateChangeObjectArray.shift().clpDoIt();
+}
+function clpHandleStateChange(self) {
+	if (self.stateChangeRequest.readyState!=4) return;
+	if (self.stateChangeRequest.status != 200) {
+		document.write(self.stateChangeRequest.responseText);
+		return;
 	}
+
+	var responseText = self.stateChangeRequest.responseText;
+
+	var links = responseText.match(/<a[^>]+id="CLP_A_[^>]+>/ig);
+	if (links && (links.length>0)) {
+		for (var i = 0 ; i < links.length; ++i) {
+			var link = links[i];
+			link.match(/id="([^"]+)"/);
+			var id = RegExp.$1;
+			link.match(/href="([^"]+)"/);
+			var href=RegExp.$1;
+			var e = document.getElementById(id);
+			if (!e) next;
+			var oldHref = clpStripId(e.href);
+			var newHref = clpStripId(href);
+			if (oldHref!=newHref) {
+				self.changes.push(e.href);
+				self.changesNew.push(href);
+			} else {
+				continue;
+			}
+			if (e) e.href = href; 
+			var realId = id.replace(/CLP_A_/i,"");
+			var imgExpr = new RegExp("<img[^>]+id=\"CLP_IMG_" + realId + "\"[^>]*>","i");
+			var img = "" + imgExpr.exec(responseText);
+			img.match(/src="([^"]+)"/);
+			var src = RegExp.$1;
+			img.match(/alt="([^"]+)"/);
+			var alt = RegExp.$1;
+			e = document.getElementById("CLP_IMG_"+realId);
+			if (e) {
+				e.src = src;
+				e.title = alt;
+				e.alt = alt;
+			}
+
+		}
+	} else {
+		document.write(responseText);
+	}
+	clpHandleNextObject(self);
 }
 function clpDoIt() {
 	if (!this.clpInit()) {
@@ -54,75 +96,22 @@ function clpDoIt() {
 	
 	var self = this;
 	this.stateChangeRequest.onreadystatechange=function() {
-		if (self.stateChangeRequest.readyState!=4) return;
-		if (self.stateChangeRequest.status != 200) {
-			document.write(self.stateChangeRequest.responseText);
-			//alert("An error occurred: " + self.stateChangeRequest.status);
+		try {
+			clpHandleStateChange(self);
+		} catch (e) {
+			alert("Sorry, an error occured:\n"+e+"\nPlease reload the page!");
 		}
-
-		var responseText = self.stateChangeRequest.responseText;
-
-		var links = responseText.match(/<a[^>]+id="CLP_A_[^>]+>/ig);
-		if (links && (links.length>0)) {
-			for (var i = 0 ; i < links.length; ++i) {
-				var link = links[i];
-				link.match(/id="([^"]+)"/);
-				var id = RegExp.$1;
-				link.match(/href="([^"]+)"/);
-				var href=RegExp.$1;
-				var e = document.getElementById(id);
-				var oldHref = clpStripId(e.href);
-				var newHref = clpStripId(href);
-				if (oldHref!=newHref) {
-					self.changes.push(e.href);
-					self.changesNew.push(href);
-				}
-				if (e) e.href = href; 
-
-				var realId = id.replace(/CLP_A_/i,"");
-				var imgExpr = new RegExp("<img[^>]+id=\"CLP_IMG_" + realId + "\"[^>]*>","i");
-				var img = "" + imgExpr.exec(responseText);
-				img.match(/src="([^"]+)"/);
-				var src = RegExp.$1;
-				img.match(/alt="([^"]+)"/);
-				var alt = RegExp.$1;
-				e = document.getElementById("CLP_IMG_"+realId);
-				if (e) {
-					e.src = src;
-					e.title = alt;
-					e.alt = alt;
-				}
-
-			}
-		} else {
-			document.write(responseText);
-		}
-		clpHandleNextObject(self);
 	};
 	if (this.stateChangeRequest.onerror) {
 		this.stateChangeRequest.onerror=function() {
-			try {
-				var errorWin = window.open('', 'errorWin');
-				errorWin.document.body.innerHTML = self.stateChangeRequest.responseText;
-			} catch (e) {
-				alert('An error occurred, but the error message cannot be '
-				     + 'displayed. This is probably because of your browser\'s '
-				     + 'pop-up blocker.\n'
-				     + 'Please allow pop-ups from this web site if you want to '
-				     + 'see the full error messages.\n'
-				     + '\n'
-				     + 'Status Code: ' + self.stateChangeRequest.status + '\n'
-				     + 'Status Description: ' + self.stateChangeRequest.statusText);
-			}
-			
-			clpHandleNextObject(self);
+			document.write(self.stateChangeRequest.responseText);
 		};
 	}
 	this.stateChangeRequest.open("GET", this.url, true);
 	this.stateChangeRequest.send(null);
 }
 function clpStripId(url) {
-	return url.replace(/clpid=\d+;/i,"");
+	return url.replace(/clpid=[^;]+;/i,"");
 }
 function clpGetStateChangeObject(url) {
 	for (var i=0; i<clpStateChangeObjectArray.length; ++i) {
@@ -135,11 +124,7 @@ function submitItemStateChange(url) {
 	while (clpSubmitItemStateChangeMutex>0) { alert("You click to fast for me"); }
 	clpSubmitItemStateChangeMutex++;
 	var newStateChangeObject = new ClpStateChangeObject(url);
-	if (clpStateChangeObjectArray.length>0) {
-		clpStateChangeObjectArray.push(newStateChangeObject);
-	} else {
-		clpStateChangeObjectArray.push(newStateChangeObject);
-		newStateChangeObject.clpDoIt();
-	}
+	clpStateChangeObjectArray.push(newStateChangeObject);
+	if (clpStateChangeObjectArray.length==1) newStateChangeObject.clpDoIt();
 	clpSubmitItemStateChangeMutex--;
 }
