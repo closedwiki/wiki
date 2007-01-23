30f9802fc303a737e9b4783a4d5a3ef713e84834
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2007-01-23 17:36:01 +0000

    Item3494: fixed test case for AutoAttachPubFiles
    Attachments are only checked for the currently requested topic and not for ever
    topic that happens to be read during one request.
    
    
    
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@12602 a00a5322-12db-0310-a70b-8735589c885e

--- test/unit/AutoAttachTests.pm
+++ test/unit/AutoAttachTests.pm
@@ -47,7 +47,7 @@ sub set_up {
 }
 
 sub set_up_topic {
-	my $this = shift;
+    my $this = shift;
     # Create topic
     my $topic = shift;
     my $text = "hi";
@@ -67,17 +67,17 @@ sub tear_down {
 # We create a topic with a missing attachment
 # This attachment should be now omitted from the resulting output
 sub addMissingAttachment {
-	my $this = shift;
-	my $topic = shift;
-	my $file = shift; 
-	my $comment = shift; 
+    my $this = shift;
+    my $topic = shift;
+    my $file = shift; 
+    my $comment = shift; 
 
-	$this->assert($session->{store}->topicExists($testweb, $topic));
-	
+    $this->assert($session->{store}->topicExists($testweb, $topic));
+
     my ($meta, $text) = $session->{store}->readTopic( 
-    				$session->{user}, $testweb, $topic );
-	
-	$meta->putKeyed( 'FILEATTACHMENT',
+    $session->{user}, $testweb, $topic );
+
+    $meta->putKeyed( 'FILEATTACHMENT',
                        {
                              name    => $file,
                              version => '',
@@ -89,8 +89,7 @@ sub addMissingAttachment {
                              attr    => ''
                         }
                        );
-	$session->{store}->saveTopic(
-					 $session->{user}, $testweb, $topic, $text, $meta );
+    $session->{store}->saveTopic($session->{user}, $testweb, $topic, $text, $meta);
 }
 
 # We create a 3 more attachment entries:
@@ -98,17 +97,16 @@ sub addMissingAttachment {
 # another two (_afile.txt and _hiddenDirectoryForPlugins) that should not
 
 sub sneakAttachmentsAddedToTopic {
-	my $this = shift;
-	my ($topic, @filenames) = @_;
-	
+    my $this = shift;
+    my ($topic, @filenames) = @_;
     my $dir = $TWiki::cfg{PubDir};
     $dir = "$dir/$testweb/$topic";
+    print STDERR "DEBUG: dir=$dir\n";
 
-	foreach my $file (@filenames) {
-		touchFile("$dir/$file");
-	}
+    foreach my $file (@filenames) {
+      touchFile("$dir/$file");
+    }
 
-	    
     mkdir $dir.'/_hiddenDirectoryForPlugins';    
 }
 
@@ -123,70 +121,70 @@ sub test_no_autoattach {
 }
 
 sub test_autoattach {
-	
-#	print "Default AutoAttachPubFiles = $TWiki::cfg{AutoAttachPubFiles}\n";	
-	$TWiki::cfg{AutoAttachPubFiles} = 1;
-#	print "AutoAttachPubFiles now = $TWiki::cfg{AutoAttachPubFiles}\n";
-	
-	my $this = shift; 
-	my $topic = "UnitTest1";
-	$this->set_up_topic($topic);
-	$this->verify_normal_attachment($topic, "afile.txt");
-	$this->verify_normal_attachment($topic, "bfile.txt");
+#   print "Default AutoAttachPubFiles = $TWiki::cfg{AutoAttachPubFiles}\n";
+    $TWiki::cfg{AutoAttachPubFiles} = 1;
+#   print "AutoAttachPubFiles now = $TWiki::cfg{AutoAttachPubFiles}\n";
+
+    my $this = shift; 
+    my $topic = "UnitTest1";
+    $this->set_up_topic($topic);
+    $this->verify_normal_attachment($topic, "afile.txt");
+    $this->verify_normal_attachment($topic, "bfile.txt");
     $this->addMissingAttachment($topic, 'bogusAttachment.txt', "I'm a figment of TWiki's imagination");
     $this->addMissingAttachment($topic, 'ressurectedComment.txt', 'ressurected attachment comment');
     $this->sneakAttachmentsAddedToTopic($topic, 'sneakedfile1.txt','sneakedfile2.txt', 'commavfilesshouldbeignored2.txt,v','_hiddenAttachment.txt', 'ressurectedComment.txt');
 
-    my ($meta, $text) = $session->{store}->readTopic($session->{user}, $testweb, $topic);
+
+    my ($meta, $text) = simulate_view($session, $testweb, $topic);
     my @attachments = $meta->find( 'FILEATTACHMENT' );
-# 	printAttachments(@attachments); # leave as comment unless debugging
+#    printAttachments(@attachments); # leave as comment unless debugging
 
     $this->foundAttachmentsMustBeGettable($meta, @attachments);
     # ASSERT the commavfile should not be found, but should be gettable.
 
-	# Our attachment correctly listed in meta data still exists:
+    # Our attachment correctly listed in meta data still exists:
 #    my $afileAttributes = $meta->get('FILEATTACHMENT', "afile.txt");
-#  	$this->assert_not_null($afileAttributes);
+#    $this->assert_not_null($afileAttributes);
     
     # Our added files now exist:
     my $sneakedfile1Attributes = $meta->get('FILEATTACHMENT', "sneakedfile1.txt");    
     my $sneakedfile2Attributes = $meta->get('FILEATTACHMENT', "sneakedfile2.txt");
-	$this->assert_not_null($sneakedfile1Attributes);
-	$this->assert_not_null($sneakedfile2Attributes);
-	
-	# We have deleted the faulty bogus reference:
+    $this->assert_not_null($sneakedfile1Attributes);
+    $this->assert_not_null($sneakedfile2Attributes);
+
+    # We have deleted the faulty bogus reference:
     my $bogusAttachmentAttributes = $meta->get('FILEATTACHMENT', "bogusAttachment.txt");
     $this->assert_null($bogusAttachmentAttributes);
     
     # And commav files are still gettable (we check earlier that it is not listable).
     my $commavfilesshouldbeignoredAttributes = $meta->get('FILEATTACHMENT', "commavfilesshouldbeignored2.txt,v");
-	$this->assert_null($commavfilesshouldbeignoredAttributes);
+    $this->assert_null($commavfilesshouldbeignoredAttributes);
 
 }
 
 sub foundAttachmentsMustBeGettable {
-   my ($this, $meta, @attachments) = @_;
+    my ($this, $meta, @attachments) = @_;
 
-	foreach my $attachment (@attachments) {
-		my $attachmentName = $attachment->{name};
-#		print "Testing file exists ".$attachmentName.": ";
-		my $attachmentAttributes = $meta->get('FILEATTACHMENT', $attachmentName);
-		$this->assert_not_null($attachmentAttributes);
-	    # print Dumper($attachmentAttributes)."\n";
-	    
-	    if ($attachmentName eq "commavfilesshouldbeignored2.txt,v") {
-	    	die "commavfilesshouldbeignored2.txt,v should not be returned in the listing";
-	    }
-	}
+    foreach my $attachment (@attachments) {
+    my $attachmentName = $attachment->{name};
+      #print "Testing file exists ".$attachmentName.": ";
+      my $attachmentAttributes = $meta->get('FILEATTACHMENT', $attachmentName);
+      $this->assert_not_null($attachmentAttributes);
+      # print Dumper($attachmentAttributes)."\n";
+
+      if ($attachmentName eq "commavfilesshouldbeignored2.txt,v") {
+        die "commavfilesshouldbeignored2.txt,v should not be returned in the listing";
+      }
+    }
 }   
 
 
 # needed for debugging (see above)
 sub printAttachments {
-	my (@attachments) = @_; 
+    my (@attachments) = @_; 
     print "\n\n-------ATTACHMENTS--------\n";
     foreach my $attachment (@attachments) {
-    	print "Attachment found: ".Dumper($attachment)."\n";
+    print "Attachment found: ".Dumper($attachment)."\n";
     }
 }
 
@@ -197,8 +195,7 @@ sub verify_normal_attachment {
     my $attachment = shift;
     
     my $user = $session->{users}->findUser($testUser1);
-	$this->assert($session->{store}->topicExists($testweb, $topic));
-
+    $this->assert($session->{store}->topicExists($testweb, $topic));
 
     open( FILE, ">/tmp/$attachment" );
     print FILE "Test attachment\n";
@@ -215,6 +212,24 @@ sub verify_normal_attachment {
     my $rev = $session->{store}->getRevisionNumber($testweb, $topic, $attachment);
     $this->assert_num_equals(1,$rev);
 
+}
+
+sub simulate_view {
+    my ($session, $web, $topic) = @_;
+
+    my $oldWebName = $session->{webName};
+    my $oldTopicName = $session->{topicName};
+
+    $session->{webName} = $web;
+    $session->{topicName} = $topic;
+
+    my ($meta, $text) = $session->{store}->readTopic($session->{user}, $web, $topic);
+
+    $session->{webName} = $oldWebName;
+    $session->{topicName} = $oldTopicName;
+
+
+    return ($meta, $text);
 }
 
 1;
