b9163d74ed79a6a5a06c6b924761ac18bf23f4fa
KennethLavrsen <KennethLavrsen@a00a5322-12db-0310-a70b-8735589c885e>
2007-01-31 18:53:41 +0000

    Item3523: Simplification of session expiry code This code is based only on mtime which is much faster and much safer as we never read the session files
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@12697 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/Client.pm
+++ lib/TWiki/Client.pm
@@ -362,8 +362,8 @@ sub finish {
 
 ---++ StaticMethod expireDeadSessions()
 
-Delete sessions and passthrough files that are sitting around but are
-really expired. This *assumes* that the sessions are stored as files.
+Delete sessions and passthrough files that are sitting around but are really expired.
+This *assumes* that the sessions are stored as files.
 
 This is a static method, but requires TWiki::cfg. It is designed to be
 run from a session or from a cron job.
@@ -371,57 +371,27 @@ run from a session or from a cron job.
 =cut
 
 sub expireDeadSessions {
-	my $time = time() || 0;
+    my $time = time() || 0;
     my $exp = $TWiki::cfg{Sessions}{ExpireAfter} || 36000; # 10 hours
     $exp = -$exp if $exp < 0;
-    local $/ = undef;
 
-	opendir(D, $TWiki::cfg{Sessions}{Dir}) || return;
-	foreach my $file ( grep { /^(passthru|cgisess)_[0-9a-f]{32}/ } readdir(D) ) {
+    opendir(D, $TWiki::cfg{Sessions}{Dir}) || return;
+    foreach my $file ( grep { /^(passthru|cgisess)_[0-9a-f]{32}/ } readdir(D) ) {
         $file = TWiki::Sandbox::untaintUnchecked(
             $TWiki::cfg{Sessions}{Dir}.'/'.$file );
-
-		my @stat = stat( $file );
-
-        # Kill old files. 'Old' is judged from the atime, then the mtime
-        # if the atime is not available, then the ctime if there is no
-        # mtime (unlikely, even in The Swamp of Despair (a.k.a Windows))
-        my $lat = $stat[8] || $stat[9] || $stat[10] || $time;
-        if( $time - $lat >= $exp ) {
-            unlink($file);
-            next;
-		}
-
-        # Just ignore unexpired passthru files
-        next if $file =~ /^passthru_/;
-
-        # The file is a session, with a file date younger than the
-        # expiry time. Read the session and expire it if the contents
-        # dictate.
-
-		open(F, $file) || next;
-		my $session = <F>;
-		close F;
-
-        if ($session =~ /SESSION_ATIME\s*=>\s*(\d+)/s) {
-            my $atime = $1;
-            # check last accessed time
-            if( $atime && $time >= $atime + $exp ) {
-                unlink($file);
-                next;
-            }
-        }
-
-        if ($session =~ /SESSION_ETIME\s*=>\s*(\d+)/s) {
-            # test explicit expiry time
-            my $etime = $1;
-            if( $etime && $time >= $etime) {
-                unlink($file);
-                next;
-            }
-        }
-	}
-	closedir D;
+        my @stat = stat( $file );
+        # CGI::Session updates the session file each time a browser views a
+        # topic setting the access and expiry time as values in the file. This 
+        # also sets the mtime (modification time) for the file which is all we need.
+        # We know that the expiry time is mtime + $TWiki::cfg{Sessions}{ExpireAfter}
+        # so we do not need to waste execution time opening and reading the file.
+        # We just check the mtime. mtime is confirmed set in both Windows and Linux
+        # As a fallback we also check ctime. Files are deleted when they expire.
+        my $lat = $stat[9] || $stat[10] || 0;
+        unlink $file if ( $time - $lat >= $exp );
+        next;
+    }
+    closedir D;
 }
 
 =pod
