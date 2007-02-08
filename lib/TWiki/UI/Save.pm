6fcfab118bf433d5f593a2de2800e2dceb78b146
OliverKrueger <OliverKrueger@a00a5322-12db-0310-a70b-8735589c885e>
2007-02-08 14:39:32 +0000

    Item3491: removed META:TOPICMOVED from template inheritance
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@12797 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki/UI/Save.pm
+++ lib/TWiki/UI/Save.pm
@@ -126,7 +126,7 @@ sub buildNewTopic {
           $session->expandVariablesOnTopicCreation( $templateText );
         foreach my $k ( keys %$templateMeta ) {
             unless( $k =~ /^_/ || $k eq 'FORM' || $k eq 'TOPICPARENT' ||
-                      $k eq 'FIELD' ) {
+                      $k eq 'FIELD' || $k eq 'TOPICMOVED' ) {
                 $newMeta->copyFrom( $templateMeta, $k );
             }
         }
