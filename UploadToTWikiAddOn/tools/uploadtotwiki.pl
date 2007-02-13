ac76d39d2e5bba02263f10d3761303b234c5dc11
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2007-02-13 07:50:35 +0000

    Item3605: Added -l, -p, -c, -h, -i, -d parameters; support for multiple files
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@12850 a00a5322-12db-0310-a70b-8735589c885e

--- UploadToTWikiAddOn/tools/uploadtotwiki.pl
+++ UploadToTWikiAddOn/tools/uploadtotwiki.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl -w
 #
 # Quick & dirty utility to attach a local file to a TWiki topic 
-# via http.
+# via http. Add-on home is at
+# http://twiki.org/cgi-bin/view/Plugins/UploadToTWikiAddOn
 #
 # (Utility for TWiki Enterprise Collaboration Platform, http://TWiki.org/)
 #
@@ -19,23 +20,56 @@
 #
 # As per the GPL, removal of this notice is prohibited.
 
-my $toolName = 'uploadtotwiki/2007-02-11';
+my $toolName = 'uploadtotwiki/2007-02-12';
+
+use strict;
 
 unless( $ARGV[2] ) {
-    print "Usage:    ./uploadtotwiki.pl <TWiki login name> <file> <view url of twiki topic> <optional comment>\n";
-    print "Example:  ./uploadtotwiki.pl MyWikiName ./smile.gif http://twiki.org/cgi-bin/view/Sandbox/UploadTest\n";
+    print "Utility to attach files to any TWiki topic. Copyright (C) 2007, Peter\@Thoeny.org\n";
+    print "Add-on home: http://twiki.org/cgi-bin/view/Plugins/UploadToTWikiAddOn\n";
+    print "Usage:\n";
+    print "  ./uploadtotwiki.pl -l <login> -c <comment> -h 1 <file(s)> <TWiki URL>\n";
+    print "Parameters:\n";
+    print "  -l login      - login name of TWiki account (optional)\n";
+    print "  -p password   - password of TWiki account (optional)\n";
+    print "  -c 'comment'  - comment of attached file (default: 'Uploaded by " . $toolName . "')\n";
+    print "  -h 1          - hide attachment, 0 or 1 (default: 0)\n";
+    print "  -i 1          - inline attachment, e.g create link in topic text (default: 0)\n";
+    print "  -d 60         - delay in seconds between uploads of multiple files (default: 30)\n";
+    print "  file(s)       - one or more local files to upload (required)\n";
+    print "  URL           - view URL of TWiki topic, http or https protocol (required, must be last)\n";
+    print "Example:\n";
+    print "  ./uploadtotwiki.pl -l MyWikiName *.gif http://twiki.org/cgi-bin/view/Sandbox/UploadTest\n";
     exit 1;
 }
-my $user    = $ARGV[0];
-my $file    = $ARGV[1];
-my $url     = $ARGV[2];
-my $comment = $ARGV[3];
-exit uploadFile( $user, $file, $url, $comment );
+
+my $switch = '';
+my %opts = ();
+my @files = ();
+foreach my $item ( @ARGV ) {
+    if( $switch ) {
+        $opts{$switch} = $item;       
+        $switch = '';
+    } elsif( $item =~ /^-([a-zA-Z0-9]+)$/ ) {
+        $switch = $1;
+    } else {
+        push( @files, $item);
+    }
+}
+my $login    = $opts{l} || '';
+my $password = $opts{p} || '';
+my $comment  = $opts{c} || "Uploaded by $toolName";
+my $hide     = $opts{h} || '';
+my $link     = $opts{i} || '';
+my $delay    = $opts{d} || '30';
+my $url      = pop( @files ) || '';
+
+exit uploadFile( $comment, $hide, $link, $delay, $url, @files );
 
 # =========================
 sub uploadFile
 {
-    my ( $theUser, $theFile, $theUrl, $theComment ) = @_;
+    my( $theComment, $theHide, $theLink, $theDelay, $theUrl, @theFiles ) = @_;
 
     require LWP;
     if ( $@ ) {
@@ -44,13 +78,7 @@ sub uploadFile
     }
     my $ua = UploadToTWiki::UserAgent->new();
     $ua->agent( $toolName );
-
-    unless( -e $theFile ) {
-        print STDERR "Error: File $theFile does not exist\n";
-        return 0;
-    }
-    my $fileName = $theFile;
-    $fileName =~ s|.*/||;
+    push @{ $ua->requests_redirectable }, 'POST';
 
     my $uploadUrl = $theUrl;
     unless( $uploadUrl =~ /^https?:/ ) {
@@ -61,27 +89,48 @@ sub uploadFile
         print STDERR "Error: This is not the URL of a TWiki topic\n";
         return 0;
     }
-    $theComment = "Uploaded by $toolName" unless( $theComment );
 
-    print "Uploading $theFile to $theUrl\n";
-
-    push @{ $ua->requests_redirectable }, 'POST';
-    my $response = $ua->post(
-        $uploadUrl,
-        [
-            'filename' => $fileName,
-            'filepath' => [ $theFile ],
-            'filecomment' => $theComment
-        ],
-        'Content_Type' => 'form-data' );
-
-    if ($response->is_success) {
-        print "File upload finished.\n";
-        return 1;
-    } else {
-        print STDERR "Error: " . $response->status_line . "\n";
+    my $todo = scalar( @theFiles );
+    unless( $todo ) {
+        print STDERR "Error: No files specified to upload\n";
         return 0;
     }
+
+    foreach my $file ( @theFiles ) {
+        unless( -e $file ) {
+            print STDERR "Error: File $file does not exist\n";
+            return 0;
+        }
+
+        my $fileName = $file;
+        $fileName =~ s|.*/||;
+
+        print "Uploading $file to $theUrl\n";
+
+        my $response = $ua->post(
+            $uploadUrl,
+            [
+                'filename'    => $fileName,
+                'filepath'    => [ $file ],
+                'filecomment' => $theComment,
+                'hidefile'    => $theHide,
+                'createlink'  => $theLink
+            ],
+            'Content_Type' => 'form-data' );
+
+        if( $response->is_success ) {
+            print "... upload finished\n";
+        } else {
+            print STDERR "Error: " . $response->status_line . "\n";
+            return 0;
+        }
+        $todo--;
+        if( $todo ) {
+            print "Wait for $theDelay seconds (be nice to the server)\n";
+            sleep( $theDelay );
+        }
+    }
+    return 1;
 }
 
 # =========================
@@ -99,14 +148,23 @@ sub uploadFile
 
     sub get_basic_credentials {
         my($this, $realm, $uri) = @_;
+        my $host = $uri->host_port();
+        $host =~ s/\:[0-9]*$//;
         local $/ = "\n";
-        print "Enter password for $user at " . $uri->host_port() . ": ";
-        system('stty -echo');
-        my $password = <STDIN>;
-        system('stty echo');
-        print "\n";
-        chomp($password);
-        return( $user, $password );
+        unless( $login ) {
+            print( "Enter TWiki login name for $host: " );
+            $login = <STDIN>;
+            chomp( $login );
+        }
+        unless( $password ) {
+            print( "Enter password for $login at $host: " );
+            system( 'stty -echo' );
+            $password = <STDIN>;
+            system( 'stty echo' );
+            print "\n";
+            chomp( $password );
+        }
+        return( $login, $password );
     }
 }
 
