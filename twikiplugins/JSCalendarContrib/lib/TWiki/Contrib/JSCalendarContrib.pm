cfb83515b9db873b3798e2c98f4c5de7e6e9c54e
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2007-05-02 09:19:36 +0000

    Item2982: cleaned up the interface to the contrib, re-added a date rendering function with a more generic interface
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@13605 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/JSCalendarContrib/lib/TWiki/Contrib/JSCalendarContrib.pm
+++ twikiplugins/JSCalendarContrib/lib/TWiki/Contrib/JSCalendarContrib.pm
@@ -1,22 +1,13 @@
 package TWiki::Contrib::JSCalendarContrib;
 
+use strict;
+
 use vars qw( $VERSION $RELEASE );
 
-use TWiki;
-
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
+$RELEASE = 'TWiki-4';
 
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
-
-
-# Helper for plugins, to add the requisite bits of the calendar
-# to the header
+# See JSCalendarContrib.txt for pod
 sub addHEAD {
     my $setup = shift;
     $setup ||= 'calendar-setup';
@@ -39,6 +30,66 @@ HERE
 <script type='text/javascript' src='$base/$setup.js'></script>
 HERE
     TWiki::Func::addToHEAD( 'JSCALENDAR_HEAD'.$setup, $head );
+}
+
+# Max width of different mishoo format components
+my %w = (
+    a => 3,	# abbreviated weekday name
+    A => 9,	# full weekday name
+    b => 3,	# abbreviated month name
+    B => 9,	# full month name
+    C => 2,	# century number
+    d => 2,	# the day of the month ( 00 .. 31 )
+    e => 2,	# the day of the month ( 0 .. 31 )
+    H => 2,	# hour ( 00 .. 23 )
+    I => 2,	# hour ( 01 .. 12 )
+    j => 3,	# day of the year ( 000 .. 366 )
+    k => 2,	# hour ( 0 .. 23 )
+    l => 2,	# hour ( 1 .. 12 )
+    m => 2,	# month ( 01 .. 12 )
+    M => 2,	# minute ( 00 .. 59 )
+    n => 1,	# a newline character
+    p => 2,	# ``PM'' or ``AM''
+    P => 2,	# ``pm'' or ``am''
+    S => 2,	# second ( 00 .. 59 )
+    s => 12,# number of seconds since Epoch
+    t => 1,	# a tab character
+    U => 2,	# the week number
+    u => 1,	# the day of the week ( 1 .. 7, 1 = MON )
+    W => 2,	# the week number
+    w => 1,	# the day of the week ( 0 .. 6, 0 = SUN )
+    V => 2,	# the week number
+    y => 2,	# year without the century ( 00 .. 99 )
+    Y => 4,	# year including the century ( ex. 1979 )
+);
+
+# See JSCalendarContrib.txt for pod
+sub renderDateForEdit {
+    my ($name, $value, $format) = @_;
+
+    $format ||= $TWiki::cfg{JSCalendarContrib}{format} || '%e %B %Y';
+
+    addHEAD('twiki');
+
+    # Work out how wide it has to be from the format
+    # SMELL: add a space because pattern skin default fonts on FF make the
+    # box half a character too narrow if the exact size is used
+    my $wide = $format.' ';
+    $wide =~ s/(%(.))/$w{$2} ? ('_' x $w{$2}) : $1/ge;
+
+    return CGI::textfield(
+        -name => $name, -id => 'id_'.$name,
+        -size => length($wide),
+        -default => $value || '')
+      . CGI::image_button(
+          -name => 'img_'.$name,
+          -onclick =>
+            "javascript: return showCalendar('id_$name','$format')",
+            -src=> TWiki::Func::getPubUrlPath() . '/' .
+              TWiki::Func::getTwikiWebname() .
+                  '/JSCalendarContrib/img.gif',
+          -alt => 'Calendar',
+          -align => 'MIDDLE');
 }
 
 1;
