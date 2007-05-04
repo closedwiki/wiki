d7445f3741a0c720db2fc1dbb85a2ed1baa6ac72
DanielRohde <DanielRohde@a00a5322-12db-0310-a70b-8735589c885e>
2007-05-04 13:18:45 +0000

    Item4017: fixed links in 'connected to' or 'owner' field bug; added new attribute (clicktooltip...)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@13625 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/RackPlannerPlugin/lib/TWiki/Plugins/RackPlannerPlugin.pm
+++ twikiplugins/RackPlannerPlugin/lib/TWiki/Plugins/RackPlannerPlugin.pm
@@ -39,7 +39,8 @@ $VERSION = '$Rev: 8670$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
  
-$REVISION = '1.004'; #dro# fixed replacement in tooltipformat bug; improved tooltipformat; improved HTML rendering performance;  added and fixed documenation;
+$REVISION = '1.005'; #dro# fixed links in 'connected to' or 'owner' field bug; added new attribute (clicktooltip...)
+#$REVISION = '1.004'; #dro# fixed replacement in tooltipformat bug; improved tooltipformat; improved HTML rendering performance;  added and fixed documenation;
 #$REVISION = '1.003'; #dro# fixed displayowner/displaynotes bug reported by TWiki:Main.PatrickTuite; added horizontal rendering feature requested by TWiki:Main.OlofStockhaus; added new attributes (columnwidth, textdir); fixed HTML validation bug;
 #$REVISION = '1.002'; #dro# allowed multiple entries in a single unit; fixed rendering bug reported by TWiki:Main.SteveWray; fixed link color bug reported by TWiki:Main.SteveWray
 #$REVISION = '1.001'; #dro# improved some features (added statistics); added attributes (rackstatformat, displayunitcolumn, unitcolumn*); renamed attribute statformat; fixed documentation; fixed tooltip bug; fixed conflict bug;
