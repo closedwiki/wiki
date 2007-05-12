9a9547710a240bde16bb1ea82544aa3983bbd34b
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2007-05-12 10:49:36 +0000

    Item4053: Added invocation of unit tests for standard extensions to test suite
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@13703 a00a5322-12db-0310-a70b-8735589c885e

--- test/unit/TWikiSuite.pm
+++ test/unit/TWikiSuite.pm
@@ -7,9 +7,10 @@ use base qw(Test::Unit::TestSuite);
 
 sub name { 'TWikiSuite' };
 
-# Assume we are run from the "test/unit" directory
+# Assumes we are run from the "test/unit" directory
 
 sub include_tests {
+    my $this = shift;
     push(@INC, '.');
     my @list;
     opendir(DIR, ".") || die "Failed to open .";
@@ -21,6 +22,23 @@ sub include_tests {
         }
     }
     closedir(DIR);
+    # Add standard extensions tests
+    if (-e "$ENV{TWIKI_HOME}/tools/MANIFEST") {
+        open(F, "$ENV{TWIKI_HOME}/tools/MANIFEST") || die $!;
+    } else {
+        open(F, "../../tools/MANIFEST") || die $!;
+    }
+    local $/ = "\n";
+    while (<F>) {
+        if (m#^!include (twikiplugins/\w+)/.*?/(\w+)$#) {
+            my $d = "../../$1/test/unit/$2";
+            next unless (-e "$d/${2}Suite.pm");
+            push(@INC, $d);
+            print STDERR "Added ${2}Suite\n";
+            $this->add_test("${2}Suite");
+        }
+    }
+    close(F);
     return @list;
 };
 
