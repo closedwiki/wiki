ea47e3c3426ffc14d94c3014bff54c6f14a50bb0
ArthurClemens <ArthurClemens@a00a5322-12db-0310-a70b-8735589c885e>
2007-05-13 17:48:16 +0000

    Item4056: version 1.1: changed sendmail script invocation to be called through bin script =sendemail=; added CSS styles for feedback notification; fixed typo 'feedbackSucces'
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@13719 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/SendEmailPlugin/lib/TWiki/Plugins/SendEmailPlugin.pm
+++ twikiplugins/SendEmailPlugin/lib/TWiki/Plugins/SendEmailPlugin.pm
@@ -25,46 +25,59 @@ use strict;
 # $VERSION is referred to by TWiki, and is the only global variable that
 # *must* exist in this package
 use vars qw( $VERSION $RELEASE $debug $pluginName );
-use vars qw( $retryCount $emailSent $successMessage $errorMessage);
+use vars qw( $successMessage $errorMessage $headerDone);
 
 # This should always be $Rev: 11069$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
 $VERSION = '$Rev: 11069$';
-$RELEASE = '1.0';
+$RELEASE = '1.1';
 
 # Name of this Plugin, only used in this module
 $pluginName = 'SendEmailPlugin';
 
-$retryCount = 5;
+$headerDone = 0;
 
+my $RETRY_COUNT                  = 5;
+my $ERROR_STATUS_TAG             = 'SendEmailErrorStatus';
+my $NOTIFICATION_CSS_CLASS       = 'sendEmailPluginNotification';
+my $NOTIFICATION_ERROR_CSS_CLASS = 'sendEmailPluginError';
+my $NOTIFICATION_ANCHOR_NAME     = 'FormPluginNotification';
+my %ERROR_STATUS = (
+    'noerror' => 1,
+    'error'   => 2,
+);
 =pod
 
 =cut
 
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.026 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1.026 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
-    $debug = TWiki::Func::getPluginPreferencesFlag("DEBUG") || TWiki::Func::getPreferencesFlag("SENDEMAILPLUGIN_DEBUG");
-    
-    $successMessage = TWiki::Func::getPluginPreferencesValue("EMAIL_SENT_SUCCESS_MESSAGE")
-        || TWiki::Func::getPluginPreferencesValue("SENDEMAILPLUGIN_EMAIL_SENT_SUCCESS_MESSAGE")
-        || '';
-    
-    $errorMessage = TWiki::Func::getPluginPreferencesValue("EMAIL_SENT_ERROR_MESSAGE")
-        || TWiki::Func::getPluginPreferencesValue("SENDEMAILPLUGIN_EMAIL_SENT_ERROR_MESSAGE")
-        || '';
+    $debug = TWiki::Func::getPluginPreferencesFlag("DEBUG")
+      || TWiki::Func::getPreferencesFlag("SENDEMAILPLUGIN_DEBUG");
 
-    TWiki::Func::registerTagHandler('SENDEMAIL',\&_handleSendEmail);
+    $successMessage =
+      TWiki::Func::getPluginPreferencesValue("EMAIL_SENT_SUCCESS_MESSAGE")
+      || TWiki::Func::getPluginPreferencesValue(
+        "SENDEMAILPLUGIN_EMAIL_SENT_SUCCESS_MESSAGE")
+      || '';
 
-    $emailSent = 0;
-    
+    $errorMessage =
+      TWiki::Func::getPluginPreferencesValue("EMAIL_SENT_ERROR_MESSAGE")
+      || TWiki::Func::getPluginPreferencesValue(
+        "SENDEMAILPLUGIN_EMAIL_SENT_ERROR_MESSAGE")
+      || '';
+
+    TWiki::Func::registerTagHandler( 'SENDEMAIL', \&_handleSendEmailTag );
+
     # Plugin correctly initialized
     return 1;
 }
@@ -73,42 +86,57 @@ sub initPlugin {
 
 =cut
 
-sub _handleSendEmail {
-    my($session, $params, $topic, $web) = @_;
-    
-    TWiki::Func::writeDebug("_handleSendEmail - emailSent=$emailSent") if $debug;
+sub _handleSendEmailTag {
+    my ( $session, $params, $topic, $web ) = @_;
 
-    return if $emailSent == 1;
-    
+    _addHeader();
+
     my $query = TWiki::Func::getCgiQuery();
-    my $sendmail = $query->param('sendmail') || '';
+    my $errorStatus = $query->param($ERROR_STATUS_TAG);
     
-    TWiki::Func::writeDebug("_handleSendEmail - sendmail=$sendmail") if $debug;
-    
-    return '' if $sendmail ne 'on';
+    TWiki::Func::writeDebug("_handleSendEmailTag; errorStatus=$errorStatus") if $debug;
 
-    my $feedbackSucces = $params->{'feedbackSucces'} || $successMessage;
-    my $feedbackError = $params->{'feedbackError'} || $errorMessage;
-        
-    my $to = '';
-    my $from = '';
-    my $cc = '';
+    return '' if !defined $errorStatus;
+
+    my $feedbackSuccess = $params->{'feedbackSuccess'} || $successMessage;
+    my $feedbackError   = $params->{'feedbackError'}   || $errorMessage;
+
+    my $message = ($errorStatus == $ERROR_STATUS{'error'}) ? $feedbackError : $feedbackSuccess;
+
+    return _wrapHtmlNotificationContainer( $message, $errorStatus );
+}
+
+=pod
+
+=cut
+
+sub sendEmail {
+    my $session = shift;
+
+    my $to      = '';
+    my $from    = '';
+    my $cc      = '';
     my $subject = '';
-    my $body = '';
-    
-	$to = $query->param('to');
-	$from = $query->param('from')
-	    || $TWiki::cfg{WebMasterEmail}
-	    || TWiki::Func::getPreferencesValue( 'WIKIWEBMASTER' )
-	    || 'twikiwebmaster@example.com';
-          
-	my $ccParam = $query->param('cc') || '';
-	$cc = $ccParam if $ccParam;
-	my $subjectParam = $query->param('subject');
-	$subject = $subjectParam if $subjectParam;
-	my $bodyParam = $query->param('body') || '';
-	$body = $bodyParam if $bodyParam;
-	    
+    my $body    = '';
+
+    my $query = TWiki::Func::getCgiQuery();
+
+    $to = $query->param('to');
+    return _finishSendEmail( $session, $ERROR_STATUS{'error'} ) if !$to;
+
+    $from = $query->param('from')
+      || $TWiki::cfg{WebMasterEmail}
+      || TWiki::Func::getPreferencesValue('WIKIWEBMASTER');
+
+    return _finishSendEmail( $session, $ERROR_STATUS{'error'} ) if !$from;
+
+    my $ccParam = $query->param('cc') || '';
+    $cc = $ccParam if $ccParam;
+    my $subjectParam = $query->param('subject');
+    $subject = $subjectParam if $subjectParam;
+    my $bodyParam = $query->param('body') || '';
+    $body = $bodyParam if $bodyParam;
+
     my $mail = <<'HERE';
 From: %FROM%
 To: %TO%
@@ -123,19 +151,64 @@ HERE
     $mail =~ s/%CC%/$cc/go;
     $mail =~ s/%SUBJECT%/$subject/go;
     $mail =~ s/%BODY%/$body/go;
-    
+
     TWiki::Func::writeDebug("mail message=$mail") if $debug;
 
-    my $error = TWiki::Func::sendEmail( $mail, $retryCount );
+    my $error = TWiki::Func::sendEmail( $mail, $RETRY_COUNT );
+    my $errorStatus = $error ? $ERROR_STATUS{'error'} : $ERROR_STATUS{'noerror'};
+    
+    TWiki::Func::writeDebug("errorStatus=$errorStatus") if $debug;
 
-    my $feedback = '';
-    if ($error) {
-        $feedback = $feedbackError;
-    } else {
-        $feedback = $feedbackSucces;
-    }
-    $emailSent = 1;
-    return $feedback;
+    _finishSendEmail( $session, $errorStatus );
+}
+
+=pod
+
+=cut
+
+sub _finishSendEmail {
+    my ( $session, $errorStatus ) = @_;
+    my $web   = $session->{webName};
+    my $topic = $session->{topicName};
+
+    my $query = TWiki::Func::getCgiQuery();
+    TWiki::Func::writeDebug("_finishSendEmail errorStatus=$errorStatus") if $debug;
+
+    $query->param( -name => $ERROR_STATUS_TAG, -value => $errorStatus );
+
+    TWiki::Func::redirectCgiQuery( undef,
+        TWiki::Func::getScriptUrl( $web, $topic, 'view' ), 1 );
+    # would pass '#'=>$NOTIFICATION_ANCHOR_NAME but the anchor removes 
+    # the ERROR_STATUS_TAG param
+}
+
+=pod
+
+=cut
+
+sub _addHeader {
+
+    return if $headerDone;
+
+    my $header = <<'EOF';
+<style type="text/css" media="all">
+@import url("%PUBURL%/%TWIKIWEB%/SendEmailPlugin/sendemailplugin.css");
+</style>
+EOF
+    TWiki::Func::addToHEAD( 'SENDEMAILPLUGIN', $header );
+    $headerDone = 1;
+}
+
+=pod
+
+=cut
+
+sub _wrapHtmlNotificationContainer {
+    my ( $text, $errorStatus ) = @_;
+
+    my $cssClass = $NOTIFICATION_CSS_CLASS;
+    $cssClass .= ' ' . $NOTIFICATION_ERROR_CSS_CLASS if ( $errorStatus == $ERROR_STATUS{'error'} );
+    return "#$NOTIFICATION_ANCHOR_NAME\n" . '<div class="' . $cssClass . '">' . $text . '</div>';
 }
 
 1;
