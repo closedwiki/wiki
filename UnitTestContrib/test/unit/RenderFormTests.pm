71b1ca833f8d898e2263b9349a91c6d7dcfb6148
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2007-06-27 16:42:18 +0000

    Item4262: missed checking in these test updates
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@14293 a00a5322-12db-0310-a70b-8735589c885e

--- UnitTestContrib/test/unit/RenderFormTests.pm
+++ UnitTestContrib/test/unit/RenderFormTests.pm
@@ -2,200 +2,240 @@ use strict;
 
 package RenderFormTests;
 
-use base qw(TWikiTestCase);
+use base qw(TWikiFnTestCase);
 
 use strict;
-use TWiki;
-use TWiki::UI::Save;
-use CGI;
-use Error qw( :try );
 
-my $testweb = "TestWeb";
+use TWiki::Meta;
+
 my $testtopic1 = "TestTopic1";
 my $testtopic2 = "TestTopic2";
-
-my $twiki;
-my $user;
-my $testuser1 = "TestUser1";
-my $testuser2 = "TestUser2";
-
-my $testtext1 = <<'HERE';
-%META:TOPICINFO{author="TWikiGuest" date="1124568292" format="1.1" version="1.2"}%
-
--- Main.TWikiGuest - 20 Aug 2005
-
-%META:FORM{name="InitializationForm"}%
-%META:FIELD{name="IssueName" attributes="M" title="Issue Name" value="_An issue_"}%
-%META:FIELD{name="IssueDescription" attributes="" title="Issue Description" value="---+ Example problem"}%
-%META:FIELD{name="Issue1" attributes="" title="Issue 1" value="*Defect*"}%
-%META:FIELD{name="Issue2" attributes="" title="Issue 2" value="Enhancement"}%
-%META:FIELD{name="Issue3" attributes="" title="Issue 3" value="Defect, None"}%
-%META:FIELD{name="Issue4" attributes="" title="Issue 4" value="Defect"}%
-%META:FIELD{name="State" attributes="H" title="State" value="Invisible"}%
-%META:FIELD{name="Anothertopic" attributes="" title="Another topic" value="GRRR "}%
-HERE
-
-my $testtext2 = <<'HERE';
-%META:TOPICINFO{author="TWikiGuest" date="1124568292" format="1.1" version="1.2"}%
-
--- Main.TWikiGuest - 20 Aug 2005
-
-%META:FORM{name="InitializationForm"}%
-%META:FIELD{name="IssueName" attributes="M" title="Issue Name" value="_An issue_"}%
-%META:FIELD{name="IssueDescription" attributes="" title="IssueDescription" value="| abc | 123 |%0d%0a| def | ghk |"}%
-%META:FIELD{name="Issue1" attributes="" title="Issue1" value="*no web*"}%
-%META:FIELD{name="Issue2" attributes="" title="Issue2" value="   * abc%0d%0a   * def%0d%0a      * geh%0d%0a   * ijk"}%
-%META:FIELD{name="Issue3" attributes="" title="Issue3" value="_hello world_"}%
-%META:FIELD{name="Issue4" attributes="" title="Issue4" value="   * high"}%
-%META:FIELD{name="State" attributes="H" title="State" value="Invisible"}%
-%META:FIELD{name="Anothertopic" attributes="" title="Another topic" value="GRRR "}%
-HERE
-
-sub new {
-    my $self = shift()->SUPER::new(@_);
-    return $self;
-}
 
 # Set up the test fixture
 sub set_up {
     my $this = shift;
     $this->SUPER::set_up();
 
-    $twiki = new TWiki();
-    $user = $twiki->{user};
+    # create a fabby little type, just to make sure it gets called
+    my $dir = File::Temp::tempdir( CLEANUP => 1 );
+    mkdir("$dir/TWiki");
+    mkdir("$dir/TWiki/Form");
+    open(F, ">$dir/TWiki/Form/Nuffin.pm");
+    print F <<HERE;
+    package TWiki::Form::Nuffin;
+    use base 'TWiki::Form::FieldDefinition';
 
-    $twiki->{store}->createWeb($user, $testweb);
-
-    $TWiki::Plugins::SESSION = $twiki;
-    TWiki::Func::saveTopicText( $testweb, $testtopic1, $testtext1, 1, 1 );
-    TWiki::Func::saveTopicText( $testweb, $testtopic2, $testtext2, 1, 1 );
-}
-
-sub tear_down {
-    my $this = shift;
-    $this->removeWebFixture($twiki, $testweb);
-    $twiki->finish();
-    $this->SUPER::tear_down();
-}
-
-sub get_HTML_tree {
-    my ($res) = @_;
-    eval 'use HTML::TreeBuilder; use HTML::Element 3.23;';
-    if( $@ ) {
-        die "$@\nUNABLE TO RUN TEST\n";
+    sub renderForEdit {
+        return ('EXTRA', "SWEET");
     }
 
-    my $tree = HTML::TreeBuilder->new_from_content($res);
-    # Analyze the tree, could use find_by_tag_name
-    return $tree->content_list();
-    $tree->delete;
+    sub renderForDisplay {
+        return "SOUR";
+    }
+    1;
+HERE
+    push(@INC, $dir);
+
+    TWiki::Func::saveTopic( $this->{test_web}, "WebPreferences", undef, <<HERE );
+   * Set WEBFORMS = InitializationForm
+HERE
+
+    my $meta = new TWiki::Meta($this->{twiki}, $this->{test_web}, $testtopic1);
+    $meta->put('FORM', { name=>"InitializationForm" });
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"IssueName",
+          attributes=>"M", title=>"Issue Name", value=>"_An issue_"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"IssueDescription",
+          attributes=>"", title=>"Issue Description", value=>"---+ Example problem"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Issue1",
+          attributes=>"", title=>"Issue 1", value=>"*Defect*"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Issue2",
+          attributes=>"", title=>"Issue 2", value=>"Enhancement"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Issue3",
+          attributes=>"", title=>"Issue 3", value=>"Defect, None"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Issue4",
+          attributes=>"", title=>"Issue 4", value=>"Defect"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"State",
+          attributes=>"H", title=>"State", value=>"Invisible"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Anothertopic",
+          attributes=>"", title=>"Another topic", value=>"GRRR "});
+
+    TWiki::Func::saveTopic( $this->{test_web}, $testtopic1, $meta, 'TT1' );
+
+    $meta = new TWiki::Meta($this->{twiki}, $this->{test_web}, $testtopic2);
+    $meta->put('FORM', { name=>"InitializationForm",
+                     });
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"IssueName",
+          attributes=>"M", title=>"Issue Name", value=>"_An issue_"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"IssueDescription",
+          attributes=>"", title=>"IssueDescription", value=>"| abc | 123 |\r\n| def | ghk |"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Issue1",
+          attributes=>"", title=>"Issue1", value=>"*no web*"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Issue2",
+          attributes=>"", title=>"Issue2", value=>",   * abc\r\n   * def\r\n      * geh\r\n   * ijk"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Issue3",
+          attributes=>"", title=>"Issue3", value=>"_hello world_"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Issue4",
+          attributes=>"", title=>"Issue4", value=>",   * high"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"State",
+          attributes=>"H", title=>"State", value=>"Invisible"});
+    $meta->putKeyed(
+        'FIELD',
+        { name=>"Anothertopic",
+          attributes=>"", title=>"Another topic", value=>"GRRR "});
+    TWiki::Func::saveTopic( $this->{test_web}, $testtopic2, $meta, 'TT2' );
 }
 
-sub get_HTML_tree_from_form {
-  return (((get_HTML_tree ( @_ ))[1]->content_list())[0]->content_list())[0]->content_list();
-}
-
-sub compare_field_from_form {
-  my ($this, $res, $child, @children) = @_;
-  my $text = (($children[$child]->content_list())[1]->content_list())[0];
-  $this->assert_str_equals($res, $text);
-}
-
-sub compare_field_from_form_fmt {
-  my ($this, $res, $child, @children) = @_;
-  my $text = (($children[$child]->content_list())[1]->content_list())[0]->as_HTML();
-  $this->assert_str_equals($res, $text);
-}
-
-# TML is rendered correctly in form fields
-sub test_TML_in_forms {
+sub setForm {
     my $this = shift;
-    my($meta, $text) = $twiki->{store}->readTopic(undef, $testweb, $testtopic1);
-    my $res = $meta->renderFormForDisplay();
-    my $render = $twiki->renderer;
-    $res = $render->getRenderedVersion($res, $testweb, $testtopic1);
-
-    my @children = get_HTML_tree_from_form( $res );
-    # Now we have 8 rows
-    # first is the header row
-    $this->assert_str_equals(((($children[0]->content_list())[0]->content_list())[0]->content_list())[0], 'InitializationForm');
-    $this->compare_field_from_form_fmt('<em>An issue</em>
-', 1, @children);
-    $this->compare_field_from_form_fmt('<h1><a name="Example_problem"></a> Example problem </h1>
-', 2, @children);
-    $this->compare_field_from_form_fmt('<strong>Defect</strong>
-', 3, @children);
-    $this->compare_field_from_form(' Enhancement ', 4, @children);
-    $this->compare_field_from_form(' Defect, None ', 5, @children);
-    $this->compare_field_from_form(' Defect ', 6, @children);
-    $this->compare_field_from_form(" $testweb.GRRR ", 7, @children);
+    TWiki::Func::saveTopic( $this->{test_web}, "InitializationForm", undef, <<HERE );
+| *Name*            | *Type*   | *Size* | *Values* |
+| Issue Name        | text     | 40     |          |
+| State             | radio    |        | none     |
+| Issue Description | label    | 10     | 5        |
+| Issue 1           | select   |        |          |
+| Issue 2           | nuffin   |        |          |
+| Issue 3           | checkbox |        |          |
+| Issue 4           | textarea |        |          |
+Topic is deliberately missing
+HERE
 }
 
-sub test_formatted_TML_in_forms {
-    my $this = shift;
-    my($meta, $text) = $twiki->{store}->readTopic(undef, $testweb, $testtopic2);
-    my $res = $meta->renderFormForDisplay();
-    my $render = $twiki->renderer;
-    $res = $render->getRenderedVersion($res, $testweb, $testtopic2);
-
-    my @children = get_HTML_tree_from_form( $res );
-    # Now we have 8 rows
-#### Why is access these fields different from above?
-    $this->compare_field_from_form_fmt('<em>An issue</em>
-', 1, @children);
-    $this->compare_field_from_form_fmt('<table border="1" cellpadding="0" cellspacing="0" class="twikiTable"><tr class="twikiTableEven"><td bgcolor="#ffffff" class="twikiFirstCol" valign="top"> abc </td><td bgcolor="#ffffff" valign="top"> 123 </td></tr><tr class="twikiTableOdd"><td bgcolor="#edf4f9" class="twikiFirstCol twikiLast" valign="top"> def </td><td bgcolor="#edf4f9" class="twikiLast" valign="top"> ghk </td></tr></table>
-', 2, @children);
-    $this->compare_field_from_form_fmt('<strong>no web</strong>
-', 3, @children);
-    $this->compare_field_from_form_fmt('<ul><li> abc <li> def <ul><li> geh </ul><li> ijk </ul>
-', 4, @children);
-    $this->compare_field_from_form_fmt('<em>hello world</em>
-', 5, @children);
-    $this->compare_field_from_form_fmt('<ul><li> high </ul>
-', 6, @children);
-  }
-
-sub get_HTML_tree_from_field {
-  return (((get_HTML_tree ( @_ ))[1]->content_list())[0]->content_list())[0]->content_list();
-}
-
-sub compare_formfield {
-  my ($this, $res, $compare) = @_;
-  my @children = get_HTML_tree($res);
-  my $text = ($children[1]->content_list())[0]->as_HTML();
-  $this->assert_str_equals($compare, $text);
-}
-
+# Simple test; no forms in place, just check value rendering
 sub test_render_formfield_raw {
 
     my $this = shift;
-    my($meta, $text) = $twiki->{store}->readTopic(undef, $testweb, $testtopic2);
-    my $render = $twiki->renderer;
+    my($meta, $text) =
+      $this->{twiki}->{store}->readTopic(undef, $this->{test_web}, $testtopic2);
+    my $render = $this->{twiki}->renderer;
     my $res;
 
-    $res = $render->renderFormField( $meta, new TWiki::Attrs('name="IssueDescription" newline="$n" bar="|"') );
-    $res = $render->getRenderedVersion($res, $testweb, $testtopic2);
-    $this->compare_formfield($res,'<table border="1" cellpadding="0" cellspacing="0" class="twikiTable"><tr class="twikiTableEven"><td bgcolor="#ffffff" class="twikiFirstCol" valign="top"> abc </td><td bgcolor="#ffffff" valign="top"> 123 </td></tr><tr class="twikiTableOdd"><td bgcolor="#edf4f9" class="twikiFirstCol twikiLast" valign="top"> def </td><td bgcolor="#edf4f9" class="twikiLast" valign="top"> ghk </td></tr></table>
-');
-    $res = $render->renderFormField( $meta, new TWiki::Attrs('name="Issue1" newline="$n" bar="|"') );
-    $res = $render->getRenderedVersion($res, $testweb, $testtopic2);
-    $this->compare_formfield($res,'<strong>no web</strong>
-');
-    $res = $render->renderFormField( $meta, new TWiki::Attrs('name="Issue2" newline="$n" bar="|"') );
-    $res = $render->getRenderedVersion($res, $testweb, $testtopic2);
-    $this->compare_formfield($res,'<ul><li> abc <li> def <ul><li> geh </ul><li> ijk </ul>
-');
-    $res = $render->renderFormField( $meta, new TWiki::Attrs('name="Issue3" newline="$n" bar="|"') );
-    $res = $render->getRenderedVersion($res, $testweb, $testtopic2);
-    $this->compare_formfield($res,'<em>hello world</em>
-');
-    $res = $render->renderFormField( $meta, new TWiki::Attrs('name="Issue4" newline="$n" bar="|"') );
-    $res = $render->getRenderedVersion($res, $testweb, $testtopic2);
-    $this->compare_formfield($res,'<ul><li> high </ul>
-');
-    return 0;
+    $res = $meta->renderFormFieldForDisplay( "IssueDescription", '| $title | $value |', { newline=>'NL', bar=>"BAR" } );
 
-    
+    $this->assert_str_equals('| IssueDescription | BAR abc BAR 123 BARNLBAR def BAR ghk BAR |', $res);
+
+    $res = $meta->renderFormFieldForDisplay( "Issue1", '$value > $title', { newline=>'NL', bar=>"BAR" } );
+    $this->assert_str_equals('*no web* > Issue1', $res);
+    $res = $meta->renderFormFieldForDisplay( "Issue2", '$value', { newline=>'NL', bar=>"BAR" } );
+    $this->assert_str_equals(',   * abcNL   * defNL      * gehNL   * ijk', $res);
+    $res = $meta->renderFormFieldForDisplay( "Issue3", '$value', { newline=>'NL', bar=>"BAR" } );
+    $this->assert_str_equals('_hello world_', $res);
+    $res = $meta->renderFormFieldForDisplay( "Issue4", '$value > $title', { newline=>'NL', bar=>"BAR" } );
+    $this->assert_str_equals(',   * high > Issue4', $res);
+    $res = $meta->renderFormFieldForDisplay( "State", '', { newline=>'NL', bar=>"BAR" } );
+    $this->assert_str_equals('', $res);
+
+}
+
+# Simple test; form in place, just check value rendering
+sub test_render_formfield_with_form {
+    my $this = shift;
+
+    $this->setForm();
+
+    my($meta, $text) =
+      $this->{twiki}->{store}->readTopic(undef, $this->{test_web}, $testtopic2);
+    my $res = $meta->renderFormForDisplay();
+    $this->assert_html_equals(<<'HERE', $res);
+<div class="twikiForm">
+<table class='twikiFormTable' border='1'><tr><th class='twikiFormTableHRow twikiFirstCol' colspan='2'>
+[[InitializationForm]]
+</th></tr><tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue Name </td><td>
+_An issue_
+</td></tr><tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue Description </td><td>
+&#124; abc &#124; 123 &#124;<br />&#124; def &#124; ghk &#124;
+</td></tr><tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue 1 </td><td>
+*no web*
+</td></tr>SOUR<tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue 3 </td><td>
+_hello world_
+</td></tr><tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue 4 </td><td>
+,   * high
+</td></tr></table>
+</div><!-- /twikiForm -->
+HERE
+    ($meta, $text) =
+      $this->{twiki}->{store}->readTopic(undef, $this->{test_web}, $testtopic1);
+    $res = $meta->renderFormForDisplay();
+    $this->assert_html_equals(<<'HERE', $res);
+<div class="twikiForm">
+<table class='twikiFormTable' border='1'><tr><th class='twikiFormTableHRow twikiFirstCol' colspan='2'>
+[[InitializationForm]]
+</th></tr><tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue Name </td><td>
+_An issue_
+</td></tr><tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue Description </td><td>
+---+ Example problem
+</td></tr><tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue 1 </td><td>
+*Defect*
+</td></tr>SOUR<tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue 3 </td><td>
+Defect, None
+</td></tr><tr valign='top'><td class='twikiFormTableRow twikiFirstCol' align='right'> Issue 4 </td><td>
+Defect
+</td></tr></table>
+</div><!-- /twikiForm -->
+HERE
+}
+
+sub test_render_for_edit {
+    my $this = shift;
+    $this->setForm();
+    my ($meta, $text) =
+      $this->{twiki}->{store}->readTopic(undef, $this->{test_web}, $testtopic1);
+    my $formDef = new TWiki::Form(
+        $this->{twiki}, $this->{test_web}, "InitializationForm" );
+    my $res = $formDef->renderForEdit($this->{test_web}, $testtopic1, $meta);
+    $this->assert_html_equals(<<'HERE', $res);
+<div class="twikiForm twikiEditForm"><table class="twikiFormTable"><tr><th class="twikiFormTableHRow" colspan="2"><a rel="nofollow" target="InitializationForm" href="/MAIN/bin/view/TemporaryRenderFormTestsTestWebRenderFormTests/InitializationForm" title="Click to see details in separate window" onclick="return launchWindow(&quot;TemporaryRenderFormTestsTestWebRenderFormTests&quot;,&quot;InitializationForm&quot;)">TemporaryRenderFormTestsTestWebRenderFormTests.InitializationForm</a> <input type="submit" name="action_replaceform" value='Replace form...' class="twikiChangeFormButton twikiButton" /></th></tr>
+<tr><th align="right"><span title="Click to see details in separate window">Issue Name</span></th><td align="left"><input type="text" name="IssueName" value="_An issue_" size="40" class="twikiInputField twikiEditFormTextField" /></td></tr>
+<tr><th align="right"><span title="Click to see details in separate window">State</span></th><td align="left"><table><tr><td><label><input type="radio" name="State" value="none"  label="none" class="twikiRadioButton twikiEditFormRadioField"/>none</label></td></tr></table></td></tr>
+<tr><th align="right"><span title="Click to see details in separate window">Issue Description</span></th><td align="left"><input type="hidden" name="IssueDescription" value="---+ Example problem"  /><div class="twikiEditFormLabelField"><nop><h1><a name="Example_problem"></a> Example problem </h1></div></td></tr>
+<tr><th align="right"><span title="Click to see details in separate window">Issue 1</span></th><td align="left"><select name="Issue1" class="twikiSelect twikiEditFormSelect" size="1"></select></td></tr>
+<tr><th align="right"><span title="Click to see details in separate window">Issue 2</span>EXTRA</th><td align="left">SWEET</td></tr>
+<tr><th align="right"><span title="Click to see details in separate window">Issue 3</span></th><td align="left"><table></table><input type="hidden" name="Issue3" value="" /></td></tr>
+<tr><th align="right"><span title="Click to see details in separate window">Issue 4</span></th><td align="left"><textarea name="Issue4"  rows="4" cols="50" class="twikiInputField twikiEditFormTextAreaField">
+Defect</textarea></td></tr> </table>  </div>
+HERE
+}
+
+sub test_render_hidden {
+    my $this = shift;
+    $this->setForm();
+    my ($meta, $text) =
+      $this->{twiki}->{store}->readTopic(undef, $this->{test_web}, $testtopic1);
+    my $formDef = new TWiki::Form(
+        $this->{twiki}, $this->{test_web}, "InitializationForm" );
+    my $res = $formDef->renderHidden($meta);
+    $this->assert_html_equals(<<'HERE', $res);
+<input type="hidden" name="IssueName" value="_An issue_"  /><input type="hidden" name="State" value="Invisible"  /><input type="hidden" name="IssueDescription" value="---+ Example problem"  /><input type="hidden" name="Issue1" value="*Defect*"  /><input type="hidden" name="Issue2" value="Enhancement"  /><input type="hidden" name="Issue3" value="Defect"  /><input type="hidden" name="Issue3" value="None"  /><input type="hidden" name="Issue4" value="Defect"  />
+HERE
 }
 
 1;
