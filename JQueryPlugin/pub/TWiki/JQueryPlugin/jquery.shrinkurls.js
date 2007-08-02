eaf726edb3b6a9a11ea13e57fa4cf60fc66ee4d5
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2007-08-02 14:38:03 +0000

    Item4415:    * fixed jquery.nifty.js for IE    * fixed jquery.tooltip.js for IE    * lost of other IE fixes    * made handling of rounded corners easier    * some enhancements to jquery.shrinkurls.js
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@14494 a00a5322-12db-0310-a70b-8735589c885e

--- JQueryPlugin/pub/TWiki/JQueryPlugin/jquery.shrinkurls.js
+++ JQueryPlugin/pub/TWiki/JQueryPlugin/jquery.shrinkurls.js
@@ -35,10 +35,9 @@
 $.fn.extend({
   shrinkUrls: function(settings) {
     settings = $.extend({
-      size:25,
       whitespace:false,
+      trunc:'tail'
     }, settings);
-
 
     return this.each(function() {
       var text = $(this).text();
@@ -47,8 +46,22 @@ $.fn.extend({
           (!settings.exclude || !text.match(settings.exclude)) &&
           (settings.whitespace || !text.match(/\s/))) {
         var txtlength = text.length;
-        var firstPart = text.substring(0,10);
-        var lastPart = text.substring(txtlength-10,txtlength);
+        var firstPart = "";
+        var lastPart = "";
+        var middlePart = "";
+        switch (settings.trunc) {
+          default:
+          case 'tail':
+            firstPart = text.substring(0,settings.size-1);
+            break;
+          case 'head':
+            lastPart = text.substring(txtlength-settings.size+1,txtlength);
+            break;
+          case 'middle':
+            firstPart = text.substring(0,settings.size/2);
+            lastPart = text.substring(txtlength-settings.size/2+1,txtlength);
+            break;
+        }
         var origText = text;
         text = firstPart + "&hellip;" + lastPart;
         var title = $(this).attr('title');
