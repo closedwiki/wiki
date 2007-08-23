d99333ddf1ab6b911576597d299d40700005168c
MichaelDaum <MichaelDaum@a00a5322-12db-0310-a70b-8735589c885e>
2007-08-23 10:25:47 +0000

    Item4430:    * fixed creating lists in the first line of the textarea    * fixed creatign a list from a selection of lines, off-by-one error    * fixed dereferencing null on some browsers    * fixed init routing to detect and make proper use of the txtarea variable    * cleaned up javascript code formatting    * added templates for default button theme    * added support for PatternSkin in TWiki-4.2    * don't rely on TwistyPlugin to load all TWikiJavascript in time
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@14603 a00a5322-12db-0310-a70b-8735589c885e

--- NatEditContrib/pub/TWiki/NatEditContrib/edit.js
+++ NatEditContrib/pub/TWiki/NatEditContrib/edit.js
@@ -68,112 +68,124 @@ function natInsertTags(tagOpen, sampleText, tagClose) {
   }
 }
 
-//used for line oriented tags - like bulleted lists
-//if you have a multiline selection, the tagOpen/tagClose is added to each line
-
-//if there is no selection, select the entire current line
-//if there is a selection, select the entire line for each line selected
+// used for line oriented tags - like bulleted lists
+// if you have a multiline selection, the tagOpen/tagClose is added to each line
+// if there is no selection, select the entire current line
+// if there is a selection, select the entire line for each line selected
 function natInsertListTag(tagOpen, sampleText, tagClose) {
-var startPos, endPos;
-  // IE
-    if (!txtarea.selectionStart) {
-    
-        var originalRange = document.selection.createRange().duplicate();
-        var mySelection = originalRange.text;
-                        if(mySelection != null){
-                                endPos = mySelection.length;
-                        } else {
-                            endPos = 0;
-                        }
-                        {//nasty cursor stuff - as createRange stuff breaks when you don't have a selection
-                                txtarea.focus();
-                                var c  = "\001";
-                                var sel = document.selection.createRange();
-var txtareaText = txtarea.value;
-//                                var original = sel.text;
-                                var dul = sel.duplicate(); 
-                                dul.moveToElementText(txtarea);
-                                sel.text = c;
-//seems there is a problem with counting \r's IE
-//                               startPos  = (dul.text.indexOf(c));
-                                 var tempText = dul.text;
-                                sel.moveStart('character',-1);
-txtarea.value = txtareaText;
-//                                sel.text = original;
 
-                                var tempText = tempText.substring(0, tempText.indexOf(c));
-                                //tempText = tempText.replace(/[\r]/g, ''); 
-                                startPos  = tempText.length;
-                        }
-                        endPos+=startPos;
+  var startPos, endPos;
+
+  if (document.selection) {
+    // IE
+    
+    var originalRange = document.selection.createRange().duplicate();
+    var mySelection = originalRange.text;
+
+    if (mySelection != null) {
+      endPos = mySelection.length;
+    } else {
+      endPos = 0;
+    }
+
+    { // SMELL: put that in a local function to prevent redundancy further down
+      // nasty cursor stuff - as createRange stuff breaks when you don't have a selection
+      txtarea.focus();
+      var txtareaText = txtarea.value; // backup
+      var c  = "\001";
+      var sel = document.selection.createRange();
+      var dul = sel.duplicate(); 
+      dul.moveToElementText(txtarea);
+      sel.text = c;
+      // seems there is a problem with counting \r's IE
+      // startPos  = (dul.text.indexOf(c));
+      var tempText = dul.text;
+      sel.moveStart('character',-1);
+      var tempText = tempText.substring(0, tempText.indexOf(c));
+      // tempText = tempText.replace(/[\r]/g, ''); 
+      startPos  = tempText.length;
+      txtarea.value = txtareaText; // restore
+    }
+    endPos+=startPos;
     
     //alert(startPos+', '+endPos);
-  } else {//FF, opera safari, etc
+  } else { 
+    // FF, opera safari, etc
+ 
     startPos = txtarea.selectionStart;
     endPos = txtarea.selectionEnd;
   }
 
-    
-    //at this point we need to expand the selection to the \n before the startPos, and after the endPos
-    var adjustedEndPos = txtarea.value.indexOf('\n', endPos+1);
-    if ((adjustedEndPos != -1) && (adjustedEndPos > endPos) && ( txtarea.value.charAt(endPos) != '\n')  && ( txtarea.value.charAt(endPos) != '\r')) {
-        endPos = adjustedEndPos;
-    }
-    var adjustedStartPos = txtarea.value.lastIndexOf('\n', startPos-1);
-    if ((adjustedStartPos != -1) && (adjustedStartPos < startPos)) {
-        startPos = adjustedStartPos+1;
-    }
-    
-    var scrollTop = txtarea.scrollTop;
-    var theSelection = (txtarea.value).substring(startPos, endPos);
-    
-    if (!theSelection) {
-      theSelection = sampleText;
-    }
-    
-    var pre =   txtarea.value.substring(0, startPos);
-    var post = txtarea.value.substring(endPos, txtarea.value.length);
-    
-    //test if it is a multi-line selection, and if so, add tagOpen&tagClose to each line
-    var lines = theSelection.split(/\r?\n/);
-    var modifiedSelection = '';
-    for (var i=0;i<lines.length;i++) {
-        var line = lines[i];
-        if ( (tagOpen == '') && (sampleText == '') && (tagClose == '') ) {
-            //special case - undent (remove 3 spaces, and bullet or numbered list if outdenting away)
-            subst = line.replace(/^   (\* |\d |\d\. )?/, '');
-        } else {
-            if (line.match(/^(   )*(   (\*|\d|\d\.) )/) &&
-                (tagOpen.match(/^(   )*(   (\*|\d|\d\.) )/))) {
-                subst = line.replace(/   (\* |\d |\d\. )/, tagOpen);
-            } else {
-                subst = tagOpen + line + tagClose;
-            }
-        }
-        modifiedSelection = modifiedSelection + subst;
-        if (i+1<lines.length) modifiedSelection = modifiedSelection + '\n';
-    }
-    txtarea.value = pre + modifiedSelection + post;
+  // at this point we need to expand the selection to the \n before the startPos, and after the endPos
+  var adjustedStartPos = txtarea.value.lastIndexOf('\n', startPos-1);
+  if (adjustedStartPos == -1) {
+    startPos = 0; // first line in textarea has no \n
+  } else if (adjustedStartPos < startPos) {
+    startPos = adjustedStartPos+1;
+  }
 
+  var adjustedEndPos = txtarea.value.indexOf('\n', endPos);
+  if (adjustedEndPos == -1) {
+    endPos = txtarea.value.length; // first line in textarea has no \n
+  } else if ((adjustedEndPos > endPos) && 
+    (txtarea.value.charAt(endPos) != '\n')  && 
+    (txtarea.value.charAt(endPos) != '\r')) {
+    endPos = adjustedEndPos;
+  }
+ 
+  var scrollTop = txtarea.scrollTop;
+  var theSelection = (txtarea.value).substring(startPos, endPos);
 
-      if (!txtarea.selectionStart) {
-      //IE
-         txtarea.focus();
-         var range = txtarea.createTextRange();
-         range.collapse(true);
-               
-         var ctrlR = pre.replace(/[^\r]/g, '');     //ranges don't seem to 'count' the \r chars :/
-                
-         range.moveStart("character", startPos-ctrlR.length);
-         range.moveEnd("character", modifiedSelection.length);
-         range.select();
+  if (!theSelection) {
+    theSelection = sampleText;
+  }
+    
+  var pre = txtarea.value.substring(0, startPos);
+  var post = txtarea.value.substring(endPos, txtarea.value.length);
+    
+  // test if it is a multi-line selection, and if so, add tagOpen&tagClose to each line
+  var lines = theSelection.split(/\r?\n/);
+  var modifiedSelection = '';
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    
+    // special case - undent (remove 3 spaces, and bullet or numbered list if outdenting away)
+    if ((tagOpen == '') && (sampleText == '') && (tagClose == '')) {
+      subst = line.replace(/^   (\* |\d+ |\d+\. )?/, '');
+    } else {
+      if (line.match(/^(   )*(   (\*|\d+|\d+\.) )/) &&
+          (tagOpen.match(/^(   )*(   (\*|\d+|\d+\.) )/))) {
+        subst = line.replace(/   (\* |\d+ |\d+\. )/, tagOpen);
       } else {
-        txtarea.focus();
-        //set new selection
-        var cPos = startPos + modifiedSelection.length;
-        txtarea.selectionStart = startPos;
-        txtarea.selectionEnd = cPos;
-        txtarea.scrollTop = scrollTop;
+        subst = tagOpen + line + tagClose;
+      }
+    }
+
+    modifiedSelection = modifiedSelection + subst;
+    if (i+1 < lines.length) 
+      modifiedSelection = modifiedSelection + '\n';
+  }
+
+  txtarea.value = pre + modifiedSelection + post;
+
+  if (document.selection) {
+    //IE
+    txtarea.focus();
+    var range = txtarea.createTextRange();
+    range.collapse(true);
+             
+    var ctrlR = pre.replace(/[^\r]/g, '');     //ranges don't seem to 'count' the \r chars :/
+              
+    range.moveStart("character", startPos-ctrlR.length);
+    range.moveEnd("character", modifiedSelection.length);
+    range.select();
+  } else {
+    txtarea.focus();
+    //set new selection
+    var cPos = startPos + modifiedSelection.length;
+    txtarea.selectionStart = startPos;
+    txtarea.selectionEnd = cPos;
+    txtarea.scrollTop = scrollTop;
   }
 }
 
@@ -226,83 +238,92 @@ function fixHeightOfTheText() {
   }
   setTimeout("establishOnResize()", 100); /* add a slight timeout not to DoS IE */
 }
+
 function establishOnResize() {
   window.onresize = fixHeightOfTheText;
 }
 
+
 function natEditInit() {
-  if (document.main) {
-    txtarea = document.main.text;
+  if (txtarea) {
+    //alert("txtarea is already "+txtarea);
   } else {
-    // some alternate form? take the first one we can find
-    var areas = document.getElementsByTagName('textarea');
-    txtarea = areas[0];
+    if (document.main) { // patternskin
+      txtarea = document.main.text;
+    } else if (document.EditForm) { // natskin
+      txtarea = document.EditForm.text;
+    } else {
+      // some alternate form? take the first one we can find
+      var areas = document.getElementsByTagName('textarea');
+      txtarea = areas[0];
+    }
   }
   fixHeightOfTheText();
   establishOnResize();
-  
-  
-  if (twiki.JQueryPluginEnabled) {
-        $(function() { 
-        // cache the question element 
-        var natEditTableDialog = $('#natEditTableDialog')[0]; 
- 
-        $('#natEditTableButtonLink').click(function() { 
-            if (!txtarea.selectionStart) {          //IE
-                        var originalRange = document.selection.createRange().duplicate();
-                        var mySelection = originalRange.text;
-                        if(mySelection != null){
-                                $.endPos = mySelection.length;
-                        } else {
-                            $.endPos = 0;
-                        }
-                        {//nasty cursor stuff - as createRange stuff breaks when you don't have a selection
-                                txtarea.focus();
-                                var c  = "\001";
-                                var sel = document.selection.createRange();
-                                
-var txtareaText = txtarea.value;
-                                                                
-                                //var original = sel.text;
-                                var dul = sel.duplicate(); 
-                                dul.moveToElementText(txtarea);
-                                sel.text = c;
-//seems there is a problem with counting \r's IE
-//                                $.startPos  = (dul.text.indexOf(c));
-                                 var tempText = dul.text;
-                                sel.moveStart('character',-1);
-                                //sel.text = original;
-                                
-txtarea.value = txtareaText;                                
 
-                                var tempText = tempText.substring(0, tempText.indexOf(c));
-                                tempText = tempText.replace(/[\r]/g, ''); 
-                                $.startPos  = tempText.length;
-                        }                
-                        $.endPos += $.startPos; 
-            }
-            $.blockUI(natEditTableDialog, { width: '275px' }); 
-        }); 
+  if (twiki.JQueryPluginEnabled) {
+
+    // establish the table dialog
+    $(function() { 
+      // cache the question element 
+      var natEditTableDialog = $('#natEditTableDialog')[0]; 
  
-        $('#yes').click(function() { 
-            //read the rows&col's and create table to that spec
-            var rows = $('#rows').val()
-            var cols = $('#columns').val();
-            $.unblockUI(); 
+      // table button 
+      $('#natEditTableButtonLink').click(function() { 
+
+        if (!txtarea.selectionStart) { //IE
+          var originalRange = document.selection.createRange().duplicate();
+          var mySelection = originalRange.text;
+          if(mySelection != null){
+            $.endPos = mySelection.length;
+          } else {
+            $.endPos = 0;
+          }
+          // SMELL: redundant, see above
+          // nasty cursor stuff - as createRange stuff breaks when you don't have a selection
+          {
+            txtarea.focus();
+            var txtareaText = txtarea.value; // backup
+            var c  = "\001";
+            var sel = document.selection.createRange();
+            var dul = sel.duplicate(); 
+            dul.moveToElementText(txtarea);
+            sel.text = c;
+            //seems there is a problem with counting \r's IE
+            //$.startPos  = (dul.text.indexOf(c));
+            var tempText = dul.text;
+            sel.moveStart('character',-1);
+            var tempText = tempText.substring(0, tempText.indexOf(c));
+            tempText = tempText.replace(/[\r]/g, ''); 
+            $.startPos  = tempText.length;
+            txtarea.value = txtareaText; // restore
+          }                
+          $.endPos += $.startPos; 
+        } // end IE
+
+        $.blockUI(natEditTableDialog, { width: '275px' }); 
+      }); 
+ 
+      // dialog buttons
+      $('#yes').click(function() { 
+        // read the rows&col's and create table to that spec
+        var rows = $('#rows').val()
+        var cols = $('#columns').val();
+        $.unblockUI(); 
             
-            var newTable = '';
-            for (var i=0;i< rows;i++) {
-                newTable += '|';
-                for (var j=0;j< cols;j++) {
-                    if (i == 0) {
-                        newTable += ' ** ';
-                    } else {
-                        newTable += '   ';
-                    }
-                    newTable += ' |';
-                }
-                newTable += '\n';
+        var newTable = '';
+        for (var i = 0; i < rows; i++) {
+          newTable += '|';
+          for (var j = 0; j < cols; j++) {
+            if (i == 0) {
+              newTable += ' ** ';
+            } else {
+              newTable += '   ';
             }
+            newTable += ' |';
+          }
+          newTable += '\n';
+        }
             txtarea.focus();
             //help IE out by re-selecting what was selected before :(
             if (!txtarea.selectionStart) {          //IE
@@ -312,26 +333,25 @@ txtarea.value = txtareaText;
                  range.moveEnd("character", $.endPos - $.startPos);
                  range.select();
              }
-            natInsertTags('','',newTable);
+        natInsertTags('','',newTable);
 
-            return false; 
-        }); 
+        return false; 
+      }); 
  
-         $('#cancel').click(function() {
-                $.unblockUI();
+      $('#cancel').click(function() {
+        $.unblockUI();
                 
-                //help IE out by re-selecting what was selected before :(
-                txtarea.focus();
-                 var range = txtarea.createTextRange();
-                 range.collapse(true);
-                 range.moveStart("character", $.startPos);
-                 range.moveEnd("character", $.endPos - $.startPos);
-                 range.select();
-            }); 
-        }); 
-    }
+        // help IE out by re-selecting what was selected before :(
+        txtarea.focus();
+        var range = txtarea.createTextRange();
+        range.collapse(true);
+        range.moveStart("character", $.startPos);
+        range.moveEnd("character", $.endPos - $.startPos);
+        range.select();
+      }); 
+    }); 
+  }
   
-
   return true;
 }
 
