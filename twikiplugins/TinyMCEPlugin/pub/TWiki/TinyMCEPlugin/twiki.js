a7b5dc7760a8cad727fb02e59cba70b24491838a
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2007-08-28 11:53:42 +0000

    Item4507: paste problems. Made the filter on spans and font tags more aggressive, to cater for pasted TWiki topic content. Risk of losing formatting from pasted HTML, but you can't have everything :-( Also added the Word paste plugin, though I don't have Word so I can't test it.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@14641 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/TinyMCEPlugin/pub/TWiki/TinyMCEPlugin/twiki.js
+++ twikiplugins/TinyMCEPlugin/pub/TWiki/TinyMCEPlugin/twiki.js
@@ -1,22 +1,5 @@
 // Content manipulation on startup
 var tinymce_plugin_setUpContent = function(editor_id, body, doc) {
-	// content is in body.innerHTML;
+    //body.innerHTML = TML2HTML.convert(body.innerHTML);
 }
 
-// onLoad handler that adds the wysiwyg_edit field to the main form in
-// an edit page
-var tinymce_plugin_addWysiwygTagToForm = function() {
-   // SMELL: this really isn't good; the main form should have an ID
-    var els = document.getElementsByName('main');
-    for (var j = 0; j < els.length;j++) {
-        var s = els[j].tagName;
-        if (s.toLowerCase() == 'form') {
-            var i = document.createElement('INPUT');
-            i.type = 'hidden';
-            i.name = 'wysiwyg_edit';
-            i.value = 'save';
-            els[j].appendChild(i);
-            return;
-        }
-    }
-};
