fe9c993c90fc3bdc670fe282a22ae7171b635b61
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2007-08-29 11:16:49 +0000

    Item4520: added TinyMCE config features to hopefully improve the word handling. However I'm flying blind; I don't have a copy of Word, so I can't test :-( Any Word user can try different configuration options by setting TINYMCEPLUGIN_INIT, however.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@14657 a00a5322-12db-0310-a70b-8735589c885e

--- TinyMCEPlugin/pub/TWiki/TinyMCEPlugin/twiki.js
+++ TinyMCEPlugin/pub/TWiki/TinyMCEPlugin/twiki.js
@@ -1,5 +1,8 @@
 // Content manipulation on startup
 var tinymce_plugin_setUpContent = function(editor_id, body, doc) {
     //body.innerHTML = TML2HTML.convert(body.innerHTML);
-}
+};
 
+function pasteWordContentCallback(type, content) {
+    return content;
+}
