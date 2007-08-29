833b3fe6effbbea8cf52708fae0c3c8fd580c041
ArthurClemens <ArthurClemens@a00a5322-12db-0310-a70b-8735589c885e>
2007-08-29 22:49:44 +0000

    Item4510: move inline scripts in WebTopBar to javascript file
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@14663 a00a5322-12db-0310-a70b-8735589c885e

--- PatternSkin/pub/TWiki/PatternSkin/pattern.js
+++ PatternSkin/pub/TWiki/PatternSkin/pattern.js
@@ -14,6 +14,20 @@ var Pattern = {
 var myrules = {
 	'.twikiFormStep h3' : function(el) {
 		Pattern.createTwikiActionFormStepSign(el);
+	},
+	'#jumpFormField' : function(el) {
+		twiki.Form.initBeforeFocusText(el,TEXT_JUMP);
+	},
+	'#quickSearchBox' : function(el) {
+		twiki.Form.initBeforeFocusText(el,TEXT_SEARCH);
+	},
+	'.patternMetaMenu .twikiInputField' : function(el) {
+		el.onfocus = function() {
+			twiki.Form.clearBeforeFocusText(this);
+		}
+		el.onblur = function() {
+			twiki.Form.restoreBeforeFocusText(this);
+		}
 	}
 };
 Behaviour.register(myrules);
