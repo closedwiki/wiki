b9730f28a5fb0f9cd4166848607ca2354e986db9
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2007-09-06 12:04:16 +0000

    Item4030: Added doc for using the calendar from user forms, plus some other tidy-ups for 4.2
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@14754 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/JSCalendarContrib/lib/TWiki/Contrib/JSCalendarContrib.pm
+++ twikiplugins/JSCalendarContrib/lib/TWiki/Contrib/JSCalendarContrib.pm
@@ -1,36 +1,25 @@
+=begin twiki
+
+Read [[%ATTACHURL%/doc/html/reference.html][the Mishoo documentation]] or
+[[%ATTACHURL%][visit the demo page]] for detailed information on using the
+calendar widget.
+
+This package also includes a small Perl module to make using the calendar
+easier from TWiki plugins. This module includes the functions:
+
+=cut
+
 package TWiki::Contrib::JSCalendarContrib;
 
 use strict;
 
-use vars qw( $VERSION $RELEASE );
+require TWiki::Func;    # The plugins API
+
+use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
 $VERSION = '$Rev$';
 $RELEASE = 'TWiki-4';
-
-# See JSCalendarContrib.txt for pod
-sub addHEAD {
-    my $setup = shift;
-    $setup ||= 'calendar-setup';
-    my $style = $TWiki::cfg{JSCalendarContrib}{style} || 'blue';
-    my $lang = $TWiki::cfg{JSCalendarContrib}{lang} || 'en';
-    my $base = '%PUBURLPATH%/%SYSTEMWEB%/JSCalendarContrib';
-    my $head = <<HERE;
-<style type='text/css' media='all'>
-  \@import url('$base/calendar-$style.css');
-  .calendar {z-index:2000;}
-</style>
-<script type='text/javascript' src='$base/calendar.js'></script>
-<script type='text/javascript' src='$base/lang/calendar-$lang.js'></script>
-HERE
-    TWiki::Func::addToHEAD( 'JSCALENDAR_HEAD', $head );
-
-    # Add the setup separately; there might be different setups required
-    # in a single HTML page.
-    $head = <<HERE;
-<script type='text/javascript' src='$base/$setup.js'></script>
-HERE
-    TWiki::Func::addToHEAD( 'JSCALENDAR_HEAD'.$setup, $head );
-}
+$SHORTDESCRIPTION = "[[http://dynarch.com/mishoo/calendar.epl][Mishoo JSCalendar]], packaged for use by plugins, skins and add-ons";
 
 # Max width of different mishoo format components
 my %w = (
@@ -63,7 +52,29 @@ my %w = (
     Y => 4,	# year including the century ( ex. 1979 )
 );
 
-# See JSCalendarContrib.txt for pod
+=begin twiki
+
+---+++ TWiki::Contrib::JSCalendarContrib::renderDateForEdit($name, $value, $format) -> $html
+
+This is the simplest way to use calendars from a plugin.
+   * =$name= is the name of the CGI parameter for the calendar
+     (it should be unique),
+   * =$value= is the current value of the parameter (may be undef)
+   * =$format= is the format to use (optional; the default is set
+     in =configure=). The
+HTML returned will display a date field and a drop-down calendar.
+Example:
+<verbatim>
+use TWiki::Contrib::JSCalendarContrib;
+...
+my $fromDate = TWiki::Contrib::JSCalendarContrib::renderDateForEdit(
+   'from', '1 April 1999');
+my $toDate = TWiki::Contrib::JSCalendarContrib::renderDateForEdit(
+   'to', undef, '%Y');
+</verbatim>
+
+=cut
+
 sub renderDateForEdit {
     my ($name, $value, $format) = @_;
 
@@ -78,7 +89,8 @@ sub renderDateForEdit {
     $wide =~ s/(%(.))/$w{$2} ? ('_' x $w{$2}) : $1/ge;
 
     return CGI::textfield(
-        -name => $name, -id => 'id_'.$name,
+        -name => $name,
+        -id => 'id_'.$name,
         -size => length($wide),
         -default => $value || '')
       . CGI::image_button(
@@ -90,6 +102,74 @@ sub renderDateForEdit {
                   '/JSCalendarContrib/img.gif',
           -alt => 'Calendar',
           -align => 'MIDDLE');
+}
+
+=begin twiki
+
+---+++ TWiki::Contrib::JSCalendarContrib::addHEAD($setup)
+
+This function will automatically add the headers for the calendar to the page
+being rendered. It's intended for use when you want more control over the
+formatting of your calendars than =renderDateForEdit= affords. =$setup= is
+the name of
+the calendar setup module; it can either be omitted, in which case the method
+described in the Mishoo documentation can be used to create calendars, or it
+can be ='twiki'=, in which case a Javascript helper function called
+'showCalendar' is added that simplifies using calendars to set a value in a
+text field. For example, say we wanted to display the date with the calendar
+icon _before_ the text field, using the format =%Y %b %e=
+<verbatim>
+# Add styles and javascript for the calendar
+use TWiki::Contrib::JSCalendarContrib;
+...
+
+sub commonTagsHandler {
+  ....
+  # Enable 'showCalendar'
+  TWiki::Contrib::JSCalendarContrib::addHEAD( 'twiki' );
+
+  my $cal = CGI::image_button(
+      -name => 'img_datefield',
+      -onclick =>
+       "return showCalendar('id_datefield','%Y %b %e')",
+      -src=> TWiki::Func::getPubUrlPath() . '/' .
+             TWiki::Func::getTwikiWebname() .
+             '/JSCalendarContrib/img.gif',
+      -alt => 'Calendar',
+      -align => 'MIDDLE' )
+    . CGI::textfield(
+      { name => 'date', id => "id_datefield" });
+  ....
+}
+</verbatim>
+The first parameter to =showCalendar= is the id of the textfield, and the second parameter is the . See the Mishoo documentation for details of the '$e %B %Y' parameter.
+
+=addHEAD= can be called from =commonTagsHandler= for adding the header to all pages, or from =beforeEditHandler= just for edit pages etc.
+
+=cut
+
+sub addHEAD {
+    my $setup = shift;
+    $setup ||= 'calendar-setup';
+    my $style = $TWiki::cfg{JSCalendarContrib}{style} || 'blue';
+    my $lang = $TWiki::cfg{JSCalendarContrib}{lang} || 'en';
+    my $base = '%PUBURLPATH%/%SYSTEMWEB%/JSCalendarContrib';
+    my $head = <<HERE;
+<style type='text/css' media='all'>
+  \@import url('$base/calendar-$style.css');
+  .calendar {z-index:2000;}
+</style>
+<script type='text/javascript' src='$base/calendar.js'></script>
+<script type='text/javascript' src='$base/lang/calendar-$lang.js'></script>
+HERE
+    TWiki::Func::addToHEAD( 'JSCALENDAR_HEAD', $head );
+
+    # Add the setup separately; there might be different setups required
+    # in a single HTML page.
+    $head = <<HERE;
+<script type='text/javascript' src='$base/$setup.js'></script>
+HERE
+    TWiki::Func::addToHEAD( 'JSCALENDAR_HEAD_'.$setup, $head );
 }
 
 1;
