103d37af3ca444793e9032a32c1c3a044ae47d7f
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2007-09-10 10:21:41 +0000

    Item3839: add build script at the root to help with building several plugins in one session Item4601: remove duplicate installer script (.pl)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN@14812 a00a5322-12db-0310-a70b-8735589c885e

---
+++ twikiplugins/BuildContrib/build.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+unless (scalar(@ARGV)) {
+    print <<DOC;
+Build an extension
+
+When run from the 'twikiplugins' directory of a TWiki checkout, this
+script will build the BuildContrib-enabled extension named in the
+first parameter. The second parameter is the build target for the extension.
+
+Examples:
+perl build.pl ActionTrackerPlugin
+perl build.pl SubscribePlugin upload
+for f in FirstPlugin SecondPlugin; do perl build.pl $f release; done
+DOC
+    exit 1;
+}
+
+my $extension = shift(@ARGV);
+
+my $extdir = "Contrib";
+if ($extension =~ /Plugin$/) {
+    $extdir = "Plugins";
+}
+
+my $scriptDir = "$extension/lib/TWiki/$extdir/$extension";
+unless (-e "$scriptDir/build.pl") {
+    die "$script not found";
+}
+
+use Cwd;
+chdir($scriptDir);
+do 'build.pl';
