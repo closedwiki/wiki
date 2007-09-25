9eec67bd67b950f25d64cc8e1d243a5d321f1826
ArthurClemens <ArthurClemens@a00a5322-12db-0310-a70b-8735589c885e>
2007-09-25 18:40:42 +0000

    Item4723: fix for headers written by TwistyPlugin not visible in TOC (make sure twisty content starts with a newline)
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@15057 a00a5322-12db-0310-a70b-8735589c885e

--- TwistyPlugin/lib/TWiki/Plugins/TwistyPlugin.pm
+++ TwistyPlugin/lib/TWiki/Plugins/TwistyPlugin.pm
@@ -44,7 +44,7 @@ $VERSION = '$Rev$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '1.4.3';
+$RELEASE = '1.4.4';
 
 $pluginName = 'TwistyPlugin';
 
@@ -393,7 +393,7 @@ sub _readCookie {
 
 sub _wrapInButtonHtml {
     my ($text) = @_;
-    return _wrapInDivHideIfNoJavascripOpen() . $text
+    return _wrapInDivHideIfNoJavascripOpen() . "\n" . $text
       . _wrapInHideDivIfNoJavascripClose();
 }
 
