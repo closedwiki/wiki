7d99baad669b9f8ee60347f5accefe08882558a0
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2007-09-27 08:56:56 +0000

    Item4713: improved the doc, as a result of testing
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@15064 a00a5322-12db-0310-a70b-8735589c885e

--- NativeSearchContrib/tools/native_search/test.pl
+++ NativeSearchContrib/tools/native_search/test.pl
@@ -5,6 +5,11 @@
 # perl test.pl -i -l NativeTWikiSearch test.pl Makefile.PL NativeTWikiSearch.xs
 #
 use NativeTWikiSearch;
-die "I need parameters, like grep!" unless scalar(@ARGV);
+die <<MOAN unless scalar(@ARGV);
+I need parameters, like grep!
+Try:
+perl test.pl -i -l NativeTWikiSearch test.pl Makefile.PL NativeTWikiSearch.xs
+If it returns at least 3 filenames and doesn't crash, it worked.
+MOAN
 my $result = NativeTWikiSearch::cgrep(\@ARGV);
 print "RESULT\n".join("\n", @$result)."\n";
