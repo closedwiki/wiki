349abb3f6ec34215d64381df162da85e0cf2ba94
MarkusHesse <MarkusHesse@a00a5322-12db-0310-a70b-8735589c885e>
2007-11-01 14:54:53 +0000

    Item4919: Initial check in of KinoSearch
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@15459 a00a5322-12db-0310-a70b-8735589c885e

--- test/unit/SearchEngineKinoSearchAddOn/SearchEngineKinoSearchAddOnSuite.pm
+++
@@ -1,8 +0,0 @@
-package SearchEngineKinoSearchAddOnSuite;
-use base qw(Unit::TestSuite);
-
-sub include_tests {
-    qw( KinoSearchTests IndexTests DocTests XlsTests PdfTests TxtTests HtmlTests);
-};
-
-1;
