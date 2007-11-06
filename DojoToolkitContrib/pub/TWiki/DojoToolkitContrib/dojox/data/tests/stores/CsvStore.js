fde2255f0e39bab8bbf7545a6a477cb256ddcd0e
SvenDowideit <SvenDowideit@a00a5322-12db-0310-a70b-8735589c885e>
2007-11-06 01:57:15 +0000

    Item4930: dojo toolkit when 1.0 - update the contrib
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@15498 a00a5322-12db-0310-a70b-8735589c885e

--- DojoToolkitContrib/pub/TWiki/DojoToolkitContrib/dojox/data/tests/stores/CsvStore.js
+++ DojoToolkitContrib/pub/TWiki/DojoToolkitContrib/dojox/data/tests/stores/CsvStore.js
@@ -29,6 +29,17 @@ dojox.data.tests.stores.CsvStore.getDatasource = function(filepath){
 				csvData += '"The Sequel to ""Dances With Wolves.""", 1982, Ridley Scott\n';
 				csvData += '"Caine Mutiny, The", 1954, "Dymtryk ""the King"", Edward"\n';
 				break;
+			case "stores/movies2.csv":
+				var csvData = "";
+				csvData += "Title, Year, Producer\n";
+				csvData += "City of God, 2002, Katia Lund\n";
+				csvData += "Rain,\"\", Christine Jeffs\n";
+				csvData += "2001: A Space Odyssey, 1968, Stanley Kubrick\n";
+				csvData += '"This is a ""fake"" movie title", 1957, Sidney Lumet\n';
+				csvData += "Alien, 1979   , Ridley Scott\n";
+				csvData += '"The Sequel to ""Dances With Wolves.""", 1982, Ridley Scott\n';
+				csvData += '"Caine Mutiny, The", 1954, "Dymtryk ""the King"", Edward"\n';
+				break;
 			case "stores/books.csv":
 				var csvData = "";
 				csvData += "Title, Author\n";
@@ -96,7 +107,26 @@ doh.register("dojox.data.tests.stores.CsvStore",
 			var csvStore = new dojox.data.CsvStore(args);
 			
 			var d = new doh.Deferred();
-            function completedAll(items){
+		function completedAll(items){
+				t.assertTrue((items.length === 7));
+				d.callback(true);
+			}
+
+			//Get everything...
+			csvStore.fetch({ onComplete: completedAll, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			return d; //Object
+		},
+		function testReadAPI_fetch_all_withEmptyStringField(t){
+			//	summary: 
+			//		Simple test of a basic fetch on CsvStore.
+			//	description:
+			//		Simple test of a basic fetch on CsvStore.
+			
+			var args = dojox.data.tests.stores.CsvStore.getDatasource("stores/movies2.csv");
+			var csvStore = new dojox.data.CsvStore(args);
+			
+			var d = new doh.Deferred();
+		function completedAll(items){
 				t.assertTrue((items.length === 7));
 				d.callback(true);
 			}
@@ -125,7 +155,6 @@ doh.register("dojox.data.tests.stores.CsvStore",
 							});
 			return d; //Object
 		},
-
 		function testReadAPI_fetch_Multiple(t){
 			//	summary: 
 			//		Simple test of a basic fetch on CsvStore of a single item.
@@ -210,9 +239,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 								onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)
 							});
 			
-            csvStore.fetchItemByIdentity({identity: "6", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
-
-
+			csvStore.fetchItemByIdentity({identity: "6", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 			return d; //Object
 		},
 		function testReadAPI_fetch_all_streaming(t){
@@ -284,7 +311,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 
 			function dumpFourthFetch(items, request){
 				t.is(5, items.length);
-                request.start = 9;
+				request.start = 9;
 				request.count = 100;
 				request.onComplete = dumpFifthFetch;
 				csvStore.fetch(request);
@@ -380,7 +407,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.is('Caine Mutiny, The', csvStore.getValue(item,"Title"));
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "6", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "6", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 			return d;
 		},	
 		function testReadAPI_getValue_2(t){
@@ -418,7 +445,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.is("Alien", csvStore.getValue(item,"Title"));
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "4", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "4", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 			return d;
 		},	
 		function testReadAPI_getValue_4(t){
@@ -437,7 +464,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.is("Stanley Kubrick", csvStore.getValue(item,"Producer"));
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "2", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "2", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 			return d;
 		},	
 
@@ -459,7 +486,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.is("Rain", names[0]);
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 			return d;
 		},
 		function testIdentityAPI_fetchItemByIdentity(t){
@@ -476,7 +503,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.assertTrue(item !== null);
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 			return d;
 		},
 
@@ -494,7 +521,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.assertTrue(item === null);
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "7", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "7", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 			return d;
 		},
 		function testIdentityAPI_fetchItemByIdentity_bad2(t){
@@ -510,7 +537,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.assertTrue(item === null);
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "-1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "-1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 			return d;
 		},
 		function testIdentityAPI_fetchItemByIdentity_bad3(t){
@@ -526,7 +553,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.assertTrue(item === null);
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "999999", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "999999", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 			return d;
 		},
 		function testIdentityAPI_getIdentity(t){
@@ -571,7 +598,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.assertEqual(null, csvStore.getIdentityAttributes(item)); 
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 		   	return d;
 		},
 		function testReadAPI_isItem(t){
@@ -592,7 +619,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.assertTrue(!csvStore.isItem(["not an item"]));
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 		   	return d;
 		},
 		function testReadAPI_hasAttribute(t){
@@ -623,7 +650,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.assertTrue(passed);
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 		   	return d;
 		},
 		function testReadAPI_containsValue(t){
@@ -655,7 +682,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.assertTrue(passed);
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "4", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "4", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 		   	return d;
 		},
 		function testReadAPI_getAttributes(t){
@@ -679,7 +706,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				}
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "4", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "4", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 		   	return d;
 		},
 
@@ -704,7 +731,7 @@ doh.register("dojox.data.tests.stores.CsvStore",
 				t.assertTrue(attributes[1] === "Producer");
 				d.callback(true);	
 			}
-            csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
+			csvStore.fetchItemByIdentity({identity: "1", onItem: onItem, onError: dojo.partial(dojox.data.tests.stores.CsvStore.error, t, d)});
 		   	return d;
 		},
 
--- DojoToolkitContrib/pub/TWiki/DojoToolkitContrib/dojox/string/tests/string.js
+++ DojoToolkitContrib/pub/TWiki/DojoToolkitContrib/dojox/string/tests/string.js
@@ -4,6 +4,7 @@ dojo.provide("dojox.string.tests.string");
 
 try{
 	dojo.require("dojox.string.tests.Builder");
+	dojo.require("dojox.string.tests.sprintf");
 } catch(e){ }
 
 }
