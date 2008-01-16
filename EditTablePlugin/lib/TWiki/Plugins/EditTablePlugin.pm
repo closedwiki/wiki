4bad10adcdcfc61186df624c2f92aac363556ab3
KennethLavrsen <KennethLavrsen@a00a5322-12db-0310-a70b-8735589c885e>
2008-01-16 01:10:41 +0000

    Item4902: Merge changes to MAIN
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@16240 a00a5322-12db-0310-a70b-8735589c885e

--- EditTablePlugin/lib/TWiki/Plugins/EditTablePlugin.pm
+++ EditTablePlugin/lib/TWiki/Plugins/EditTablePlugin.pm
@@ -147,7 +147,7 @@ sub addViewModeHeadersToHead {
 
     my $header = <<'EOF';
 <style type="text/css" media="all">
-@import url("%PUBURL%/%SYSTEMWEB%/EditTablePlugin/edittable.css");
+@import url("%PUBURL%/%TWIKIWEB%/EditTablePlugin/edittable.css");
 </style>
 EOF
     TWiki::Func::addToHEAD( 'EDITTABLEPLUGIN', $header );
@@ -179,9 +179,9 @@ sub addEditModeHeadersToHead {
       . $assetUrl . '" />';
     $header .= <<'EOF';
 <style type="text/css" media="all">
-@import url("%PUBURL%/%SYSTEMWEB%/EditTablePlugin/edittable.css");
+@import url("%PUBURL%/%TWIKIWEB%/EditTablePlugin/edittable.css");
 </style>
-<script type="text/javascript" src="%PUBURL%/%SYSTEMWEB%/EditTablePlugin/edittable.js"></script>
+<script type="text/javascript" src="%PUBURL%/%TWIKIWEB%/EditTablePlugin/edittable.js"></script>
 EOF
 
     TWiki::Func::addToHEAD( 'EDITTABLEPLUGIN', $header );
