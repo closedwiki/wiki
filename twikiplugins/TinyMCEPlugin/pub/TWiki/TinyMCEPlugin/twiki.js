abe8fadeb07a59debaebabb94e6531fadecf785a
CrawfordCurrie <CrawfordCurrie@a00a5322-12db-0310-a70b-8735589c885e>
2008-01-21 12:35:39 +0000

    Item5254: non-fatal JS error fixed
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x02@16265 a00a5322-12db-0310-a70b-8735589c885e

--- twikiplugins/TinyMCEPlugin/pub/TWiki/TinyMCEPlugin/twiki.js
+++ twikiplugins/TinyMCEPlugin/pub/TWiki/TinyMCEPlugin/twiki.js
@@ -3,3 +3,4 @@ function setEditBoxHeight(inRowCount){}
 function initTextAreaStyles(){var iframe=document.getElementById(IFRAME_ID);if(iframe==null)return;var node=iframe.parentNode;var counter=0;while(node!=document){if(node.nodeName=='TABLE'){node.style.height='auto';var selectboxes=node.getElementsByTagName('SELECT');var i,ilen=selectboxes.length;for(i=0;i<ilen;++i){selectboxes[i].style.marginLeft=selectboxes[i].style.marginRight='2px';selectboxes[i].style.fontSize='94%';}
 break;}
 node=node.parentNode;}}
+function handleKeyDown(e){if(!e)e=window.event;var code;if(e.keyCode)code=e.keyCode;if(code==27)return false;return true;}
