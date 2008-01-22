8045ccd34e7b24474b779243e83affb0ecc9f234
KennethLavrsen <KennethLavrsen@a00a5322-12db-0310-a70b-8735589c885e>
2008-01-22 00:21:41 +0000

    Item5243: Preparing 4.2.0 release
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x02@16270 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki.pm
+++ lib/TWiki.pm
@@ -166,7 +166,7 @@ BEGIN {
     # DO NOT CHANGE THE FORMAT OF $VERSION
     # Automatically expanded on checkin of this module
     $VERSION = '$Date$ $Rev$ ';
-    $RELEASE = 'TWiki-4.2.0-rc2';
+    $RELEASE = 'TWiki-4.2.0';
     $VERSION =~ s/^.*?\((.*)\).*: (\d+) .*?$/$RELEASE, $1, build $2/;
 
     # Default handlers for different %TAGS%
