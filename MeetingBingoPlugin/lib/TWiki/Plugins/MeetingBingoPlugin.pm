a422ac195b27af118fe15c9f2039868acf9ca7b2
KennethLavrsen <KennethLavrsen@a00a5322-12db-0310-a70b-8735589c885e>
2008-01-30 05:46:12 +0000

    Item5300: MeetingBingoPlugin has less than flawless spelling in mission critical data giving a poor Out of the Box experience
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@16308 a00a5322-12db-0310-a70b-8735589c885e

--- MeetingBingoPlugin/lib/TWiki/Plugins/MeetingBingoPlugin.pm
+++ MeetingBingoPlugin/lib/TWiki/Plugins/MeetingBingoPlugin.pm
@@ -45,7 +45,7 @@ $VERSION = '$Rev: 12445$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '1.0';
+$RELEASE = '1.1';
 
 # Short description of this plugin
 # One line description, is shown in the %TWIKIWEB%.TextFormattingRules topic:
