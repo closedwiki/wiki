3429b1a25d47697f3e5ac617770dec2a07aaca8d
StephaneLenclud <StephaneLenclud@a00a5322-12db-0310-a70b-8735589c885e>
2008-02-13 22:56:34 +0000

    Item5364: AnyWikiDrawPlugin trying to make it work with BuildContrib.
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/MAIN/twikiplugins@16396 a00a5322-12db-0310-a70b-8735589c885e

--- AnyWikiDrawPlugin/lib/TWiki/Plugins/AnyWikiDrawPlugin/build.pl
+++ AnyWikiDrawPlugin/lib/TWiki/Plugins/AnyWikiDrawPlugin/build.pl
@@ -1,41 +1,52 @@
 #!/usr/bin/perl -w
 #
-# Build file for TWiki Draw Plugin
+# Example build class. Copy this file to the equivalent place in your
+# plugin or contrib and edit.
 #
+# Read the comments at the top of lib/TWiki/Contrib/Build.pm for
+# details of how the build process works, and what files you
+# have to provide and where.
+#
+# Requires the environment variable TWIKI_LIBS (a colon-separated path
+# list) to be set to point at the build system and any required dependencies.
+# Usage: ./build.pl [-n] [-v] [target]
+# where [target] is the optional build target (build, test,
+# install, release, uninstall), test is the default.`
+# Two command-line options are supported:
+# -n Don't actually do anything, just print commands
+# -v Be verbose
+#
+
 # Standard preamble
 BEGIN {
-  foreach my $pc (split(/:/, $ENV{TWIKI_LIBS})) {
-    unshift @INC, $pc;
-  }
+    unshift @INC, split( /:/, $ENV{TWIKI_LIBS} );
 }
 
 use TWiki::Contrib::Build;
 
 # Declare our build package
-package AnyWikiDrawPluginBuild;
-
-@AnyWikiDrawPluginBuild::ISA = ( "TWiki::Contrib::Build" );
+package BuildBuild;
+use base qw( TWiki::Contrib::Build );
 
 sub new {
     my $class = shift;
     return bless( $class->SUPER::new( "AnyWikiDrawPlugin" ), $class );
 }
 
-# Override the build target to build the java code
+# Example: Override the build target
 sub target_build {
-  my $this = shift;
+    my $this = shift;
 
-  $this->SUPER::target_build();
+    $this->SUPER::target_build();
 
-  $this->pushd($this->{basedir}."/lib/TWiki/Plugins/AnyWikiDrawPlugin");
-#  $this->sys_action("ant -f build.xml build");
-  $this->popd();
+    # Do other build stuff here
 }
 
+package main;
+
 # Create the build object
-$build = new AnyWikiDrawPluginBuild();
+$build = new BuildBuild();
 
 # Build the target on the command line, or the default target
-
 $build->build($build->{target});
 
