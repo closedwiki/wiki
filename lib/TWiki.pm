99d59c0f44b372fbb02e42c2acefc53db32944a7
SvenDowideit <SvenDowideit@a00a5322-12db-0310-a70b-8735589c885e>
2008-04-14 03:06:46 +0000

    Item5288: META tag doesn't call expandStandardEscapes()
    git-svn-id: http://develop.twiki.org/svn/twiki/branches/TWikiRelease04x02@16658 a00a5322-12db-0310-a70b-8735589c885e

--- lib/TWiki.pm
+++ lib/TWiki.pm
@@ -3787,23 +3787,24 @@ sub META {
     return '' unless $meta;
 
     my $option = $params->{_DEFAULT} || '';
+    my $result = '';
 
     if( $option eq 'form' ) {
         # META:FORM and META:FIELD
-        return $meta->renderFormForDisplay( $this->templates );
+        $result = $meta->renderFormForDisplay( $this->templates );
     } elsif ( $option eq 'formfield' ) {
         # a formfield from within topic text
-        return $meta->renderFormFieldForDisplay( $params );
+        $result = $meta->renderFormFieldForDisplay( $params );
     } elsif( $option eq 'attachments' ) {
         # renders attachment tables
-        return $this->attach->renderMetaData( $web, $topic, $meta, $params );
+        $result = $this->attach->renderMetaData( $web, $topic, $meta, $params );
     } elsif( $option eq 'moved' ) {
-        return $this->renderer->renderMoved( $web, $topic, $meta, $params );
+        $result = $this->renderer->renderMoved( $web, $topic, $meta, $params );
     } elsif( $option eq 'parent' ) {
-        return $this->renderer->renderParent( $web, $topic, $meta, $params );
+        $result = $this->renderer->renderParent( $web, $topic, $meta, $params );
     }
 
-    return '';
+    return expandStandardEscapes($result);
 }
 
 # Remove NOP tag in template topics but show content. Used in template
