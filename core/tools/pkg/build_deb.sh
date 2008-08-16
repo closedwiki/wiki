f7e14db93d685815721799d728ec39b9edb13ee2
SvenDowideit <SvenDowideit@a00a5322-12db-0310-a70b-8735589c885e>
2008-08-16 04:50:09 +0000

    Item5614: getting closer to a nicely working debian 4.2.2
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@17422 a00a5322-12db-0310-a70b-8735589c885e

--- core/tools/pkg/build_deb.sh
+++ core/tools/pkg/build_deb.sh
@@ -4,21 +4,21 @@ if [ -e /tmp/build_deb ]; then
 	echo '/tmp/build_deb already exists, please move aside'
 	exit -1;
 fi
-if [ ! -e TWiki-4.2.1.tgz ]; then
-	echo 'need TWiki-4.2.1.tgz file to build'
+if [ ! -e TWiki-4.2.2.tgz ]; then
+	echo 'need TWiki-4.2.2.tgz file to build'
 	exit -1;
 fi
 
 mkdir /tmp/build_deb
-cp TWiki-4.2.1.tgz /tmp/build_deb/twiki_4.2.1.orig.tar.gz
+cp TWiki-4.2.2.tgz /tmp/build_deb/twiki_4.2.2.orig.tar.gz
 
-mkdir /tmp/build_deb/twiki-4.2.1 
+mkdir /tmp/build_deb/twiki-4.2.2 
 
-cp -r debian /tmp/build_deb/twiki-4.2.1
-cd /tmp/build_deb/twiki-4.2.1
+cp -r debian /tmp/build_deb/twiki-4.2.2
+cd /tmp/build_deb/twiki-4.2.2
 find . -name .svn -exec rm -rf '{}' \;
 
-tar zxvf /tmp/build_deb/twiki_4.2.1.orig.tar.gz
+tar zxvf /tmp/build_deb/twiki_4.2.2.orig.tar.gz
 
 #patch it
 #fakeroot debian/rules patch
--- core/tools/pkg/debian/mod_perl_startup.pl
+++ core/tools/pkg/debian/mod_perl_startup.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl
+
 $ENV{MOD_PERL} =~ /mod_perl/ or die "mod_perl_startup called, but mod_perl not used!";
 use lib qw( /usr/lib/cgi-bin/twiki );
 require 'setlib.cfg';
