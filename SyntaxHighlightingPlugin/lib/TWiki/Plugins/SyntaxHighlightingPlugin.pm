97e93d2bf655ea1ebd29bba4d062a6f115e38209
AndrewRJones <AndrewRJones@a00a5322-12db-0310-a70b-8735589c885e>
2008-09-11 16:20:57 +0000

    Item5995: Update SyntaxHighlightingPlugin for 4.2
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@17497 a00a5322-12db-0310-a70b-8735589c885e

--- SyntaxHighlightingPlugin/lib/TWiki/Plugins/SyntaxHighlightingPlugin.pm
+++ SyntaxHighlightingPlugin/lib/TWiki/Plugins/SyntaxHighlightingPlugin.pm
@@ -1,6 +1,7 @@
 #
 # TWiki WikiClone ($wikiversion has version info)
 #
+# Copyright (C) 2008 Andrew Jones, andrewjones86@gmail.com
 # Copyright (C) 2000-2001 Andrea Sterbini, a.sterbini@flashnet.it
 # Copyright (C) 2001 Peter Thoeny, Peter@Thoeny.com
 #
@@ -18,57 +19,39 @@
 # =========================
 #
 # This is the Syntax Highlighting TWiki plugin.
-# written by
+# originally written by
 # Nicolas Tisserand (tisser_n@epita.fr), Nicolas Burrus (burrus_n@epita.fr)
 # and Perceval Anichini (anichi_p@epita.fr)
+# Current version by Andrew Jones (andrewjones86@gmail.com)
 # 
 # It uses enscript as syntax highlighter.
 # 
-# Use it in your twiki text by writing %begin language% ... %end%
+# Use it in your twiki text by writing %CODE{"language"}% ... %ENDCODE%
 # with language = ada asm awk bash c changelog c++ csh delphi diff diffs diffu elisp fortran fortran_pp haskell html idl inf java javascript
 # ksh m4 mail makefile maple matlab modula_2 nested nroff objc outline pascal perl postscript python rfc scheme sh skill sql states synopsys
 # tcl tcsh tex vba verilog vhdl vrml wmlscript zsh 
 
 package TWiki::Plugins::SyntaxHighlightingPlugin;
+use strict;
 
-use IPC::Open2;
+use IPC::Run qw( run ) ;
 
-use vars qw(
-	    $web $topic $user $installWeb $VERSION $RELEASE $debug
-	    );
+use vars qw(    $VERSION
+                $RELEASE
+                $NO_PREFS_IN_TOPIC
+                $SHORTDESCRIPTION
+                $pluginName
+                %langs
+                );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
-$VERSION = '$Rev$';
+$VERSION = '1.000';
+$SHORTDESCRIPTION = 'Highlights code fragments for many languages using ==enscript==';
+$RELEASE = 'TWiki-4.2';
+$NO_PREFS_IN_TOPIC = 1;
 
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$pluginName = 'SyntaxHighlightingPlugin';
 
-
-sub pipeThru
-{
-    my $out;
-
-    $pid = open2( \*Reader, \*Writer, $_[0]);
-
-    print Writer $_[1];
-    close(Writer);
-
-    while (<Reader>)
-    {
-	$out .= $_;
-    }
-    close (Reader);
-
-    return $out;
-}
-
-sub highlight
-{
-    my %langs = (
+%langs = (
 		 "ada" => "ada",
 		 "asm" => "asm",
 		 "awk" => "awk",
@@ -123,59 +106,115 @@ sub highlight
 		 "vrml" => "vrml",
 		 "wmlscript" => "wmlscript",
 		 "zsh" => "zsh"
-		 );
-    
-    return "<font color=\"red\"> Syntax Highlighting: error: $_[0]: undefined language </font>" unless exists $langs{lc($_[0])};
-    
-    my ($lang, $text, $nb_option, $nb_start) = @_;
-    
-    my $highlighted = pipeThru("enscript --color --language=html --highlight=$langs{lc($lang)} -o - -q", $text);
-    
-    if ($highlighted =~ s/.*\<PRE\>\n(.*?)\n?\<\/PRE\>.*/$1/os)
-    {
-	if ($nb_option eq " numbered")
-	{
-	    my $line = ($nb_start eq "") ? 1 : $nb_start;
-	    $highlighted =~ s/(^.*)/sprintf("<b><font color=\"#000000\">%5d<\/font><\/b>\t%s", $line++, $1)/mgeo
-	}
-	return "<table width=\"100%\" border=\"0\" cellpadding=\"5\" cellspacing=\"0\"><tr><td bgcolor=\"#FFFFFF\"><pre>".$highlighted."</pre><\/td><\/tr><\/table>";
-    }	
-    else
-    {
-	return "<font color=\"red\"> Syntax Highlighting: internal error  </font>";
-    }
-}
+    );
 
 # =========================
 
-sub initPlugin
-{
-    ( $topic, $web, $user, $installWeb ) = @_;
+sub initPlugin {
+    my( $topic, $web, $user, $installWeb ) = @_;
     
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        &TWiki::Func::writeWarning( "Version mismatch between SyntaxHighlightingPlugin and Plugins.pm" );
+    if( $TWiki::Plugins::VERSION < 1.026 ) {
+        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
         return 0;
     }
     
-    # Get plugin debug flag
-    $debug = &TWiki::Func::getPreferencesFlag( "SYNTAXHIGHLIGHTINGPLUGIN_DEBUG" );
-    
     # Plugin correctly initialized
-    &TWiki::Func::writeDebug( "- TWiki::Plugins::SyntaxHighlightingPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
+    _Debug("initPlugin( $web.$topic ) is OK");
+    
     return 1;
 }
 
-# =========================
+sub commonTagsHandler {
 
-sub startRenderingHandler
-{
-    &TWiki::Func::writeDebug( "- SyntaxHighlightingPlugin::startRenderingHandler( $_[1] )" ) if $debug;
+    $_[0] =~ s/%CODE{(.*?)}%\s*(.*?)%ENDCODE%/&_handleTag/egs;
 
-    # matches %begin [numbered][:n] language% ... %end%
-    $_[0] =~ s/^%begin( numbered)?(?:\:(\d+))? ([^%]*?)%\n(.*?)^%end%$/highlight($3, $4, $1, $2)/megos;
 }
 
 # =========================
+
+sub _handleTag {
+    
+    my %params = TWiki::Func::extractParameters($1);
+    my $lang = lc$params{lang} || lc$params{_DEFAULT}; # language
+    my $num = lc$params{num} || lc$params{number} || lc$params{numbered}; # start line number
+    my $code = $2; # code to highlight
+    
+    unless( _definedLang( $lang ) ){
+        return _returnError("Language is either undefined or unsupported. Check %TWIKIWEB%.$pluginName for more information.");
+    }
+    
+    my $highlighted = _highlight($code, $lang);
+    
+    if ($highlighted =~ s/.*\<PRE\>\n(.*?)\n?\<\/PRE\>.*/$1/os){
+        if ($num) {
+            my $line = $num;
+            $highlighted =~ s/(^.*)/sprintf("<b><font color=\"#000000\">%5d<\/font><\/b>\t%s", $line++, $1)/mgeo
+        }
+        my $out = "<!-- !$pluginName -->\n";
+        $out .= "<pre class='syntaxHighlightingPlugin'>$highlighted</pre>";
+        $out .= "\n<!-- end !$pluginName -->";
+        return $out;
+    } else {
+        _Warn('Error with enscript while highlighting. Check its installed correctly and the path is correct');
+        _returnError('Internal error. Notify you administrator at %WIKIWEBMASTER%.');
+    }
+}
+
+# =========================
+
+# runs enscript to highlight the code
+sub _highlight {
+    my( $code, $lang ) = @_;
+    
+    my $enscript = $TWiki::cfg{Plugins}{$pluginName}{EnscriptPath} || 'enscript';
+    
+    my @cmd;
+    push @cmd, $enscript;
+    push @cmd, qw( --color --language=html --output - --silent) ;
+    push @cmd, "--highlight=$langs{lc($lang)}" ;
+    my $out = '' ;
+    run \@cmd, \$code, \$out ;
+
+    return $out;
+}
+
+# checks the language is supported
+sub _definedLang {
+    
+    my( $lang ) = @_;
+    
+    if( exists $langs{lc($_[0])} ){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+# =========================
+
+# output an error to screen
+sub _returnError {
+    my( $text ) = @_;
+
+    my $out = '<span class="twikiAlert">';
+    $out .= "%TWIKIWEB%.$pluginName - $text";
+    $out .= '</span>';
+
+    return $out;
+}
+
+# =========================
+
+sub _Debug {
+    my $text = shift;
+    my $debug = $TWiki::cfg{Plugins}{$pluginName}{Debug} || 0;
+    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}: $text" ) if $debug;
+}
+
+sub _Warn {
+    my $text = shift;
+    TWiki::Func::writeWarning( "- TWiki::Plugins::${pluginName}: $text" );
+}
 
 1;
