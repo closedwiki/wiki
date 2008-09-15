395e315d472a06dffba0b64e1ae9a7141a7a2550
OliverKrueger <OliverKrueger@a00a5322-12db-0310-a70b-8735589c885e>
2008-09-15 16:40:01 +0000

    Item5996: Adding EXPAND feature. Removing noise from debug.txt
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@17515 a00a5322-12db-0310-a70b-8735589c885e

--- NewUserPlugin/lib/TWiki/Plugins/NewUserPlugin.pm
+++ NewUserPlugin/lib/TWiki/Plugins/NewUserPlugin.pm
@@ -26,7 +26,7 @@ use constant DEBUG => 0; # toggle me
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
 $VERSION = '$Rev$';
-$RELEASE = 'v1.04';
+$RELEASE = 'v1.05';
 $SHORTDESCRIPTION = 'Create a user topic if it does not exist yet';
 $NO_PREFS_IN_TOPIC = 1;
 
@@ -51,7 +51,7 @@ sub initPlugin {
 }
 
 ###############################################################################
-# unfortunately we can't use the initializeUserHandler as TWiki is not 
+# unfortunately we can't use the initializeUserHandler as TWiki is not
 # fully initialized then. even the beforeCommonTagsHandler get's called in
 # a half-init state in the middle of TWiki::new. so we have to wait for
 # the TWiki object to be fully initialized, i.e. its i18n subsystem
@@ -59,16 +59,40 @@ sub beforeCommonTagsHandler {
   return if !defined($TWiki::Plugins::SESSION->{i18n}) || $done;
   $done = 1;
 
-  writeDebug("called beforeCommonTagsHandler");
+  writeDebug("called beforeCommonTagsHandler") if DEBUG;
 
   my $wikiUserName = TWiki::Func::getWikiUserName();
   my $mainWeb = TWiki::Func::getMainWebname();
 
   return if TWiki::Func::topicExists($mainWeb, $wikiUserName);
 
-  writeDebug("NO home topic found for $wikiUserName");
+  writeDebug("NO home topic found for $wikiUserName") if DEBUG;
   createUserTopic($wikiUserName)
 }
+
+###############################################################################
+# decodes escape codes and expands VARs
+sub expandVariables {
+  my $text     = $_[0];
+  my $wikiName = $_[1];
+  my $mainWeb  = $_[2];
+
+  # remove surrounding quotation marks
+  if ( $text =~ m/^\"(.*)\"$/ ) {
+    $text = $1;
+  }
+
+  # decode $dollar and $percnt
+  $text =~ s/\$dollar/\$/g;
+  $text =~ s/\$percnt/%/g;
+
+  # expand vars
+  $text = TWiki::Func::expandCommonVariables($text, $wikiName, $mainWeb, undef);
+
+  return $text;
+}
+
+
 
 ###############################################################################
 # creates a user topic for the given wikiUserName
@@ -77,12 +101,12 @@ sub createUserTopic {
 
   my $twikiWeb = TWiki::Func::getTwikiWebname();
   my $mainWeb = TWiki::Func::getMainWebname();
-  my $newUserTemplate = 
+  my $newUserTemplate =
     TWiki::Func::getPreferencesValue('NEWUSERTEMPLATE') || 'NewLdapUserTemplate';
   my $tmplTopic;
   my $tmplWeb;
 
-  # search the NEWUSERTEMPLATE 
+  # search the NEWUSERTEMPLATE
   $newUserTemplate =~ s/^\s+//go;
   $newUserTemplate =~ s/\s+$//go;
   $newUserTemplate =~ s/\%TWIKIWEB\%/$twikiWeb/g;
@@ -90,13 +114,13 @@ sub createUserTopic {
   $newUserTemplate =~ s/\%MAINWEB\%/$mainWeb/g;
 
   # in Main
-  ($tmplWeb, $tmplTopic) = 
+  ($tmplWeb, $tmplTopic) =
     TWiki::Func::normalizeWebTopicName($mainWeb, $newUserTemplate);
 
   unless (TWiki::Func::topicExists($tmplWeb, $tmplTopic)) {
 
     # in TWiki
-    ($tmplWeb, $tmplTopic) = 
+    ($tmplWeb, $tmplTopic) =
       TWiki::Func::normalizeWebTopicName($twikiWeb, $newUserTemplate);
 
     unless (TWiki::Func::topicExists($tmplWeb, $tmplTopic)) {
@@ -105,7 +129,7 @@ sub createUserTopic {
     }
   }
 
-  writeDebug("newusertemplate = $tmplWeb.$tmplTopic");
+  writeDebug("newusertemplate = $tmplWeb.$tmplTopic") if DEBUG;
 
   # read the template
   my $text = TWiki::Func::readTopicText($tmplWeb, $tmplTopic);
@@ -122,11 +146,21 @@ sub createUserTopic {
   $text =~ s/\%USERNAME\%/$loginName/go;
   $text =~ s/\%WIKIUSERNAME\%/$wikiUserName/go;
 
-  writeDebug("saving new home topic $mainWeb.$wikiName");
+  writeDebug("saving new home topic $mainWeb.$wikiName") if DEBUG;
   my $errorMsg = TWiki::Func::saveTopicText($mainWeb, $wikiName, $text);
 
   if ($errorMsg) {
     writeWarning("error during save of $tmplWeb.$tmplTopic: $errorMsg");
+  } elsif ( $text =~ m/\%EXPAND\{/ ) {
+
+    # expanding VARs in a second phase, after the topic file was created (to get correct $meta objects)
+    $text =~ s/\%EXPAND\{(.*?)\}\%/&expandVariables($1, $wikiName, $mainWeb)/ge;
+
+    writeDebug("expanding vars in new home topic $mainWeb.$wikiName") if DEBUG;
+    my $errorExpandMsg = TWiki::Func::saveTopicText($mainWeb, $wikiName, $text);
+    if ($errorExpandMsg) {
+      writeWarning("error during save of var expanded version of $mainWeb.$wikiName: $errorExpandMsg");
+    }
   }
 }
 
