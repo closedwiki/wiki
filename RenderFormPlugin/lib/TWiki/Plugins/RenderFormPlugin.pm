aa896227feab43c7fe459766f55c594c9c8085ef
DanielRohde <DanielRohde@a00a5322-12db-0310-a70b-8735589c885e>
2008-10-04 18:35:59 +0000

    Item6047: initial import of RenderFormPlugin
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@17571 a00a5322-12db-0310-a70b-8735589c885e

---
+++ RenderFormPlugin/lib/TWiki/Plugins/RenderFormPlugin.pm
@@ -0,0 +1,38 @@
+package TWiki::Plugins::RenderFormPlugin;
+
+use strict;
+
+use vars qw( $VERSION $RELEASE $debug $pluginName );
+
+$VERSION = '$Rev$';
+
+$RELEASE = 'Dakar';
+
+$pluginName = 'RenderFormPlugin';
+
+sub initPlugin {
+    my( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $TWiki::Plugins::VERSION < 1.026 ) {
+        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+        return 0;
+    }
+
+    # Plugin correctly initialized
+    return 1;
+}
+
+sub commonTagsHandler {
+    # do not uncomment, use $_[0], $_[1]... instead
+    ### my ( $text, $topic, $web ) = @_;
+
+    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+
+    # do custom extension rule, like for example:
+    use TWiki::Plugins::RenderFormPlugin::Core;
+    eval {
+        $_[0] =~ s/%RENDERFORM{(.*)}%/TWiki::Plugins::RenderFormPlugin::Core::render($1,$_[1],$_[2])/ge;
+    };
+    TWiki::Func::writeWarning($@) if $@;
+}
