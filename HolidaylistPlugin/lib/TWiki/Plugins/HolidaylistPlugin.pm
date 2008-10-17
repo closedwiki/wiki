5ac9705cba01a8ec66f840dd05790db75593fb0a
DanielRohde <DanielRohde@a00a5322-12db-0310-a70b-8735589c885e>
2008-10-17 07:19:23 +0000

    Item6051: released v1.025
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@17660 a00a5322-12db-0310-a70b-8735589c885e

--- HolidaylistPlugin/lib/TWiki/Plugins/HolidaylistPlugin.pm
+++ HolidaylistPlugin/lib/TWiki/Plugins/HolidaylistPlugin.pm
@@ -89,7 +89,7 @@ $VERSION = '$Rev$';
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
 
-$REVISION = '1.0.25'; #dro# added div tag with style overflow:auto requested by Matthew Thomson; added query parameters feature (hlp_&lt;attribute&gt; in URIs); added option form feature (new attributes: showoptions, optionspos, optionsformat) requested by Matthew Thomson
+$REVISION = '1.0.25'; #dro# added div tag with style overflow:auto requested by Matthew Thomson; added query parameters feature (hlp_&lt;attribute&gt; in URIs); added option form feature (new attributes: showoptions, optionspos, optionsformat) requested by Matthew Thomson; improved performance; fixed minor icon related bugs;
 #$REVISION = '1.0.24'; #dro# added statistics feature requested by TWiki:Main.GarySprague
 #$REVISION = '1.0.23'; #kjl# fixed Item5190 - does not like whitespace after the smiley. This makes the plugin work with TWiki 4.2.0 and Wysiwyg
 #$REVISION = '1.0.22'; #dro# added documentation requested by TWiki:Main.PeterThoeny; fixed typo (on=off bug)
