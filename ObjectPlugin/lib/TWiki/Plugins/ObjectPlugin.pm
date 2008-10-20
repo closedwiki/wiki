1dbd065647921019f24a29a3dab561b7b5651145
SvenDowideit <SvenDowideit@a00a5322-12db-0310-a70b-8735589c885e>
2008-10-20 05:51:09 +0000

    Item6084: small non-changes
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@17669 a00a5322-12db-0310-a70b-8735589c885e

--- ObjectPlugin/lib/TWiki/Plugins/ObjectPlugin.pm
+++ ObjectPlugin/lib/TWiki/Plugins/ObjectPlugin.pm
@@ -1,41 +1,11 @@
-# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+# ObjectPlugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
-# Copyright (C) 2001-2006 Peter Thoeny, peter@thoeny.org
-# and TWiki Contributors. All Rights Reserved. TWiki Contributors
-# are listed in the AUTHORS file in the root of this distribution.
-# NOTE: Please extend that file, not this notice.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version. For
-# more details read LICENSE in the root of this distribution.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# For licensing info read LICENSE file in the TWiki root.
+# Copyright (C) TWiki:Main.PiersGoodhew & TWikiContributors.
 
 =pod
 
 ---+ package ObjectPlugin
 
-under construction.
-
-__NOTE:__ To interact with TWiki use ONLY the official API functions
-in the TWiki::Func module. Do not reference any functions or
-variables elsewhere in TWiki, as these are subject to change
-without prior warning, and your plugin may suddenly stop
-working.
-
-For increased performance, all handlers except initPlugin are
-disabled below. *To enable a handler* remove the leading DISABLE_ from
-the function name. For efficiency and clarity, you should comment out or
-delete the whole of handlers you don't use before you release your
-plugin (or you can put __END__ on a line of it's own and move dead
-code below that line; Perl ignores anything after __END__).
 =cut
 
 # change the package name and $pluginName!!!
@@ -59,7 +29,7 @@ use constant {
 # This should always be $Rev: 9813$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
-$VERSION = '0.9b';
+$VERSION = '$Rev$';
 
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
@@ -77,28 +47,6 @@ $pluginName = 'ObjectPlugin';
    * =$user= - the login name of the user
    * =$installWeb= - the name of the web the plugin is installed in
 
-REQUIRED
-
-Called to initialise the plugin. If everything is OK, should return
-a non-zero value. On non-fatal failure, should write a message
-using TWiki::Func::writeWarning and return 0. In this case
-%FAILEDPLUGINS% will indicate which plugins failed.
-
-In the case of a catastrophic failure that will prevent the whole
-installation from working safely, this handler may use 'die', which
-will be trapped and reported in the browser.
-
-You may also call =TWiki::Func::registerTagHandler= here to register
-a function to handle variables that have standard TWiki syntax - for example,
-=%MYTAG{"my param" myarg="My Arg"}%. You can also override internal
-TWiki variable handling functions this way, though this practice is unsupported
-and highly dangerous!
-
-__Note:__ Please align variables names with the Plugin name, e.g. if 
-your Plugin is called FooBarPlugin, name variables FOOBAR and/or 
-FOOBARSOMETHING. This avoids namespace issues.
-
-
 =cut
 
 sub initPlugin {
@@ -110,11 +58,7 @@ sub initPlugin {
         return 0;
     }
 
-    # Example code of how to get a preference value, register a variable handler
-    # and register a RESTHandler. (remove code you do not need)
-
-    # Get plugin preferences, variables defined by:
-    #   * Set EXAMPLE = ...
+	#TODO: these should be moved into Config.spec for performance.
 	$objectPluginDefHeight = TWiki::Func::getPreferencesValue( "\U$pluginName\E_HEIGHT" );
     $objectPluginDefWidth = TWiki::Func::getPreferencesValue( "\U$pluginName\E_WIDTH" );
     $objectPluginDefController = TWiki::Func::getPreferencesValue( "\U$pluginName\E_CONTROLLER" );
@@ -130,7 +74,7 @@ sub initPlugin {
 	#$kQTControllerHeight = 16;
 	#$kWMVControllerHeight = 46;
 	#$kMediaFileExts = ("mov", "wmv", "swf", "mpg", "mpeg", "mp3", "rm", "mp4", "3gpp")
-	$kMediaFileExtsPattern => join("|", kMediaFileExts);
+	$kMediaFileExtsPattern = join("|", kMediaFileExts);
 
     # Plugin correctly initialized
     return 1;
