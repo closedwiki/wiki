286dc87f62ff38870327ae70aea5a937ed30d989
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2009-02-13 04:33:42 +0000

    Item6180: {PROXY}{HOST} supports domain with and without protocol (defensive coding)
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@17818 a00a5322-12db-0310-a70b-8735589c885e

--- HeadlinesPlugin/lib/TWiki/Plugins/HeadlinesPlugin.pm
+++ HeadlinesPlugin/lib/TWiki/Plugins/HeadlinesPlugin.pm
@@ -30,7 +30,7 @@ use strict;
 use vars qw($VERSION $RELEASE $isInitialized $doneHeader);
 
 $VERSION = '$Rev$';
-$RELEASE = '2.2';
+$RELEASE = '2.21';
 
 # =========================
 sub initPlugin {
