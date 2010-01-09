ae8a03ed01aa3d33cdc986aefc51fc15b5ccfa75
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-01-09 08:06:00 +0000

    Item6383: Initial version of ShareMePlugin
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18273 a00a5322-12db-0310-a70b-8735589c885e

---
+++ ShareMePlugin/lib/TWiki/Plugins/ShareMePlugin.pm
@@ -0,0 +1,176 @@
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2001-2010 Peter Thoeny, peter@thoeny.org
+# Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
+# and TWiki Contributors. All Rights Reserved. TWiki Contributors
+# are listed in the AUTHORS file in the root of this distribution.
+# NOTE: Please extend that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For licensing info read LICENSE file in the TWiki root.
+
+=pod
+
+---+ package ShareMePlugin
+
+=cut
+
+package TWiki::Plugins::ShareMePlugin;
+
+# Always use strict to enforce variable scoping
+use strict;
+
+require TWiki::Func;    # The plugins API
+require TWiki::Plugins; # For the API version
+
+use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $installWeb $debug $defaultSites $pluginName $NO_PREFS_IN_TOPIC $siteDefs );
+
+$VERSION = '2010-01-09 (18272)';
+
+$RELEASE = 'TWiki';
+
+$SHORTDESCRIPTION = 'Icon bar to share a TWiki page on popular social media sites such as Facebook, StumbleUpon, Twitter';
+
+$NO_PREFS_IN_TOPIC = 0;
+
+# Name of this Plugin, only used in this module
+$pluginName = 'ShareMePlugin';
+
+=pod
+
+---++ initPlugin($topic, $web, $user, $installWeb) -> $boolean
+   * =$topic= - the name of the topic in the current CGI query
+   * =$web= - the name of the web in the current CGI query
+   * =$user= - the login name of the user
+   * =$installWeb= - the name of the web the plugin is installed in
+
+=cut
+
+sub initPlugin {
+    my( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $TWiki::Plugins::VERSION < 1.026 ) {
+        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+        return 0;
+    }
+
+    # Get plugin settings
+    $debug = TWiki::Func::getPreferencesFlag( "SHAREMEPLUGIN_DEBUG" );
+    $defaultSites = TWiki::Func::getPreferencesValue( "SHAREMEPLUGIN_DEFAULTSITES" );
+
+    undef $siteDefs;
+
+    # register the _EXAMPLETAG function to handle %EXAMPLETAG{...}%
+    # This will be called whenever %EXAMPLETAG% or %EXAMPLETAG{...}% is
+    # seen in the topic text.
+    TWiki::Func::registerTagHandler( 'SHAREME', \&_SHAREME );
+
+    # Plugin correctly initialized
+    return 1;
+}
+
+=pod
+
+---++ _SHAREME()
+
+This handles the %SHAREME{...}% variable
+
+=cut
+
+sub _SHAREME {
+    my( $session, $params, $theTopic, $theWeb ) = @_;
+
+    _initSitesDef() unless $siteDefs;
+
+    my @sites = _strToArr( $params->{_DEFAULT} || $defaultSites );
+    my $text = _siteIcons( $theWeb, $theTopic, @sites );
+
+    return $text;
+}
+
+=pod
+
+---++ _initSitesDef()
+
+Initialize the site definitions from the plugin table
+
+=cut
+
+sub _initSitesDef {
+    my $siteDefTopic = TWiki::Func::getPreferencesValue( "SHAREMEPLUGIN_SITEDEFINITIONS" );
+    my( $defWeb, $defTopic ) = TWiki::Func::normalizeWebTopicName( $installWeb, $siteDefTopic );
+    foreach( split( /\n/, TWiki::Func::readTopicText( $defWeb, $defTopic, undef, 1 ) ) ) {
+        # example: | Technorati | %ATTACHURL%/technorati.png | http://technorati.com/faves?add=$link |
+        if( /\| *(.*?) *\| *[^\/]*\/([^ \|]*) \| *(.*?) \|/ ) {
+            $siteDefs->{$1} = {
+                img => "$defWeb/$defTopic/$2",
+                url => $3,
+            };
+        }
+    }
+    # add a dummy entry to avoid a re-init at each variable expansion in case of error
+    $siteDefs->{_dummy} = { img => '', url => '' } unless $siteDefs;
+}
+
+=pod
+
+---++ _siteIcons()
+
+Return HTML text with site icons
+
+=cut
+
+sub _siteIcons {
+    my( $theWeb, $theTopic, @sites ) = @_;
+
+    # no newlines so that icons can be placed in a TWiki table
+    my $text = '<span class="shareme">';
+    $text .= '<style type="text/css">'
+          .  ' .shareme-hover { width: 16px; height: 16px; opacity: .4; -moz-opacity: .4; filter: alpha(opacity=40); }'
+          .  ' .shareme-hover:hover { opacity: 1; -moz-opacity: 1; filter: alpha(opacity=100); }</style>';
+    my $pubUrl = TWiki::Func::getPubUrlPath();
+    my $link = "\%ENCODE{\%SCRIPTURL{view}\%/$theWeb/$theTopic}\%";
+    my $title = "\%ENCODE{\%SPACEOUT{$theTopic}\%}\%";
+    my $summary = "\%ENCODE{\%SPACEOUT{$theTopic}\% in $theWeb web on \%HTTP_HOST\%}%";
+    foreach my $site ( @sites ) {
+        next unless $siteDefs->{$site};
+        my $url = $siteDefs->{$site}{url};
+        $url =~ s/\$link/$link/g;
+        $url =~ s/\$title/$title/g;
+        $url =~ s/\$summary/$summary/g;
+        $url =~ s/\$site/\%HTTP_HOST\%/g;
+        $text .= "<a href='$url' target='_shareme' rel='nofollow' style='border-style: none;'>"
+              . "<img src='$pubUrl/$siteDefs->{$site}{img}' alt='$site'"
+              . " title='$site' class='shareme-hover'"
+              . " style='background: transparent url($pubUrl/$siteDefs->{$site}{img});' />"
+              . "</a> ";
+    }
+    $text .= "</span>";
+    return $text;
+}
+
+=pod
+
+---++ _strToArray()
+
+Convert a comma-sapce delimited list to an array.
+
+=cut
+
+sub _strToArr {
+    my( $str ) = @_;
+    my @arr = map { s/^ *(.*?) */$1/; $_ } split( /, */, $str );
+    return @arr;
+}
+
+1;
