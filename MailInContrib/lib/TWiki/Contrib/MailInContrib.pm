2a9da0c40bf205540d98d0ef7ea469cf45ed0025
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-04-25 23:14:50 +0000

    Item6433: Doc improvements; changing TWIKIWEB to SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18525 a00a5322-12db-0310-a70b-8735589c885e

--- MailInContrib/lib/TWiki/Contrib/MailInContrib.pm
+++ MailInContrib/lib/TWiki/Contrib/MailInContrib.pm
@@ -31,15 +31,8 @@ use Error qw( :try );
 use vars qw ( $VERSION $RELEASE );
 use Carp;
 
-# This should always be $Rev: 10183$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev: 10183$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$RELEASE = '2010-04-25';
 
 BEGIN {
     $SIG{__DIE__} = sub { Carp::confess $_[0] };
