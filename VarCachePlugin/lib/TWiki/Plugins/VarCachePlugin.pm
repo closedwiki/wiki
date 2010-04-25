8aec92d18e7938821e31e918f3e3ed7ed070fac4
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-04-25 07:33:18 +0000

    Item6433: Doc fix: Changing TWIKIWEB to SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18518 a00a5322-12db-0310-a70b-8735589c885e

--- VarCachePlugin/lib/TWiki/Plugins/VarCachePlugin.pm
+++ VarCachePlugin/lib/TWiki/Plugins/VarCachePlugin.pm
@@ -25,15 +25,8 @@ use vars qw(
         $debug $paramMsg
     );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '2010-04-06';
+$RELEASE = '2010-04-25';
 
 $pluginName = 'VarCachePlugin';  # Name of this Plugin
 
