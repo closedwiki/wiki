671d951b40fb89ecdc12d722ba52fb117440158a
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-05-03 08:51:55 +0000

    Item6390: Adding TWiki::Contrib::OpenIdRpContrib module (prearing for build)
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18577 a00a5322-12db-0310-a70b-8735589c885e

---
+++ OpenIdRpContrib/lib/TWiki/Contrib/OpenIdRpContrib.pm
@@ -0,0 +1,27 @@
+# Module of TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2010 TWiki Inc and TWiki Contributors. All
+# Rights Reserved. TWiki Contributors are listed in the AUTHORS
+# file in the root of the TWiki distribution. NOTE: Please extend
+# that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# As per the GPL, removal of this notice is prohibited.
+
+package TWiki::Contrib::OpenIdRpContrib;
+use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
+
+$VERSION = '$Rev$';
+$RELEASE = '2010-05-03';
+$SHORTDESCRIPTION = "Authenticate OpenID users as a Relying Party (RP) or consumer site";
+
+1;
