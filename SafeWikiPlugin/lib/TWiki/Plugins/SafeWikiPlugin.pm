460b059c014e5f3750deb45fa610f811ade27a49
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-05-05 05:55:00 +0000

    Item6433: Doc improvements; replacing TWIKIWEB with SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18587 a00a5322-12db-0310-a70b-8735589c885e

--- SafeWikiPlugin/lib/TWiki/Plugins/SafeWikiPlugin.pm
+++ SafeWikiPlugin/lib/TWiki/Plugins/SafeWikiPlugin.pm
@@ -1,4 +1,25 @@
-# See bottom of file for notices
+# Plugin for TWiki Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2007-2008 C-Dot Consultants http://c-dot.co.uk
+# Copyright (C) 2008-2010 TWiki Contributor. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root of
+# the TWiki distribution.
+# Author: Crawford Currie
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# This notice must be retained in all copies or derivatives of this
+# code.
+
 package TWiki::Plugins::SafeWikiPlugin;
 
 use strict;
@@ -8,8 +29,7 @@ use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $NO_PREFS_IN_TOPIC
              $URIFILTER $CODEFILTER );
 
 $VERSION = '$Rev$';
-
-$RELEASE = '1.0';
+$RELEASE = '1.1';
 
 $SHORTDESCRIPTION = 'Secure your TWiki so it can\'t be used for mounting phishing attacks';
 
@@ -76,22 +96,3 @@ sub _filterHandler {
 }
 
 1;
-__DATA__
-
-Copyright (C) 2007-2008 C-Dot Consultants http://c-dot.co.uk
-All rights reserved
-Author: Crawford Currie
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details, published at
-http://www.gnu.org/copyleft/gpl.html
-
-This notice must be retained in all copies or derivatives of this
-code.
--- SafeWikiPlugin/lib/TWiki/Plugins/SafeWikiPlugin/Leaf.pm
+++ SafeWikiPlugin/lib/TWiki/Plugins/SafeWikiPlugin/Leaf.pm
@@ -1,4 +1,24 @@
-# See bottom of file for notices
+# Plugin code for TWiki Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2007-2008 C-Dot Consultants http://c-dot.co.uk
+# Copyright (C) 2008-2010 TWiki Contributor. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root of
+# this distribution.
+# Author: Crawford Currie
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# This notice must be retained in all copies or derivatives of this
+# code.
 
 package TWiki::Plugins::SafeWikiPlugin::Leaf;
 
@@ -29,22 +49,3 @@ sub isLeaf {
 }
 
 1;
-__DATA__
-
-Copyright (C) 2007-2008 C-Dot Consultants http://c-dot.co.uk
-All rights reserved
-Author: Crawford Currie
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details, published at
-http://www.gnu.org/copyleft/gpl.html
-
-This notice must be retained in all copies or derivatives of this
-code.
