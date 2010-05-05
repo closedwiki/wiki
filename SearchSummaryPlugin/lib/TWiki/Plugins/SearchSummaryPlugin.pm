edaad0b4f793b115e2d0894281dcfda4dc80a8bb
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-05-05 06:11:39 +0000

    Item6433: Doc improvements; replacing TWIKIWEB with SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18589 a00a5322-12db-0310-a70b-8735589c885e

--- SearchSummaryPlugin/lib/TWiki/Plugins/SearchSummaryPlugin.pm
+++ SearchSummaryPlugin/lib/TWiki/Plugins/SearchSummaryPlugin.pm
@@ -1,3 +1,24 @@
+# Plugin code for TWiki Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2007 Escape Studios.
+# Copyright (C) 2008-2010 TWiki Contributor. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root of
+# the TWiki distribution.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# This notice must be retained in all copies or derivatives of this
+# code.
+
 package TWiki::Plugins::SearchSummaryPlugin;
 
 use strict;
