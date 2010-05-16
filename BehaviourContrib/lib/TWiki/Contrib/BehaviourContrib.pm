2e70f3b645bc664fd2837d66554be404ae41cd1d
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-05-16 04:43:57 +0000

    Item6433: Doc improvements; replacing TWIKIWEB with SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18693 a00a5322-12db-0310-a70b-8735589c885e

--- BehaviourContrib/lib/TWiki/Contrib/BehaviourContrib.pm
+++ BehaviourContrib/lib/TWiki/Contrib/BehaviourContrib.pm
@@ -1,7 +1,7 @@
 package TWiki::Contrib::BehaviourContrib;
 use vars qw( $VERSION );
 $VERSION = '$Rev$';
-$RELEASE = '1.3.1';
+$RELEASE = '1.4';
 
 =begin twiki
 
@@ -22,7 +22,7 @@ sub commonTagsHandler {
 =cut
 
 sub addHEAD {
-    my $base = '%PUBURLPATH%/%TWIKIWEB%/BehaviourContrib';
+    my $base = '%PUBURLPATH%/%SYSTEMWEB%/BehaviourContrib';
     my $USE_SRC =
       TWiki::Func::getPreferencesValue('BEHAVIOURCONTRIB_DEBUG') ?
           '_src' : '';
