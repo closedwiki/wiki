9b54162d7abe37fb5e81a044a1b33ac33832cacb
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-05-18 06:05:01 +0000

    Item6433: Doc improvements
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18716 a00a5322-12db-0310-a70b-8735589c885e

--- TwistyPlugin/lib/TWiki/Plugins/TwistyPlugin.pm
+++ TwistyPlugin/lib/TWiki/Plugins/TwistyPlugin.pm
@@ -1,7 +1,8 @@
 # Plugin for TWiki Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2005 Rafael Alvarez, soronthar@sourceforge.net
-# Copyright (C) 2010 Peter Thoeny, peter@thoeny.org
+# Copyright (C) 2005-2010 Peter Thoeny, peter@thoeny.org and
+# TWiki Contributors.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -37,15 +38,8 @@ use vars
   $prefMode $prefShowLink $prefHideLink $prefRemember
   $defaultMode $defaultShowLink $defaultHideLink $defaultRemember );
 
-# This should always be $Rev: 15653 (19 Nov 2007) $ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev: 15653$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '1.5';
+$RELEASE = '1.6';
 
 $pluginName = 'TwistyPlugin';
 
