00b03c8c7217a5fe4d04ead6a165c59f9f2df6de
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-05-20 01:05:54 +0000

    Item6466: Garbled configure screen after showing error message - fixing copyright
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18732 a00a5322-12db-0310-a70b-8735589c885e

--- core/lib/TWiki/Configure/Checkers/BasicSanity.pm
+++ core/lib/TWiki/Configure/Checkers/BasicSanity.pm
@@ -1,7 +1,7 @@
 #
 # TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2000-2006 TWiki Contributors.
+# Copyright (C) 2000-2010 TWiki Contributors.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
