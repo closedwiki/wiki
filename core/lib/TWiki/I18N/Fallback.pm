1b74735789b8d1de295e8b3ceddd045ce5292073
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-05-21 21:40:29 +0000

    Item6318: Fixing copyright info for Helsinki release
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@18751 a00a5322-12db-0310-a70b-8735589c885e

--- core/lib/TWiki/I18N/Fallback.pm
+++ core/lib/TWiki/I18N/Fallback.pm
@@ -1,9 +1,11 @@
 # Module of TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 1999-2007 TWiki Contributors.
-# All Rights Reserved. TWiki Contributors
-# are listed in the AUTHORS file in the root of this distribution.
-# NOTE: Please extend that file, not this notice.
+# Copyright (C) 1999-2010 TWiki Contributors. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root of
+# this distribution. NOTE: Please extend that file, not this notice.
+#
+# Additional copyrights apply to some or all of the code in this
+# file as follows:
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,7 +18,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
 # As per the GPL, removal of this notice is prohibited.
-
+#
 # TWiki::I18N::Fallback - a fallback class for when
 # Locale::Maketext isn't available.
 
