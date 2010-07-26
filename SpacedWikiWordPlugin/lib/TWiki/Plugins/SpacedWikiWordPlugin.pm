e0e4ef051e6438272822abc1700e96036b00872d
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-07-26 02:25:34 +0000

    Item6530: Doc fixes
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@19234 a00a5322-12db-0310-a70b-8735589c885e

--- SpacedWikiWordPlugin/lib/TWiki/Plugins/SpacedWikiWordPlugin.pm
+++ SpacedWikiWordPlugin/lib/TWiki/Plugins/SpacedWikiWordPlugin.pm
@@ -24,15 +24,8 @@ use vars qw(
   $web $topic $user $installWeb $VERSION $RELEASE $debug %dontSpaceSet $spaceOutWikiWordLinks $spaceOutUnderscoreLinks $removeAnchorDashes
 );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$RELEASE = '2010-07-25';
 
 # =========================
 sub initPlugin {
