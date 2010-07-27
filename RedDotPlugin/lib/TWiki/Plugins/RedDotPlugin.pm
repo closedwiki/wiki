a6e5e8608e82bad34e7bf1c1fe9651407ad6278a
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-07-27 08:03:43 +0000

    Item6530: Doc fixes; changing TWIKIWEB to SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@19273 a00a5322-12db-0310-a70b-8735589c885e

--- RedDotPlugin/lib/TWiki/Plugins/RedDotPlugin.pm
+++ RedDotPlugin/lib/TWiki/Plugins/RedDotPlugin.pm
@@ -1,6 +1,7 @@
 # Plugin for TWiki Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2005-2006 Michael Daum <micha@nats.informatik.uni-hamburg.de>
+# Copyright (C) 2005-2010 TWiki Contributors
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -28,7 +29,7 @@ use vars qw(
 
 
 $VERSION = '$Rev$';
-$RELEASE = '1.40';
+$RELEASE = '2010-07-27';
 $NO_PREFS_IN_TOPIC = 1;
 $SHORTDESCRIPTION = 'Renders edit-links as little red dots';
 
