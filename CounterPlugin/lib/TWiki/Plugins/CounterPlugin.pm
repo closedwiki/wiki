a912e5f9c38380206115686e5cfff9145b084dc9
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-09-05 07:03:35 +0000

    Item6530: Doc improvements; changed TWIKIWEB to SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@19433 a00a5322-12db-0310-a70b-8735589c885e

--- CounterPlugin/lib/TWiki/Plugins/CounterPlugin.pm
+++ CounterPlugin/lib/TWiki/Plugins/CounterPlugin.pm
@@ -1,7 +1,7 @@
 # Plugin for TWiki Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
-# Copyright (C) 2001-2003 Peter Thoeny, peter@thoeny.com
+# Copyright (C) 2003 TWiki:Main.RahulMundke
+# Copyright (C) 2003-2010 TWiki:TWiki.TWikiContributor
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -14,41 +14,9 @@
 # GNU General Public License for more details, published at 
 # http://www.gnu.org/copyleft/gpl.html
 #
-# =========================
-#
-# This is an empty TWiki plugin. Use it as a template
-# for your own plugins; see TWiki.TWikiPlugins for details.
-#
-# Each plugin is a package that may contain these functions:        VERSION:
-#
-#   initPlugin              ( $topic, $web, $user, $installWeb )    1.000
-#   initializeUserHandler   ( $loginName, $url, $pathInfo )         1.010
-#   registrationHandler     ( $web, $wikiName, $loginName )         1.010
-#   commonTagsHandler       ( $text, $topic, $web )                 1.000
-#   startRenderingHandler   ( $text, $web )                         1.000
-#   outsidePREHandler       ( $text )                               1.000
-#   insidePREHandler        ( $text )                               1.000
-#   endRenderingHandler     ( $text )                               1.000
-#   beforeEditHandler       ( $text, $topic, $web )                 1.010
-#   afterEditHandler        ( $text, $topic, $web )                 1.010
-#   beforeSaveHandler       ( $text, $topic, $web )                 1.010
-#   writeHeaderHandler      ( $query )                              1.010  Use only in one Plugin
-#   redirectCgiQueryHandler ( $query, $url )                        1.010  Use only in one Plugin
-#   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
-#   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
-#
-# initPlugin is required, all other are optional. 
-# For increased performance, all handlers except initPlugin are
-# disabled. To enable a handler remove the leading DISABLE_ from
-# the function name. Remove disabled handlers you do not need.
-#
-# NOTE: To interact with TWiki use the official TWiki functions 
-# in the TWiki::Func module. Do not reference any functions or
-# variables elsewhere in TWiki!!
-
 
 # =========================
-package TWiki::Plugins::CounterPlugin;    # change the package name and $pluginName!!!
+package TWiki::Plugins::CounterPlugin;
 
 # =========================
 #This is plugin specific variable
@@ -56,17 +24,8 @@ use vars qw(
         $web $topic $user $installWeb $VERSION $RELEASE $debug 
     );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
-
-$debug = 1;
+$RELEASE = '2010-09-05';
 
 # =========================
 sub initPlugin
@@ -78,9 +37,12 @@ sub initPlugin
         &TWiki::Func::writeWarning( "Version mismatch between CounterPlugin and Plugins.pm" );
         return 0;
     }
-   	
+
+    # Get plugin debug flag
+    $debug = TWiki::Func::getPreferencesFlag( "COUNTERPLUGIN_DEBUG" );
+
     # Plugin correctly initialized
-    &TWiki::Func::writeDebug( "- TWiki::Plugins:CounterPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
+    TWiki::Func::writeDebug( "- TWiki::Plugins:CounterPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
     return 1;
 }
 
@@ -92,32 +54,29 @@ sub commonTagsHandler
 
 #-------------------------------------------------------------------------------------------------
 
-sub _handleTag()
+sub _handleTag
 {
-	# increment the counter and throw up the page with this count
-	my $FileLocation = &TWiki::Func::getWorkArea( 'CounterPlugin' );
-	my $DataFile = 'visitor_count.txt';
-	my $CounterFile = "$FileLocation/$DataFile";
-    	&TWiki::Func::writeDebug( "- TWiki::Plugins:CounterPlugin::FileLocation is $FileLocation" );
-	
-	if ( open(FILE , '<', $CounterFile) )
-	{
-	    &TWiki::Func::writeDebug("Opened $DataFile file successfully");
-	    $Count = <FILE>;
-	    close FILE;
-	}
-	else
-	{
-	    # File doesn't exist
-	    $Count = 0;
-	}
-	
-	open(FILE, '>', $CounterFile) || die "Can't open $DataFile file";
-	++$Count;
-	print FILE $Count;
-	close FILE;
-	
-	return $Count;
+    # increment the counter and throw up the page with this count
+    my $FileLocation = &TWiki::Func::getWorkArea( 'CounterPlugin' );
+    my $DataFile = 'visitor_count.txt';
+    my $CounterFile = "$FileLocation/$DataFile";
+    TWiki::Func::writeDebug( "- TWiki::Plugins:CounterPlugin::_handleTag - FileLocation is $FileLocation" ) if $debug;
+
+    if ( open(FILE , '<', $CounterFile) ) {
+        TWiki::Func::writeDebug("   Opened $DataFile file successfully") if $debug;
+        $Count = <FILE>;
+        close FILE;
+    } else {
+        # File doesn't exist
+        $Count = 0;
+    }
+
+    open(FILE, '>', $CounterFile) || die "Can't open $DataFile file";
+    ++$Count;
+    print FILE $Count;
+    close FILE;
+
+    return $Count;
 }
 
 1;
