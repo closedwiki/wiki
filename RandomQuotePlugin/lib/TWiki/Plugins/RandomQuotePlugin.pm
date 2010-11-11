361bbe8de76bce157fa822b966b229d2f0419064
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-11-11 05:45:22 +0000

    Item6530: Doc improvements & fixing TWIKIWEB to SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@19826 a00a5322-12db-0310-a70b-8735589c885e

--- RandomQuotePlugin/lib/TWiki/Plugins/RandomQuotePlugin.pm
+++ RandomQuotePlugin/lib/TWiki/Plugins/RandomQuotePlugin.pm
@@ -1,7 +1,10 @@
-# Plugin for TWiki Collaboration Platform, http://TWiki.org/
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
 # Copyright (C) 2000-2003 Peter Thoeny, peter@thoeny.com
+# Copyright (C) 2006-2010 TWiki Contributors. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root of
+# this distribution. NOTE: Please extend that file, not this notice.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -13,38 +16,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details, published at 
 # http://www.gnu.org/copyleft/gpl.html
-#
-# =========================
-#
-# This is the default TWiki plugin. Use EmptyPlugin.pm as a template
-# for your own plugins; see TWiki.TWikiPlugins for details.
-#
-# Each plugin is a package that may contain these functions:        VERSION:
-#
-#   initPlugin              ( $topic, $web, $user, $installWeb )    1.000
-#   registrationHandler     ( $web, $wikiName, $loginName )         1.010
-#   commonTagsHandler       ( $text, $topic, $web )                 1.000
-#   startRenderingHandler   ( $text, $web )                         1.000
-#   outsidePREHandler       ( $text )                               1.000
-#   insidePREHandler        ( $text )                               1.000
-#   endRenderingHandler     ( $text )                               1.000
-#   beforeEditHandler       ( $text, $topic, $web )                 1.010
-#   afterEditHandler        ( $text, $topic, $web )                 1.010
-#   beforeSaveHandler       ( $text, $topic, $web )                 1.010
-#   writeHeaderHandler      ( $query )                              1.010  Use only in one Plugin
-#   redirectCgiQueryHandler ( $query, $url )                        1.010  Use only in one Plugin
-#   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
-#   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
-#
-# initPlugin is required, all other are optional. 
-# For increased performance, unused handlers are disabled. To
-# enable a handler remove the leading DISABLE_ from the function
-# name. Remove disabled handlers you do not need.
-#
-# NOTE: To interact with TWiki use the official TWiki functions 
-# in the TWiki::Func module. Do not reference any functions or
-# variables elsewhere in TWiki!!
-
 
 # =========================
 package TWiki::Plugins::RandomQuotePlugin;
@@ -55,15 +26,8 @@ use vars qw(
         $debug $doOldInclude $renderingWeb
     );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$RELEASE = '2010-11-10';
 
 $pluginName = 'RandomQuotePlugin';  # Name of this Plugin
 
@@ -111,41 +75,6 @@ sub startRenderingHandler
     # This handler is called by getRenderedVersion just before the line loop
 
     $renderingWeb = $_[1];
-}
-
-# =========================
-sub outsidePREHandler
-{
-### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
-
-    ##TWiki::Func::writeDebug( "- ${pluginName}::outsidePREHandler( $renderingWeb.$topic )" ) if $debug;
-
-    # This handler is called by getRenderedVersion, once per line, before any changes,
-    # for lines outside <pre> and <verbatim> tags. 
-    # Use it to define customized rendering rules
-
-    # do custom extension rule, like for example:
-    # $_[0] =~ s/old/new/go;
-
-    # render deprecated *_text_* as "bold italic" text:
-    $_[0] =~ s/(^|\s)\*_([^\s].*?[^\s])_\*(\s|$)/$1<strong><em>$2<\/em><\/strong>$3/go;
-
-    # Use alternate %Web:WikiName% syntax (versus the standard Web.WikiName).
-    # This is an old JosWiki render option. (Uncomment for JosWiki compatibility)
-#   $_[0] =~ s/(^|\s|\()\%([^\s].*?[^\s]):([^\s].*?[^\s])\%/&TWiki::internalLink($2,$3,"$2:$3",$1,1)/geo;
-
-    # Use "forced" non-WikiName links (i.e. %Linkname%)
-    # This is an old JosWiki render option. (Uncomment for JosWiki compatibility)
-#   $_[0] =~ s/(^|\s|\()\%([^\s].*?[^\s])\%/&TWiki::internalLink($web,$2,$2,$1,1)/geo;
-
-    # Use "forced" non-WikiName links (i.e. %Web.Linkname%)
-    # This is an old JosWiki render option combined with the new Web.LinkName notation
-    # (Uncomment for JosWiki compatibility)
-#   $_[0] =~ s/(^|\s|\()\%([a-zA-Z0-9]+)\.(.*?[^\s])\%(\s|\)|$)/&TWiki::internalLink($2,$3,$3,$1,1)/geo;
-
-    # Use <link>....</link> links
-    # This is an old JosWiki render option. (Uncomment for JosWiki compatibility)
-#   $_[0] =~ s/<link>(.*?)<\/link>/&TWiki::internalLink("",$web,$1,$1,"",1)/geo;
 }
 
 # =========================
