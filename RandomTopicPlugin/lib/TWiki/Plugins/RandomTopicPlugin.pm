bc960d986fbee2932d92119c62141d3ed76d6710
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-11-11 05:08:28 +0000

    Item6530: Doc improvements
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@19823 a00a5322-12db-0310-a70b-8735589c885e

--- RandomTopicPlugin/lib/TWiki/Plugins/RandomTopicPlugin.pm
+++ RandomTopicPlugin/lib/TWiki/Plugins/RandomTopicPlugin.pm
@@ -1,6 +1,9 @@
 # Plugin for TWiki Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2003 Micahel Sparks
+# Copyright (C) 2006-2010 TWiki Contributors. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root of
+# this distribution. NOTE: Please extend that file, not this notice.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -21,15 +24,8 @@ use vars qw(
             $VERSION $RELEASE @topicList $defaultIncludes $defaultExcludes
     );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$RELEASE = '2010-11-10';
 
 
 sub initPlugin {
