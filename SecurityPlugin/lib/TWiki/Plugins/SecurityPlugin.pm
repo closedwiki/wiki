afe8fb8181ce62cf863a08b43039312c1924d484
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2010-12-31 06:13:18 +0000

    Item6530: Doc improvements
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20057 a00a5322-12db-0310-a70b-8735589c885e

--- SecurityPlugin/lib/TWiki/Plugins/SecurityPlugin.pm
+++ SecurityPlugin/lib/TWiki/Plugins/SecurityPlugin.pm
@@ -13,46 +13,9 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details, published at 
 # http://www.gnu.org/copyleft/gpl.html
-#
-# =========================
-#
-# This is an empty TWiki plugin. Use it as a template
-# for your own plugins; see TWiki.TWikiPlugins for details.
-#
-# Each plugin is a package that may contain these functions:        VERSION:
-#
-#   earlyInitPlugin         ( )                                     1.020
-#   initPlugin              ( $topic, $web, $user, $installWeb )    1.000
-#   initializeUserHandler   ( $loginName, $url, $pathInfo )         1.010
-#   registrationHandler     ( $web, $wikiName, $loginName )         1.010
-#   beforeCommonTagsHandler ( $text, $topic, $web )                 1.024
-#   commonTagsHandler       ( $text, $topic, $web )                 1.000
-#   afterCommonTagsHandler  ( $text, $topic, $web )                 1.024
-#   startRenderingHandler   ( $text, $web )                         1.000
-#   outsidePREHandler       ( $text )                               1.000
-#   insidePREHandler        ( $text )                               1.000
-#   endRenderingHandler     ( $text )                               1.000
-#   beforeEditHandler       ( $text, $topic, $web )                 1.010
-#   afterEditHandler        ( $text, $topic, $web )                 1.010
-#   beforeSaveHandler       ( $text, $topic, $web )                 1.010
-#   afterSaveHandler        ( $text, $topic, $web, $errors )        1.020
-#   writeHeaderHandler      ( $query )                              1.010  Use only in one Plugin
-#   redirectCgiQueryHandler ( $query, $url )                        1.010  Use only in one Plugin
-#   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
-#   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
-#
-# initPlugin is required, all other are optional. 
-# For increased performance, all handlers except initPlugin are
-# disabled. To enable a handler remove the leading DISABLE_ from
-# the function name. Remove disabled handlers you do not need.
-#
-# NOTE: To interact with TWiki use the official TWiki functions 
-# in the TWiki::Func module. Do not reference any functions or
-# variables elsewhere in TWiki!!
-
 
 # =========================
-package TWiki::Plugins::SecurityPlugin;    # change the package name and $pluginName!!!
+package TWiki::Plugins::SecurityPlugin;
 
 # =========================
 use vars qw(
@@ -60,15 +23,8 @@ use vars qw(
         $debug $exampleCfgVar
     );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$RELEASE = '2010-12-30';
 
 $pluginName = 'SecurityPlugin';  # Name of this Plugin
 
@@ -86,8 +42,7 @@ sub initPlugin
     # Get plugin debug flag
     $debug = TWiki::Func::getPluginPreferencesFlag( "DEBUG" );
 
-    # Get plugin preferences, the variable defined by:          * Set EXAMPLE = ...
-#    $exampleCfgVar = TWiki::Func::getPluginPreferencesValue( "EXAMPLE" ) || "default";
+    # Get plugin preferences
     $SecurityLockedCfgVar = TWiki::Func::getPluginPreferencesValue( "SECURITYLOCKED" ) || "LOCKED";
     $SecurityUnlockedCfgVar = TWiki::Func::getPluginPreferencesValue( "SECURITYUNLOCKED" ) || "UNLOCKED";
     
@@ -125,9 +80,6 @@ sub commonTagsHandler
     # This is the place to define customized tags and variables
     # Called by TWiki::handleCommonTags, after %INCLUDE:"..."%
 
-    # do custom extension rule, like for example:
-    # $_[0] =~ s/%XYZ%/&handleXyz()/ge;
-    # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/ge;
     $_[0] =~ s/%SECURITY%/&handleSecurity()/ge;
 }
 
