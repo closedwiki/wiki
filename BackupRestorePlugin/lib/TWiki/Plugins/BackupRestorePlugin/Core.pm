ba5a03feab89fbb9b678692f21da81af685827c4
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-01-12 05:23:40 +0000

    Item6631: Backup & restore utility, initial version - lazy loading of core module
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20172 a00a5322-12db-0310-a70b-8735589c885e

--- BackupRestorePlugin/lib/TWiki/Plugins/BackupRestorePlugin/Core.pm
+++ BackupRestorePlugin/lib/TWiki/Plugins/BackupRestorePlugin/Core.pm
@@ -17,50 +17,38 @@
 #
 # For licensing info read LICENSE file in the TWiki root.
 
-package TWiki::Plugins::BackupRestorePlugin;
+package TWiki::Plugins::BackupRestorePlugin::Core;
 
 use strict;
 
 require TWiki::Func;    # The plugins API
 require TWiki::Plugins; # For the API version
 
-use vars qw(
-    $VERSION $RELEASE $SHORTDESCRIPTION $NO_PREFS_IN_TOPIC
-    $core $baseTopic $baseWeb $user
-  );
-
-$VERSION = '$Rev$';
-$RELEASE = '2011-01-11';
-$SHORTDESCRIPTION = 'Administrator utility to backup, restore and upgrade a TWiki site';
-$NO_PREFS_IN_TOPIC = 1;
-
 #==================================================================
-sub initPlugin {
-    ( $baseTopic, $baseWeb, $user ) = @_;
-
-    # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.026 ) {
-        TWiki::Func::writeWarning( "Version mismatch between BackupRestorePlugin and Plugins.pm" );
-        return 0;
-    }
-
-    $core = undef;
-    TWiki::Func::registerTagHandler( 'BACKUPRESTORE', \&_BACKUPRESTORE );
-
-    # Plugin correctly initialized
-    return 1;
+sub new {
+    my $class = shift;
+    my $this = {
+        BaseTopic  => shift,
+        BaseWeb    => shift,
+        User       => shift,
+        Debug      => $TWiki::cfg{Plugins}{BackupRestorePlugin}{Debug} || 0,
+        BackupDir  => $TWiki::cfg{Plugins}{BackupRestorePlugin}{BackupDir} || '/tmp',
+        KeepNumBUs => $TWiki::cfg{Plugins}{BackupRestorePlugin}{KeepNumberOfBackups} || '5',
+    };
+    bless( $this, $class );
+    TWiki::Func::writeDebug( "- BackupRestorePlugin constructor" ) if $this->{Debug};
+    return $this;
 }
 
 #==================================================================
-sub _BACKUPRESTORE {
-#    my($session, $params, $theTopic, $theWeb) = @_;
+sub BACKUPRESTORE {
+    my( $this, $session, $params, $theTopic, $theWeb ) = @_;
 
-    # delay loading core module until run-time
-    unless( $core ) {
-        require TWiki::Plugins::BackupRestorePlugin::Core;
-        $core = new TWiki::Plugins::BackupRestorePlugin::Core( $baseTopic, $baseWeb, $user );
-    }
-    return $core->BACKUPRESTORE( @_ );
+    TWiki::Func::writeDebug( "- BackupRestorePlugin->BACKUPRESTORE" ) if $this->{Debug};
+
+    my $text = "Placeholder for BACKUPRESTORE, user !$this->{User}";
+
+    return $text;
 }
 
 #==================================================================
