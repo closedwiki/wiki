b80ecdf1e56433389c823c675ababd0191fe80db
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-01-15 04:23:31 +0000

    Item6530: Doc improvements; changing TWIKIWEB to SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20216 a00a5322-12db-0310-a70b-8735589c885e

--- LdapPlugin/lib/TWiki/Plugins/LdapPlugin.pm
+++ LdapPlugin/lib/TWiki/Plugins/LdapPlugin.pm
@@ -1,9 +1,29 @@
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-#      TWiki LDAP Plugin
+# Copyright (C) 2002-2004 TWiki:Main.GerardHickey
+# Copyright (C) 2006-2007 TWiki:Main.AndreasVoegele
+# Copyright (C) 2007-2011 TWiki:TWiki.TWikiContributor
 #
-#      Written by Gerard Hickey <hickey@kernelrom.com>
-#      Modified by Gerald Skerbitz <gsker@tcfreenet.org> to use extractNameValuePair() and provide for
-#                  multiple attributes and multiple records.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# As per the GPL, removal of this notice is prohibited.
+#
+#
+# TWiki LDAP Plugin
+#
+#     Written by Gerard Hickey <hickey@kernelrom.com>
+#     Modified by Gerald Skerbitz <gsker@tcfreenet.org> to use extractNameValuePair() and provide
+#                 for multiple attributes and multiple records.
 #     Modified by PatrickNomblot - 26 Jun 2003 to add JPeg Photo
 #     Modified by Gerald Skerbitz 24 Oct 2003 to fix bug with shortvs long fields
 #     Modified by PatrickNomblot - 9 Dec 2003 to change JPeg Photo to not have IMG hradcoded
@@ -27,17 +47,12 @@ package TWiki::Plugins::LdapPlugin;
 
 use strict;
 
-use vars qw($VERSION $RELEASE $debug $pluginName);
+use vars qw(
+    $VERSION $RELEASE $debug $pluginName
+  );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
-$VERSION = '11 Jul 2007';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$VERSION = '$Rev$';
+$RELEASE = '2011-01-14';
 
 # Name of this Plugin, only used in this module
 $pluginName = 'LdapPlugin';
