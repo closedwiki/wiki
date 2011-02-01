1db86547f1432d3eb1b723aae0f6508b82a62bbf
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-02-01 19:13:00 +0000

    Item6638: Doc improvements; XHTML fixes; changing TWIKIWEB to SYSTEMWEB
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20349 a00a5322-12db-0310-a70b-8735589c885e

--- XmlXslPlugin/lib/TWiki/Plugins/XmlXslPlugin.pm
+++ XmlXslPlugin/lib/TWiki/Plugins/XmlXslPlugin.pm
@@ -1,8 +1,9 @@
-#
-# TWiki WikiClone ($wikiversion has version info)
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2000-2001 Andrea Sterbini, a.sterbini@flashnet.it
 # Copyright (C) 2001 Peter Thoeny, Peter@Thoeny.com
+# Copyright (C) 2001 TWiki:Main.MartinCleaver
+# Copyright (C) 2007-2011 TWiki Contributors. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -14,28 +15,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Gener al Public License for more details, published at 
 # http://www.gnu.org/copyleft/gpl.html
-#
-# =========================
-#
-#
-# Each plugin is a package that contains the subs:
-#
-#   initPlugin           ( $topic, $web, $user, $installWeb )
-#   commonTagsHandler    ( $text, $topic, $web )
-#   startRenderingHandler( $text, $web )
-#   outsidePREHandler    ( $text )
-#   insidePREHandler     ( $text )
-#   endRenderingHandler  ( $text )
-#
-# initPlugin is required, all other are optional. 
-# For increased performance, all handlers except initPlugin are
-# disabled. To enable a handler remove the leading DISABLE_ from
-# the function name.
-# 
-# NOTE: To interact with TWiki use the official TWiki functions
-# in the &TWiki::Func module. Do not reference any functions or
-# variables elsewhere in TWiki!!
-
 
 # =========================
 package TWiki::Plugins::XmlXslPlugin;
@@ -43,18 +22,10 @@ package TWiki::Plugins::XmlXslPlugin;
 # =========================
 use vars qw(
         $web $topic $user $installWeb $VERSION $RELEASE $debug
-        $exampleCfgVar
     );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$RELEASE = '2011-02-01';
 
 
 # =========================
@@ -68,14 +39,12 @@ sub initPlugin
         return 0;
     }
 
-    # Get plugin preferences, the variable defined by:         
-    $exampleCfgVar = &TWiki::Func::getPreferencesValue( "XMLXSLPLUGIN_EXAMPLE" ) || "default";
-
     # Get plugin debug flag
-    $debug = &TWiki::Func::getPreferencesFlag( "XMLXSLPLUGIN_DEBUG" ) || 1;
+    $debug = &TWiki::Func::getPreferencesFlag( "XMLXSLPLUGIN_DEBUG" ) || 0;
 
     # Plugin correctly initialized
-    # writeDebug( "- TWiki::Plugins::XmlXslPlugin::initPlugin( $web.$topic ) is OK" );
+    writeDebug( "- TWiki::Plugins::XmlXslPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
+
     return 1;
 }
 
@@ -91,7 +60,7 @@ sub endRenderingHandler
 {
 ### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
 
-  #  writeDebug( "- XmlXslPlugin::endRenderingHandler( $web.$topic )" );
+    writeDebug( "- XmlXslPlugin::endRenderingHandler( $web.$topic )" ) if $debug;
     # in here because we don't want our output HTML escaped.
     return unless ($_[0] =~ /%XMLXSLTRANSFORM/os);
 
@@ -99,7 +68,7 @@ sub endRenderingHandler
     my $ctr = 0;
     $_[0] =~ s/%XMLXSLTRANSFORM{(.*)}%/&applyXmlToXsl($1, $ctr++, $xmlstring)/geo;
 
-    $_[0] = $xmlstring . "</SCRIPT>" . $_[0];
+    $_[0] = $xmlstring . "</script>" . $_[0];
 
     # This handler is called by getRenderedVersion just after the line loop
 }
@@ -154,14 +123,14 @@ $xmlTopicText
 		if ($xmlTopicFile) {
 			#the topic does exist so read from the file
 			my ($xmlTopicMeta, $xmlTopicText) = &TWiki::Func::readTopic($xmlWebName, $xmlTopicName);
-			$xmldataisland = "<XML id=\"$id\">$xmlTopicText</XML>";
+			$xmldataisland = "<xml id=\"$id\">$xmlTopicText</xml>";
 		} else {
 			#the topic does not exist so put in anything
 			$xmldataisland = "<font size='+2'>The XML you specified, $xmlsource, does not exist </font>";	
 		}
 	}
 	if ($xslsource=~ /(^http|.xml|.xsl)/) {
-		$xsldataisland = "<XML id=\"style$id\" src=\"$xslsource\"></XML>";
+		$xsldataisland = "<xml id=\"style$id\" src=\"$xslsource\"></xml>";
 	} else {
 		#get the web name and the topic name
 		my ($xslWebName, $xslTopicName) = getWebTopic( $xslsource );
@@ -170,7 +139,7 @@ $xmlTopicText
 		if ($xslTopicFile) {
 			#the topic does exist so read from the file
 			my ($xslTopicMeta, $xslTopicText) = &TWiki::Func::readTopic($xslWebName, $xslTopicName);
-			$xsldataisland = "<XML id=\"style$id\">$xslTopicText</XML>";
+			$xsldataisland = "<xml id=\"style$id\">$xslTopicText</xml>";
 		} else {
 			#the topic does not exist so put in just the XML
 			$xsldataisland = "<font size='+2'>The XSL you specified, $xslsource, does not exist </font>";
@@ -179,7 +148,7 @@ $xmlTopicText
 	#get the script
 	$script = getScriptText($id);
 	#create division
-	$division = "<DIV id=\"showResult$id\"></DIV>";
+	$division = "<div id=\"showResult$id\"></div>";
 
 	$_[2] .= "\n<!--XMLASXSLSCRIPT START-->\n$script\n<!--XMLASXSLSCRIPT END-->\n";
 	return "\n<!--XMLASXSL START-->\n<!--CSS START-->\n$cssisland\n<!--CSS END-->\n<!--XMLDATAISLAND START-->\n$xmldataisland\n<!--XMLDATAISLAND END-->\n<!--XSLDATAISLAND START-->$xsldataisland\n<!--XSLDATAISLAND END-->\n<!--DIVISION START-->$division\n<!--DIVISION END-->\n<!--XMLASXSL END-->\n";
@@ -212,7 +181,7 @@ sub getScriptGenericText
    my ($sourceId) =  @_;
    $sourceId = $sourceId || "";
    my $script =<<'END';
-<SCRIPT event="onload" for="window"> // used to be  , but this causes problems if two invocations on the same page.
+<script event="onload" for="window"> // used to be  , but this causes problems if two invocations on the same page.
 // Parse error formatting function
 function reportParseError%SOURCE%(error)
 {
@@ -222,14 +191,14 @@ function reportParseError%SOURCE%(error)
   	}
   	r = "<font face=Verdana size=2><font size=4>XML Error loading '" + 
       	error.url + "'</font>" +
-      	"<P><B>" + error.reason + 
-      	"</B></P></font>";
+      	"<p><b>" + error.reason + 
+      	"</b></p></font>";
   	if (error.line > 0)
-    		r += "<font size=3><XMP>" +
+    		r += "<font size='3'><xmp>" +
     	"at line " + error.line + ", character " + error.linepos +
     	"\n" + error.srcText +
     	"\n" + s + "^" +
-    	"</XMP></font>";
+    	"</xmp></font>";
   	return r;
 }
 
@@ -237,7 +206,7 @@ function reportParseError%SOURCE%(error)
 function reportRuntimeError%SOURCE%(exception)
 {
   	return "<font face=Verdana size=2><font size=4>XSL Runtime Error</font>" +
-      	"<P><B>" + exception.description + "</B></P></font>";
+      	"<p><b>" + exception.description + "</b></p></font>";
 }
 END
    $script =~ s/%SOURCE%/$sourceId/g;
