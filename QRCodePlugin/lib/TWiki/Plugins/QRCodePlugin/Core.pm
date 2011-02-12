860ace9477896b980410c1da310e142a8571d453
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-02-12 21:27:45 +0000

    Item6647: Lazy loading of core and CPAN modules for better performance when QRCODE is not used - adding Core.pm
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20429 a00a5322-12db-0310-a70b-8735589c885e

---
+++ QRCodePlugin/lib/TWiki/Plugins/QRCodePlugin/Core.pm
@@ -0,0 +1,84 @@
+# Plugin module for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2001-2011 Peter Thoeny, peter@thoeny.org
+# Copyright (C) 2011 TWiki Contributors. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root of
+# this distribution. NOTE: Please extend that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For licensing info read LICENSE file in the TWiki root.
+
+package TWiki::Plugins::QRCodePlugin::Core;
+
+use strict;
+
+require TWiki::Func;    # The plugins API
+
+#==========================
+sub handleQRCODE {
+    my( $session, $params, $theTopic, $theWeb ) = @_;
+
+    my $text  = $params->{_DEFAULT};
+    my $pEcc  = $params->{ecc} || 'M';
+    my $pVer  = $params->{version} || '8';
+    my $pSize = $params->{size} || '4';
+
+    return "QRCode Plugin Error: QRCode text is missing." unless( $text );
+
+    require GD::Barcode::QRcode;
+    $pVer = 0 if( $pVer eq 'auto' );
+    my $image;
+    eval {
+        $image = GD::Barcode::QRcode->new( $text,
+            {ECC => $pEcc, Version => $pVer, ModuleSize => $pSize} )->plot->png;
+    };
+    return "QRCode Plugin Error: $@" if( $@ );
+
+    my( $dir, $fileName ) = _makeFilename( $theWeb, $theTopic, "$text-$pEcc-$pVer-$pSize" );
+
+    open( PNG, "> $dir/$fileName" )
+        or return "QRCode Plugin Error: Can't write temporary file $fileName.";
+    binmode( PNG );
+    print PNG $image;
+    close( PNG );
+
+    my $html = '<img src="' . TWiki::Func::getPubUrlPath()
+             . "/$theWeb/$theTopic/$fileName\" />";
+    return $html;
+}
+
+#==========================
+sub _makeFilename
+{
+    my ( $web, $topic, $text ) = @_;
+
+    my $dir = TWiki::Func::getPubDir() . "/$web";
+    unless( -e "$dir" ) {
+        umask( 002 );
+        mkdir( $dir, 0775 );
+    }
+    $dir .= "/$topic";
+    unless( -e "$dir" ) {
+        umask( 002 );
+        mkdir( $dir, 0775 );
+    }
+
+    use Digest::MD5  qw(md5_hex);
+    my $md5 = md5_hex( $text );
+
+    my $name = "_QRCodePlugin_$md5.png";
+
+    return( $dir, $name );
+}
+
+#==========================
+1;
