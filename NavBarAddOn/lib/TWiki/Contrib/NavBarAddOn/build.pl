5a78b08912b5efcc63a2d0b0e73665122e5090b1
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-03-11 07:53:46 +0000

    Item6660: Major add-on rewrite - adding build stuff
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20655 a00a5322-12db-0310-a70b-8735589c885e

---
+++ NavBarAddOn/lib/TWiki/Contrib/NavBarAddOn/build.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+BEGIN {
+    unshift @INC, split( /:/, $ENV{TWIKI_LIBS} );
+}
+use TWiki::Contrib::Build;
+
+$build = new TWiki::Contrib::Build('NavBarAddOn');
+
+$build->{UPLOADTARGETWEB} = 'Plugins';
+$build->{UPLOADTARGETPUB} = 'http://twiki.org/p/pub';
+$build->{UPLOADTARGETSCRIPT} = 'http://twiki.org/cgi-bin';
+$build->{UPLOADTARGETSUFFIX} = '';
+
+$build->build($build->{target});
