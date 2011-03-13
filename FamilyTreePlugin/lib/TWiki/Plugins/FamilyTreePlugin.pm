80fa8dff643106c0a8dfe82bee20595c018d1efb
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-03-13 05:17:43 +0000

    Item6638: Adding GPL header to Perl module
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20687 a00a5322-12db-0310-a70b-8735589c885e

--- FamilyTreePlugin/lib/TWiki/Plugins/FamilyTreePlugin.pm
+++ FamilyTreePlugin/lib/TWiki/Plugins/FamilyTreePlugin.pm
@@ -1,3 +1,19 @@
+# Plugin for TWiki Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2008 Crawford Currie 2005 http://c-dot.co.uk
+# Copyright (C) 2008-2011 TWiki Contributors. All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
+
 =pod
 
 ---+ package FamilyTreePlugin
