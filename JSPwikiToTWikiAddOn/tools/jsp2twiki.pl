1fc0972eaa2d2a63e694b6129e7ebf9e97ad4798
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-03-16 06:34:52 +0000

    Item6638: Doc fixes
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20748 a00a5322-12db-0310-a70b-8735589c885e

--- JSPwikiToTWikiAddOn/tools/jsp2twiki.pl
+++ JSPwikiToTWikiAddOn/tools/jsp2twiki.pl
@@ -1,3 +1,19 @@
+# Utility for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2003 TWiki:Main.NilsBoysen 
+# Copyright (C) 2008-2011 TWiki:TWiki.TWikiContributor
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
+
 #!/usr/bin/perl
 
 use strict;
