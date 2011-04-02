041e589e4bae9ef13915c09ce40c484cf7586257
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-04-02 22:29:36 +0000

    Item6674: TWiki-5.0.2 release - updating copyright
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20917 a00a5322-12db-0310-a70b-8735589c885e

--- TWikiNetSkinPlugin/lib/TWiki/Plugins/TWikiNetSkinPlugin.pm
+++ TWikiNetSkinPlugin/lib/TWiki/Plugins/TWikiNetSkinPlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (c) 2007-2010 TWiki Inc. All Rights Reserved.
+# Copyright (c) 2007-2011 TWiki Inc. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
