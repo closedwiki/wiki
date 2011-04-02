e861e043552ee6d9c2e718a218137dd42626e0ec
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-04-02 04:58:37 +0000

    Item6674: TWiki-5.0.2 release - updating copyright
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20853 a00a5322-12db-0310-a70b-8735589c885e

--- core/lib/TWiki/Store/SearchAlgorithms/PurePerl.pm
+++ core/lib/TWiki/Store/SearchAlgorithms/PurePerl.pm
@@ -1,6 +1,6 @@
 # Module of TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2007-2010 TWiki Contributors. All Rights Reserved.
+# Copyright (C) 2007-2011 TWiki Contributors. All Rights Reserved.
 # TWiki Contributors are listed in the AUTHORS file in the root
 # of this distribution. NOTE: Please extend that file, not this notice.
 #
