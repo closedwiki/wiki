64e96f5867942f47d67f7ef9dbccab399b88287e
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-04-02 22:09:57 +0000

    Item6674: TWiki-5.0.2 release - updating copyright
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20903 a00a5322-12db-0310-a70b-8735589c885e

--- SlideShowPlugin/lib/TWiki/Plugins/SlideShowPlugin/SlideShow.pm
+++ SlideShowPlugin/lib/TWiki/Plugins/SlideShowPlugin/SlideShow.pm
@@ -1,6 +1,6 @@
 # Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2002-2010 Peter Thoeny, peter@thoeny.org, and
+# Copyright (C) 2002-2011 Peter Thoeny, peter[at]thoeny.org, and
 # TWiki Contributors.
 #
 # This program is free software; you can redistribute it and/or
