b2d0784506d950674da3f1e407f2698174258ff6
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-04-02 01:59:42 +0000

    Item6674: TWiki-5.0.2 release - updating copyright
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20843 a00a5322-12db-0310-a70b-8735589c885e

--- core/lib/TWiki/Compatibility.pm
+++ core/lib/TWiki/Compatibility.pm
@@ -1,6 +1,6 @@
 # Module of TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 1999-2010 TWiki Contributors. All Rights Reserved.
+# Copyright (C) 1999-2011 TWiki Contributors. All Rights Reserved.
 # TWiki Contributors are listed in the AUTHORS file in the root of 
 # this distribution.
 # NOTE: Please extend that file, not this notice.
