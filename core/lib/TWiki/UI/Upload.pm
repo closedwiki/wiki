1f0bc8f59e881e31934822ba8e2189c9d499d1fc
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-04-02 04:30:04 +0000

    Item6674: TWiki-5.0.2 release - updating copyright
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20850 a00a5322-12db-0310-a70b-8735589c885e

--- core/lib/TWiki/UI/Upload.pm
+++ core/lib/TWiki/UI/Upload.pm
@@ -1,6 +1,6 @@
 # TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 1999-2010 Peter Thoeny, peter@thoeny.org
+# Copyright (C) 1999-2011 Peter Thoeny, peter[at]thoeny.org
 # and TWiki Contributors. All Rights Reserved. TWiki Contributors
 # are listed in the AUTHORS file in the root of this distribution.
 # NOTE: Please extend that file, not this notice.
