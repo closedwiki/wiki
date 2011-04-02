9a21e39e0eb0e681a542bab5e3de87f92c7622f0
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-04-02 21:40:56 +0000

    Item6674: TWiki-5.0.2 release - updating copyright
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20890 a00a5322-12db-0310-a70b-8735589c885e

--- InterwikiPlugin/lib/TWiki/Plugins/InterwikiPlugin.pm
+++ InterwikiPlugin/lib/TWiki/Plugins/InterwikiPlugin.pm
@@ -1,7 +1,8 @@
 # Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
-# Copyright (C) 2001-2007 Peter Thoeny, peter@thoeny.org
+# Copyright (C) 2001-2011 Peter Thoeny, peter[at]thoeny.org
+# Copyright (C) 2008-2011 TWiki Contributor
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
