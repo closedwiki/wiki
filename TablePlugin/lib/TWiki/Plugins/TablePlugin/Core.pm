c4d7eb95b41e17d75b4f33395cfbe50a7ffeda00
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-04-02 22:18:30 +0000

    Item6674: TWiki-5.0.2 release - updating copyright
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@20909 a00a5322-12db-0310-a70b-8735589c885e

--- TablePlugin/lib/TWiki/Plugins/TablePlugin/Core.pm
+++ TablePlugin/lib/TWiki/Plugins/TablePlugin/Core.pm
@@ -1,8 +1,8 @@
 # Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2001-2003 John Talintyre, jet@cheerful.com
-# Copyright (C) 2001-2010 Peter Thoeny, peter@thoeny.org
-# Copyright (C) 2005-2010 TWiki Contributors
+# Copyright (C) 2001-2011 Peter Thoeny, peter[at]thoeny.org
+# Copyright (C) 2005-2011 TWiki Contributors
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
