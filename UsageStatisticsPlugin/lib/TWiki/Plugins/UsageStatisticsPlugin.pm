3f75b897c588051eec6488cb41fb6f6d0bee7370
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-04-20 21:31:59 +0000

    Item6686: Initial version of UsageStatisticsPlugin - adding skeleton code, using lazy loading of code that expands %USERSTATISTICS{}%
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@21047 a00a5322-12db-0310-a70b-8735589c885e

--- UsageStatisticsPlugin/lib/TWiki/Plugins/UsageStatisticsPlugin.pm
+++ UsageStatisticsPlugin/lib/TWiki/Plugins/UsageStatisticsPlugin.pm
@@ -1,1 +1,70 @@
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2011 e-Ecosystems Inc
+# Copyright (C) 2011 TWiki Contributors
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# As per the GPL, removal of this notice is prohibited.
+
+package TWiki::Plugins::UsageStatisticsPlugin;
+
+
+# =========================
+use vars qw(
+        $web $topic $user $installWeb $VERSION $RELEASE $debug $core
+    );
+
+# Plugin version
+$VERSION = '$Rev$';
+$RELEASE = '2011-04-26';
+
+$moduleLoaded = 0;
+
+# =========================
+sub initPlugin
+{
+    ( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if( $TWiki::Plugins::VERSION < 1.1 ) {
+        TWiki::Func::writeWarning( "Version mismatch between UsageStatisticsPlugin and Plugins.pm" );
+        return 0;
+    }
+
+    # Get plugin debug flag
+    $debug = TWiki::Func::getPreferencesFlag( "USAGESTATISTICSPLUGIN_DEBUG" );
+
+    TWiki::Func::registerTagHandler( 'USAGESTATISTICS', \&_USAGESTATISTICS );
+
+    # Plugin correctly initialized
+    TWiki::Func::writeDebug( "- TWiki::Plugins::UsageStatisticsPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
+
+    return 1;
+}
+
+# =========================
+sub _USAGESTATISTICS
+{
+#   my ( $session, $params, $theTopic, $theWeb ) = @_;
+
+    # Lazy loading, e.g. compile core module only when needed
+    unless( $core ) {
+        require TWiki::Plugins::UsageStatisticsPlugin::Core;
+        $core = new TWiki::Plugins::UsageStatisticsPlugin::Core( $debug );
+    }
+    return $core->VarUSAGESTATISTICS( @_ );
+}
+
+# =========================
 1;
--- UsageStatisticsPlugin/lib/TWiki/Plugins/UsageStatisticsPlugin/Core.pm
+++ UsageStatisticsPlugin/lib/TWiki/Plugins/UsageStatisticsPlugin/Core.pm
@@ -1,1 +1,86 @@
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2011 e-Ecosystems Inc
+# Copyright (C) 2011 TWiki Contributors
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# As per the GPL, removal of this notice is prohibited.
+
+package TWiki::Plugins::UsageStatisticsPlugin::Core;
+
+# =========================
+sub new {
+    my ( $class, $debug ) = @_;
+
+    my $this = {
+          Debug          => $debug,
+        };
+    bless( $this, $class );
+    TWiki::Func::writeDebug( "- UsageStatisticsPlugin Core constructor" ) if $this->{Debug};
+
+    return $this;
+}
+
+# =========================
+sub VarUSAGESTATISTICS
+{
+    my ( $this, $session, $params ) = @_;
+
+    my $action = $params->{action} || '';
+    TWiki::Func::writeDebug( "- UsageStatisticsPlugin USAGESTATISTICS{\"$action\"}" ) if $this->{Debug};
+    if( $action eq 'overview' ) {
+        return $this->_overviewStats( $session, $params );
+    } elsif( $action eq 'user' ) {
+        return $this->_userStats( $session, $params );
+    } elsif( $action eq 'monthlist' ) {
+        return $this->_monthList( $session, $params );
+    } elsif( $action ) {
+        return "%<nop>USAGESTATISTICS{}% ERROR: Parameter =action=\"$action\"= is not supported"; 
+    } else {
+        return "%<nop>USAGESTATISTICS{}% ERROR: Parameter =action= is required";
+    }
+}
+
+# =========================
+sub _overviewStats
+{
+    my ( $this, $session, $params ) = @_;
+
+    my $text = "FIXME: Overview Stats";
+
+    return $text;
+}
+
+# =========================
+sub _userStats
+{
+    my ( $this, $session, $params ) = @_;
+    
+    my $text = "FIXME: User Stats";
+    
+    return $text;
+}
+
+# =========================
+sub _monthList
+{
+    my ( $this, $session, $params ) = @_;
+    
+    my $text = "FIXME: Month List";
+    
+    return $text;
+}
+
+# =========================
 1;
