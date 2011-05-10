ce31282220612b9007da6b80cf6f8baa7251a012
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-05-10 07:10:40 +0000

    Item6701: Almost rewrite of plugin, cleaning up to use TWiki Func API
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@21184 a00a5322-12db-0310-a70b-8735589c885e

--- URLCachePlugin/lib/TWiki/Plugins/URLCachePlugin.pm
+++ URLCachePlugin/lib/TWiki/Plugins/URLCachePlugin.pm
@@ -1,6 +1,7 @@
 # Plugin for TWiki Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2005 Arild Bergh
+# Copyright (C) 2008-2011 TWiki Contributors
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -27,15 +28,8 @@ use vars qw(
         $debug $exampleCfgVar
     );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
-
-# This is a free-form string you can use to "name" your own plugin version.
-# It is *not* used by the build automation tools, but is reported as part
-# of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = 'Dakar';
+$RELEASE = '2011-05-08';
 
 $pluginName = 'URLCachePlugin';  # Name of this Plugin
 
@@ -77,36 +71,38 @@ sub getDirectory
 }
 
 #create a basic text only page
-sub HTML2Twiki{
-    use Date::Format;
-    my @lt = localtime(time);
-    my $date = time2str("%C", time);
-    my $title;
-    my $url = $_[1];
+sub HTML2Twiki
+{
+    my $text = shift;
+    my $url = shift;
+
+    my $date = TWiki::Func::formatTime( time(), '$year-$mo-$day $hour:$min', 'servertime' );
     use HTML::FormatText; 
     use HTML::Parse;
-    my $x = parse_html($_[0]);
+    my $x = parse_html($text);
     my $ascii = HTML::FormatText->new->format($x); 
-    ($title) = $_[0] =~ m/<title.*?>(.*?)<\/title>/ig;
-    return "---+++$title\n\n---++++Downloaded from $url at $date\n\n$ascii";
+    my( $title ) = $text =~ m/<title.*?>(.*?)<\/title>/ig;
+    return "---+ $title\n\n__Note:__ Downloaded from $url on $date\n\n$ascii";
 }
 
 #download the page to be cached
-#tried to use LWP::Simple, but found that this will not work on some websites that require a 
-#user agent setting
-sub getPage{
-    use LWP::UserAgent;
-    my $ua = LWP::UserAgent->new; 
-    $ua->protocols_allowed( [ 'http', 'https'] ); 
-    $ua->agent("Mozilla/8.0"); 
-    $ua->timeout( 30 ); 
-    my $request = HTTP::Request->new(GET, $_[0]); 
-    $request->header('Accept' => 'text/html'); 
-    $res = $ua->request($request); 
-    if ( !$res->is_success ) { 
-        return ""; 
-    } else {
-        return $res->content;
+sub getPage
+{
+    my $url = shift;
+
+    my $response = TWiki::Func::getExternalResource( $url );
+    return "" if( $response->is_error() );
+    return $response->content();
+}
+
+#download the url and save to file
+sub mirrorUrl
+{
+    my $url = shift;
+    my $file = shift;
+    my $content = getPage( $url );
+    if( $content ) {
+        TWiki::Func::saveFile( $file, $content );
     }
 }
 
@@ -117,132 +113,99 @@ sub beforeSaveHandler
 #quit if this has the no-cache flag set
     if ($_[0] =~ /\Q<!--NOCACHE-->/){return;}
 #is this a wiki that is not to be cached?
-    my @ignore_webs = split(",", TWiki::Func::getPluginPreferencesValue("IGNORE"));
+    my @ignore_webs = split( /, */, TWiki::Func::getPluginPreferencesValue("IGNORE"));
     my @hit = grep /$_[2]/, @ignore_webs;
     if (@hit){return;}
-    
+
 # to avoid recursive cahcing we have a flag, so only the original page that is being saved will be processed
-    if ($ALREADYHERE == 1){return;}
+    return if( $ALREADYHERE );
     $ALREADYHERE = 1;
-    use LWP::Simple; 
-    use HTML::LinkExtor;
-    use File::Basename;
-    use Date::Format;
-    use Time::ParseDate;
-    my @lt = localtime(time);
-    my $date = time2str("%C", time);
-    my $urls = '(http|https)';
-    my $createtopic_urls = '(\+http|\+https)';
+
     my $ltrs = '\w';
     my $gunk = '/#~:.?+=&%@!\-,';
     my $punc = '.:?\-';
     my $any  = "${ltrs}${gunk}${punc}";
-    my (@l, @links, @createtopic_links, @pagelinks, $linkarray, @res, $tmp, $parser);
-    my ($htmlpage, $img, $oopsUrl, $page_content, $topic, $cachedfrom, $imageurl);
+    $_[0] =~ s/(^|\s)(\+?)(https? : [$any] +? )(?=[$punc]*[^$any]|$)/$1 . _cacheURL( $2, $3, $_[2], $_[1] )/igex;
 
-# pick up all the URLs to cache (i.e. stored locally with a link to them)
-    (@res) = $_[0] =~ m/(^|\s)($urls : [$any] +? )(?=[$punc]*[^$any]|$)/igx;
-#we get back three units for each URL, not yet sure how to turn this off (if indeed you can)
-    if ($res[0]){ 
-        for ($i = 0; $i < @res; $i++){
-            if( $res[$i] =~ /^(.{10}.*)$/ ){
-                push( @links, $1 );
-            }
-        }
-    }
-    
-# now get the links that want to be stored as new topics
-    (@res) = $_[0] =~ m/(^|\s)($createtopic_urls : [$any] +? )(?=[$punc]*[^$any]|$)/igx;
-    if ($res[0]){ 
-        for ($i = 0; $i < @res; $i++){
-            if( $res[$i] =~ /^.(.{10}.*)$/ ) {
-                push( @createtopic_links, $1 );
-            }
-        }
-    }
+    #reset the flag
+    $ALREADYHERE = 0;
+}
 
-        # use Data:Dumper;
-        # print STDERR Dumper( @createtopic_links );
-
-#if none we exit at this point
-    if (@links == 0 && @createtopic_links == 0) {
-        $ALREADYHERE = 0;
-        return; 
-    }
+sub _cacheURL
+{
+    my( $plus, $url, $web, $topic ) = @_;
 
 #set some inital variables
-    my $twiki_pubdir = getDirectory($_[2], $_[1]);
-    my $twiki_url = TWiki::Func::getUrlHost() . TWiki::Func::getPubUrlPath() . "/".$_[2]. "/".$_[1];    
-    my $twiki_thisurl = TWiki::Func::getViewUrl($_[2], $_[1]);
-    my $topic_header = "%META:TOPICINFO{author=\"".TWiki::Func::getWikiName()."\" date=\"".time."\" format=\"1.0\" version=\"1.0\"}%\n%META:TOPICPARENT{name=\"".$_[1]."\"}%\n";
-    
+    my $twiki_pubdir = getDirectory( $web, $topic );
+    my $twiki_url = TWiki::Func::getUrlHost() . TWiki::Func::getPubUrlPath() . "/$web/$topic";
+    my $twiki_thisurl = TWiki::Func::getViewUrl($web, $topic );
+
+    if( $plus ) {
+#this is a +http url.
 #first we do the ones that is to be new topics (i.e. searchable)
 #here we strip off the tag, keep the title at top and make a note of where it's come from
-    for ($i = 0; $i < @createtopic_links; $i++){
-        $page_content = getPage($createtopic_links[$i]);
-        if ( $page_content =~ /^(..*)$/ ) {
-            $htmlpage = HTML2Twiki($1, $createtopic_links[$i]);
+        my $page_content = getPage( $url );
+        if( $page_content =~ /^(.+)$/s ) {
+            my $htmlpage = HTML2Twiki( $1, $url );
 #whenever we have a successful download we first save the new topic
 # and then do a search and replace and replace the link to the new topic
-            $topic = substr($createtopic_links[$i], 6);
-            $topic =~ s/\W/ /g;
-            $topic =~ s/(\w*) /\u\L$1/g; 
-            TWiki::Func::saveTopicText( $_[2], $topic, $topic_header . $htmlpage, 1, 1); # save topic text 
-            if( !$oopsUrl ) { 
-                $tmp = $createtopic_links[$i];
-                $tmpnew = "[[" . $topic . "][" . $tmp . "]]";
-                $_[0] =~ s/\+\Q$tmp/$tmpnew/igx;
+            my $newTopic = substr( $url, 6 );
+            $newTopic =~ s/\W/ /g;
+            $newTopic =~ s/(\w*) /\u\L$1/g;
+            my $oops = TWiki::Func::saveTopicText( $web, $newTopic, $topic_header . $htmlpage, 1, 1); # save topic text 
+            unless( $oops ) {
+                return "[[$newTopic][$url]]";
             }
         }
-    }
-    
+
+    } else {
+#normal http url.
 #here we proces the list of files that we want to cahce separately
 #we download each page and change the image & stylesheet links in it
-    for ($i = 0; $i < @links; $i++){
-        $htmlpage = getPage($links[$i]);
-        if (length($htmlpage) > 0){
-            $parser = HTML::LinkExtor->new(undef);
-            $parser->parse($htmlpage);
-            @pagelinks = $parser->links;
-            $img = basename($links[$i]);
-            $imageurl = $links[$i];
-            if ($img){
+        my $htmlpage = getPage( $url );
+        if( $htmlpage ) {
+            use HTML::LinkExtor;
+            my $parser = HTML::LinkExtor->new(undef);
+            $parser->parse( $htmlpage );
+            my @pagelinks = $parser->links;
+            use File::Basename;
+            my $img = basename( $url );
+            my $imageurl = $url;
+            if( $img ) {
                 $imageurl =~ s/\Q$img//;
             }
-            foreach $linkarray (@pagelinks) {
+            foreach my $linkarray ( @pagelinks ) {
                 my @element = @$linkarray;
                 my $elt_type = shift @element;
-                while (@element) {
+                while( @element ) {
                     my ($attr_name, $attr_value) = splice(@element, 0, 2);
                     if (($elt_type eq 'img' && $attr_name eq 'src') || ($elt_type eq 'link' && $attr_name eq 'href')) {
                         $img = basename($attr_value);
 #check if this image link already has a full URL in it
-                        if ($attr_value =~ /http/){
-                            mirror($attr_value, $twiki_pubdir. "/" . $img); 
+                        if ($attr_value =~ /^http/){
+                            mirrorUrl( $attr_value, "$twiki_pubdir/$img" );
                             $htmlpage =~ s/\Q$attr_value/$img/igx;
                         } else {
-                            mirror("$imageurl$attr_value", $twiki_pubdir. "/" . $img); 
+                            mirrorUrl( "$imageurl$attr_value", "$twiki_pubdir/$img " );
                             $htmlpage =~ s/\Q$attr_value/$img/igx;
                         }
                     }
                 }
             }
 #finally we save the modified HTML page
-            $topic = substr($links[$i], 6);
-            $topic =~ s/\W//g;
+            my $file = substr( $url, 6 );
+            $file =~ s/\W//g;
 #here we add a line at the top of the page showing that it's cached
-            $cachedfrom = "<div style='text-align: center;'><pre>Downloaded from <a href='$links[$i]'>$links[$i]</a>\non $date\n<a href='$twiki_thisurl'>Return to Twiki</a></pre></div><hr>";
+            my $cachedfrom = "<div style='text-align: center;'><pre>Downloaded from <a href='$url'>$url</a>\n"
+                           . "on $date\n<a href='$twiki_thisurl'>Return to TWiki</a></pre></div><hr />";
             $htmlpage =~ s/(<body.*?>)(.*?)/$1$2$cachedfrom/ig;
-            TWiki::Func::saveFile( $twiki_pubdir. "/" . $topic . ".html", $htmlpage);
+            TWiki::Func::saveFile( "$twiki_pubdir/$file.html", $htmlpage );
 #whenever we have a successful download we do a search and replace and replace the link to the local
 #cache rather than the online version [[ToRead][http://abergh.com/webmail]]
-            $tmp = $links[$i];
-            $tmpnew = "[[" . $twiki_url . "/" . $topic . ".html][" . $tmp . "]]";
-            $_[0] =~ s/(^|\s)\Q$tmp/$1$tmpnew/igx;
+            return "[[$twiki_url/$file.html][$url]]";
         }
     }
-    #reset the flag
-    $ALREADYHERE = 0;
+    return $url;
 }
 
 1;
