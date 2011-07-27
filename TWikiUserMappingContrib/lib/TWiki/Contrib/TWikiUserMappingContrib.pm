97080e3c64e30ccb3e4846252d61e9dc53be6476
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-07-27 23:12:45 +0000

    Item6778: Usability: Use PreferencesPlugin to edit user profile settings
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@21848 a00a5322-12db-0310-a70b-8735589c885e

--- TWikiUserMappingContrib/lib/TWiki/Contrib/TWikiUserMappingContrib.pm
+++ TWikiUserMappingContrib/lib/TWiki/Contrib/TWikiUserMappingContrib.pm
@@ -20,6 +20,6 @@ package TWiki::Contrib::TWikiUserMappingContrib;
 use strict;
 
 our $VERSION = '$Rev$';
-our $RELEASE = '2011-07-26';
+our $RELEASE = '2011-07-27';
 our $SHORTDESCRIPTION = 'Traditional TWiki topic based user management and user mapping';
 
