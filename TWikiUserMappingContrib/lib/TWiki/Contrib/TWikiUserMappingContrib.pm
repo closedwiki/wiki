641c15f2433a2ab700b54706364c1814537aca8d
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2011-08-01 22:49:56 +0000

    Item6782: Usability: Add search filter to UserList - updating docs
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@21868 a00a5322-12db-0310-a70b-8735589c885e

--- TWikiUserMappingContrib/lib/TWiki/Contrib/TWikiUserMappingContrib.pm
+++ TWikiUserMappingContrib/lib/TWiki/Contrib/TWikiUserMappingContrib.pm
@@ -20,6 +20,6 @@ package TWiki::Contrib::TWikiUserMappingContrib;
 use strict;
 
 our $VERSION = '$Rev$';
-our $RELEASE = '2011-07-28';
+our $RELEASE = '2011-08-01';
 our $SHORTDESCRIPTION = 'Traditional TWiki topic based user management and user mapping';
 
