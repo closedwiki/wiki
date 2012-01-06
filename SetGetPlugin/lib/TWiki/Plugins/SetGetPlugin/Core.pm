5e5688ebc822b415d5ef84e4550098a9add21fd3
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-01-06 21:58:35 +0000

    Item6828: Removing write to persistent if already exists, and adding data dump - small follow-up fix: Use proper XHTML; use year 2012 for (C)
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@22462 a00a5322-12db-0310-a70b-8735589c885e

--- SetGetPlugin/lib/TWiki/Plugins/SetGetPlugin/Core.pm
+++ SetGetPlugin/lib/TWiki/Plugins/SetGetPlugin/Core.pm
@@ -1,7 +1,7 @@
 # Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2010-2011 Peter Thoeny, peter[at]thoeny.org
-# Copyright (C) 2010-2011 TWiki Contributors
+# Copyright (C) 2010-2012 Peter Thoeny, peter[at]thoeny.org
+# Copyright (C) 2010-2012 TWiki Contributors
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -57,7 +57,7 @@ sub VarDUMP
     if( defined $params->{format} ) {
         $format = $params->{format};
     } else {
-        $format = "key: \$key, value: \$value <br>";
+        $format = "key: \$key, value: \$value <br />";
     }
 
     if( defined $params->{separator} ) {
