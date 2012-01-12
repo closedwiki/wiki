fd55692fa7845c2c746ed83869fc237583cd8b37
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-01-12 01:36:01 +0000

    Item6796: Fixing copyright year to 2012
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@22477 a00a5322-12db-0310-a70b-8735589c885e

--- core/lib/TWiki/Net.pm
+++ core/lib/TWiki/Net.pm
@@ -1,6 +1,6 @@
 # Module of TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2001-2011 Peter Thoeny, peter[at]thoeny.org
+# Copyright (C) 2001-2012 Peter Thoeny, peter[at]thoeny.org
 # and TWiki Contributors. All Rights Reserved. TWiki Contributors
 # are listed in the AUTHORS file in the root of this distribution.
 # NOTE: Please extend that file, not this notice.
--- core/lib/TWiki/Plugins.pm
+++ core/lib/TWiki/Plugins.pm
@@ -1,7 +1,7 @@
 # Module of TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2000-2001 Andrea Sterbini, a.sterbini@flashnet.it
-# Copyright (C) 1999-2011 Peter Thoeny, peter[at]thoeny.org
+# Copyright (C) 1999-2012 Peter Thoeny, peter[at]thoeny.org
 # and TWiki Contributors. All Rights Reserved. TWiki Contributors
 # are listed in the AUTHORS file in the root of this distribution.
 # NOTE: Please extend that file, not this notice.
