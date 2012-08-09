69b0cc58d8231ff240e7c00c1030c3d59456018f
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-08-09 23:49:20 +0000

    Item6907: Initial version of PercentCompletePlugin - adding code for form field type "percent"
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@23130 a00a5322-12db-0310-a70b-8735589c885e

--- PercentCompletePlugin/lib/TWiki/Form/Percent.pm
+++ PercentCompletePlugin/lib/TWiki/Form/Percent.pm
@@ -1,1 +1,67 @@
-# Placeholder module
+# Module of TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2012 Peter Thoeny, peter[at]thoeny.org
+# Copyright (C) 2012 TWiki Contributors. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root of
+# this distribution. NOTE: Please extend that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# As per the GPL, removal of this notice is prohibited.
+#
+# This packages subclasses TWiki::Form::FieldDefinition to implement
+# the =percent= type
+
+package TWiki::Form::Percent;
+use base 'TWiki::Form::FieldDefinition';
+
+use strict;
+
+# ========================================================
+sub new {
+    my $class = shift;
+    my $this = $class->SUPER::new( @_ );
+    my $size = $this->{size} || '0';
+    $size =~ s/[^\d]//g;
+    $size = 8 if( !$size || $size < 1 );
+    $this->{size} = $size;
+    return $this;
+}
+
+# ========================================================
+sub renderForDisplay {
+    my ( $this, $format, $value ) = @_;
+
+    require TWiki::Plugins::PercentCompletePlugin;
+    my $text = TWiki::Plugins::PercentCompletePlugin::renderForDisplay( $value );
+    $format =~ s/\$title/$this->{title}/g;
+    $format =~ s/\$value/$text/g;
+
+    return $format;
+}
+
+# ========================================================
+sub renderForEdit {
+    my( $this, $web, $topic, $value ) = @_;
+
+    require TWiki::Plugins::PercentCompletePlugin;
+#    TWiki::Plugins::PercentCompletePlugin::addHEAD();
+    my $text = TWiki::Plugins::PercentCompletePlugin::renderForEdit( $this->{name}, $value );
+
+    my $session = $this->{session};
+    $text = $session->renderer->getRenderedVersion(
+        $session->handleCommonTags( $text, $web, $topic ));
+
+    return ( '', $text );
+}
+
+# ========================================================
+1;
