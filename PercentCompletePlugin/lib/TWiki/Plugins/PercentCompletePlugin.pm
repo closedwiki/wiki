c22cc8f4ac8b488e3348385aa50ea4257e9f5c89
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-08-10 07:42:13 +0000

    Item6907: Initial version of !PercentCompletePlugin - small doc fixes; better installation instructions
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@23155 a00a5322-12db-0310-a70b-8735589c885e

--- PercentCompletePlugin/lib/TWiki/Plugins/PercentCompletePlugin.pm
+++ PercentCompletePlugin/lib/TWiki/Plugins/PercentCompletePlugin.pm
@@ -32,7 +32,7 @@ require TWiki::Func;    # The plugins API
 
 # ========================================================
 our $VERSION = '$Rev$';
-our $RELEASE = '2011-08-09';
+our $RELEASE = '2011-08-10';
 our $SHORTDESCRIPTION = "Percent complete selector, for use in TWiki forms and TWiki applications";
 our $NO_PREFS_IN_TOPIC = 1;
 our $headerDone;
