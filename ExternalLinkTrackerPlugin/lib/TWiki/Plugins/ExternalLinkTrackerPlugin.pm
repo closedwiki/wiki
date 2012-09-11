688918542f0518c9162bf373df03c9edaaac7fb6
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-09-11 05:20:56 +0000

    Item6926: initial version of ExternalLinkTrackerPlugin - implementation of plugin module; lazy loading of core module handling EXLINK; simple link without tracking done
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@23309 a00a5322-12db-0310-a70b-8735589c885e

--- ExternalLinkTrackerPlugin/lib/TWiki/Plugins/ExternalLinkTrackerPlugin.pm
+++ ExternalLinkTrackerPlugin/lib/TWiki/Plugins/ExternalLinkTrackerPlugin.pm
@@ -1,1 +1,73 @@
-# work in progress
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2012 FIXME
+# Copyright (C) 2012 TWiki Contributors. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root
+# of this distribution.
+# NOTE: Please extend that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For licensing info read LICENSE file in the TWiki root.
+
+package TWiki::Plugins::ExternalLinkTrackerPlugin;
+
+use strict;
+
+require TWiki::Func;    # The plugins API
+require TWiki::Plugins; # For the API version
+
+#==================================================================
+our $VERSION = '$Rev$';
+our $RELEASE = '2012-09-10';
+our $SHORTDESCRIPTION = 'Track and report on users clicking on external links';
+our $NO_PREFS_IN_TOPIC = 1;
+
+my $core;
+
+#==================================================================
+sub initPlugin {
+
+    $core = undef;
+    TWiki::Func::registerTagHandler( 'EXLINK', \&_EXLINK );
+
+    # Plugin correctly initialized
+    return 1;
+}
+
+#==================================================================
+sub _EXLINK {
+    my( $session, $params ) = @_;
+
+    # delay loading core module until run-time
+    unless( $core ) {
+        my $type = 'cgi';
+        if( $session && $session->can( 'inContext' ) ) {
+            $type = 'cli' if( $session->inContext( 'command_line' ) );
+        } elsif( ! $ENV{GATEWAY_INTERFACE} && ! $ENV{MOD_PERL} ) {
+            $type = 'cli';
+        }
+        require TWiki::Plugins::ExternalLinkTrackerPlugin::Core;
+        my $cfg = {
+          ScriptType => $type,
+        };
+        $core = new TWiki::Plugins::ExternalLinkTrackerPlugin::Core( $cfg );
+    }
+    my $query = TWiki::Func::getCgiQuery();
+    foreach my $key ( $query->param ) {
+        next if( defined $params->{$key} );
+        $params->{$key} = $query->param( $key );
+    }
+    return $core->EXLINK( $params );
+}
+
+#==================================================================
+1;
--- ExternalLinkTrackerPlugin/lib/TWiki/Plugins/ExternalLinkTrackerPlugin/Core.pm
+++ ExternalLinkTrackerPlugin/lib/TWiki/Plugins/ExternalLinkTrackerPlugin/Core.pm
@@ -1,1 +1,100 @@
-# work in progress
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2012 FIXME
+# Copyright (C) 2012 TWiki Contributors. All Rights Reserved.
+# TWiki Contributors are listed in the AUTHORS file in the root
+# of this distribution.
+# NOTE: Please extend that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For licensing info read LICENSE file in the TWiki root.
+
+package TWiki::Plugins::ExternalLinkTrackerPlugin::Core;
+
+use strict;
+
+our $defTopic = 'ExternalLinkTrackerDefinition';
+
+#==================================================================
+sub new {
+    my ( $class, $this ) = @_;
+
+    $this->{Debug} = $TWiki::cfg{Plugins}{ExternalLinkTrackerPlugin}{Debug} || 0;
+
+    bless( $this, $class );
+
+    $this->_loadLinkDefinition();
+    $this->_writeDebug( "constructor" );
+
+    return $this;
+}
+
+#==================================================================
+sub _loadLinkDefinition {
+    my( $this ) = @_;
+
+    my $text = TWiki::Func::readTopic( $TWiki::cfg{UsersWebName}, $defTopic );
+    foreach my $line ( split ( /\n/, $text ) ) {
+        if( $line =~ /^ *\| *([A-Za-z0-9\-_\.]+) * \| *(.*?) *\| *(.*?) *\|/ ) {
+            my $id = lc( $1 );
+            $this->{Def}{$id}{Name} = $2;
+            $this->{Def}{$id}{URL} = $3;
+        }
+    }
+    if( $this->{Debug} ) {
+        require Data::Dumper;
+        $this->_writeDebug( Data::Dumper::Dumper( $this->{Def} ) );
+    }
+}
+
+#==================================================================
+sub EXLINK {
+    my( $this, $params ) = @_;
+
+    my $action = $params->{action} || '';
+    $this->{Debug} = 1 if( $action eq 'debug' );
+    my $text = '';
+
+    if( ! $action ) {
+        my $id = lc( $params->{_DEFAULT} );
+        if( $this->{Def}{$id} ) {
+            # TODO: Link to redirect URL
+            $text = '[['
+                  . $this->{Def}{$id}{URL}
+                  . ']['
+                  . $this->{Def}{$id}{Name}
+                  . ']]';
+        } else {
+            $text = '(Link ID not found)';
+        }
+
+    } elsif( $action = 'redirect' ) {
+        # TODO: record link action and redirect to link target
+        return '';
+    }
+    return $text;
+}
+
+#==================================================================
+sub _writeDebug {
+    my( $this, $text ) = @_;
+
+    return unless( $this->{Debug} );
+    if( $this->{ScriptType} eq 'cli' ) {
+        print "DEBUG ExternalLinkTrackerPlugin: $text\n";
+    } else {
+        TWiki::Func::writeDebug( "- ExternalLinkTrackerPlugin: $text" );
+    }
+}
+
+#==================================================================
+1;
