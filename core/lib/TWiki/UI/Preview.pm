9b5fe65b347157012b61aaa5985207ea992fd0f7
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-10-16 20:21:11 +0000

    Item6992: Removing junk, SETTINGSTOPIC is not set or used anywhere
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@23664 a00a5322-12db-0310-a70b-8735589c885e

--- core/lib/TWiki/UI/Preview.pm
+++ core/lib/TWiki/UI/Preview.pm
@@ -120,8 +120,6 @@ sub preview {
     my $templatetopic = $query->param( 'templatetopic');
     #ASSERT($templatetopic ne '%TEMPLATETOPIC%') if DEBUG;
     $tmpl =~ s/%TEMPLATETOPIC%/$templatetopic/go;
-    #this one's worrying, its special, and not set much at all
-    #$tmpl =~ s/%SETTINGSTOPIC%/$settingstopic/go;
     my $newtopic = $query->param( 'newtopic' );
     #ASSERT($newtopic ne '%NEWTOPIC%') if DEBUG;
     $tmpl =~ s/%NEWTOPIC%/$newtopic/go;
