15b79c77284e3d75b276205fe04b45955cf7323e
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-11-15 06:58:09 +0000

    Item7020: Categorize TWiki Variables - updating plugin docs
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@24059 a00a5322-12db-0310-a70b-8735589c885e

--- SendMailPlugin/lib/TWiki/Plugins/SendMailPlugin.pm
+++ SendMailPlugin/lib/TWiki/Plugins/SendMailPlugin.pm
@@ -27,7 +27,7 @@ require TWiki::Func;    # The plugins API
 require TWiki::Plugins; # For the API version
 
 our $VERSION = '$Rev$';
-our $RELEASE = '2012-06-08';
+our $RELEASE = '2012-11-14';
 
 # One line description, is shown in the %SYSTEMWEB%.TextFormattingRules topic:
 our $SHORTDESCRIPTION = 'Send e-mail from actions in TWiki topics, useful for workflow automation';
