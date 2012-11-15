35db921ff7e6c83b37c352d582207f430b51f2ba
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-11-15 07:29:47 +0000

    Item7020: Categorize TWiki Variables - updating plugin docs
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@24077 a00a5322-12db-0310-a70b-8735589c885e

--- VarCachePlugin/lib/TWiki/Plugins/VarCachePlugin.pm
+++ VarCachePlugin/lib/TWiki/Plugins/VarCachePlugin.pm
@@ -23,7 +23,7 @@ use strict;
 
 # =========================
 our $VERSION = '$Rev$';
-our $RELEASE = '2012-09-07';
+our $RELEASE = '2012-11-14';
 
 # =========================
 our $SHORTDESCRIPTION = 'Cache TWiki variables in selected topics for faster page rendering';
