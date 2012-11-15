e0ddb537924f9c51f138046abed00f5a6d53e4d3
HideyoImazu <HideyoImazu@a00a5322-12db-0310-a70b-8735589c885e>
2012-11-15 07:18:43 +0000

    Item7039: SCRIPTURL and SCRIPTURLPATH now takes web and topic parameters. Fn_SCRIPTURL.pm now tests those parameters.
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@24072 a00a5322-12db-0310-a70b-8735589c885e

--- UnitTestContrib/test/unit/Fn_SCRIPTURL.pm
+++ UnitTestContrib/test/unit/Fn_SCRIPTURL.pm
@@ -30,6 +30,43 @@ sub test_SCRIPTURL {
     $this->assert_str_equals("$TWiki::cfg{ScriptUrlPath}/view.dot", $result);
 
     $result = $this->{twiki}->handleCommonTags(
+        '%SCRIPTURLPATH{"view" web="Foo"}%',
+        $this->{test_web}, $this->{test_topic});
+    $this->assert_str_equals("$TWiki::cfg{ScriptUrlPath}/view.dot/Foo",
+                             $result);
+
+    $result = $this->{twiki}->handleCommonTags(
+        '%SCRIPTURLPATH{"view" web="Foo.Bar"}%',
+        $this->{test_web}, $this->{test_topic});
+    $this->assert_str_equals("$TWiki::cfg{ScriptUrlPath}/view.dot/Foo/Bar",
+                             $result);
+
+    $result = $this->{twiki}->handleCommonTags(
+        '%SCRIPTURLPATH{"view" web="Foo" topic="TestTopic"}%',
+        $this->{test_web}, $this->{test_topic});
+    $this->assert_str_equals(
+        "$TWiki::cfg{ScriptUrlPath}/view.dot/Foo/TestTopic", $result);
+
+    $result = $this->{twiki}->handleCommonTags(
+        '%SCRIPTURLPATH{"view" topic="TestTopic"}%',
+        $this->{test_web}, $this->{test_topic});
+    $this->assert_str_equals(
+        "$TWiki::cfg{ScriptUrlPath}/view.dot/$this->{test_web}/TestTopic",
+        $result);
+
+    $result = $this->{twiki}->handleCommonTags(
+        '%SCRIPTURLPATH{"view" topic="Foo.TestTopic"}%',
+        $this->{test_web}, $this->{test_topic});
+    $this->assert_str_equals(
+        "$TWiki::cfg{ScriptUrlPath}/view.dot/Foo/TestTopic", $result);
+
+    $result = $this->{twiki}->handleCommonTags(
+        '%SCRIPTURLPATH{"view" web="Bar" topic="Foo.TestTopic"}%',
+        $this->{test_web}, $this->{test_topic});
+    $this->assert_str_equals(
+        "$TWiki::cfg{ScriptUrlPath}/view.dot/Foo/TestTopic", $result);
+
+    $result = $this->{twiki}->handleCommonTags(
         "%SCRIPTURLPATH{snarf}%", $this->{test_web}, $this->{test_topic});
     $this->assert_str_equals("sausages", $result);
 }
