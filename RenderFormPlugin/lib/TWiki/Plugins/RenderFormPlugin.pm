393681cb32c6da4e52b2b9bc453ea69356ae9728
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-12-03 06:19:58 +0000

    Item7020: Add category to VarRENDERFORM - updating docs & additinal doc enhancements
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@24295 a00a5322-12db-0310-a70b-8735589c885e

--- RenderFormPlugin/lib/TWiki/Plugins/RenderFormPlugin.pm
+++ RenderFormPlugin/lib/TWiki/Plugins/RenderFormPlugin.pm
@@ -1,12 +1,29 @@
+# Plugin for TWiki Enterprise Collaboration Platform, http://TWiki.org/
+#
+# Copyright (C) 2008-2010 TWiki:Main.DanielRohde
+# Copyright (C) 2008-2012 TWiki Contributors
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# As per the GPL, removal of this notice is prohibited.
+
 package TWiki::Plugins::RenderFormPlugin;
 
 use strict;
 
 use vars qw( $VERSION $RELEASE $REVISION $debug $pluginName );
 
-$VERSION = '$Rev: 17581 (05 Oct 2008) $';
-
-$RELEASE = 'Dakar';
+$VERSION = '$Rev$';
+$RELEASE = '2012-12-02';
 
 
 $REVISION = '1.004'; #dro# fixed TWiki 5.0 problems
