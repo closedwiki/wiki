158625a5762e87395e6a33c0c722440644a5ef5c
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2012-12-17 01:37:56 +0000

    Item7077: Initial version of DatePickerPlugin - adding sponsor info
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@24523 a00a5322-12db-0310-a70b-8735589c885e

--- DatePickerPlugin/lib/TWiki/Plugins/DatePickerPlugin.pm
+++ DatePickerPlugin/lib/TWiki/Plugins/DatePickerPlugin.pm
@@ -38,7 +38,7 @@ require TWiki::Func;    # The plugins API
 
 # ========================================================
 our $VERSION = '$Rev$';
-our $RELEASE = '2012-12-12';
+our $RELEASE = '2012-12-16';
 our $SHORTDESCRIPTION = "Pop-up calendar with date picker, for use in TWiki forms, HTML forms and TWiki plugins";
 our $NO_PREFS_IN_TOPIC = 1;
 
