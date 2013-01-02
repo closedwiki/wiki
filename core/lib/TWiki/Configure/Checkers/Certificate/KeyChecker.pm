4824a9ef9d64bb74864e1ebd6e9ca04323389c3a
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2013-01-02 06:40:36 +0000

    Item7088: Update copyright to 2013 - Happy New Year!
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@24647 a00a5322-12db-0310-a70b-8735589c885e

--- core/lib/TWiki/Configure/Checkers/Certificate/KeyChecker.pm
+++ core/lib/TWiki/Configure/Checkers/Certificate/KeyChecker.pm
@@ -1,9 +1,28 @@
-# -*- mode: CPerl; -*-
-# TWiki off-line task management framework addon for the TWiki Enterprise Collaboration Platform, http://TWiki.org/
+# TWiki Enterprise Collaboration Platform, http://TWiki.org/
 #
-# Copyright (C) 2011 Timothe Litt <litt at acm dot org>
-# License is at end of module.
-# Removal of Copyright and/or License prohibited.
+# Copyright (C) 2011-2013 Timothe Litt <litt at acm dot org>
+# Copyright (C) 2011-2013 Peter Thoeny, peter[at]thoeny.org
+# and TWiki Contributors. All Rights Reserved. TWiki Contributors
+# are listed in the AUTHORS file in the root of this distribution.
+# NOTE: Please extend that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+#
+# As per the GPL, removal of this notice is prohibited.
+#
+# This is an original work by Timothe Litt.
+#
+# TWiki off-line task management framework addon
+# -*- mode: CPerl; -*-
 
 use strict;
 use warnings;
@@ -224,18 +243,5 @@ Key Information: %s %s %s key", ucfirst $key->{encrypted}, $key->{type}, $key->{
     $sts .= $this->ERROR( $errors ) if( $errors );
     return $sts;
 }
+
 1;
-__END__
-
-This is an original work by Timothe Litt.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 3
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details, published at
-http://www.gnu.org/copyleft/gpl.html
