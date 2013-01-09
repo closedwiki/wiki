3843a76dd6a94ff04ffeee22c702135bf260c694
PeterThoeny <PeterThoeny@a00a5322-12db-0310-a70b-8735589c885e>
2013-01-09 03:19:53 +0000

    Item7122: Caching of Efetch records - thanks IanHolmes
    git-svn-id: http://develop.twiki.org/svn/twiki/trunk@24742 a00a5322-12db-0310-a70b-8735589c885e

--- EFetchPlugin/lib/TWiki/Plugins/EFetchPlugin.pm
+++ EFetchPlugin/lib/TWiki/Plugins/EFetchPlugin.pm
@@ -1,7 +1,7 @@
 # Plugin for TWiki Collaboration Platform, http://TWiki.org/
 #
 # Copyright (C) 2004 Cole Beck, cole.beck@vanderbilt.edu
-# Copyright (C) 2004-2010 TWiki:TWiki.TWikiContributor
+# Copyright (C) 2004-2012 TWiki:TWiki.TWikiContributor
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -29,13 +29,43 @@ package TWiki::Plugins::EFetchPlugin;
 use vars qw(
         $web $topic $user $installWeb $VERSION $RELEASE $pluginName
         $debug $doOldInclude $renderingWeb
+        %pubmedCache $pubmedCacheFilename $pubmedCacheLockfile
+        $retrievalError $maxAuthors
     );
 
 $VERSION = '$Rev$';
-$RELEASE = '2010-12-12';
+$RELEASE = '2012-13-08';
 
 $pluginName = 'EFetchPlugin';  # Name of this Plugin
+$maxAuthors = 10;  # IH, 9/5/07
 use LWP::Simple;
+use DB_File;  # IH, 9/5/2007
+
+# wrapper for LWP's get method that caches results -- IH, 9/5/2007
+$pubmedCacheFilename = $TWiki::cfg{DataDir} . "/" . $pluginName . ".dbm";
+$pubmedCacheLockfile = $pubmedCacheFilename . ".lock";
+$retrievalError = "Document retrieval error";
+
+sub getFromPubmedCache {
+    my ($pmid, $rettype) = @_;
+    my $url = "http://www.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?rettype=$rettype&retmode=text&db=pubmed&id=$pmid";
+    my $key = "$pmid/$rettype";
+    if (exists $pubmedCache{$key}) {
+	TWiki::Func::writeDebug( "- ${pluginName}::getFromPubmedCache( $pmid, $rettype ): found in cache" ) if $debug;
+	return $pubmedCache{$key};
+    }
+    my $ret;
+    for (my $retry = 0; $retry < 4; ++$retry) {
+	$ret = get($url);
+	unless (!defined($ret) || $ret =~ /$retrievalError/) {
+	    TWiki::Func::writeDebug( "- ${pluginName}::getFromPubmedCache( $pmid, $rettype ): not found in cache, fetched from NCBI" ) if $debug;
+	    $pubmedCache{$key} = $ret;
+	    return $ret;
+	}
+    }
+    TWiki::Func::writeDebug( "- ${pluginName}::getFromPubmedCache( $pmid, $rettype ): not found in cache, not found at NCBI" ) if $debug;
+    return undef;
+}
 
 # =========================
 sub initPlugin
@@ -78,18 +108,66 @@ sub commonTagsHandler
         $_[0] =~ s/%INCLUDE:"([^%\"]*?)"%/TWiki::handleIncludeFile( $1, $_[1], $_[2], "" )/geo;
     }
 
+    # assume locked by default, unless we have any %PMID... lines in the page, in which case check the lockfile/cache access times
+    my $locked = 1;
+    if ($_[0] =~ /%PMID/) {
+	$locked = 0;
+	if (-e $pubmedCacheLockfile) {
+	    # lockfile exists, but check for stale locks
+	    my @lockStat = stat $pubmedCacheLockfile;
+	    my $lockTime = $lockStat[9];
+	    my $now = time;
+	    if ($lockTime >= $now - 60) { # allow 60 seconds for pages with lots of references
+		$locked = 1;
+	    } else {
+		# double-check last access time of cache file, just to be sure this isn't a REEEEALLY slow page load
+		if (-e $pubmedCacheFilename) {
+		    my @cacheStat = stat $pubmedCacheFilename;
+		    my $cacheTime = $cacheStat[9];
+		    my $now = time;
+		    if ($cacheTime >= $now - 10) { # allow 10 seconds for slow GET requests to pubmed
+			$locked = 1;
+		    }
+		}
+		if (!$locked) {
+		    # lockfile is stale, so grab the lock
+		    utime $now, $now, $pubmedCacheLockfile;
+		}
+	    }
+	} else {  # lockfile doesn't exist, so create it
+	    local *DUMMY;
+	    open DUMMY, ">$pubmedCacheLockfile";
+	    close DUMMY;
+	}
+
+	TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] ): DB is ".($locked?"locked":"unlocked") ) if $debug;
+    }
+
+    unless ($locked) {
+	tie %pubmedCache, 'DB_File', $pubmedCacheFilename or die "Trying to tie ${pluginName} cache to $pubmedCacheFilename: $!";
+    }
+
     # do custom extension rule, like for example:
     # $_[0] =~ s/%XYZ%/&handleXyz()/ge;
     # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/ge;
     $_[0] =~ s/%PMID{([0-9]{1,9}?)}%/&fetch($1)/ge;
     $_[0] =~ s/%PMIDL{(.*?)}%/&link($1)/ge;
     $_[0] =~ s/%PMIDC{([0-9]{1,9}?)}%/&citelink($1)/ge;
+
+    unless ($locked) {
+	untie %pubmedCache;
+	unlink $pubmedCacheLockfile;
+    }
 }
 
 # =========================
 sub fetch {
     my $pmid = $_[0];
-    my $results=get("http://www.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?rettype=abstract&retmode=text&db=pubmed&id=$pmid");
+    my $results=getFromPubmedCache($pmid,"abstract");  # IH, 9/5/07
+    my $link = "http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=pubmed&dopt=Abstract&list_uids=$pmid";
+    $results =~ s!($TWiki::regex{linkProtocolPattern}\:\S*[^\s\.,\);\:])([\s\.,\);\:])!<a href="$1">$1</a>$3!g;
+    $results =~ s!$pmid!<a href="$link">$pmid</a>!;
+    $results =~ s!\n1:\s*([^\n]*\S.*?)\s*?\n!\n<a href="$link">$1</a>\n!;
     return "<pre>".$results."</pre>";
 }
 
@@ -108,7 +186,12 @@ sub link {
 # =========================
 sub citelink {
     my $pmid = $_[0];
-    my $results=get("http://www.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?rettype=medline&retmode=text&db=pubmed&id=$pmid");
+    my $results=getFromPubmedCache($pmid,"medline");  # IH, 9/5/07
+    my $url = "http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=pubmed&dopt=Abstract&list_uids=$pmid";
+    return "[[$url][PMID $pmid]]" unless defined $results;
+    if ($results =~ /$retrievalError/) {
+	return "[[$url][PubMed ID $pmid not yet in database]]";
+    }
 #create an array for each line
     @string=split /\n/, $results;
     $pcmd='';
@@ -136,13 +219,21 @@ sub citelink {
     }
     $au=$ti=$so='';
     if(exists $rec->{'AU'}) {
-	$au=join(', ', @{$rec->{'AU'}});
+	# limit number of authors -- IH, 8/4/07
+	if (@{$rec->{'AU'}} > $maxAuthors) {
+	    $au = $rec->{'AU'}->[0] . " <i>et al</i>";
+	} else {
+	    $au=join(', ', @{$rec->{'AU'}});
+	}
+	$au =~ s/\b([A-Z]+[a-z]+[A-Z][A-Za-z]+)\b/<nop>$1/g;  # prevent accidental wiki links -- IH, 8/4/07
     }
     if(exists $rec->{'TI'}) {
 	$ti=join(' ', @{$rec->{'TI'}});
+	$ti =~ s/\b([A-Z]+[a-z]+[A-Z][A-Za-z]+)\b/<nop>$1/g;  # prevent accidental wiki links -- IH, 8/4/07
     }
     if(exists $rec->{'SO'}) {
         $so=join(' ', @{$rec->{'SO'}});
+	$so =~ s/\b([A-Z]+[a-z]+[A-Z][A-Za-z]+)\b/<nop>$1/g;  # prevent accidental wiki links -- IH, 8/4/07
     }
     $link="[[http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=pubmed&dopt=Abstract&list_uids=$pmid][$ti]]";
     return $au.".&nbsp;&nbsp;".$link."&nbsp;&nbsp;".$so;
